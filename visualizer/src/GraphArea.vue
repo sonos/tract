534b712a249213abb080299381f3e89881e6f89a
Romain <romain.liautaud@snips.ai>
2018-06-11 13:55:26 +0200

    Added support for metanode collapsing.

--- visualizer/src/GraphArea.vue
+++ visualizer/src/GraphArea.vue
@@ -1,20 +1,18 @@
 <template></template>
 
 <script>
-  import { Hierarchy } from './helpers'
+  import { Hierarchy, stringToColor } from './helpers'
   import { graphStyle } from './styles'
 
+  import jquery from 'jquery'
+
   import cytoscape from 'cytoscape'
-  // import dagre from 'cytoscape-dagre'
-  // import klay from 'cytoscape-klay'
+  import expand from 'cytoscape-expand-collapse'
   import layout from './layout'
-  // import expand from 'cytoscape-expand-collapse'
 
   // Register extensions.
-  // cytoscape.use(dagre)
-  // cytoscape.use(klay)
   cytoscape.use(layout)
-  // cytoscape.use(expand)
+  cytoscape.use(expand, jquery)
 
   export default {
     props: {
@@ -52,12 +50,15 @@
               id: n.name,
               op: n.op_name,
               oid: n.id,
+              type: 'node',
               name: hierarchy.getName(n.name),
-              // parent: hierarchy.getParent(n.name),
+              parent: hierarchy.getParent(n.name),
+              background: 'hsl(' + stringToColor(n.op_name) + ', 100%, 90%)',
+              border: 'hsl(' + stringToColor(n.op_name) + ', 40%, 80%)',
             }
           }))
 
-        // let metanodes = hierarchy.getMetanodes()
+        let metanodes = hierarchy.getMetanodes()
 
         let edges = this.graph[1]
           .filter(e => e.from_node !== null && e.to_node !== null)
@@ -69,8 +70,23 @@
             }
           }))
 
-        // let graph = nodes.concat(metanodes, edges)
-        let graph = nodes.concat(edges)
+        let graph = nodes.concat(metanodes, edges)
+
+        // let graph = nodes.concat(edges)
+        // DEBUG(liautaud)
+        // let graph = [
+        //   {data: {id: 'a', op: 'noconst', name: 'a'}},
+        //   {data: {id: 'b', op: 'noconst', name: 'b'}},
+        //   {data: {id: 'c', op: 'noconst', name: 'c'}},
+        //   {data: {id: 'd', op: 'noconst', name: 'd'}},
+        //   {data: {id: 'e', op: 'noconst', name: 'e'}},
+        //   {data: {id: 'ab', source: 'a', target: 'b'}},
+        //   {data: {id: 'ac', source: 'a', target: 'c'}},
+        //   {data: {id: 'bd', source: 'b', target: 'd'}},
+        //   {data: {id: 'cd', source: 'c', target: 'd'}},
+        //   {data: {id: 'de', source: 'd', target: 'e'}},
+        //   {data: {id: 'ae', source: 'a', target: 'e'}},
+        // ]
 
         this.instance = cytoscape({
           container: this.$el,
@@ -84,8 +100,20 @@
 
         this.layout = this.instance.layout({
           name: 'tensorflow',
-          // name: 'dagre',
         })
+
+        this.expand = this.instance.expandCollapse({
+          layoutBy: {
+            name: "tensorflow",
+            randomize: false,
+            fit: true,
+            padding: 80,
+          },
+          fisheye: false,
+          undoable: false,
+        })
+
+        this.expand.collapseAll()
 
         this.layout.run()
       }
--- visualizer/src/layout.js
+++ visualizer/src/layout.js
@@ -1,5 +1,7 @@
 import kiwi from 'kiwi.js'
 
+
+// Shorthands for Kiwi.js.
 const kvar = kiwi.Variable
 const kexp = kiwi.Expression
 const keq = kiwi.Operator.Eq
@@ -8,64 +10,100 @@ const kle = kiwi.Operator.Le
 const kstrong = kiwi.Strength.strong
 const kweak = kiwi.Strength.weak
 
+
+// Positionning constants, in pixels.
+const leftPadding = 0
+const rightPadding = 0
+const topPadding = 90
+const bottomPadding = 0
+const xNodeSep = 90
+const yNodeSep = 60
+const xConstSep = 80
+const yConstSep = 30
+
+
 /**
  * A custom Cytoscape layout for Tensorflow graphs.
  *
  * It uses the Kiwi.js linear solver to express and optimize
- * the linear constraints for the spacing between nodes.
+ * the linear constraints for the spacing between nodes, and
+ * falls back to the Dagre layout algorithm if the linear
+ * solver fails to find an optimal solution.
  */
 class TensorflowLayout {
   constructor(options) {
     this.options = options
-  }
+    this.cy = options.cy
+    this.eles = options.eles
+    this.nodes = options.eles.nodes()
+    this.edges = options.eles.edges()
 
-  run() {
-    let options = this.options
-    let cy = options.cy
-    let eles = options.eles
-    let nodes = eles.nodes()
-    let edges = eles.edges()
+    let bb = options.boundingBox || {
+      x1: 0,
+      y1: 0,
+      w: this.cy.width(),
+      h: this.cy.height()
+    }
 
-    let bb = options.boundingBox || { x1: 0, y1: 0, w: cy.width(), h: cy.height() }
     if (bb.x2 === undefined){ bb.x2 = bb.x1 + bb.w; }
     if (bb.w === undefined){ bb.w = bb.x2 - bb.x1; }
     if (bb.y2 === undefined){ bb.y2 = bb.y1 + bb.h; }
     if (bb.h === undefined){ bb.h = bb.y2 - bb.y1; }
 
-    // Positionning constants, in pixels.
-    const leftPadding = 0
-    const rightPadding = 0
-    const topPadding = 90
-    const bottomPadding = 0
-    const xNodeSep = 100
-    const yNodeSep = 50
-    const xConstSep = 80
-    const yConstSep = 50
+    this.bb = bb
+  }
 
-    // Initialize the variables of the linear problem.
+  run() {
+    let idx = {}
+    this.nodes.forEach((n, i) => idx[n.id()] = i)
+
+    let positions = {}
+
+    try {
+      positions = this.solveLinear(idx)
+    } catch (e) {
+      positions = this.solveDagre(idx)
+    }
+
+    this.nodes.layoutPositions(this, this.options, function(node){
+      node = typeof node === "object" ? node : this
+      return positions[idx[node.id()]]
+    })
+
+    return this
+  }
+
+  /** Tries to position the nodes using the linear solver. */
+  solveLinear(idx) {
+    console.log('Solving using the linear solver.')
+
+    let nodes = this.nodes
     let solver = new kiwi.Solver()
     let xs = nodes.map(_ => new kvar())
     let ys = nodes.map(_ => new kvar())
-    nodes.forEach(n => {
-      solver.addEditVariable(xs[n.data('oid')], kstrong)
-      solver.addEditVariable(ys[n.data('oid')], kstrong)
-      solver.suggestValue(xs[n.data('oid')], bb.w / 2)
-    })
 
-    console.log(bb.w / 2)
+    nodes.forEach(n => {
+      let nid = idx[n.id()]
+
+      solver.addEditVariable(xs[nid], kstrong)
+      solver.addEditVariable(ys[nid], kstrong)
+      solver.suggestValue(xs[nid], this.bb.w / 2)
+    })
 
     // Ensure the nodes stay in the bounding box.
     nodes.forEach(n => {
-      solver.createConstraint(xs[n.data('oid')], kge, bb.x1 + leftPadding)
-      solver.createConstraint(xs[n.data('oid')], kle, bb.x2 + rightPadding, kstrong)
+      let nid = idx[n.id()]
 
-      solver.createConstraint(ys[n.data('oid')], kge, bb.y1 + topPadding)
-      solver.createConstraint(ys[n.data('oid')], kle, bb.y2 + bottomPadding, kstrong)
+      solver.createConstraint(xs[nid], kge, this.bb.x1 + leftPadding)
+      solver.createConstraint(xs[nid], kle, this.bb.x2 + rightPadding, kstrong)
+
+      solver.createConstraint(ys[nid], kge, this.bb.y1 + topPadding)
+      solver.createConstraint(ys[nid], kle, this.bb.y2 + bottomPadding, kstrong)
     })
 
     // Add constraints to predecessors.
     nodes.forEach(n => {
-      let nid = n.data('oid')
+      let nid = idx[n.id()]
 
       /**
        * Rules for non-constant predecessors.
@@ -74,22 +112,22 @@ class TensorflowLayout {
       let prevNodesSize = prevNodes.length
 
       prevNodes.forEach((m, i) => {
-        let mid = m.data('oid')
+        let mid = idx[m.id()]
 
         // The successor is below all the predecessors.
         solver.createConstraint(ys[nid], kge, new kexp(ys[mid], yNodeSep))
 
         // All the predecessors are spaced from each other.
         if (i > 0) {
-          let pid = prevNodes[i - 1].data(oid)
+          let pid = idx[prevNodes[i - 1].id()]
           solver.createConstraint(xs[mid], kge, new kexp(xs[pid], xNodeSep))
         }
       })
 
       // The successor is aligned with the middle of the predecessors.
       if (prevNodesSize > 0) {
-        let xmean = prevNodes.map(m => [1 / prevNodesSize, xs[m.data('oid')]])
-        solver.createConstraint(xs[nid], keq, new kexp(...xmean))
+        let xmean = prevNodes.map(m => [1 / prevNodesSize, xs[idx[m.id()]]])
+        solver.createConstraint(xs[nid], keq, new kexp(...xmean), kstrong)
       }
 
       /**
@@ -99,40 +137,37 @@ class TensorflowLayout {
       let prevConstsSize = prevConsts.length
 
       prevConsts.forEach((m, i) => {
-        let mid = m.data('oid')
+        let mid = idx[m.id()]
 
         // The successor is left of all the constant predecessors.
         solver.createConstraint(xs[nid], kge, new kexp(xs[mid], xConstSep))
 
         // All the constant predecessors are spaced from each other.
         if (i > 0) {
-          let pid = prevConsts[i - 1].data(oid)
+          let pid = idx[prevConsts[i - 1].id()]
           solver.createConstraint(ys[mid], kge, new kexp(ys[pid], yConstSep))
         }
       })
 
       // The successor is aligned with the middle of the predecessors.
       if (prevConstsSize > 0) {
-        let ymean = prevConsts.map(m => [1 / prevConstsSize, ys[m.data('oid')]])
+        let ymean = prevConsts.map(m => [1 / prevConstsSize, ys[idx[m.id()]]])
         solver.createConstraint(ys[nid], keq, new kexp(...ymean))
       }
     })
 
-    // solver.createConstraint(xs[0], kge, bb.w / 2)
-    solver.suggestValue(xs[0], bb.w / 2)
-    solver.suggestValue(ys[0], 0)
     solver.updateVariables()
 
-    nodes.layoutPositions(this, options, function(node){
-      node = typeof node === "object" ? node : this
-      console.log(node.id(), xs[node.data('oid')].value(), ys[node.data('oid')].value())
-      return {
-        x: xs[node.data('oid')].value(),
-        y: ys[node.data('oid')].value(),
-      }
-    })
+    return nodes.map(node => ({
+      x: xs[idx[node.id()]].value(),
+      y: ys[idx[node.id()]].value(),
+    }))
+  }
 
-    return this
+  /** Tries to position the nodes using Dagre. */
+  solveDagre(idx) {
+    console.log('Solving using Dagre.')
+    // TODO(liautaud)
   }
 }
 
