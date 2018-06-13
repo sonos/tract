88f2d4ab280f2bfc3e80aa92a89e1553028342f2
Romain <romain.liautaud@snips.ai>
2018-06-13 15:59:35 +0200

    Added an export button for edge values.

--- visualizer/src/ValueDisplay.vue
+++ visualizer/src/ValueDisplay.vue
@@ -7,12 +7,18 @@
       <span v-if="!isOnly">Unknown (depends on input)</span>
       <span v-else-if="shape.length == 0">{{ content[0] }}</span>
       <span v-else-if="isSmall" style="white-space: pre;">{{ matrix.toString() }}</span>
-      <v-btn
-        large color="primary"
-        v-else
-        @click="displayDialog = true">
-        Display
-      </v-btn>
+      <div v-else>
+        <v-btn
+          large color="primary"
+          @click="displayDialog = true">
+          Display
+        </v-btn>
+        <v-btn
+          large
+          @click="download">
+          Export
+        </v-btn>
+      </div>
     </v-card-text>
     <v-dialog v-model="displayDialog" max-width="850px">
       <v-card>
@@ -58,6 +64,22 @@
       matrix() {
         return numjs.array(this.content).reshape(this.shape)
       },
+    },
+
+    methods: {
+      download() {
+        let data = this.matrix.toJSON()
+
+        let blob = new Blob([data], {type: 'text/json'})
+        let e = document.createEvent('MouseEvents')
+        let a = document.createElement('a')
+
+        a.download = 'edge-content.json'
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+      }
     }
   }
 </script>
