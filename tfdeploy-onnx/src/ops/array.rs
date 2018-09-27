43faebb1403529f3daf1e0792a80e0bd7e597fc0
Mathieu Poumeyrol <kali@zoy.org>
2018-09-27 19:28:57 +0200

    plug Constant in

--- tfdeploy-onnx/src/ops/array.rs
+++ tfdeploy-onnx/src/ops/array.rs
@@ -1,7 +1,17 @@
+use tfdeploy::ops::prelude::*;
 use tfdeploy::ops as tfdops;
 
 use ops::OpRegister;
+use pb::NodeProto;
 
 pub fn register_all_ops(reg: &mut OpRegister) {
-    reg.insert("Expand", |_| Ok(Box::new(tfdops::array::MultiBroadcastTo::default())));
+    reg.insert("Constant", constant);
+    reg.insert("Expand", |_| {
+        Ok(Box::new(tfdops::array::MultiBroadcastTo::default()))
+    });
+}
+
+pub fn constant(node: &NodeProto) -> TfdResult<Box<Op>> {
+    let value: Tensor = node.get_attr_tensor("value")?;
+    Ok(Box::new(tfdops::konst::Const::new(value.into())))
 }
