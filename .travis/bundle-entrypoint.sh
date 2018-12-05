4ac64cffc4829500b5581fd5a8c26d51ff4b44fb
Mathieu Poumeyrol <kali@zoy.org>
2018-12-05 19:11:08 +0100

    stderr/stdout

--- .travis/bundle-entrypoint.sh
+++ .travis/bundle-entrypoint.sh
@@ -71,14 +71,14 @@ then
         $CACHEDIR/tflite_benchmark_model_$tflite \
             --graph=$CACHEDIR/inception_v3_2016_08_28_frozen.tflite \
             --num_runs=1 \
-        > bench
+        2> bench
         usec=`cat bench | grep 'curr=' | tail -1 | sed "s/.*=//"`
         sec=`echo "scale=6; $usec / 1000000" | bc -l`
         echo net.inceptionv3.tflite_$tflite.pass $sec >> metrics
 
         $CACHEDIR/tflite_benchmark_model_$tflite \
             --graph=$CACHEDIR/hey_snips_v3.1.tflite \
-        > bench
+        2> bench
         usec=`cat bench | grep 'curr=' | tail -1 | sed "s/.*=//"`
         sec=`echo "scale=6; $usec / 1000000" | bc -l`
         echo net.hey_snips_v31.tflite_$tflite.400ms $sec >> metrics
