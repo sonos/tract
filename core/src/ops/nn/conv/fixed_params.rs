3c4a0f4781813c02fc18d8a8fb07c64f5934a45f
Mathieu Poumeyrol <kali@zoy.org>
2018-12-13 16:18:12 +0100

    im2col

--- core/src/ops/nn/conv/fixed_params.rs
+++ core/src/ops/nn/conv/fixed_params.rs
@@ -2,11 +2,13 @@ use ndarray::prelude::*;
 use ops::prelude::*;
 
 use ops::nn::{DataFormat, PaddingSpec, Patch};
+use super::im2col::Im2Col;
 
 use insideout::InsideOut;
 
 #[derive(Debug, Clone)]
 pub struct FixedParamsConv<D: Datum> {
+    im2col: Im2Col<D>,
     kernel_is_hwio: bool,
     patch: Patch,
     kernel: Array2<D>,
@@ -75,7 +77,10 @@ impl<D: Datum> FixedParamsConv<D> {
             })
             .inside_out()?;
 
+        let im2col = Im2Col::new(patch.clone(), m, k, n, group);
+
         Ok(FixedParamsConv {
+            im2col,
             kernel_is_hwio,
             patch,
             kernel,
@@ -93,43 +98,8 @@ impl<D> FixedParamsConv<D>
 where
     D: Datum + Clone + ::ndarray::LinalgScalar + ::std::ops::AddAssign<D> + PartialEq,
 {
-
-    pub(super) fn im2col<'i>(&'i self, input: &'i ArrayViewD<'i, D>) -> TractResult<Array2<D>> {
-        let input_shape = &self.patch.input_shape;
-        let mut mega_matrix = unsafe {
-            Array2::<D>::uninitialized((self.k, self.n * input_shape.n_dim() * self.group))
-        };
-        let visitor = self.patch.wrap(input);
-        let ci_per_group = input_shape.c_dim() / self.group;
-        for i in 0..input_shape.n_dim() {
-            for g in 0..self.group {
-                let mm_offset = self.n * (g + (i * self.group));
-                let mut coords = vec!(0; input_shape.rank());
-                coords[input_shape.n_axis()] = i;
-                for (mut spatial, mut col) in ndarray::indices(&*self.patch.output_spatial_shape)
-                    .into_iter()
-                    .zip(
-                        mega_matrix
-                            .slice_axis_mut(Axis(1), (mm_offset..(mm_offset + self.n)).into())
-                            .axis_iter_mut(Axis(1)),
-                    )
-                {
-                    let mut col = col.iter_mut();
-                    coords[input_shape.h_axis()..][..input_shape.hw_rank()].copy_from_slice(spatial.slice());
-                    for ci in 0..ci_per_group {
-                        coords[input_shape.c_axis()] = ci + g * ci_per_group;
-                        for v in visitor.at(&*coords) {
-                            *col.next().expect("geometry error in conv") = v.unwrap_or(D::zero());
-                        }
-                    }
-                }
-            }
-        }
-        Ok(mega_matrix)
-    }
-
     pub(super) fn convolve<'i>(&'i self, input: &'i ArrayViewD<'i, D>) -> TractResult<ArrayD<D>> {
-        let mega_matrix = self.im2col(input)?;
+        let mega_matrix = self.im2col.im2col(input)?;
         let mut output = unsafe { ArrayD::<D>::uninitialized(&*self.full_output_shape) };
         let input_shape = &self.patch.input_shape;
         let co_per_group = self.full_output_shape[input_shape.c_axis()] / self.group;
