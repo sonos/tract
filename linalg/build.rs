6f3c6b81eab92c8ccaac9d917517c3f171b51e9b
Mathieu Poumeyrol <kali@zoy.org>
2019-01-21 17:40:35 +0100

    organize arm32 stuff a bit

--- linalg/build.rs
+++ linalg/build.rs
@@ -6,13 +6,13 @@ fn main() {
     let arch = var("CARGO_CFG_TARGET_ARCH").unwrap();
     if arch == "arm" || arch == "armv7" {
         cc::Build::new()
-            .file("arm-vfp2/arm-vfpv2_mm_s4x4.c")
+            .file("arm32/armvfpv2/armvfpv2_mm_s4x4.c")
             .flag("-marm")
             .flag("-mfpu=vfp")
             .static_flag(true)
             .compile("armvfpv2");
         cc::Build::new()
-            .file("armv7neon/armv7neon_mm_s8x4.c")
+            .file("arm32/armv7neon/armv7neon_mm_s8x4.c")
             .flag("-marm")
             .flag("-mfpu=neon")
             .flag("-mfloat-abi=hard")
