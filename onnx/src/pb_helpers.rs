9108b04bc1ca717cc0a77f6d858dfc7e46ad19fa
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-02-25 23:03:53 +0000

    (Refactor errors in pb_helpers, less spam)

--- onnx/src/pb_helpers.rs
+++ onnx/src/pb_helpers.rs
@@ -1,30 +1,73 @@
 use crate::pb::*;
 use tract_core::*;
 
+use std::borrow::Cow;
+
+pub trait Reason {
+    fn reason(&self) -> Cow<str>;
+}
+
+impl<'a> Reason for &'a str {
+    fn reason(&self) -> Cow<str> {
+        (*self).into()
+    }
+}
+
+impl<F> Reason for F
+where
+    F: Fn() -> String,
+{
+    fn reason(&self) -> Cow<str> {
+        self().into()
+    }
+}
+
 impl NodeProto {
+    pub fn expect<R: Reason>(&self, cond: bool, what: R) -> TractResult<()> {
+        ensure!(
+            cond,
+            "Node {} ({}): expected {}",
+            self.get_name(),
+            self.get_op_type(),
+            what.reason()
+        );
+        Ok(())
+    }
+
+    pub fn expect_attr<R: Reason>(&self, attr: &str, cond: bool, what: R) -> TractResult<()> {
+        ensure!(
+            cond,
+            "Node {} ({}), attribute '{}': expected {}",
+            self.get_name(),
+            self.get_op_type(),
+            attr,
+            what.reason()
+        );
+        Ok(())
+    }
+
+    pub fn expect_attr_ok_or_else<T, R: Reason>(
+        &self, attr: &str, result: Option<T>, what: R,
+    ) -> TractResult<T> {
+        match result {
+            Some(v) => Ok(v),
+            None => Err(self.expect_attr(attr, false, what).unwrap_err()),
+        }
+    }
+
     fn get_attr_opt(&self, name: &str) -> TractResult<Option<&AttributeProto>> {
         Ok(self.get_attribute().iter().find(|a| a.get_name() == name))
     }
 
     fn get_attr_opt_with_type(
-        &self,
-        name: &str,
-        ty: AttributeProto_AttributeType,
+        &self, name: &str, ty: AttributeProto_AttributeType,
     ) -> TractResult<Option<&AttributeProto>> {
         let attr = if let Some(a) = self.get_attr_opt(name)? {
             a
         } else {
             return Ok(None);
         };
-        if attr.get_field_type() != ty {
-            bail!(
-                "Node {} ({}) expected attribute {} to be {:?}",
-                self.get_name(),
-                self.get_op_type(),
-                name,
-                ty
-            )
-        }
+        self.expect_attr(name, attr.get_field_type() == ty, || format!("{:?}", ty))?;
         Ok(Some(attr))
     }
 
@@ -36,14 +79,7 @@ impl NodeProto {
     }
 
     pub fn get_attr_tensor(&self, name: &str) -> TractResult<Tensor> {
-        Ok(self.get_attr_opt_tensor(name)?.ok_or_else(|| {
-            format!(
-                "Node {} ({}) expected tensor attribute '{}'",
-                self.get_name(),
-                self.get_op_type(),
-                name
-            )
-        })?)
+        self.expect_attr_ok_or_else(name, self.get_attr_opt_tensor(name)?, "tensor")
     }
 
     pub fn get_attr_opt_str(&self, name: &str) -> TractResult<Option<&str>> {
@@ -54,14 +90,7 @@ impl NodeProto {
     }
 
     pub fn get_attr_str(&self, name: &str) -> TractResult<&str> {
-        Ok(self.get_attr_opt_str(name)?.ok_or_else(|| {
-            format!(
-                "Node {} ({}) expected string attribute '{}'",
-                self.get_name(),
-                self.get_op_type(),
-                name
-            )
-        })?)
+        self.expect_attr_ok_or_else(name, self.get_attr_opt_str(name)?, "string")
     }
 
     pub fn get_attr_opt_int(&self, name: &str) -> TractResult<Option<i64>> {
@@ -72,14 +101,7 @@ impl NodeProto {
     }
 
     pub fn get_attr_int(&self, name: &str) -> TractResult<i64> {
-        Ok(self.get_attr_opt_int(name)?.ok_or_else(|| {
-            format!(
-                "Node {} ({}) expected int attribute '{}'",
-                self.get_name(),
-                self.get_op_type(),
-                name
-            )
-        })?)
+        self.expect_attr_ok_or_else(name, self.get_attr_opt_int(name)?, "int")
     }
 
     pub fn get_attr_opt_float(&self, name: &str) -> TractResult<Option<f32>> {
@@ -90,14 +112,7 @@ impl NodeProto {
     }
 
     pub fn get_attr_float(&self, name: &str) -> TractResult<f32> {
-        Ok(self.get_attr_opt_float(name)?.ok_or_else(|| {
-            format!(
-                "Node {} ({}) expected float attribute '{}'",
-                self.get_name(),
-                self.get_op_type(),
-                name
-            )
-        })?)
+        self.expect_attr_ok_or_else(name, self.get_attr_opt_float(name)?, "float")
     }
 
     pub fn get_attr_opt_ints(&self, name: &str) -> TractResult<Option<&[i64]>> {
@@ -108,14 +123,7 @@ impl NodeProto {
     }
 
     pub fn get_attr_ints(&self, name: &str) -> TractResult<&[i64]> {
-        Ok(self.get_attr_opt_ints(name)?.ok_or_else(|| {
-            format!(
-                "Node {} ({}) expected list of ints attribute '{}'",
-                self.get_name(),
-                self.get_op_type(),
-                name
-            )
-        })?)
+        self.expect_attr_ok_or_else(name, self.get_attr_opt_ints(name)?, "list of ints")
     }
 
     pub fn get_attr_opt_floats(&self, name: &str) -> TractResult<Option<&[f32]>> {
@@ -126,41 +134,30 @@ impl NodeProto {
     }
 
     pub fn get_attr_floats(&self, name: &str) -> TractResult<&[f32]> {
-        Ok(self.get_attr_opt_floats(name)?.ok_or_else(|| {
-            format!(
-                "Node {} ({}) expected list of floats attribute '{}'",
-                self.get_name(),
-                self.get_op_type(),
-                name
-            )
-        })?)
+        self.expect_attr_ok_or_else(name, self.get_attr_opt_floats(name)?, "list of floats")
     }
 
     pub fn get_attr_usize_tvec(&self, name: &str) -> TractResult<TVec<usize>> {
         let ints = self.get_attr_ints(name)?;
-        ensure!(
-            ints.iter().any(|&x| x < 0),
-            "Node {} ({}) expected a list of non-negatve ints in attribute '{}'",
-            self.get_name(),
-            self.get_op_type(),
-            name
-        );
+        for i in ints.iter() {
+            self.expect_attr(name, *i >= 0, "list of non-negative ints")?;
+        }
         Ok(ints.iter().map(|&x| x as _).collect())
     }
 
     pub fn get_attr_opt_int_tvec(&self, name: &str) -> TractResult<Option<TVec<i64>>> {
-        self.get_attr_opt_ints(name)?.map(Into::into)
+        Ok(self.get_attr_opt_ints(name)?.map(Into::into))
     }
 
     pub fn get_attr_int_tvec(&self, name: &str) -> TractResult<TVec<i64>> {
-        self.get_attr_ints(name)?.into()
+        Ok(self.get_attr_ints(name)?.into())
     }
 
     pub fn get_attr_opt_float_tvec(&self, name: &str) -> TractResult<Option<TVec<f32>>> {
-        self.get_attr_opt_floats(name)?.map(Into::into)
+        Ok(self.get_attr_opt_floats(name)?.map(Into::into))
     }
 
     pub fn get_attr_float_tvec(&self, name: &str) -> TractResult<TVec<f32>> {
-        self.get_attr_floats(name)?.into()
+        Ok(self.get_attr_floats(name)?.into())
     }
 }
