76c9bbddd82aba3d5fb6f96d432e0032ebbbf2f9
Alaa Saade <alaa.saade@snips.ai>
2019-02-27 16:39:24 +0100

    Fix gathering on scalar index (regular tensor indexing)

--- core/src/ops/array/gather.rs
+++ core/src/ops/array/gather.rs
@@ -15,11 +15,11 @@ impl Op for Gather {
 impl Gather {
     fn eval_t<T: Datum>(
         &self,
-        data: &SharedTensor,
+        data: SharedTensor,
         indices: &SharedTensor,
     ) -> TractResult<SharedTensor> {
         let data_view = data.to_array_view::<T>()?;
-        let rank = data_view.shape().len() as i64;
+        let rank = data.shape().len() as i64;
         let axis = {
             let axis_res: TractResult<i64> = {
                 if 0 <= self.axis && self.axis <= rank - 1 {
@@ -32,6 +32,14 @@ impl Gather {
             };
             axis_res? as usize
         };
+
+        if indices.shape().len() == 0 {
+            return Ok(data_view
+                .index_axis(Axis(axis), *indices.to_scalar::<i64>()? as usize)
+                .to_owned()
+                .into());
+        }
+
         let mut output_shape: Vec<usize> = vec![];
         for (idx, dim) in data_view.shape().to_vec().iter().enumerate() {
             if idx != axis {
@@ -62,7 +70,7 @@ impl StatelessOp for Gather {
     fn eval(&self, mut inputs: TVec<SharedTensor>) -> TractResult<TVec<SharedTensor>> {
         let (data, indices) = args_2!(inputs);
         Ok(tvec!(dispatch_datum!(Self::eval_t(data.datum_type())(
-            &self, &data, &indices
+            &self, data, &indices
         ))?))
     }
 }
@@ -82,5 +90,26 @@ impl InferenceRulesOp for Gather {
             outputs[0].rank.bex(),
         )?;
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_should_gather_scalar_index() {
+        let data = Tensor::from(arr1(&[1i64, 2, 3]));
+        let gatherer = Gather::new(0);
+        for idx in 2..3 {
+            let index = Tensor::from(arr0(idx as i64));
+            let outputs = gatherer
+                .eval(tvec![data.clone().into(), index.into()])
+                .unwrap();
+            let output = &outputs[0];
+            //            println!("{:?}", output);
+            assert_eq!(output.shape().len(), 0);
+            assert_eq!(*output.to_scalar::<i64>().unwrap(), idx + 1);
+        }
     }
 }
