9eca01fde888166e1dea66f6c5b2a6a65ad0396f
Alaa Saade <alaa.saade@snips.ai>
2019-02-27 16:36:49 +0100

    Allow concating on negative axis, and tensors who share a common super type

--- core/src/ops/array/concat.rs
+++ core/src/ops/array/concat.rs
@@ -4,15 +4,39 @@ use crate::ops::prelude::*;
 
 #[derive(Debug, Clone, new)]
 pub struct Concat {
-    axis: usize,
+    axis: i64,
 }
 
 impl Concat {
+    fn resolve_axis(&self, rank: i64) -> TractResult<usize> {
+        let axis = {
+            let axis_res: TractResult<i64> = {
+                if 0 <= self.axis && self.axis <= rank - 1 {
+                    Ok(self.axis)
+                } else if -rank <= self.axis && self.axis < 0 {
+                    Ok(self.axis + rank)
+                } else {
+                    bail!("Illegal combination of values for rank and axis")
+                }
+            };
+            axis_res? as usize
+        };
+        Ok(axis)
+    }
+
     /// Evaluates the operation given the input tensors.
-    fn eval_t<T: Datum + Copy>(&self, inputs: TVec<SharedTensor>) -> TractResult<TVec<SharedTensor>> {
-        let mats: TractResult<Vec<ArrayViewD<T>>> =
-            inputs.iter().map(|mat| mat.to_array_view()).collect();
-        let result = ::ndarray::stack(Axis(self.axis as usize), &*mats?)?;
+    fn eval_t<T: Datum + Copy>(
+        &self,
+        inputs: TVec<SharedTensor>,
+    ) -> TractResult<TVec<SharedTensor>> {
+        let axis = self.resolve_axis(inputs[0].shape().len() as i64)?;
+        // FIXME: too many copies
+        let mats: Vec<ArrayD<T>> = inputs
+            .iter()
+            .map(|mat| Ok(mat.cast_to::<T>()?.into_owned().into_array::<T>()?))
+            .collect::<TractResult<_>>()?;
+        let views: Vec<_> = mats.iter().map(|mat| mat.view()).collect();
+        let result = ::ndarray::stack(Axis(axis), &views)?;
         Ok(tvec![result.into()])
     }
 }
@@ -26,7 +50,10 @@ impl Op for Concat {
 impl StatelessOp for Concat {
     /// Evaluates the operation given the input tensors.
     fn eval(&self, inputs: TVec<SharedTensor>) -> TractResult<TVec<SharedTensor>> {
-        dispatch_copy!(Self::eval_t(inputs[0].datum_type())(self, inputs))
+        let super_type: DatumType =
+            DatumType::super_type_for(inputs.iter().map(|x| x.datum_type()))
+                .ok_or_else(|| format!("No supertype found"))?;
+        dispatch_copy!(Self::eval_t(super_type)(self, inputs))
     }
 }
 
@@ -41,27 +68,25 @@ impl InferenceRulesOp for Concat {
         s.equals(&outputs[0].datum_type, &inputs[0].datum_type)?;
         s.equals(&outputs[0].rank, &inputs[0].rank)?;
         let n = inputs.len() as usize;
-        s.equals_all((0..n).map(|i| (&inputs[i].datum_type).bex()).collect())?;
         s.equals_all((0..n).map(|i| (&inputs[i].rank).bex()).collect())?;
-        s.equals(
-            crate::analyser::rules::expr::SumExp::new(
-                (0..n)
-                    .map(|i| (&inputs[i].shape[self.axis]).bex())
-                    .collect(),
-            ),
-            &outputs[0].shape[self.axis],
-        )?;
-        for axis in 0..self.axis {
-            s.equals(&outputs[0].shape[axis], &inputs[0].shape[axis])?;
-            s.equals_all((0..n).map(|i| inputs[i].shape[axis].bex()).collect())?;
-        }
-        s.given(&inputs[0].rank, move |s, axes| {
-            let axes = axes as usize;
-            for axis in (self.axis + 1)..axes {
+        s.given(&inputs[0].rank, move |s, rank| {
+            let axis = self.resolve_axis(rank as i64)?;
+            s.equals(
+                crate::analyser::rules::expr::SumExp::new(
+                    (0..n).map(|i| (&inputs[i].shape[axis]).bex()).collect(),
+                ),
+                &outputs[0].shape[axis],
+            )?;
+            for axis in 0..axis {
+                s.equals(&outputs[0].shape[axis], &inputs[0].shape[axis])?;
+                s.equals_all((0..n).map(|i| inputs[i].shape[axis].bex()).collect())?;
+            }
+            for axis in (axis + 1)..(rank as usize) {
                 s.equals(&outputs[0].shape[axis], &inputs[0].shape[axis])?;
                 s.equals_all((0..n).map(|i| inputs[i].shape[axis].bex()).collect())?;
             }
             Ok(())
-        })
+        })?;
+        Ok(())
     }
 }
