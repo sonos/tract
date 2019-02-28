85d286502426b035eb50e8bede0066ae9657e50c
Mathieu Poumeyrol <kali@zoy.org>
2019-02-28 21:23:25 +0100

    kill old tf pools impl

--- tensorflow/src/ops/nn/mod.rs
+++ tensorflow/src/ops/nn/mod.rs
@@ -1,3 +1,4 @@
+use tract_core::ops::nn::{DataFormat, PaddingSpec};
 use tract_core::ops::prelude::*;
 
 use crate::ops::OpRegister;
@@ -5,20 +6,51 @@ use crate::tfpb::node_def::NodeDef;
 
 pub mod conv2d;
 pub mod fused_batch_norm;
-pub mod local_patch;
 pub mod pools;
 pub mod s2b;
 
 pub fn register_all_ops(reg: &mut OpRegister) {
-    reg.insert("AvgPool", pools::pool::<pools::AvgPooler>);
+    reg.insert("AvgPool", pools::avgpool);
     reg.insert("Conv2D", conv2d::conv2d);
     reg.insert("FusedBatchNorm", fused_batch_norm::fused_batch_norm);
-    reg.insert("MaxPool", pools::pool::<pools::MaxPooler>);
+    reg.insert("MaxPool", pools::maxpool);
     reg.insert("Relu", with_T!(::tract_core::ops::nn::Relu));
     reg.insert("Sigmoid", with_T!(::tract_core::ops::nn::Sigmoid));
     reg.insert("Softmax", Softmax::build);
     reg.insert("SpaceToBatchND", s2b::space_to_batch_nd);
     reg.insert("BatchToSpaceND", s2b::batch_to_space_nd);
+}
+
+pub fn strides(pb: &NodeDef) -> TractResult<Vec<usize>> {
+    let strides: Vec<usize> = pb.get_attr_list_int("strides")?;
+    if strides.len() != 4 || strides[0] != 1 && strides[3] != 1 {
+        Err(format!(
+            "strides must be of the form [1, h, v, 1], found {:?}",
+            strides
+        ))?
+    };
+    Ok(strides)
+}
+
+pub fn data_format(pb: &NodeDef) -> TractResult<DataFormat> {
+    let df = if pb.get_attr_opt_raw_str("data_format")?.unwrap_or(b"NHWC") == b"NHWC" {
+        DataFormat::NHWC
+    } else {
+        DataFormat::NCHW
+    };
+    Ok(df)
+}
+
+pub fn padding(pb: &NodeDef) -> TractResult<PaddingSpec> {
+    let padding = pb.get_attr_raw_str("padding")?;
+    match padding {
+        b"VALID" => Ok(PaddingSpec::Valid),
+        b"SAME" => Ok(PaddingSpec::SameUpper),
+        s => Err(format!(
+            "unsupported Padding {}",
+            String::from_utf8_lossy(s)
+        ))?,
+    }
 }
 
 #[derive(Debug, Clone)]
