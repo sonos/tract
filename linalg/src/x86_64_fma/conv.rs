0789e8d6a87c4548bf0bf97b1276aac4cd01c0b2
Mathieu Poumeyrol <kali@zoy.org>
2019-03-10 10:06:41 +0100

    fma test gard

--- linalg/src/x86_64_fma/conv.rs
+++ linalg/src/x86_64_fma/conv.rs
@@ -79,6 +79,9 @@ mod test {
     proptest! {
         #[test]
         fn conv(pb in strat_conv_1d()) {
+            if !is_x86_feature_detected!("fma") {
+                return Ok(())
+            }
             let (kernel_offsets, data_offsets) = pb.offsets();
             let conv = PackedConv::<SConvFma16x6, f32>::new(pb.co, kernel_offsets, data_offsets);
             let found = pb.run(&conv);
