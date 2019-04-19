d66621af4f0f282021a25743367ba9b07730314b
Mathieu Poumeyrol <kali@zoy.org>
2019-04-19 11:39:18 +0200

    rewrite main dw loop in unsafe

--- core/src/ops/nn/conv/depth_wise.rs
+++ core/src/ops/nn/conv/depth_wise.rs
@@ -3,6 +3,8 @@ use crate::ops::nn::Patch;
 use ndarray::*;
 use std::iter::Sum;
 
+use unsafe_unwrap::UnsafeUnwrap;
+
 #[derive(Debug, Clone, new)]
 pub struct DepthWise<T>
 where
@@ -23,13 +25,16 @@ where
         let img = img.to_array_view::<T>()?;
         let visitor = self.patch.wrap(&img);
         let output_shape = self.patch.output_full_shape(self.out_channels);
-        let mut output = Array3::<T>::from_shape_fn((output_shape[0], output_shape[1], output_shape[2]), |coords| {
-            let coords = [coords.0, coords.1, coords.2];
-            let it = visitor.at(&coords);
-            let channel = coords[self.patch.input_shape.c_axis()];
-            let kernel = self.kernel_chw.slice_axis(Axis(0), (channel..=channel).into());
-            kernel.iter().zip(it).map(|(&k, v)| k * v.unwrap_or(T::zero())).sum()
-        });
+        let mut output = Array3::<T>::from_shape_fn(
+            (output_shape[0], output_shape[1], output_shape[2]),
+            |coords| {
+                let coords = [coords.0, coords.1, coords.2];
+                let it = visitor.at(&coords);
+                let channel = coords[self.patch.input_shape.c_axis()];
+                let kernel = self.kernel_chw.slice_axis(Axis(0), (channel..=channel).into());
+                kernel.iter().zip(it).map(|(&k, v)| k * v.unwrap_or(T::zero())).sum()
+            },
+        );
         if let Some(ref bias) = self.bias {
             output += bias;
         }
@@ -41,13 +46,26 @@ where
         let img = img.to_array_view::<T>()?;
         let visitor = self.patch.wrap(&img);
         let output_shape = self.patch.output_full_shape(self.out_channels);
-        let mut output = Array4::<T>::from_shape_fn((output_shape[0], output_shape[1], output_shape[2], output_shape[3]), |coords| {
-            let coords = [coords.0, coords.1, coords.2, coords.3];
-            let it = visitor.at(&coords);
-            let channel = coords[self.patch.input_shape.c_axis()];
-            let kernel = self.kernel_chw.slice_axis(Axis(0), (channel..=channel).into());
-            kernel.iter().zip(it).map(|(&k, v)| k * v.unwrap_or(T::zero())).sum()
-        });
+        let len = self.patch.kernel_spatial_shape.iter().cloned().product::<usize>();
+        let c_axis = self.patch.input_shape.c_axis();
+        let kernel = self.kernel_chw.as_slice_memory_order().unwrap();
+        let kernel_stride: usize = self.kernel_chw.strides()[0] as usize;
+        let mut output = Array4::<T>::from_shape_fn(
+            (output_shape[0], output_shape[1], output_shape[2], output_shape[3]),
+            |coords| {
+                let coords = [coords.0, coords.1, coords.2, coords.3];
+                let mut it = visitor.at(&coords);
+                unsafe {
+                    let channel = coords.get_unchecked(c_axis);
+                    let k = kernel.get_unchecked((kernel_stride * channel)..);
+                    let mut sum = T::zero();
+                    for i in 0..len {
+                        sum += *k.get_unchecked(i) * it.next().unsafe_unwrap().unwrap_or(T::zero())
+                    }
+                    sum
+                }
+            },
+        );
         if let Some(ref bias) = self.bias {
             output += bias;
         }
