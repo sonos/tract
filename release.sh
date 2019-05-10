69dc9b2e01b89f1e8cd055fda085bc919abd1091
Mathieu Poumeyrol <kali@zoy.org>
2019-05-10 10:46:41 +0200

    release linalg/0.3.1

--- release.sh
+++ release.sh
@@ -27,7 +27,7 @@ set_version() {
     FILE=$1
     VERSION=$2
     sed -i.back "s/^version *= *\".*\"/version = \"$2\"/" $FILE
-    for dep in `grep "^tract-" $FILE | cut -d " " -f 1
+    for dep in `grep "^tract-" $FILE | cut -d " " -f 1`
     do
         cargo add --manifest-path $FILE $dep@$VERSION
     done
