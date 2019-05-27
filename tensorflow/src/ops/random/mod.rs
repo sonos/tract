fdd22ff647bc8c2b12aac1f5e2f9d1be862a1d6b
Mathieu Poumeyrol <kali@zoy.org>
2019-05-27 18:14:13 +0200

    impl RandomUniformInt (for i32)

--- tensorflow/src/ops/random/mod.rs
+++ tensorflow/src/ops/random/mod.rs
@@ -2,9 +2,8 @@ mod philox;
 mod random_uniform;
 
 use crate::model::TfOpRegister;
-use crate::tfpb::node_def::NodeDef;
-
 
 pub fn register_all_ops(reg: &mut TfOpRegister) {
     reg.insert("RandomUniform", random_uniform::random_uniform);
+    reg.insert("RandomUniformInt", random_uniform::random_uniform_int);
 }
