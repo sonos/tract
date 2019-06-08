fe431e94cf121e5f2dc4cddd5679cc3056c3af1a
Mathieu Poumeyrol <kali@zoy.org>
2019-06-08 13:07:15 +0200

    tweak onnxruntime script for multiple inputs

--- harness/onnx-test-suite/debug-utils/save_all.py
+++ harness/onnx-test-suite/debug-utils/save_all.py
@@ -1,23 +1,36 @@
 import sys
 import numpy
+import onnx
+from onnx import numpy_helper
 import onnxruntime as rt
 
 model = sys.argv[1]
-print(model)
+output_name = sys.argv[2]
+
+print("model: " + model)
+print("output: " + output_name)
 sess = rt.InferenceSession(model)
 
-input_data = sys.argv[2]
-input_data = numpy.load(input_data)
+inputs = {}
+for i in range(3, len(sys.argv)):
+    input_data = sys.argv[i]
+    if input_data.endswith(".npz"):
+        input_data = numpy.load(input_data)
+        input_data = numpy.squeeze(input_data, 0)
+        name = sess.get_inputs()[i-3].name
+    elif input_data.endswith(".pb"):
+        new_tensor = onnx.TensorProto()
+        with open(input_data, 'rb') as f:
+            new_tensor.ParseFromString(f.read())
+        name = new_tensor.name
+        input_data = numpy_helper.to_array(new_tensor)
+    inputs[name] = input_data
 
-input_data = numpy.squeeze(input_data["inputs"], 0)
-
-input_name = sess.get_inputs()[0].name
-pred_onnx = sess.run(None, {input_name: input_data})
-
-outputs = {input_name: input_data}
+outputs = inputs.copy()
+pred_onnx = sess.run(None, inputs)
 
 for ix, output in enumerate(sess.get_outputs()):
     print(output.name)
     outputs[output.name] = pred_onnx[ix]
 
-numpy.savez(sys.argv[3], **outputs)
+numpy.savez(output_name, **outputs)
