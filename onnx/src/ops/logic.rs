600283b4a203b98b2d26e4e8768e6fe084423f01
Mathieu Poumeyrol <kali@zoy.org>
2019-06-09 13:51:15 +0200

    impl onnx.Where as core Iff

--- onnx/src/ops/logic.rs
+++ onnx/src/ops/logic.rs
@@ -10,4 +10,6 @@ pub fn register_all_ops(reg: &mut OnnxOpRegister) {
     reg.insert("Equal", |_, _| Ok((Box::new(tractops::logic::Equals::default()),vec!())));
     reg.insert("Greater", |_, _| Ok((Box::new(tractops::logic::Greater::default()),vec!())));
     reg.insert("Less", |_, _| Ok((Box::new(tractops::logic::Lesser::default()),vec!())));
+
+    reg.insert("Where", |_, _| Ok((Box::new(tractops::logic::Iff::default()),vec!())));
 }
