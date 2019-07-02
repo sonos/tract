b63eb94cc530baa78f1494fc8742a4fc01f46b00
Mathieu Poumeyrol <kali@zoy.org>
2019-07-02 15:11:24 +0200

    Relu

--- kaldi/src/ops.rs
+++ kaldi/src/ops.rs
@@ -5,4 +5,5 @@ mod affine;
 pub fn register_all_ops(reg: &mut KaldiOpRegister) {
     reg.insert("FixedAffineComponent", affine::affine_component);
     reg.insert("NaturalGradientAffineComponent", affine::affine_component);
+    reg.insert("RectifiedLinearComponent", |_,_| Ok(Box::new(tract_core::ops::nn::Relu::default())));
 }
