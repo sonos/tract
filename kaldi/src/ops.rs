e765fa522731aca93a0d319986a12ab0e5e0ba02
Mathieu Poumeyrol <kali@zoy.org>
2019-07-02 16:06:44 +0200

    renorm

--- kaldi/src/ops.rs
+++ kaldi/src/ops.rs
@@ -1,9 +1,11 @@
 use crate::model::KaldiOpRegister;
 
 mod affine;
+mod renorm;
 
 pub fn register_all_ops(reg: &mut KaldiOpRegister) {
     reg.insert("FixedAffineComponent", affine::affine_component);
     reg.insert("NaturalGradientAffineComponent", affine::affine_component);
     reg.insert("RectifiedLinearComponent", |_,_| Ok(Box::new(tract_core::ops::nn::Relu::default())));
+    reg.insert("NormalizeComponent", renorm::renorm);
 }
