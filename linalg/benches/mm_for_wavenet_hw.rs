9cd83c4add055037fce33c1401d39408a2568d97
Mathieu Poumeyrol <kali@zoy.org>
2019-07-07 12:22:11 +0200

    align data in benches

--- linalg/benches/mm_for_wavenet_hw.rs
+++ linalg/benches/mm_for_wavenet_hw.rs
@@ -3,31 +3,38 @@ extern crate criterion;
 extern crate tract_linalg;
 use criterion::Criterion;
 
+pub fn vec(len: usize, align: usize) -> *mut f32 {
+    let layout = std::alloc::Layout::from_size_align(
+        len*std::mem::size_of::<f32>(),
+        align).unwrap();
+    unsafe { std::alloc::alloc_zeroed(layout) as *mut f32 }
+}
+
 fn pack_a(c: &mut Criterion, m: usize, k: usize, n: usize) {
     c.bench_function(&format!("pack_a_{}x{}x{}", m, k, n), move |b| {
         let mm = (tract_linalg::ops().smm)(m, k, n);
-        let a = vec![0.0; m * k];
-        let mut pa = vec![0.0; mm.packed_a_len()];
-        b.iter(move || mm.pack_a(pa.as_mut_ptr(), a.as_ptr(), k as _, 1))
+        let a = vec(m * k, 4);
+        let pa = vec(mm.packed_a_len(), mm.packed_a_alignment());
+        b.iter(move || mm.pack_a(pa, a, k as _, 1))
     });
 }
 
 fn pack_b(c: &mut Criterion, m: usize, k: usize, n: usize) {
     c.bench_function(&format!("pack_b_{}x{}x{}", m, k, n), move |be| {
         let mm = (tract_linalg::ops().smm)(m, k, n);
-        let b = vec![0.0; n * k];
-        let mut pb = vec![0.0; mm.b_pack().len()];
-        be.iter(move || mm.b_pack().pack(pb.as_mut_ptr(), b.as_ptr(), n as _, 1))
+        let b = vec(n * k, 4);
+        let pb = vec(mm.b_pack().len(), mm.b_pack().alignment());
+        be.iter(move || mm.b_pack().pack(pb, b, n as _, 1))
     });
 }
 
 fn mat_mul_prepacked(c: &mut Criterion, m: usize, k: usize, n: usize) {
     c.bench_function(&format!("mat_mul_prepacked_{}x{}x{}", m, k, n), move |be| {
         let mm = (tract_linalg::ops().smm)(m, k, n);
-        let a = vec![0.0; mm.packed_a_len()];
-        let b = vec![0.0; mm.b_pack().len()];
+        let pa = vec(mm.packed_a_len(), mm.packed_a_alignment());
+        let pb = vec(mm.b_pack().len(), mm.b_pack().alignment());
         let mut c = vec![0.0; m * n];
-        be.iter(move || mm.mat_mul_prepacked(a.as_ptr(), b.as_ptr(), c.as_mut_ptr(), n as _, 1))
+        be.iter(move || mm.mat_mul_prepacked(pa, pb, c.as_mut_ptr(), n as _, 1))
     });
 }
 
