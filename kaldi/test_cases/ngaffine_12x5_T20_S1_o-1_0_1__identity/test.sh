c66728dfde60ab64bc2f375940d64c73c7804cd9
tbluche <teo777@gmail.com>
2019-07-09 14:10:53 +0200

    simpler convolution test case for Kaldi models

---
+++ kaldi/test_cases/ngaffine_12x5_T20_S1_o-1_0_1__identity/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+nnet3-compute --frames-per-chunk=21 --frame-subsampling-factor=1 model.raw.txt ark:input.ark ark,t:predicted.ark 2>.error
+fail=0
+diff output.ark predicted.ark || fail=1;
+rm predicted.ark
+echo "fail=$fail" && [ $fail -gt 0 ] && exit 1;
