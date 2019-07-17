d982b44cf1cb5a490db23eeeadde380aca2e8821
tbluche <teo777@gmail.com>
2019-07-17 18:25:24 +0200

    fix kaldi test of delay with subsampling

--- kaldi/test_cases/delay_5_D20_T259_S3/test.sh
+++ kaldi/test_cases/delay_5_D20_T259_S3/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nnet3-compute --frames-per-chunk=265 --frame-subsampling-factor=3 model.raw.txt ark:input.ark ark,t:predicted.ark 2>.error
+nnet3-compute --frames-per-chunk=260 --frame-subsampling-factor=3 model.raw.txt ark:input.ark ark,t:predicted.ark 2>.error
 fail=0
 diff output.ark predicted.ark || fail=1;
 rm predicted.ark
