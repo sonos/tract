5cb5027f6561323fdebe68f0c6bb6a91da3c7d02
Mathieu Poumeyrol <kali@zoy.org>
2019-07-20 11:05:16 +0200

    tweak macos ci on travis

--- .travis/native.sh
+++ .travis/native.sh
@@ -23,7 +23,12 @@ cargo build --release
 cargo test --release --all
 cargo build --release --benches
 
-if [ -z "$TRAVIS" -a -z "$PARTIAL_CI"]
+if [ -n "$TRAVIS" -a -z "$PARTIAL_CI"]
+then
+    exit 0
+fi
+
+if [ -z "$TRAVIS" ]
 then
     sh .travis/debug-tests.sh
     sh .travis/tf.sh
