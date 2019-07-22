28474495f90cdbbacb98776a16dfe9801a5c3997
Mathieu Poumeyrol <kali@zoy.org>
2019-07-22 17:50:44 +0200

    light refacto

--- core/src/ops/array/downsample.rs
+++ core/src/ops/array/downsample.rs
@@ -68,7 +68,10 @@ impl InferenceRulesOp for Downsample {
             for i in 0..(r as usize) {
                 if i == self.axis {
                     s.given(&inputs[0].shape[i], move |s, d| {
-                        s.equals(&outputs[0].shape[i], (d - self.modulo).div_ceil(self.stride.to_dim()))
+                        s.equals(
+                            &outputs[0].shape[i],
+                            (d - self.modulo).div_ceil(self.stride.to_dim()),
+                        )
                     })?
                 } else {
                     s.equals(&inputs[0].shape[i], &outputs[0].shape[i])?
@@ -81,50 +84,60 @@ impl InferenceRulesOp for Downsample {
     inference_op_as_op!();
 }
 
-pub fn pull_downsample_up(
+fn pull_downsample_up(
     model: &TypedModel,
     down_node: &TypedNode,
 ) -> TractResult<Option<TypedModelPatch>> {
+    let down_op = down_node.op_as::<Downsample>().unwrap();
     if let Some(prec) = model.single_prec(down_node.id)? {
-        let down_op = down_node.op_as::<Downsample>().unwrap();
         if let Some(crop_op) = prec.op_as::<ops::array::Crop>() {
-            // TODO: different axis
-            let modulo = (down_op.modulo + crop_op.prune[down_op.axis].0) % down_op.stride;
-            let left = (down_op.modulo + crop_op.prune[down_op.axis].0) / down_op.stride;
-            let mut patch = TypedModelPatch::default();
-            patch.tap_model(model, prec.inputs[0])?;
-            let input_outlet = prec.inputs[0].clone();
-            let input_fact = model.outlet_fact(input_outlet).unwrap();
-            let input_dim = input_fact.shape.dim(down_op.axis);
-            let final_len = down_node.outputs[0].fact.shape.dim(down_op.axis);
-            let mut downed = input_fact.clone();
-            let midway_len = (input_dim - modulo).div_ceil(down_op.stride.into());
-            downed.shape.set_dim(down_op.axis, midway_len.clone())?;
-            patch.chain(
-                "down",
-                Downsample::new(down_op.axis, down_op.stride, modulo),
-                tvec!(downed),
-            )?;
-            let mut new_prunes = crop_op.prune.clone();
-            new_prunes[down_op.axis].0 = left;
-            new_prunes[down_op.axis].1 = (midway_len.to_dim() - final_len.to_dim() - left).to_integer()? as usize;
-            let new_crop = patch.chain(
-                "crop",
-                ops::array::Crop::new(new_prunes),
-                tvec!(down_node.outputs[0].fact.clone()),
-            )?;
-            patch.shunt_outside(OutletId::new(down_node.id, 0), OutletId::new(new_crop, 0))?;
-            return Ok(Some(patch));
+            return pull_downsample_over_crop(model, prec, crop_op, down_node, down_op);
         }
     }
     Ok(None)
 }
 
+fn pull_downsample_over_crop(
+    model: &TypedModel,
+    crop_node: &TypedNode,
+    crop_op: &ops::array::Crop,
+    down_node: &TypedNode,
+    down_op: &Downsample,
+) -> TractResult<Option<TypedModelPatch>> {
+    let modulo = (down_op.modulo + crop_op.prune[down_op.axis].0) % down_op.stride;
+    let left = (down_op.modulo + crop_op.prune[down_op.axis].0) / down_op.stride;
+    let mut patch = TypedModelPatch::default();
+    patch.tap_model(model, crop_node.inputs[0])?;
+    let input_outlet = crop_node.inputs[0].clone();
+    let input_fact = model.outlet_fact(input_outlet).unwrap();
+    let input_len = input_fact.shape.dim(down_op.axis);
+    let final_len = down_node.outputs[0].fact.shape.dim(down_op.axis);
+    let mut downed = input_fact.clone();
+    let midway_len = (input_len - modulo).div_ceil(down_op.stride.into());
+    downed.shape.set_dim(down_op.axis, midway_len.clone())?;
+    patch.chain(
+        &down_node.name,
+        Downsample::new(down_op.axis, down_op.stride, modulo),
+        tvec!(downed),
+    )?;
+    let mut new_prunes = crop_op.prune.clone();
+    new_prunes[down_op.axis].0 = left;
+    new_prunes[down_op.axis].1 =
+        (midway_len.to_dim() - final_len.to_dim() - left).to_integer()? as usize;
+    let new_crop = patch.chain(
+        &crop_node.name,
+        ops::array::Crop::new(new_prunes),
+        tvec!(down_node.outputs[0].fact.clone()),
+    )?;
+    patch.shunt_outside(OutletId::new(down_node.id, 0), OutletId::new(new_crop, 0))?;
+    return Ok(Some(patch));
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use proptest::prelude::*;
     use crate::ops;
+    use proptest::prelude::*;
     use proptest::test_runner::TestCaseResult;
 
     fn crop_then_down_strat() -> BoxedStrategy<(usize, usize, usize, usize, usize)> {
