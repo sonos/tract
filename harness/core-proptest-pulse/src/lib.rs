f487e654a6899c47afb025571cd53d6abf6a6967
Mathieu Poumeyrol <kali@zoy.org>
2019-08-02 12:36:46 +0200

    move pulsing proptests to harness

---
+++ harness/core-proptest-pulse/src/lib.rs
@@ -0,0 +1,190 @@
+#![cfg(test)]
+
+use tract_core::ndarray::*;
+use tract_core::internal::*;
+use proptest::prelude::*;
+use proptest::proptest;
+use proptest::test_runner::TestCaseResult;
+use proptest::*;
+use tract_core::dimfact;
+use tract_core::shapefact;
+
+mod pad_plus_conv;
+
+fn proptest_regular_against_pulse(
+    model: InferenceModel,
+    pulse: usize,
+    input_array: ArrayD<f32>,
+    axis: usize,
+) -> TestCaseResult {
+    let mut ref_model = model.clone();
+    ref_model
+        .set_input_fact(0, TensorFact::dt_shape(f32::datum_type(), input_array.shape()))?;
+    let input = Tensor::from(input_array.clone());
+    let plan = SimplePlan::new(&ref_model).unwrap();
+    let outputs = plan.run(tvec!(input.clone())).unwrap();
+
+    let model = model.into_normalized().unwrap();
+
+    let pulsed = PulsedModel::new(&model, pulse).unwrap();
+    let output_fact = pulsed.output_fact(0).unwrap().clone();
+
+    let output_stream_axis = output_fact.axis;
+    let delay = output_fact.delay;
+    let mut initial_output_shape = output_fact.shape.clone();
+    initial_output_shape[output_stream_axis] = 0;
+
+    let pulsed_plan = SimplePlan::new(pulsed).unwrap();
+    let mut state = SimpleState::new(&pulsed_plan).unwrap();
+
+    let mut got: ArrayD<f32> = ArrayD::zeros(&*initial_output_shape);
+    let mut output_len = None;
+
+    let mut written = 0;
+    loop {
+        let to_write_in_chunk = pulse.min(input_array.shape()[axis].saturating_sub(written));
+        let mut chunk: ArrayD<f32> = input_array
+            .slice_axis(Axis(axis), (written..written + to_write_in_chunk).into())
+            .to_owned();
+        written += to_write_in_chunk;
+        if to_write_in_chunk < pulse {
+            let mut filler_shape = input_array.shape().to_vec();
+            filler_shape[axis] = pulse - to_write_in_chunk;
+            chunk = stack(
+                Axis(axis),
+                &[chunk.view(), ArrayD::from_elem(filler_shape, std::f32::NAN).view()],
+            )
+            .unwrap();
+            output_len = output_fact.dim.eval(written as _);
+            state.session_state.known_stream_len = Some(written)
+        }
+        let mut outputs = state.run(tvec!(Tensor::from(chunk.to_owned()).into())).unwrap();
+        got = stack(
+            Axis(output_stream_axis),
+            &[got.view(), outputs.remove(0).to_array_view::<f32>().unwrap()],
+        )
+        .unwrap();
+        if let Some(output_len) = output_len {
+            if got.shape()[output_stream_axis] >= output_len as usize + delay {
+                break;
+            }
+        }
+    }
+
+    let pulsed_output = got.slice_axis(
+        Axis(output_stream_axis),
+        (output_fact.delay..output_fact.delay + output_len.unwrap() as usize).into(),
+    );
+
+    prop_assert_eq!(pulsed_output, outputs[0].to_array_view::<f32>().unwrap());
+    Ok(())
+}
+
+proptest! {
+    #[test]
+    fn proptest_crop(pulse in 1i32..3, input_len in 0i32..10, begin in 0i32..3, end in 0i32..3) {
+        use tract_core::ops::array::Crop;
+        let input_len = input_len + begin + end;
+        let mut model = InferenceModel::default();
+        let _ = model
+            .add_source("a", TensorFact::dt_shape(f32::datum_type(), shapefact!(S)))
+            .unwrap();
+        model.chain_default("slice", Crop::new(vec![(begin as usize, end as usize)])).unwrap();
+        model.auto_outputs().unwrap();
+
+        let input = Array1::range(1.0f32, input_len as f32 + 1.0, 1.0);
+        proptest_regular_against_pulse(model, pulse as _, input.into_dyn(), 0)?;
+    }
+
+    #[test]
+    fn proptest_pad(pulse in 1i32..3, input_len in 0i32..10, begin in 0i32..3, end in 0i32..3) {
+        use tract_core::ops::array::{ Pad, PadMode };
+        let mut model = InferenceModel::default();
+        let _ = model
+            .add_source("a", TensorFact::dt_shape(f32::datum_type(), shapefact!(S)))
+            .unwrap();
+        model.chain_default("pad", Pad::new(vec![(begin as _, end as _)],
+            PadMode::Constant(Arc::new(Tensor::from(-1f32))))).unwrap();
+        model.auto_outputs().unwrap();
+
+        let input = Array1::range(1.0f32, input_len as f32 + 1.0, 1.0);
+        proptest_regular_against_pulse(model, pulse as _, input.into_dyn(), 0)?;
+    }
+}
+
+fn vec(len: impl Strategy<Value = usize>) -> impl Strategy<Value = Vec<f32>> {
+    len.prop_flat_map(|l| proptest::collection::vec(-5..5, l..=l))
+        .prop_map(|v| v.into_iter().map(|f| f as f32).collect())
+}
+
+#[test]
+fn test_simple_conv() {
+    use tract_core::ops::cnn::*;
+
+    let mut model = InferenceModel::default();
+    let ker = model.add_const("kernel", tensor3(&[[[0.5f32, 1.0, -0.1]]])).unwrap();
+    let _ = model
+        .add_source("a", TensorFact::dt_shape(f32::datum_type(), shapefact!(1, 1, S))) // NCT
+        .unwrap();
+
+    let conv = model.chain_default("conv", Conv::default()).unwrap();
+    model.add_edge(OutletId::new(ker, 0), InletId::new(conv, 1)).unwrap();
+    model.auto_outputs().unwrap();
+
+    let input = arr3(&[[[1.0f32, 0.0, 0.0, 0.0, 0.0, 2.0, 0.0, 0.0]]]);
+    proptest_regular_against_pulse(model, 4, input.into_dyn(), 2).unwrap();
+}
+
+#[test]
+fn test_pad_after_1() {
+    use tract_core::ops::array::{Pad, PadMode};
+    let mut model = InferenceModel::default();
+    let _ =
+        model.add_source("a", TensorFact::dt_shape(f32::datum_type(), shapefact!(S))).unwrap();
+    model
+        .chain_default(
+            "pad",
+            Pad::new(vec![(0, 1)], PadMode::Constant(Arc::new(Tensor::from(-1f32)))),
+        )
+        .unwrap();
+    model.auto_outputs().unwrap();
+
+    let input = arr1(&[]);
+    proptest_regular_against_pulse(model, 1, input.into_dyn(), 0).unwrap();
+}
+
+#[test]
+fn test_pad_before_1() {
+    use tract_core::ops::array::{Pad, PadMode};
+    let mut model = InferenceModel::default();
+    let _ =
+        model.add_source("a", TensorFact::dt_shape(f32::datum_type(), shapefact!(S))).unwrap();
+    model
+        .chain_default(
+            "pad",
+            Pad::new(vec![(1, 0)], PadMode::Constant(Arc::new(Tensor::from(-1f32)))),
+        )
+        .unwrap();
+    model.auto_outputs().unwrap();
+
+    let input = arr1(&[1.0]);
+    proptest_regular_against_pulse(model, 1, input.into_dyn(), 0).unwrap();
+}
+
+#[test]
+fn test_pad_before_2() {
+    use tract_core::ops::array::{Pad, PadMode};
+    let mut model = InferenceModel::default();
+    let _ =
+        model.add_source("a", TensorFact::dt_shape(f32::datum_type(), shapefact!(S))).unwrap();
+    model
+        .chain_default(
+            "pad",
+            Pad::new(vec![(1, 0)], PadMode::Constant(Arc::new(Tensor::from(-1f32)))),
+        )
+        .unwrap();
+    model.auto_outputs().unwrap();
+
+    let input = arr1(&[1.0, 2.0]);
+    proptest_regular_against_pulse(model, 2, input.into_dyn(), 0).unwrap();
+}
---
+++ harness/core-proptest-pulse/src/pad_plus_conv.rs
@@ -0,0 +1,234 @@
+use tract_core::ndarray::*;
+use tract_core::internal::*;
+use proptest::proptest;
+use proptest::test_runner::TestCaseResult;
+use proptest::*;
+use tract_core::ops::array::PadMode;
+use tract_core::dimfact;
+use tract_core::shapefact;
+
+use super::*;
+
+#[derive(Debug, Clone)]
+struct PadPlusConvProblem {
+    pad_before: usize,
+    pad_after: usize,
+    pad_mode: PadMode,
+    stride: usize,
+    dilation: usize,
+    pulse: usize,
+    ker: Array3<f32>,
+    input: Array3<f32>,
+}
+
+impl Arbitrary for PadPlusConvProblem {
+    type Parameters = ();
+    type Strategy = BoxedStrategy<Self>;
+
+    fn arbitrary_with(_: Self::Parameters) -> BoxedStrategy<PadPlusConvProblem> {
+        (1usize..3, vec(1usize..3), 1usize..3, 0usize..15, 0usize..15, 1usize..3, bool::ANY)
+            .prop_flat_map(|(stride, ker, dil, pad_before, pad_after, pulse_factor, edge)| {
+                let min_input = (ker.len() * dil).max(pulse_factor * stride);
+                (
+                    Just(stride),
+                    Just(ker),
+                    Just(dil),
+                    Just(pad_before),
+                    Just(pad_after),
+                    Just(stride * pulse_factor),
+                    vec(min_input..3 * min_input),
+                    Just(edge)
+                )
+            })
+            .prop_map(|(stride, ker, dilation, pad_before, pad_after, pulse, input, edge)| {
+                let pad_mode = if edge && pad_before < pulse {
+                    PadMode::Edge
+                } else {
+                    PadMode::Constant(Tensor::from(9999f32).into())
+                };
+                let input = Array3::from_shape_vec((1, 1, input.len()), input).unwrap(); // NCHW
+                let ker = Array3::from_shape_vec((1, 1, ker.len()), ker).unwrap(); // OIHW
+                PadPlusConvProblem { pad_before, pad_after, pad_mode, stride, dilation, pulse, ker, input }
+            })
+            .boxed()
+    }
+}
+
+impl PadPlusConvProblem {
+    pub fn run(&self) -> TestCaseResult {
+        use tract_core::ops::array::Pad;
+        use tract_core::ops::cnn::*;
+        let mut model = InferenceModel::default();
+        let _ = model
+            .add_source("a", TensorFact::dt_shape(f32::datum_type(), shapefact!(1, 1, S)))
+            .unwrap();
+        if self.pad_before > 0 || self.pad_after > 0 {
+            model
+                .chain_default(
+                    "pad",
+                    Pad::new(
+                        vec![(0, 0), (0, 0), (self.pad_before, self.pad_after)],
+                        self.pad_mode.clone(),
+                    ),
+                )
+                .unwrap();
+        }
+        let mut conv = Conv::default();
+        conv.dilations = Some(tvec!(self.dilation));
+        conv.strides = Some(tvec!(self.stride));
+        let conv = model.chain_default("conv", conv).unwrap();
+        model.plug_const(InletId::new(conv, 1), "kernel", self.ker.clone()).unwrap();
+        model.auto_outputs().unwrap();
+        proptest_regular_against_pulse(model, self.pulse as _, self.input.clone().into_dyn(), 2)
+    }
+}
+
+proptest! {
+    #[test]
+    fn proptest_conv(pb in PadPlusConvProblem::arbitrary()) { pb.run().unwrap() }
+}
+
+#[test]
+fn conv_1() {
+    PadPlusConvProblem {
+        pad_before: 0,
+        pad_after: 0,
+        pad_mode: PadMode::Constant(tensor0(9999f32).into()),
+        stride: 1,
+        dilation: 1,
+        pulse: 1,
+        ker: arr3(&[[[0.0f32]]]),
+        input: arr3(&[[[0.0f32, 0.0]]]),
+    }
+    .run()
+    .unwrap()
+}
+
+#[test]
+fn conv_2() {
+    PadPlusConvProblem {
+        pad_before: 0,
+        pad_after: 0,
+        pad_mode: PadMode::Constant(tensor0(9999f32).into()),
+        stride: 2,
+        dilation: 2,
+        pulse: 2,
+        ker: arr3(&[[[0.0f32]]]),
+        input: arr3(&[[[0.0f32, 0.0]]]),
+    }
+    .run()
+    .unwrap()
+}
+
+#[test]
+fn conv_3() {
+    PadPlusConvProblem {
+        pad_before: 0,
+        pad_after: 0,
+        pad_mode: PadMode::Constant(tensor0(9999f32).into()),
+        stride: 2,
+        dilation: 1,
+        pulse: 2,
+        ker: arr3(&[[[0.0f32]]]),
+        input: arr3(&[[[0.0f32, 0.0, 0.0]]]),
+    }
+    .run()
+    .unwrap()
+}
+
+#[test]
+fn conv_4() {
+    PadPlusConvProblem {
+        pad_before: 0,
+        pad_after: 0,
+        pad_mode: PadMode::Constant(tensor0(9999f32).into()),
+        stride: 2,
+        dilation: 2,
+        pulse: 2,
+        ker: arr3(&[[[0.0f32]]]),
+        input: arr3(&[[[0.0f32, 0.0, 0.0]]]),
+    }
+    .run()
+    .unwrap()
+}
+
+#[test]
+fn conv_5() {
+    PadPlusConvProblem {
+        pad_before: 2,
+        pad_after: 0,
+        pad_mode: PadMode::Constant(tensor0(9999f32).into()),
+        stride: 2,
+        dilation: 1,
+        pulse: 2,
+        ker: arr3(&[[[0.0f32, 1.0]]]),
+        input: arr3(&[[[1.0f32, 0.0]]]),
+    }
+    .run()
+    .unwrap()
+}
+
+#[test]
+fn conv_6() {
+    PadPlusConvProblem {
+        pad_before: 0,
+        pad_after: 0,
+        pad_mode: PadMode::Constant(tensor0(9999f32).into()),
+        stride: 2,
+        dilation: 1,
+        pulse: 2,
+        ker: arr3(&[[[0.0f32]]]),
+        input: arr3(&[[[0.0f32, 0.0, 0.0]]]),
+    }
+    .run()
+    .unwrap()
+}
+
+#[test]
+fn conv_7() {
+    PadPlusConvProblem {
+        pad_before: 0,
+        pad_after: 1,
+        pad_mode: PadMode::Edge,
+        stride: 1,
+        dilation: 1,
+        pulse: 1,
+        ker: arr3(&[[[0.0f32]]]),
+        input: arr3(&[[[0.0f32]]]),
+    }
+    .run()
+    .unwrap()
+}
+
+#[test]
+fn conv_8() {
+    PadPlusConvProblem {
+        pad_before: 1,
+        pad_after: 0,
+        pad_mode: PadMode::Edge,
+        stride: 2,
+        dilation: 2,
+        pulse: 2,
+        ker: arr3(&[[[0.0f32]]]),
+        input: arr3(&[[[0.0f32, 0.0f32]]]),
+    }
+    .run()
+    .unwrap()
+}
+
+#[test]
+fn conv_kaldi_librispeech() {
+    PadPlusConvProblem {
+        pad_before: 5,
+        pad_after: 15,
+        pad_mode: PadMode::Edge,
+        stride: 3,
+        dilation: 1,
+        pulse: 9,
+        ker: arr3(&[[[1f32, 0f32, 0f32, 0f32, 0f32]]]),
+        input: Array3::from_shape_vec((1,1,10), (1..=10).map(|i| i as f32).collect()).unwrap()
+    }
+    .run()
+    .unwrap()
+}
+
