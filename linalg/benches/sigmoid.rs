87babf63a8996197a0d653ed4fa4b077a122ee85
Mathieu Poumeyrol <kali@zoy.org>
2019-08-15 11:31:27 +0200

    small sigmoid bench

---
+++ linalg/benches/sigmoid.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate criterion;
+extern crate tract_linalg;
+use criterion::Criterion;
+
+fn ssigmoid(c:&mut Criterion, n: usize) {
+    c.bench_function(&format!("ssigmoid_{}", n), move |be| {
+        let mut s = (0..n).map(|i| i as f32 / 10.0).collect::<Vec<f32>>();
+        let ref op = tract_linalg::ops().ssigmoid;
+        be.iter(|| op(&mut s) );
+    });
+}
+
+fn bs(c: &mut Criterion) {
+    ssigmoid(c, 4);
+    ssigmoid(c, 8);
+    ssigmoid(c, 128);
+    ssigmoid(c, 1024);
+}
+
+criterion_group!(benches, bs);
+criterion_main!(benches);
