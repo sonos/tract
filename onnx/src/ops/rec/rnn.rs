344383b361ce49f8b7c3473c52b3bd22918fb6b0
Mathieu Poumeyrol <kali@zoy.org>
2019-08-16 10:41:41 +0200

    fix support of optional i/o in onnx rec op

--- onnx/src/ops/rec/rnn.rs
+++ onnx/src/ops/rec/rnn.rs
@@ -6,15 +6,38 @@ use tract_core::ops as core_ops;
 
 pub fn rnn(_ctx: &ParsingContext, pb: &NodeProto) -> TractResult<(Box<dyn InferenceOp>, Vec<String>)> {
     let mut rnn = RNN::default();
-    rnn.want_output_0_y = pb.get_output().get(0).map(|s| !s.is_empty()).unwrap_or(false);
-    rnn.want_output_1_y_h = pb.get_output().get(1).map(|s| !s.is_empty()).unwrap_or(false);
+
+    let mut real_input_count = 3;
+    let mut options = (3..6).map(|i| {
+        pb.get_input().get(i).filter(|s| !s.is_empty()).map(|_| {
+            real_input_count += 1;
+            real_input_count - 1
+        })
+    });
+    rnn.optional_bias_input = options.next().unwrap();
+    rnn.optional_sequence_lens_input = options.next().unwrap();
+    rnn.optional_initial_h_input = options.next().unwrap();
+
+    let mut real_output_count = 0;
+    let mut options = (0..2).map(|i| {
+        pb.get_output().get(i).filter(|s| !s.is_empty()).map(|_| {
+            real_output_count += 1;
+            real_output_count - 1
+        })
+    });
+    rnn.optional_y_output = options.next().unwrap();
+    rnn.optional_y_h_output = options.next().unwrap();
+
     Ok((Box::new(rnn), vec![]))
 }
 
 #[derive(Debug, Clone, new)]
 pub struct RNN {
-    pub want_output_0_y: bool,
-    pub want_output_1_y_h: bool,
+    pub optional_bias_input: Option<usize>,
+    pub optional_sequence_lens_input: Option<usize>,
+    pub optional_initial_h_input: Option<usize>,
+    pub optional_y_output: Option<usize>,
+    pub optional_y_h_output: Option<usize>,
     pub fore: Box<dyn StatelessOp>,
     pub back: Box<dyn StatelessOp>,
     pub initial_h: Option<Tensor>,
@@ -23,8 +46,11 @@ pub struct RNN {
 impl Default for RNN {
     fn default() -> RNN {
         RNN {
-            want_output_0_y: false,
-            want_output_1_y_h: false,
+            optional_bias_input: None,
+            optional_sequence_lens_input: None,
+            optional_initial_h_input: None,
+            optional_y_output: None,
+            optional_y_h_output: None,
             fore: Box::new(core_ops::nn::Tanh::new(f32::datum_type().into())),
             back: Box::new(core_ops::nn::Tanh::new(f32::datum_type().into())),
             initial_h: None,
@@ -49,6 +75,14 @@ impl InferenceRulesOp for RNN {
         inputs: &'p [TensorProxy],
         outputs: &'p [TensorProxy],
     ) -> InferenceResult {
+        let input_count = 3
+            + self.optional_bias_input.is_some() as usize
+            + self.optional_sequence_lens_input.is_some() as usize
+            + self.optional_initial_h_input.is_some() as usize;
+        check_input_arity(&inputs, input_count)?;
+        let output_count =
+            self.optional_y_output.is_some() as usize + self.optional_y_h_output.is_some() as usize;
+        check_output_arity(&outputs, output_count)?;
         s.equals(&inputs[0].datum_type, &inputs[1].datum_type)?;
         s.equals(&inputs[0].datum_type, &inputs[2].datum_type)?;
         s.equals(&inputs[0].datum_type, &outputs[0].datum_type)?;
@@ -58,26 +92,37 @@ impl InferenceRulesOp for RNN {
         s.equals(&inputs[1].shape[0], &inputs[2].shape[0])?; // num_directions
         s.equals(&inputs[1].shape[1], &inputs[2].shape[1])?; // hidden_size
         s.equals(&inputs[1].shape[1], &inputs[2].shape[2])?; // hidden_size
-        if inputs.len() > 3 {
-            // bias
-            s.equals(&inputs[3].datum_type, &inputs[0].datum_type)?;
-            s.equals(&inputs[3].rank, 2)?;
-            s.equals(&inputs[3].shape[0], &inputs[2].shape[0])?; // num_directions
-            s.equals(&inputs[3].shape[1], 2 * inputs[2].shape[2].bex())?; // 2 * hidden_size
+        if let Some(bias) = self.optional_bias_input {
+            s.equals(&inputs[bias].datum_type, &inputs[0].datum_type)?;
+            s.equals(&inputs[bias].rank, 2)?;
+            s.equals(&inputs[bias].shape[0], &inputs[2].shape[0])?; // num_directions
+            s.equals(&inputs[bias].shape[1], 2 * inputs[2].shape[2].bex())?; // 6 * hidden_size
         }
-        if outputs.len() > 0 {
-            s.equals(&outputs[0].rank, 4)?;
-            s.equals(&outputs[0].shape[0], &inputs[0].shape[0])?; // seq_lentgh
-            s.equals(&outputs[0].shape[1], &inputs[1].shape[0])?; // num_directions
-            s.equals(&outputs[0].shape[2], &inputs[0].shape[1])?; // batch_size
-            s.equals(&outputs[0].shape[3], &inputs[2].shape[2])?; // hidden_size
+        if let Some(seq_len) = self.optional_sequence_lens_input {
+            s.equals(&inputs[seq_len].rank, 1)?;
+            s.equals(&inputs[seq_len].shape[0], &inputs[0].shape[1])?; // batch_size
         }
-        if outputs.len() > 1 {
-            s.equals(&inputs[0].datum_type, &outputs[1].datum_type)?;
-            s.equals(&outputs[1].rank, 3)?;
-            s.equals(&outputs[1].shape[0], &inputs[1].shape[0])?; // num_directions
-            s.equals(&outputs[1].shape[1], &inputs[0].shape[1])?; // batch_size
-            s.equals(&outputs[1].shape[2], &inputs[2].shape[2])?; // hidden_size
+        if let Some(initial_h) = self.optional_initial_h_input {
+            s.equals(&inputs[initial_h].datum_type, &inputs[0].datum_type)?;
+            s.equals(&inputs[initial_h].rank, 3)?;
+            s.equals(&inputs[initial_h].shape[0], &inputs[1].shape[0])?; // num_directions
+            s.equals(&inputs[initial_h].shape[1], &inputs[0].shape[1])?; // batch_size
+            s.equals(&inputs[initial_h].shape[2], &inputs[2].shape[2])?; // hidden_size
+        }
+        if let Some(y) = self.optional_y_output {
+            s.equals(&outputs[y].datum_type, &inputs[0].datum_type)?;
+            s.equals(&outputs[y].rank, 4)?;
+            s.equals(&outputs[y].shape[0], &inputs[0].shape[0])?; // seq_lenght
+            s.equals(&outputs[y].shape[1], &inputs[1].shape[0])?; // num_directions
+            s.equals(&outputs[y].shape[2], &inputs[0].shape[1])?; // batch_size
+            s.equals(&outputs[y].shape[3], &inputs[2].shape[2])?; // hidden_size
+        }
+        if let Some(y_h) = self.optional_y_h_output {
+            s.equals(&outputs[y_h].datum_type, &inputs[0].datum_type)?;
+            s.equals(&outputs[y_h].rank, 3)?;
+            s.equals(&outputs[y_h].shape[0], &inputs[1].shape[0])?; // num_directions
+            s.equals(&outputs[y_h].shape[1], &inputs[0].shape[1])?; // batch_size
+            s.equals(&outputs[y_h].shape[2], &inputs[2].shape[2])?; // hidden_size
         }
         Ok(())
     }
@@ -91,8 +136,8 @@ impl StatelessOp for RNN {
         let w: ArrayView3<f32> = inputs[1].to_array_view::<f32>()?.into_dimensionality()?; // [num_directions, hidden_size, input_size]
         let r: ArrayView3<f32> = inputs[2].to_array_view::<f32>()?.into_dimensionality()?; // [num_directions, hidden_size, hidden_size]
 
-        let bias = if let Some(bias) = inputs.get(3) {
-            Some(bias.to_array_view::<f32>()?.into_dimensionality::<Ix2>()?) // [num_directions, 2*hidden_size]
+        let bias = if let Some(ix) = self.optional_bias_input {
+            Some(inputs[ix].to_array_view::<f32>()?.into_dimensionality::<Ix2>()?) // [num_directions, 6*hidden_size]
         } else {
             None
         };
@@ -102,17 +147,8 @@ impl StatelessOp for RNN {
         let num_directions = w.shape()[0];
         let hidden_size = r.shape()[2];
 
-        let mut output_0_y = if self.want_output_0_y {
-            Some(Array4::<f32>::zeros((seq_length, num_directions, batch_size, hidden_size)))
-        } else {
-            None
-        };
-
-        let mut output_1_y_h = if self.want_output_1_y_h {
-            Some(Array3::<f32>::zeros((num_directions, batch_size, hidden_size)))
-        } else {
-            None
-        };
+        let mut output_y = self.optional_y_output.map(|_| Array4::<f32>::zeros((seq_length, num_directions, batch_size, hidden_size)));
+        let mut output_y_h = self.optional_y_h_output.map(|_| Array3::<f32>::zeros((num_directions, batch_size, hidden_size)));
 
         for dir in 0..num_directions {
             let w = w.index_axis_move(Axis(0), dir);
@@ -143,24 +179,18 @@ impl StatelessOp for RNN {
                     .into_dimensionality::<Ix2>()?;
                 ht.assign(&ht1);
 
-                if let Some(ref mut o) = output_0_y {
+                if let Some(ref mut o) = output_y {
                     o.index_axis_mut(Axis(0), ix).index_axis_move(Axis(0), dir).assign(&ht);
                 }
             }
-            if let Some(ref mut o) = output_1_y_h {
+            if let Some(ref mut o) = output_y_h {
                 o.index_axis_mut(Axis(0), dir).assign(&ht);
             }
         }
 
-        let mut outputs = tvec!(
-            output_0_y.map(|o| o.into_arc_tensor()),
-            output_1_y_h.map(|o| o.into_arc_tensor()),
-        );
-        while outputs.len() > 0 && outputs.last().unwrap().is_none() {
-            outputs.pop();
-        }
-        Ok(outputs.into_iter()
-            .map(|t| t.unwrap_or(Tensor::from(0.0f32).into_arc_tensor()))
-            .collect())
+        let mut outputs = tvec!();
+        outputs.extend(output_y.into_iter().map(|t| t.into_arc_tensor()));
+        outputs.extend(output_y_h.into_iter().map(|t| t.into_arc_tensor()));
+        Ok(outputs)
     }
 }
