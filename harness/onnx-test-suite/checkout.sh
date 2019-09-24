e5dd0d42ea61aad24df93df592a31ce35e9bf960
Mathieu Poumeyrol <kali@zoy.org>
2019-09-24 09:42:56 +0200

    introduce 1.5.0 testsuite

--- harness/onnx-test-suite/checkout.sh
+++ harness/onnx-test-suite/checkout.sh
@@ -7,18 +7,19 @@ then
     CACHEDIR=`dirname $0`/../../.cached
 fi
 
-mkdir -p $CACHEDIR
-cd $CACHEDIR
+mkdir -p $CACHEDIR/onnx
+cd $CACHEDIR/onnx
 
-find .
-
-if [ ! -e onnx/onnx/backend/test/data ]
-then
-    (
-        rm -rf onnx
-        uuid=$(uuidgen)
-        git clone https://github.com/onnx/onnx onnx-$uuid
-        (cd onnx-$uuid ; git checkout v1.4.1)
-        mv onnx-$uuid onnx
-    )
-fi
+for version in 1.4.1 1.5.0
+do
+    if [ ! -e onnx-$version/backend/test/data ]
+    then
+        (
+            rm -rf onnx-$version
+            tmp=$(mktemp -d -p .)
+            git clone https://github.com/onnx/onnx $tmp
+            (cd $tmp ; git checkout v$version)
+            mv $tmp onnx-$version
+        )
+    fi
+done
