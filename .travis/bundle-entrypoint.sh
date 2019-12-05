071c5c0b0bfdc567989d8f38d0abeed027c88eb2
Mathieu Poumeyrol <kali@zoy.org>
2019-12-05 15:22:20 +0100

    csv changes in criterion

--- .travis/bundle-entrypoint.sh
+++ .travis/bundle-entrypoint.sh
@@ -29,8 +29,8 @@ echo binary_size.cli $binary_size_cli > metrics
     for bench in `find target/criterion -path "*/new/*" -name raw.csv`
     do
         group=`cat $bench | tail -1 | cut -d , -f 1 | cut -d . -f 1`
-        nanos=`cat $bench | tail -1 | cut -d , -f 4 | cut -d . -f 1`
-        iter=`cat $bench | tail -1 | cut -d , -f 5 | cut -d . -f 1`
+        nanos=`cat $bench | tail -1 | cut -d , -f 6 | cut -d . -f 1`
+        iter=`cat $bench | tail -1 | cut -d , -f 8 | cut -d . -f 1`
         time=$((nanos/iter))
         echo microbench.${group}.${func:-none}.${value:-none} $(($nanos/$iter)) >> metrics
     done
