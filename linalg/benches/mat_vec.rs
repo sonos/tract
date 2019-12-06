4b04d40bfb04fd3039a9c2f740013ef7a36b2307
Mathieu Poumeyrol <kali@zoy.org>
2019-12-06 14:19:52 +0100

    fix bench vec_mat

--- linalg/benches/mat_vec.rs
+++ linalg/benches/mat_vec.rs
@@ -16,10 +16,11 @@ fn mat_vec_mul(c: &mut Criterion) {
             BenchmarkId::from_parameter(format!("{}x{}", m, k)),
             &(m, k),
             |be, (&m, &k)| {
-                let mm = (tract_linalg::ops().smmm)(m, k, 1);
+                let mut mm = (tract_linalg::ops().smmm)(m, k, 1);
                 let pa = vec(mm.a_pack().len(), mm.a_pack().alignment());
                 let b = vec![0.0; k];
                 let mut c = vec![0.0; m];
+                unsafe { mm.b_vec_from_data(); }
                 be.iter(move || unsafe { mm.run(pa, b.as_ptr(), c.as_mut_ptr(), &[]) });
             },
         );
