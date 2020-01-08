9567648ce93cc85b4257e4fb7e0e93c511893320
Mathieu Poumeyrol <kali@zoy.org>
2020-01-08 11:50:44 +0100

    improve Scan mapping display

--- core/src/ops/scan/mod.rs
+++ core/src/ops/scan/mod.rs
@@ -1,4 +1,5 @@
 use crate::internal::*;
+use std::fmt;
 
 mod codegen;
 mod inference;
@@ -7,7 +8,7 @@ mod typed;
 pub use inference::InferenceScan;
 pub use typed::TypedScan;
 
-#[derive(Debug, Clone, new)]
+#[derive(Clone, new)]
 pub enum InputMapping<C: Clone> {
     Full { slot: usize },
     State { initializer: StateInitializer },
@@ -38,7 +39,17 @@ impl<C: Clone> InputMapping<C> {
     }
 }
 
-#[derive(Debug, Clone, new)]
+impl<C: Clone> fmt::Debug for InputMapping<C> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            InputMapping::Full { slot } => write!(fmt, "Full, from outer input {}", slot),
+            InputMapping::State { initializer } => write!(fmt, "State initialized by: {:?}", initializer),
+            InputMapping::Scan { slot, axis, .. } => write!(fmt, "Scan outer input {} (axis: {})", slot, axis),
+        }
+    }
+}
+
+#[derive(Clone, new)]
 pub struct OutputMapping<C: Clone, F: Clone> {
     pub full_slot: Option<usize>,
     pub axis: usize,
@@ -51,6 +62,22 @@ pub struct OutputMapping<C: Clone, F: Clone> {
 impl<C: Clone, F: Clone> OutputMapping<C, F> {
     pub fn invisible(&self) -> bool {
         self.full_slot.is_none() && self.last_value_slot.is_none()
+    }
+}
+
+impl<C: Clone, F:Clone> fmt::Debug for OutputMapping<C, F> {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "axis:{} ", self.axis)?;
+        if self.state {
+            write!(fmt, "State, ")?;
+        }
+        if let Some(last_value_slot) = self.last_value_slot {
+            write!(fmt, "Last value to {},", last_value_slot)?;
+        }
+        if let Some(full_slot) = self.full_slot {
+            write!(fmt, "Full value to {},", full_slot)?;
+        }
+        Ok(())
     }
 }
 
