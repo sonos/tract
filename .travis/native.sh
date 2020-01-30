e3d816d738c8de4801e13b499fc1f30132b0c70c
Mathieu Poumeyrol <kali@zoy.org>
2020-01-30 11:59:28 +0100

    ifne not widely available

--- .travis/native.sh
+++ .travis/native.sh
@@ -88,8 +88,8 @@ fi
      -i Sx20xf32 --pulse 8 cost -q \
      --assert-cost "FMA(F32)=2060448,Div(F32)=24576,Buffer(F32)=2920"
 
-./target/release/tract $CACHEDIR/hey_snips_v4_model17.pb \
-     -i Sx20xf32 --pulse 8 dump --op-name AddAxis | ifne false
+[ ! -z "$(./target/release/tract $CACHEDIR/hey_snips_v4_model17.pb -i Sx20xf32 --pass type dump --op-name AddAxis)" ]
+[ -z "$(./target/release/tract $CACHEDIR/hey_snips_v4_model17.pb -i Sx20xf32 dump --op-name AddAxis)" ]
 
 ( cd kaldi/test_cases ; TRACT_RUN=../../target/release/tract ./run_all.sh )
 
