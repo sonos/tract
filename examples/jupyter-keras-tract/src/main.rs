7ff4203daa08690c03ec0b692f3c48c5c90e3099
Mathieu Poumeyrol <kali@zoy.org>
2020-02-15 10:33:23 +0100

    update jupyter example

--- examples/jupyter-keras-tract/src/main.rs
+++ examples/jupyter-keras-tract/src/main.rs
@@ -1,10 +1,9 @@
 use rand::*;
-use tract_core::ndarray;
-use tract_core::prelude::*;
+use tract_tensorflow::prelude::*;
 
 fn main() -> TractResult<()> {
     // load the model
-    let mut model = tract_tensorflow::tensorflow().model_for_path("./my_model.pb")?;
+    let mut model = tensorflow().model_for_path("./my_model.pb")?;
 
     // specify input type and shape
     model.set_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec![10, 100]))?;
@@ -13,7 +12,7 @@ fn main() -> TractResult<()> {
     // Generate some input data for the model
     let mut rng = thread_rng();
     let vals: Vec<_> = (0..1000).map(|_| rng.gen::<f32>()).collect();
-    let input = ndarray::arr1(&vals).into_shape((10, 100)).unwrap();
+    let input = tract_ndarray::arr1(&vals).into_shape((10, 100)).unwrap();
 
     // Make an execution plan for the model
     let plan = SimplePlan::new(&model).unwrap();
