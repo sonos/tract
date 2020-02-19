87adaaeee7528ae45d775a66b8ee4774594360c0
Mathieu Poumeyrol <kali@zoy.org>
2020-02-19 10:17:15 +0100

    wip

--- linalg/src/arm64/arm64simd.rs
+++ linalg/src/arm64/arm64simd.rs
@@ -1,8 +1,14 @@
 use crate::frame::mmm::*;
+use crate::frame::sigmoid::*;
+use crate::frame::tanh::*;
 
 extern "C" {
     #[no_mangle]
     fn arm64simd_smmm_8x8(op: *const MatMatMulKerSpec<f32, f32, f32, f32>) -> isize;
+    #[no_mangle]
+    fn arm64simd_ssigmoid_4n(ptr: *mut f32, count: usize);
+    #[no_mangle]
+    fn arm64simd_stanh_8n(ptr: *mut f32, count: usize);
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -33,8 +39,52 @@ impl MatMatMulKer<f32, f32, f32, f32> for SMatMatMul8x8 {
     }
 }
 
+#[derive(Copy, Clone, Debug)]
+pub struct SSigmoid4;
+
+impl SigmoidKer<f32> for SSigmoid4 {
+    #[inline(always)]
+    fn name() -> &'static str {
+        "arm64simd"
+    }
+    #[inline(always)]
+    fn nr() -> usize {
+        4
+    }
+    #[inline(always)]
+    fn alignment_bytes() -> usize {
+        16
+    }
+    #[inline(never)]
+    fn run(buf: &mut [f32]) {
+        unsafe { arm64simd_ssigmoid_4n(buf.as_mut_ptr(), buf.len()) }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct STanh8;
+
+impl TanhKer<f32> for STanh8 {
+    #[inline(always)]
+    fn name() -> &'static str {
+        "arm64simd"
+    }
+    #[inline(always)]
+    fn nr() -> usize {
+        8
+    }
+    #[inline(always)]
+    fn alignment_bytes() -> usize {
+        32
+    }
+    #[inline(never)]
+    fn run(buf: &mut [f32]) {
+        unsafe { arm64simd_stanh_8n(buf.as_mut_ptr(), buf.len()) }
+    }
+}
+
 #[cfg(test)]
-mod test {
-    mmm_kernel_tests!(true, crate::arm64::arm64simd::SMatMatMul8x8, f32, f32, f32, f32);
-    mmm_frame_tests!(true, crate::arm64::arm64simd::SMatMatMul8x8, f32, f32, f32, f32);
+mod tests {
+    test_mmm_kernel_f32!(crate::arm64::arm64simd::SMatMatMul8x8, test_SMatMatMul8x8, true);
+    sigmoid_frame_tests!(true, crate::arm64::arm64simd::SSigmoid4);
 }
