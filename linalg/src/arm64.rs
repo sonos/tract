a349974d34f3cc48603a93f4b6c3c80e6b8ade34
Mathieu Poumeyrol <kali@zoy.org>
2020-02-19 13:18:08 +0100

    plug-in asm ssigmoid and stanh

--- linalg/src/arm64.rs
+++ linalg/src/arm64.rs
@@ -3,10 +3,14 @@ mod arm64simd;
 use crate::Ops;
 
 use crate::frame::MatMatMulImpl;
+use crate::frame::SigmoidImpl;
+use crate::frame::TanhImpl;
 
 pub fn plug(ops: &mut Ops) {
     log::info!("arm64simd activated for smmm");
     ops.smmm = Box::new(|m, k, n| {
         Box::new(MatMatMulImpl::<arm64simd::SMatMatMul8x8, f32, f32, f32, f32>::new(m, k, n))
     });
+    ops.ssigmoid = Box::new(|| Box::new(SigmoidImpl::<arm64simd::SSigmoid4, f32>::new()));
+    ops.stanh = Box::new(|| Box::new(TanhImpl::<arm64simd::STanh4, f32>::new()));
 }
