45a4bd658fd45427bd48799a123da872f65b5b39
Mathieu Poumeyrol <kali@zoy.org>
2020-02-19 13:16:33 +0100

    simple tanh kernel

--- linalg/src/arm64/arm64simd.rs
+++ linalg/src/arm64/arm64simd.rs
@@ -6,9 +6,9 @@ extern "C" {
     #[no_mangle]
     fn arm64simd_smmm_8x8(op: *const MatMatMulKerSpec<f32, f32, f32, f32>) -> isize;
     #[no_mangle]
-    fn arm64simd_ssigmoid_4n(ptr: *mut f32, count: usize);
+    fn arm64simd_sigmoid_f32_4n(ptr: *mut f32, count: usize);
     #[no_mangle]
-    fn arm64simd_stanh_8n(ptr: *mut f32, count: usize);
+    fn arm64simd_tanh_f32_4n(ptr: *mut f32, count: usize);
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -57,29 +57,29 @@ impl SigmoidKer<f32> for SSigmoid4 {
     }
     #[inline(never)]
     fn run(buf: &mut [f32]) {
-        unsafe { arm64simd_ssigmoid_4n(buf.as_mut_ptr(), buf.len()) }
+        unsafe { arm64simd_sigmoid_f32_4n(buf.as_mut_ptr(), buf.len()) }
     }
 }
 
 #[derive(Copy, Clone, Debug)]
-pub struct STanh8;
+pub struct STanh4;
 
-impl TanhKer<f32> for STanh8 {
+impl TanhKer<f32> for STanh4 {
     #[inline(always)]
     fn name() -> &'static str {
         "arm64simd"
     }
     #[inline(always)]
     fn nr() -> usize {
-        8
+        4
     }
     #[inline(always)]
     fn alignment_bytes() -> usize {
-        32
+        16
     }
     #[inline(never)]
     fn run(buf: &mut [f32]) {
-        unsafe { arm64simd_stanh_8n(buf.as_mut_ptr(), buf.len()) }
+        unsafe { arm64simd_tanh_f32_4n(buf.as_mut_ptr(), buf.len()) }
     }
 }
 
@@ -87,4 +87,5 @@ impl TanhKer<f32> for STanh8 {
 mod tests {
     test_mmm_kernel_f32!(crate::arm64::arm64simd::SMatMatMul8x8, test_SMatMatMul8x8, true);
     sigmoid_frame_tests!(true, crate::arm64::arm64simd::SSigmoid4);
+    tanh_frame_tests!(true, crate::arm64::arm64simd::STanh4);
 }
