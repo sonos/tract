02fc351081d2bad5163b97cce989406c664c28a4
Mathieu Poumeyrol <kali@zoy.org>
2020-02-22 10:47:29 +0100

    16x6 -> 8x8

--- linalg/src/x86_64_fma/mmm.rs
+++ linalg/src/x86_64_fma/mmm.rs
@@ -4,7 +4,7 @@ extern "C" {
     #[no_mangle]
     fn fma_mmm_f32_16x6(op: *const MatMatMulKerSpec<f32, f32, f32, f32>) -> isize;
     #[no_mangle]
-    fn fma_mmm_i8_16x6(op: *const MatMatMulKerSpec<i8, i8, i8, i32>) -> isize;
+    fn fma_mmm_i8_8x8(op: *const MatMatMulKerSpec<i8, i8, i8, i32>) -> isize;
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -36,20 +36,20 @@ impl MatMatMulKer<f32, f32, f32, f32> for MatMatMulF32x16x6 {
 }
 
 #[derive(Copy, Clone, Debug)]
-pub struct MatMatMulI8x16x6;
+pub struct MatMatMulI8x8x8;
 
-impl MatMatMulKer<i8, i8, i8, i32> for MatMatMulI8x16x6 {
+impl MatMatMulKer<i8, i8, i8, i32> for MatMatMulI8x8x8 {
     #[inline(always)]
     fn name() -> &'static str {
         "fma"
     }
     #[inline(always)]
     fn mr() -> usize {
-        16
+        8
     }
     #[inline(always)]
     fn nr() -> usize {
-        6
+        8
     }
     fn alignment_bytes_packed_a() -> usize {
         32
@@ -59,18 +59,18 @@ impl MatMatMulKer<i8, i8, i8, i32> for MatMatMulI8x16x6 {
     }
     #[inline(never)]
     fn kernel(spec: &MatMatMulKerSpec<i8, i8, i8, i32>) -> isize {
-        unsafe { fma_mmm_i8_16x6(spec) }
+        unsafe { fma_mmm_i8_8x8(spec) }
     }
 }
-
-test_mmm_kernel_i8!(
-    crate::x86_64_fma::mmm::MatMatMulI8x16x6,
-    test_MatMatMulI8x16x6,
-    is_x86_feature_detected!("fma")
-);
 
 test_mmm_kernel_f32!(
     crate::x86_64_fma::mmm::MatMatMulF32x16x6,
     test_MatMatMulF32x16x6,
+    is_x86_feature_detected!("fma")
+);
+
+test_mmm_kernel_i8!(
+    crate::x86_64_fma::mmm::MatMatMulI8x8x8,
+    test_MatMatMulI8x8x8,
     is_x86_feature_detected!("fma")
 );
