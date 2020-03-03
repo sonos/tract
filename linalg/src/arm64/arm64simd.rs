8b42eca2cfcd363cba91d657384a6e66c5ab07e3
Mathieu Poumeyrol <kali@zoy.org>
2020-03-03 14:34:26 +0100

    start work on mmm_i8

--- linalg/src/arm64/arm64simd.rs
+++ linalg/src/arm64/arm64simd.rs
@@ -6,6 +6,8 @@ extern "C" {
     #[no_mangle]
     fn arm64simd_mmm_f32_8x8(op: *const MatMatMulKerSpec<f32, f32, f32, f32>) -> isize;
     #[no_mangle]
+    fn arm64simd_mmm_i8_8x8(op: *const MatMatMulKerSpec<i8, i8, i8, i32>) -> isize;
+    #[no_mangle]
     fn arm64simd_sigmoid_f32_4n(ptr: *mut f32, count: usize);
     #[no_mangle]
     fn arm64simd_tanh_f32_4n(ptr: *mut f32, count: usize);
@@ -36,6 +38,62 @@ impl MatMatMulKer<f32, f32, f32, f32> for MatMatMulF32x8x8 {
     #[inline(never)]
     fn kernel(op: &MatMatMulKerSpec<f32, f32, f32, f32>) -> isize {
         unsafe { arm64simd_mmm_f32_8x8(op) }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct MatMatMulI8x8x8;
+
+impl MatMatMulKer<i8, i8, i8, i32> for MatMatMulI8x8x8 {
+    #[inline(always)]
+    fn name() -> &'static str {
+        "arm64simd"
+    }
+    #[inline(always)]
+    fn mr() -> usize {
+        8
+    }
+    #[inline(always)]
+    fn nr() -> usize {
+        8
+    }
+    fn alignment_bytes_packed_a() -> usize {
+        16
+    }
+    fn alignment_bytes_packed_b() -> usize {
+        16
+    }
+    #[inline(never)]
+    fn kernel(op: &MatMatMulKerSpec<i8, i8, i8, i32>) -> isize {
+        unsafe { arm64simd_mmm_i8_8x8(op) }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct MatMatMulI8xI32x8x8;
+
+impl MatMatMulKer<i8, i8, i32, i32> for MatMatMulI8xI32x8x8 {
+    #[inline(always)]
+    fn name() -> &'static str {
+        "arm64simd"
+    }
+    #[inline(always)]
+    fn mr() -> usize {
+        8
+    }
+    #[inline(always)]
+    fn nr() -> usize {
+        8
+    }
+    fn alignment_bytes_packed_a() -> usize {
+        16
+    }
+    fn alignment_bytes_packed_b() -> usize {
+        16
+    }
+    #[inline(never)]
+    fn kernel(op: &MatMatMulKerSpec<i8, i8, i32, i32>) -> isize {
+        unsafe { arm64simd_mmm_i8_8x8(op as *const _ as _) }
     }
 }
 
@@ -83,9 +141,16 @@ impl TanhKer<f32> for TanhF32x4n {
     }
 }
 
+test_mmm_kernel_f32!(crate::arm64::arm64simd::MatMatMulF32x8x8, test_MatMatMulF32x8x8, true);
+test_mmm_kernel_i8!(crate::arm64::arm64simd::MatMatMulI8x8x8, test_MatMatMulI8x8x8, true);
+test_mmm_kernel_i8_i32!(
+    crate::arm64::arm64simd::MatMatMulI8xI32x8x8,
+    test_MatMatMulI8xI32x8x8,
+    true
+);
+
 #[cfg(test)]
-mod tests {
-    test_mmm_kernel_f32!(crate::arm64::arm64simd::MatMatMulF32x8x8, test_MatMatMulF32x8x8, true);
+mod test_simd {
     sigmoid_frame_tests!(true, crate::arm64::arm64simd::SigmoidF32x4n);
     tanh_frame_tests!(true, crate::arm64::arm64simd::TanhF32x4n);
 }
