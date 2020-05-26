df87e678430adbab3e18aaa730d4b36b6ef37936
Mathieu Poumeyrol <kali@zoy.org>
2020-05-26 10:38:54 +0200

    simplify expansion + use it on LSTM

--- hir/src/ops/expandable.rs
+++ hir/src/ops/expandable.rs
@@ -1,6 +1,10 @@
 use crate::internal::*;
 use tract_core::internal::*;
 
+pub fn expand<E: Expansion>(e: E) -> Box<dyn InferenceOp> {
+    Box::new(Box::new(e) as Box<dyn Expansion>)
+}
+
 pub trait Expansion:
     tract_core::dyn_clone::DynClone
     + std::fmt::Debug
@@ -11,6 +15,13 @@ pub trait Expansion:
 {
     fn name(&self) -> &'static str;
     fn op_families(&self) -> &'static [&'static str];
+    fn validation(&self) -> Validation {
+        Validation::Accurate
+    }
+
+    fn nboutputs(&self) -> TractResult<usize> {
+        Ok(1)
+    }
 
     fn wire(
         &self,
@@ -29,11 +40,6 @@ pub trait Expansion:
 
 tract_core::dyn_clone::clone_trait_object!(Expansion);
 
-#[derive(Clone, Debug, new, Hash)]
-pub struct Expandable {
-    pub expansion: Box<dyn Expansion>,
-}
-
 impl Hash for Box<dyn Expansion> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         std::hash::Hash::hash(&self.type_id(), state);
@@ -41,20 +47,20 @@ impl Hash for Box<dyn Expansion> {
     }
 }
 
-tract_linalg::impl_dyn_hash!(Expandable);
+tract_linalg::impl_dyn_hash!(Box<dyn Expansion>);
 
-impl Op for Expandable {
+impl Op for Box<dyn Expansion> {
     fn name(&self) -> Cow<str> {
-        self.expansion.name().into()
+        self.as_ref().name().into()
     }
     fn op_families(&self) -> &'static [&'static str] {
-        self.expansion.op_families()
+        self.as_ref().op_families()
     }
     not_a_typed_op!();
     not_a_pulsed_op!();
 }
 
-impl StatelessOp for Expandable {
+impl StatelessOp for Box<dyn Expansion> {
     fn eval(&self, inputs: TVec<Arc<Tensor>>) -> TractResult<TVec<Arc<Tensor>>> {
         let mut adhoc = TypedModel::default();
         let wires = inputs
@@ -62,20 +68,20 @@ impl StatelessOp for Expandable {
             .enumerate()
             .map(|(ix, i)| adhoc.add_source(format!("adhoc-source-{}", ix), TypedFact::from(&**i)))
             .collect::<TractResult<TVec<OutletId>>>()?;
-        let wires = self.expansion.wire("adhoc", &mut adhoc, &*wires)?;
+        let wires = self.wire("adhoc", &mut adhoc, &*wires)?;
         adhoc.set_output_outlets(&*wires)?;
         SimplePlan::new(adhoc)?.run(inputs.into_iter().map(|t| t.into_tensor()).collect())
     }
 }
 
-impl InferenceRulesOp for Expandable {
+impl InferenceRulesOp for Box<dyn Expansion> {
     fn rules<'r, 'p: 'r, 's: 'r>(
         &'s self,
         s: &mut Solver<'r>,
         inputs: &'p [TensorProxy],
         outputs: &'p [TensorProxy],
     ) -> InferenceResult {
-        self.expansion.rules(s, inputs, outputs)
+        self.as_ref().rules(s, inputs, outputs)
     }
 
     fn to_typed(
@@ -86,7 +92,7 @@ impl InferenceRulesOp for Expandable {
         mapping: &HashMap<OutletId, OutletId>,
     ) -> TractResult<TVec<OutletId>> {
         let inputs = node.inputs.iter().map(|i| mapping[i]).collect::<Vec<_>>();
-        self.expansion.wire(&node.name, target, &inputs)
+        self.wire(&node.name, target, &inputs)
     }
 
     as_op!();
