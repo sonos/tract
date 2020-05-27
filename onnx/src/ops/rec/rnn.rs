10c1a522333ecdcc5c6f34d91f675f35e8d68e40
Mathieu Poumeyrol <kali@zoy.org>
2020-05-27 07:55:00 +0200

    broken wip

--- onnx/src/ops/rec/rnn.rs
+++ onnx/src/ops/rec/rnn.rs
@@ -2,7 +2,6 @@ use crate::model::ParsingContext;
 use crate::pb::*;
 use tract_hir::internal::*;
 use tract_hir::ops;
-use tract_ndarray::prelude::*;
 
 pub fn rnn(
     _ctx: &ParsingContext,
@@ -19,7 +18,7 @@ pub fn rnn(
     rnn.optional_y_output = options.next().unwrap();
     rnn.optional_y_h_output = options.next().unwrap();
 
-    Ok((Box::new(rnn), vec![]))
+    Ok((expand(rnn), vec![]))
 }
 
 #[derive(Debug, Clone, new, Hash)]
@@ -49,9 +48,9 @@ impl Default for RNN {
     }
 }
 
-impl Op for RNN {
-    fn name(&self) -> Cow<str> {
-        "RNN".into()
+impl Expansion for RNN {
+    fn name(&self) -> &'static str {
+        "RNN"
     }
 
     fn validation(&self) -> Validation {
@@ -59,10 +58,7 @@ impl Op for RNN {
     }
 
     op_onnx!();
-    not_a_typed_op!();
-}
 
-impl InferenceRulesOp for RNN {
     fn rules<'r, 'p: 'r, 's: 'r>(
         &'s self,
         s: &mut Solver<'r>,
@@ -125,20 +121,17 @@ impl InferenceRulesOp for RNN {
         Ok(self.optional_y_output.is_some() as usize + self.optional_y_h_output.is_some() as usize)
     }
 
-    as_op!();
-
     #[allow(non_snake_case)]
-    fn to_typed(
+    fn wire(
         &self,
-        _source: &InferenceModel,
-        node: &InferenceNode,
+        prefix: &str,
         target: &mut TypedModel,
-        mapping: &HashMap<OutletId, OutletId>,
+        inputs: &[OutletId],
     ) -> TractResult<TVec<OutletId>> {
         use ops::{array, math, matmul, scan};
 
-        let x_fact = target.outlet_fact(mapping[&node.inputs[0]])?.clone();
-        let r_fact = target.outlet_fact(mapping[&node.inputs[2]])?;
+        let x_fact = target.outlet_fact(inputs[0])?.clone();
+        let r_fact = target.outlet_fact(inputs[2])?;
 
         let b_size = x_fact.shape.dim(1).to_integer().unwrap() as usize;
         let h_size = r_fact.shape.dim(2).to_integer().unwrap() as usize;
@@ -152,7 +145,7 @@ impl InferenceRulesOp for RNN {
         macro_rules! target_wire {
             ($name: ident = $op: expr, $($param: expr),*) => {
                 let $name = target.wire_node(
-                    format!("{}-{}", node.name, stringify!($name)),
+                    format!("{}.{}", prefix, stringify!($name)),
                     $op, [$($param),*].as_ref())?[0];
             }
         };
@@ -160,7 +153,7 @@ impl InferenceRulesOp for RNN {
         macro_rules! wire {
             ($name: ident = $op: expr, $($param: expr),*) => {
                 let $name = body.wire_node(
-                    format!("{}-{}", node.name, stringify!($name)),
+                    format!("{}.{}", prefix, stringify!($name)),
                     $op, [$($param),*].as_ref())?[0];
             }
         };
@@ -169,7 +162,7 @@ impl InferenceRulesOp for RNN {
         // scan outer interface: idem
         // scann inner interface: [chunk=1, batch_size, input_size]
         // onnx inner interface: [batch_size, input_size]
-        outer_inputs.push(mapping[&node.inputs[0]]);
+        outer_inputs.push(inputs[0]);
         input_mapping.push(scan::InputMapping::Scan { slot: 0, axis: 0, chunk: 1.to_dim() });
         let mut x_source_fact = x_fact.clone();
         x_source_fact.shape.set_dim(0, 1.to_dim())?;
@@ -178,21 +171,21 @@ impl InferenceRulesOp for RNN {
 
         // W: onnx interface: [num_directions, 3*hidden_size, input_size]
         // scan interfaces: [3*hidden_size, input_size]
-        target_wire!(w = AxisOp::Rm(0), mapping[&node.inputs[1]]);
+        target_wire!(w = AxisOp::Rm(0), inputs[1]);
         outer_inputs.push(w);
         input_mapping.push(scan::InputMapping::Full { slot: 1 });
         let W = body.add_source("w", target.outlet_fact(w)?.clone())?.into();
 
         // R: onnx interface: [num_directions, 3*hidden_size, hidden_size]
         // scan interfaces: [3*hidden_size, hidden_size]
-        target_wire!(r = AxisOp::Rm(0), mapping[&node.inputs[2]]);
+        target_wire!(r = AxisOp::Rm(0), inputs[2]);
         outer_inputs.push(r);
         input_mapping.push(scan::InputMapping::Full { slot: 2 });
         let R = body.add_source("r", target.outlet_fact(r)?.clone())?.into();
 
         // B: onnx interface: [num_directions, 6*hidden_size]
         let b = if let Some(slot) = self.optional_bias_input {
-            target_wire!(b = AxisOp::Rm(0), mapping[&node.inputs[slot]]);
+            target_wire!(b = AxisOp::Rm(0), inputs[slot]);
             outer_inputs.push(b);
             input_mapping.push(scan::InputMapping::Full { slot });
             let b = body.add_source("b", target.outlet_fact(b)?.clone())?.into();
@@ -202,7 +195,7 @@ impl InferenceRulesOp for RNN {
         };
 
         if let Some(slot) = self.optional_sequence_lens_input {
-            outer_inputs.push(mapping[&node.inputs[slot]]);
+            outer_inputs.push(inputs[slot]);
         }
 
         // initial h, optional: onnx: [num_directions, batch_size, hidden_size]
@@ -210,7 +203,7 @@ impl InferenceRulesOp for RNN {
         // scan inner: [chunk=1, batch_size, hidden_size]
         // onnx inner: [batch_size, hidden_size]
         let initializer = if let Some(initial_h_input) = self.optional_initial_h_input {
-            target_wire!(h = AxisOp::Rm(0), mapping[&node.inputs[initial_h_input]]);
+            target_wire!(h = AxisOp::Rm(0), inputs[initial_h_input]);
             target_wire!(h_chunk = AxisOp::Add(0), h);
             outer_inputs.push(h_chunk);
             scan::StateInitializer::FromInput(initial_h_input)
@@ -263,7 +256,7 @@ impl InferenceRulesOp for RNN {
         };
 
         let scan_outputs = target.wire_node(
-            &*node.name,
+            prefix,
             scan::Scan::new(
                 body,
                 input_mapping,
@@ -286,73 +279,3 @@ impl InferenceRulesOp for RNN {
     }
 }
 
-impl StatelessOp for RNN {
-    fn eval(&self, inputs: TVec<Arc<Tensor>>) -> TractResult<TVec<Arc<Tensor>>> {
-        let x: ArrayView3<f32> = inputs[0].to_array_view::<f32>()?.into_dimensionality()?; // [seq_length, batch_size, input_size]
-        let w: ArrayView3<f32> = inputs[1].to_array_view::<f32>()?.into_dimensionality()?; // [num_directions, hidden_size, input_size]
-        let r: ArrayView3<f32> = inputs[2].to_array_view::<f32>()?.into_dimensionality()?; // [num_directions, hidden_size, hidden_size]
-
-        let bias = if let Some(ix) = self.optional_bias_input {
-            Some(inputs[ix].to_array_view::<f32>()?.into_dimensionality::<Ix2>()?)
-        // [num_directions, 6*hidden_size]
-        } else {
-            None
-        };
-
-        let seq_length = x.shape()[0];
-        let batch_size = x.shape()[1];
-        let num_directions = w.shape()[0];
-        let hidden_size = r.shape()[2];
-
-        let mut output_y = self
-            .optional_y_output
-            .map(|_| Array4::<f32>::zeros((seq_length, num_directions, batch_size, hidden_size)));
-        let mut output_y_h = self
-            .optional_y_h_output
-            .map(|_| Array3::<f32>::zeros((num_directions, batch_size, hidden_size)));
-
-        for dir in 0..num_directions {
-            let w = w.index_axis_move(Axis(0), dir);
-            let r = r.index_axis_move(Axis(0), dir);
-
-            let mut ht = if let Some(init) = self.optional_initial_h_input {
-                inputs[init]
-                    .to_array_view::<f32>()?
-                    .index_axis_move(Axis(0), dir)
-                    .to_owned()
-                    .into_dimensionality()?
-            } else {
-                Array2::<f32>::zeros((batch_size, hidden_size)).into()
-            };
-
-            for ix in 0..seq_length {
-                let ix = if dir == 0 { ix } else { seq_length - 1 - ix };
-                let x = x.index_axis_move(Axis(0), ix);
-
-                let mut ht1 = x.dot(&w.t()) + ht.dot(&r.t()); // batch_size x 4*hidden_size
-                if let Some(bias) = bias {
-                    ht1 += &bias.slice(s!(dir, 0..hidden_size));
-                    ht1 += &bias.slice(s!(dir, hidden_size..2 * hidden_size));
-                }
-
-                let ht1 = self.fore.as_stateless().unwrap().eval(tvec!(ht1.into_arc_tensor()))?[0]
-                    .to_array_view::<f32>()?
-                    .to_owned()
-                    .into_dimensionality::<Ix2>()?;
-                ht.assign(&ht1);
-
-                if let Some(ref mut o) = output_y {
-                    o.index_axis_mut(Axis(0), ix).index_axis_move(Axis(0), dir).assign(&ht);
-                }
-            }
-            if let Some(ref mut o) = output_y_h {
-                o.index_axis_mut(Axis(0), dir).assign(&ht);
-            }
-        }
-
-        let mut outputs = tvec!();
-        outputs.extend(output_y.into_iter().map(|t| t.into_arc_tensor()));
-        outputs.extend(output_y_h.into_iter().map(|t| t.into_arc_tensor()));
-        Ok(outputs)
-    }
-}
