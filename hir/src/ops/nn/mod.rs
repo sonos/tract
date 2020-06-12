d467bf60f791fcf6476c22acc76b9d9f3e62055b
Mathieu Poumeyrol <kali@zoy.org>
2020-06-12 17:12:41 +0200

    exotic activations are now expansions

--- hir/src/ops/nn/mod.rs
+++ hir/src/ops/nn/mod.rs
@@ -8,7 +8,4 @@ pub use global_pools::*;
 pub use layer_max::*;
 pub use reduce::{Reduce, Reducer};
 
-pub use tract_core::ops::nn::{
-    elu, hard_sigmoid, leaky_relu, parametric_softplus, scaled_tanh, selu, sigmoid, softplus,
-    softsign, threshold_relu, DataFormat,
-};
+pub use tract_core::ops::nn::{sigmoid, DataFormat};
