5f12bfea9b0b0ba2261a4533f7533d6ebeb8f88a
Mathieu Poumeyrol <kali@zoy.org>
2020-06-27 17:17:33 +0200

    layer softmax/logsoftmax as expansions

--- hir/src/ops/nn/layer_max.rs
+++ hir/src/ops/nn/layer_max.rs
@@ -86,42 +86,13 @@ pub struct LayerLogSoftmax {
 
 tract_linalg::impl_dyn_hash!(LayerLogSoftmax);
 
-impl LayerLogSoftmax {
-    fn eval_t<
-        T: Datum + tract_num_traits::Float + tract_num_traits::FromPrimitive + ::std::iter::Sum,
-    >(
-        &self,
-        input: Arc<Tensor>,
-    ) -> TractResult<Tensor> {
-        let mut softmax = LayerSoftmax::new(self.axis).eval_t::<T>(input)?;
-        softmax.as_slice_mut::<T>()?.iter_mut().for_each(|x| *x = x.ln());
-        Ok(softmax)
-    }
-}
-
-impl Op for LayerLogSoftmax {
+impl Expansion for LayerLogSoftmax {
     fn name(&self) -> Cow<str> {
         "LayerLogSoftmax".into()
     }
-    fn info(&self) -> TractResult<Vec<String>> {
-        Ok(vec![format!("axis: {}", self.axis)])
-    }
-    fn validation(&self) -> Validation {
-        Validation::Rounding
-    }
+
     op_hir!();
-    not_a_typed_op!();
-}
 
-impl StatelessOp for LayerLogSoftmax {
-    fn eval(&self, mut inputs: TVec<Arc<Tensor>>) -> TractResult<TVec<Arc<Tensor>>> {
-        let input = args_1!(inputs);
-        let t = dispatch_floatlike!(Self::eval_t(input.datum_type())(self, input))?;
-        Ok(tvec!(t.into_arc_tensor()))
-    }
-}
-
-impl InferenceRulesOp for LayerLogSoftmax {
     fn rules<'r, 'p: 'r, 's: 'r>(
         &'s self,
         solver: &mut Solver<'r>,
@@ -131,21 +102,17 @@ impl InferenceRulesOp for LayerLogSoftmax {
         rules(solver, inputs, outputs)
     }
 
-    as_op!();
-
-    fn to_typed(
+    fn wire(
         &self,
-        source: &InferenceModel,
-        node: &InferenceNode,
+        name: &str,
         target: &mut TypedModel,
-        mapping: &HashMap<OutletId, OutletId>,
+        inputs: &[OutletId],
     ) -> TractResult<TVec<OutletId>> {
-        let softmax =
-            InferenceOp::to_typed(&LayerSoftmax::new(self.axis), source, node, target, mapping)?[0];
+        let softmax = LayerSoftmax { axis: self.axis }.wire(name, target, inputs)?;
         target.wire_node(
-            format!("{}-logsoftmax", node.name),
+            format!("{}.logsoftmax", name),
             tract_core::ops::math::ln(),
-            &[softmax],
+            &softmax,
         )
     }
 }
@@ -157,57 +124,12 @@ pub struct LayerSoftmax {
 
 tract_linalg::impl_dyn_hash!(LayerSoftmax);
 
-impl LayerSoftmax {
-    fn eval_t<
-        T: Datum + tract_num_traits::Float + tract_num_traits::FromPrimitive + ::std::iter::Sum,
-    >(
-        &self,
-        input: Arc<Tensor>,
-    ) -> TractResult<Tensor> {
-        let array = input.into_tensor().into_array::<T>()?;
-        let shape = array.shape().to_vec();
-        let axis =
-            if self.axis < 0 { shape.len() as isize + self.axis } else { self.axis } as usize;
-        let first_dim: usize = array.shape()[0..axis].iter().product();
-        let second_dim: usize = array.len() / first_dim;
-        let mut array = array.into_shape((first_dim, second_dim))?;
-        array.outer_iter_mut().for_each(|mut layer| {
-            // https://jamesmccaffrey.wordpress.com/2016/03/04/the-max-trick-when-computing-softmax/
-            let max: Option<T> = layer
-                .iter()
-                .max_by(|a, b| a.partial_cmp(&b).unwrap_or(::std::cmp::Ordering::Equal))
-                .cloned();
-            layer.mapv_inplace(|x| (x - max.unwrap()).exp());
-            let divisor = layer.iter().cloned().sum();
-            layer.mapv_inplace(|x| x / divisor);
-        });
-        Ok(array.into_shape(shape)?.into_tensor())
-    }
-}
-
-impl Op for LayerSoftmax {
+impl Expansion for LayerSoftmax {
     fn name(&self) -> Cow<str> {
         "LayerSoftmax".into()
     }
-    fn info(&self) -> TractResult<Vec<String>> {
-        Ok(vec![format!("axis: {}", self.axis)])
-    }
-    fn validation(&self) -> Validation {
-        Validation::Rounding
-    }
     op_hir!();
-    not_a_typed_op!();
-}
 
-impl StatelessOp for LayerSoftmax {
-    fn eval(&self, mut inputs: TVec<Arc<Tensor>>) -> TractResult<TVec<Arc<Tensor>>> {
-        let input = args_1!(inputs);
-        let t = dispatch_floatlike!(Self::eval_t(input.datum_type())(self, input))?;
-        Ok(tvec!(t.into_arc_tensor()))
-    }
-}
-
-impl InferenceRulesOp for LayerSoftmax {
     fn rules<'r, 'p: 'r, 's: 'r>(
         &'s self,
         solver: &mut Solver<'r>,
@@ -217,42 +139,36 @@ impl InferenceRulesOp for LayerSoftmax {
         rules(solver, inputs, outputs)
     }
 
-    as_op!();
-
-    fn to_typed(
+    fn wire(
         &self,
-        _source: &InferenceModel,
-        node: &InferenceNode,
+        name: &str,
         target: &mut TypedModel,
-        mapping: &HashMap<OutletId, OutletId>,
+        inputs: &[OutletId],
     ) -> TractResult<TVec<OutletId>> {
-        use tract_core::ops::nn;
-        let input = mapping[&node.inputs[0]];
+        use tract_core::ops::{math, nn};
+        let input = inputs[0];
         let rank = target.outlet_fact(input)?.rank();
         let axis = if self.axis < 0 { rank as isize + self.axis } else { self.axis } as usize;
         let reducing_axes = (axis..rank).collect::<TVec<usize>>();
         let maxes = target.wire_node(
-            format!("{}-max", node.name),
+            format!("{}.max", name),
             nn::Reduce::new(reducing_axes.clone(), nn::Reducer::Max),
             &[input],
         )?[0];
         let normed = target.wire_node(
-            format!("{}-normed", node.name),
-            tract_core::ops::math::sub::bin_typed(),
+            format!("{}.normed", name),
+            math::sub::bin_typed(),
             &[input, maxes],
         )?[0];
-        let exp = target.wire_node(
-            format!("{}-exp", node.name),
-            tract_core::ops::math::exp(),
-            &[normed],
-        )?[0];
+        let exp =
+            target.wire_node(format!("{}.exp", name), tract_core::ops::math::exp(), &[normed])?[0];
         let sum = target.wire_node(
-            format!("{}-sum", node.name),
+            format!("{}.sum", name),
             nn::Reduce::new(reducing_axes, nn::Reducer::Sum),
             &[exp],
         )?[0];
         target.wire_node(
-            format!("{}-softmax", node.name),
+            format!("{}.softmax", name),
             tract_core::ops::math::div::bin_typed(),
             &[exp, sum],
         )
