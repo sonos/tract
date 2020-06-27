391cb78ba052300ae9de3e5ee2ccf97aac27a34b
Mathieu Poumeyrol <kali@zoy.org>
2020-06-27 17:23:29 +0200

    tf layerhardmax too

--- tensorflow/src/ops/nn/mod.rs
+++ tensorflow/src/ops/nn/mod.rs
@@ -20,7 +20,7 @@ pub fn register_all_ops(reg: &mut TfOpRegister) {
     reg.insert("Relu", |_, _| Ok(expand(tract_hir::ops::activations::Clip::new(Some(0.0), None))));
     reg.insert("Relu6", |_, _| Ok(expand(tract_hir::ops::activations::Clip::new(Some(0.0), Some(6.0)))));
     reg.insert("Sigmoid", |_, _| Ok(Box::new(tract_hir::ops::nn::sigmoid())));
-    reg.insert("Softmax", |_, _| Ok(Box::new(LayerSoftmax::new(1))));
+    reg.insert("Softmax", |_, _| Ok(expand(LayerSoftmax::new(1))));
     reg.insert("SpaceToBatchND", s2b::space_to_batch_nd);
     reg.insert("BatchToSpaceND", s2b::batch_to_space_nd);
 }
