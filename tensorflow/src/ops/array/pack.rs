3c3f15554d53e74b2dd64c59c933ab8114803a50
Mathieu Poumeyrol <kali@zoy.org>
2020-06-28 18:43:44 +0200

    tf.pack as extension

--- tensorflow/src/ops/array/pack.rs
+++ tensorflow/src/ops/array/pack.rs
@@ -8,10 +8,8 @@ pub fn pack(_ctx: &ParsingContext, pb: &NodeDef) -> TractResult<Box<dyn Inferenc
     let n = pb.input.len();
     let axis = pb.get_attr_int("axis")?;
 
-    Ok(Box::new(Pack::new(dtype, n, axis)))
+    Ok(expand(Pack::new(dtype, n, axis)))
 }
-
-//TODO: incorporate as Concat
 
 #[derive(Debug, Clone, new, Hash)]
 pub struct Pack {
@@ -22,53 +20,13 @@ pub struct Pack {
 
 tract_linalg::impl_dyn_hash!(Pack);
 
-impl Pack {
-    /// Evaluates the operation given the input tensors.
-    fn eval_t<T: Datum>(&self, inputs: TVec<Arc<Tensor>>) -> TractResult<TVec<Arc<Tensor>>> {
-        use tract_ndarray::Axis;
-        let arrays =
-            inputs.iter().map(|m| Ok(m.cast_to::<T>()?)).collect::<TractResult<Vec<_>>>()?;
-        let views: Vec<_> = arrays
-            .iter()
-            .map(|v| v.to_array_view::<T>().unwrap().insert_axis(Axis(self.axis)))
-            .collect();
-        let mut shape = views[0].shape().to_vec();
-        shape[self.axis] = views.iter().map(|v| v.shape()[self.axis]).sum();
-        let mut array = tract_ndarray::Array::<T, _>::default(&*shape);
-        let mut offset = 0;
-        for v in views {
-            let len = v.shape()[self.axis];
-            array.slice_axis_mut(Axis(self.axis), (offset..(offset + len)).into()).assign(&v);
-            offset += len;
-        }
-        Ok(tvec![array.into_arc_tensor()])
-    }
-}
-
-impl Op for Pack {
+impl Expansion for Pack {
     fn name(&self) -> Cow<str> {
         "Pack".into()
     }
 
     op_tf!();
-    not_a_typed_op!();
-}
 
-impl StatelessOp for Pack {
-    /// Evaluates the operation given the input tensors.
-    fn eval(&self, inputs: TVec<Arc<Tensor>>) -> TractResult<TVec<Arc<Tensor>>> {
-        let dt = DatumType::super_type_for(inputs.iter().map(|dt| dt.datum_type()))
-            .ok_or("Could not find a supertype")?;
-        match dt {
-            DatumType::TDim => self.eval_t::<TDim>(inputs),
-            DatumType::I32 => self.eval_t::<i32>(inputs),
-            DatumType::F32 => self.eval_t::<f32>(inputs),
-            _ => panic!("unsupported type"),
-        }
-    }
-}
-
-impl InferenceRulesOp for Pack {
     fn rules<'r, 'p: 'r, 's: 'r>(
         &'s self,
         s: &mut Solver<'r>,
@@ -106,97 +64,40 @@ impl InferenceRulesOp for Pack {
         s.equals(&outputs[0].shape[axis], self.n.to_dim())
     }
 
-    as_op!();
-
-    fn to_typed(
+    fn wire(
         &self,
-        _source: &InferenceModel,
-        node: &InferenceNode,
-        target: &mut TypedModel,
-        mapping: &HashMap<OutletId, OutletId>,
+        prefix: &str,
+        model: &mut TypedModel,
+        inputs: &[OutletId],
     ) -> TractResult<TVec<OutletId>> {
-        let dt = node.inputs.iter().map(|i| Ok(target.outlet_fact(mapping[i])?.datum_type)).collect::<TractResult<TVec<DatumType>>>()?;
+        let dt = inputs
+            .iter()
+            .map(|&i| Ok(model.outlet_fact(i)?.datum_type))
+            .collect::<TractResult<TVec<DatumType>>>()?;
         let dt = DatumType::super_type_for(dt.iter()).ok_or("No supertype")?;
-        let inputs: TVec<OutletId> = node
-            .inputs
+        let inputs: TVec<OutletId> =
+            inputs
             .iter()
             .enumerate()
             .map(|(ix, &o)| {
-                let wire = mapping[&o];
-                let wire = target.wire_node(
-                    format!("{}-cast-{}", node.name, ix),
+                let wire = model.wire_node(
+                    format!("{}.cast-{}", prefix, ix),
                     tract_hir::ops::cast(dt),
-                    &[wire]
+                    &[o],
                 )?[0];
-                Ok(target.wire_node(
-                    format!("{}-add_dims-{}", node.name, ix),
+                Ok(model.wire_node(
+                    format!("{}.add_dims-{}", prefix, ix),
                     AxisOp::Add(self.axis),
-                    [wire].as_ref(),
+                    &[wire]
                 )?[0])
             })
             .collect::<TractResult<TVec<OutletId>>>()?;
-        target.wire_node(
-            &*node.name,
-            tract_hir::ops::array::TypedConcat::new(
+        model.wire_node(
+            prefix,
+            tract_hir::ops::array::TypedConcat::concat_vars(
                 self.axis as usize,
-                tvec!(tract_hir::ops::array::ConcatSlice::Var; node.inputs.len()),
-            ),
+                inputs.len()),
             &*inputs,
         )
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #![allow(non_snake_case)]
-    use super::*;
-    use tract_num_traits::Zero;
-
-    #[test]
-    fn pack_0() {
-        let inputs = tvec![rctensor1(&[1, 4]), rctensor1(&[2, 5]), rctensor1(&[3, 6]),];
-        assert_eq!(
-            Pack::new(DatumType::I32, 3, 0).eval(inputs.clone()).unwrap().remove(0),
-            rctensor2(&[[1, 4], [2, 5], [3, 6]])
-        );
-        assert_eq!(
-            Pack::new(DatumType::I32, 3, 1).eval(inputs.clone()).unwrap().remove(0),
-            rctensor2(&[[1, 2, 3], [4, 5, 6]])
-        );
-    }
-
-    #[test]
-    fn pack_1() {
-        let pack = Pack::new(DatumType::I32, 3, 0);
-        let input = rctensor1::<i32>(&[]);
-        let exp = rctensor2::<i32, _>(&[[]]);
-        let found = pack.eval(tvec![input]).unwrap();
-
-        if let Err(e) = exp.close_enough(&found[0], false) {
-            panic!("expected: {:?} found: {:?}. {:?}", exp, found[0], e)
-        }
-    }
-
-    #[test]
-    fn inference_1() {
-        let mut pack = Pack::new(DatumType::I32, 2, 0);
-        let a = InferenceFact::from(Tensor::from(0i32));
-        let b = InferenceFact::from(Tensor::from(TDim::zero()));
-        let any = InferenceFact::default();
-        let (_, output_facts, _) = pack.infer_facts(tvec![&a, &b], tvec![&any], tvec!()).unwrap();
-        let exp: TVec<InferenceFact> =
-            tvec!(InferenceFact::dt_shape(DatumType::TDim, vec![2usize]));
-        assert_eq!(output_facts, exp)
-    }
-
-    #[test]
-    fn inference_2() {
-        let mut pack = Pack::new(DatumType::I32, 2, 0);
-        let a = InferenceFact::from(rctensor0(0i32));
-        let b = InferenceFact::from(rctensor0(TDim::zero()));
-        let any = InferenceFact::default();
-        let (_, output_facts, _) = pack.infer(tvec![&a, &b], tvec![&any], tvec!()).unwrap();
-        let exp: TVec<InferenceFact> = tvec!(tensor1(&[TDim::zero(), TDim::zero()]).into());
-        assert_eq!(output_facts, exp);
     }
 }
