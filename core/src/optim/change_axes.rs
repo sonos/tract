b72f07504a86acc44f230e77631b197938efcb9a
Mathieu Poumeyrol <kali@zoy.org>
2020-07-12 12:36:20 +0200

    do more loops internally

--- core/src/optim/change_axes.rs
+++ core/src/optim/change_axes.rs
@@ -10,24 +10,29 @@ pub struct ChangeAxes;
 
 impl TypedPass for ChangeAxes {
     fn pass(&self, model: &mut TypedModel) -> TractResult<bool> {
-        let mut suggestions = vec![];
-        for n in model.eval_order()? {
-            let node = model.node(n);
-            for suggestion in node.op.suggested_axis_changes()? {
-                let outlet = suggestion.0.as_outlet(&node);
-                suggestions.push(AxisChange { outlet, op: suggestion.1 })
-            }
-        }
         let mut interfaces = model.output_outlets()?.to_vec();
         interfaces.extend(model.input_outlets()?.iter());
-        for suggestion in suggestions.into_iter() {
-            if change_axes(model, &suggestion, &interfaces, &[])
-                .chain_err(|| format!("Applying {:?}", suggestion))?
-                .is_some()
-            {
-                return Ok(true);
+        let mut done_something = false;
+        'top: loop {
+            for n in model.eval_order()? {
+                for suggestion in model.node(n).op.suggested_axis_changes()? {
+                    let outlet = suggestion.0.as_outlet(&model.node(n));
+                    let change = AxisChange { outlet, op: suggestion.1 };
+                    if change_axes(
+                        model,
+                        &change,
+                        &interfaces,
+                        &[],
+                    )
+                    .chain_err(|| format!("Applying {:?}", change))?
+                    .is_some()
+                    {
+                        done_something = true;
+                        continue 'top;
+                    }
+                }
             }
+            return Ok(done_something)
         }
-        Ok(false)
     }
 }
