e3fce40d6137784ad2fcca5e01e8f17740bbeb54
Mathieu Poumeyrol <kali@zoy.org>
2020-07-30 09:51:20 +0200

    wip

--- nnef/src/primitives.rs
+++ nnef/src/primitives.rs
@@ -5,8 +5,8 @@ use tract_core::internal::*;
 use crate::model::{AugmentedInvocation, ModelBuilder};
 
 pub type Primitives = HashMap<
-String,
-Arc<dyn Fn(&mut ModelBuilder, &AugmentedInvocation) -> TractResult<TVec<OutletId>>>,
+    String,
+    Arc<dyn Fn(&mut ModelBuilder, &AugmentedInvocation) -> TractResult<TVec<OutletId>>>,
 >;
 
 pub fn primitives() -> Primitives {
@@ -20,9 +20,9 @@ pub fn primitives() -> Primitives {
         ($nnef: ident, $tract: expr) => {
             primitives.insert(
                 stringify!($nnef).to_string(),
-                Arc::new(|b, i| multi_ew(b, i, Box::new($tract))),
-                );
-        }
+                Arc::new(|b, i| multiary_elementwise(b, i, Box::new($tract))),
+            );
+        };
     };
 
     mew!(add, math::add::bin_typed());
@@ -40,13 +40,19 @@ pub fn primitives() -> Primitives {
 
     mew!(select, logic::Iff);
 
+    primitives.insert("sum_reduce".to_string(), Arc::new(reduce));
+    primitives.insert("max_reduce".to_string(), Arc::new(reduce));
+    primitives.insert("min_reduce".to_string(), Arc::new(reduce));
+
+    primitives.insert("matmul".to_string(), Arc::new(matmul));
+
     primitives
 }
 
 fn external(
     builder: &mut ModelBuilder,
     invocation: &AugmentedInvocation,
-    ) -> TractResult<TVec<OutletId>> {
+) -> TractResult<TVec<OutletId>> {
     let type_name = invocation.invocation.generic_type_name.unwrap_or(TypeName::Scalar);
     let dt = if type_name == TypeName::Scalar { f32::datum_type() } else { todo!() };
     let shape = invocation.named_arg("shape")?.to_shape_fact(builder)?;
@@ -56,7 +62,7 @@ fn external(
 fn variable(
     builder: &mut ModelBuilder,
     invocation: &AugmentedInvocation,
-    ) -> TractResult<TVec<OutletId>> {
+) -> TractResult<TVec<OutletId>> {
     let type_name = invocation.invocation.generic_type_name.unwrap_or(TypeName::Scalar);
     let dt = if type_name == TypeName::Scalar { f32::datum_type() } else { todo!() };
     let shape = invocation.named_arg("shape")?.to_shape_fact(builder)?;
@@ -65,17 +71,17 @@ fn variable(
 }
 
 /*
-   fragment conv( input: tensor<scalar>, filter: tensor<scalar>,
-   bias: tensor<scalar> = 0.0, border: string = 'constant',
-   padding: (integer,integer)[] = [], stride: integer[] = [],
-   dilation: integer[] = [], groups: integer = 1 )
-   -> ( output: tensor<scalar> );
-   */
+fragment conv( input: tensor<scalar>, filter: tensor<scalar>,
+bias: tensor<scalar> = 0.0, border: string = 'constant',
+padding: (integer,integer)[] = [], stride: integer[] = [],
+dilation: integer[] = [], groups: integer = 1 )
+-> ( output: tensor<scalar> );
+*/
 
 fn conv(
     builder: &mut ModelBuilder,
     invocation: &AugmentedInvocation,
-    ) -> TractResult<TVec<OutletId>> {
+) -> TractResult<TVec<OutletId>> {
     use tract_core::ops::cnn::{ConvUnary, KernelFormat, PaddingSpec, PoolSpec};
     use tract_core::ops::nn::DataFormat;
     let input = invocation.pos_arg(0)?.to_wire(builder)?;
@@ -96,7 +102,7 @@ fn conv(
             padding.row(0).iter().map(|x| *x as usize).collect(),
             padding.row(1).iter().map(|x| *x as usize).collect(),
             false,
-            )
+        )
     };
     let pool_spec = PoolSpec::new(
         DataFormat::NCHW,
@@ -105,7 +111,7 @@ fn conv(
         Some(dilation.as_finite().unwrap().into()),
         Some(stride.as_finite().unwrap().into()),
         Some(kernel.shape()[0]),
-        );
+    );
     let op = ConvUnary::new(
         pool_spec,
         KernelFormat::OIHW,
@@ -113,15 +119,74 @@ fn conv(
         group as usize,
         Some(bias.clone()),
         None,
-        );
+    );
     builder.model.wire_node("", op, &[input])
 }
 
-fn multi_ew(
+/*
+ *   fragment sum_reduce( input: tensor<scalar>, axes: integer[], normalize: logical = false ) -> ( output: tensor<scalar> );
+ *   fragment max_reduce( input: tensor<scalar>, axes: integer[] ) -> ( output: tensor<scalar> );
+ *   + max, min, argmax, armmin, any, all
+ */
+
+fn reduce(
+    builder: &mut ModelBuilder,
+    invocation: &AugmentedInvocation,
+) -> TractResult<TVec<OutletId>> {
+    use tract_core::ops::nn::{Reduce, Reducer};
+    let input = invocation.pos_arg(0)?.to_wire(builder)?;
+    let axes = invocation.pos_arg(1)?.to_tensor(builder)?;
+    let axes = axes.cast_to::<i64>()?;
+    let axes = axes.as_slice::<i64>()?.iter().map(|&i| i as usize).collect::<TVec<_>>();
+    let reducer = match invocation.invocation.id.split("_").next().unwrap() {
+        "sum" => Reducer::Sum,
+        "min" => Reducer::Min,
+        "max" => Reducer::Max,
+        _ => bail!("unsupported reducer: {}", invocation.invocation.id),
+    };
+    let mut wire = builder.model.wire_node("", Reduce::new(axes.clone(), reducer), &[input])?;
+    if let Some(norm_arg) = invocation.get_pos_arg(2) {
+        let tensor = norm_arg.to_tensor(builder)?;
+        if tensor.cast_to_scalar::<bool>()? {
+            let input_shape = &builder.model.outlet_fact(input)?.shape;
+            let cardinality = axes.iter().map(|ax| input_shape.dim(*ax)).maybe_product()?;
+            let cardinality = tensor0(cardinality).broadcast_into_rank(input_shape.rank())?;
+            wire = builder.model.wire_node(
+                "",
+                tract_core::ops::math::div::unary(cardinality.into_arc_tensor()),
+                &[input],
+            )?;
+        }
+    }
+    Ok(wire)
+}
+
+/*
+ * fragment matmul( A: tensor<scalar>, B: tensor<scalar>, transposeA: logical = false, transposeB: logical = false ) -> ( C: tensor<scalar> );
+ */
+fn matmul(
+    builder: &mut ModelBuilder,
+    invocation: &AugmentedInvocation,
+) -> TractResult<TVec<OutletId>> {
+    dbg!(&invocation);
+    let a = invocation.named_arg("A")?.to_wire(builder)?;
+    let b = invocation.named_arg("B")?.to_wire(builder)?;
+    let a_trans =
+        invocation.named_arg("transposeA")?.to_tensor(builder)?.cast_to_scalar::<bool>()?;
+    let b_trans =
+        invocation.named_arg("transposeB")?.to_tensor(builder)?.cast_to_scalar::<bool>()?;
+    builder.model.wire_node(
+        "",
+        tract_core::ops::matmul::MatMul { a_trans, b_trans, c_trans: false, q_params: None },
+        &[a, b],
+    )
+}
+
+fn multiary_elementwise(
     builder: &mut ModelBuilder,
     invocation: &AugmentedInvocation,
     op: Box<dyn TypedOp>,
-    ) -> TractResult<TVec<OutletId>> {
+) -> TractResult<TVec<OutletId>> {
     let inputs = invocation
         .invocation
         .arguments
@@ -144,5 +209,5 @@ fn multicast(builder: &mut ModelBuilder, inputs: &[OutletId]) -> TractResult<TVe
             (r..max_rank)
                 .try_fold(i, |w, n| Ok(builder.model.wire_node("", AxisOp::Add(n), &[w])?[0]))
         })
-    .collect()
+        .collect()
 }
