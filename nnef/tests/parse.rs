a4d45df34af871c4cf7b09d01b7896e1281ca04f
Mathieu Poumeyrol <kali@zoy.org>
2020-08-03 19:24:52 +0200

    ast parse/dump loop

--- nnef/tests/parse.rs
+++ nnef/tests/parse.rs
@@ -1,5 +1,3 @@
-
-
 #[test]
 fn parse_alexnet() {
     let content = std::fs::read_to_string("tests/alexnet.nnef").unwrap();
@@ -7,7 +5,37 @@ fn parse_alexnet() {
 }
 
 #[test]
+fn parse_dump_parse_alexnet() {
+    let content = std::fs::read_to_string("tests/alexnet.nnef").unwrap();
+    let ast = tract_nnef::parser::parse_document(&content).unwrap();
+    let mut dumped = vec!();
+    tract_nnef::dump::Dumper::new(&mut dumped).document(&ast).unwrap();
+
+    let dumped = String::from_utf8(dumped).unwrap();
+    let ast2 = tract_nnef::parser::parse_document(&dumped).unwrap();
+
+    assert_eq!(ast, ast2);
+}
+
+#[test]
 fn parse_stdlib() {
     let content = std::fs::read_to_string("stdlib.nnef").unwrap();
     tract_nnef::parser::parse_fragments(&content).unwrap();
+}
+
+#[test]
+fn parse_dump_parse_stdlib() {
+    let content = std::fs::read_to_string("stdlib.nnef").unwrap();
+    let ast = tract_nnef::parser::parse_fragments(&content).unwrap();
+    let mut dumped = vec!();
+    tract_nnef::dump::Dumper::new(&mut dumped).fragments(&ast).unwrap();
+
+    let dumped = String::from_utf8(dumped).unwrap();
+    println!("{}", dumped);
+    let ast2 = tract_nnef::parser::parse_fragments(&dumped).unwrap();
+
+    assert_eq!(ast.len(), ast2.len());
+    for (a, b) in ast.iter().zip(ast2.iter()) {
+        assert_eq!(a, b);
+    }
 }
