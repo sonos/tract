b2751b0d8f3510c5d03a96cf0c9dad05684adee6
Mathieu Poumeyrol <kali@zoy.org>
2020-08-10 13:27:43 +0200

    fix some test config

--- harness/onnx-test-suite/include-passing-ignored.sh
+++ harness/onnx-test-suite/include-passing-ignored.sh
@@ -5,14 +5,25 @@ set -x
 for bundle in *.txt
 do
     prefix=$(echo $bundle | sed s/\.txt// | tr '.-' '__')
-    cargo test -- --ignored $prefix::optim:: | grep '::optim::.* ok'  | cut -d ' ' -f 2 | sed "s/::/:/g" | cut -d : -f 3 > /tmp/new
-    cat /tmp/new 
-    cat $bundle >> /tmp/new
-    sort /tmp/new > $bundle
-    rm /tmp/new
-    cargo test -- --ignored $prefix::plain:: | grep '::plain::.* ok'  | cut -d ' ' -f 2 | sed "s/::/:/g" | cut -d : -f 3 | sed 's/$/ dynsize/'> /tmp/new
-    cat /tmp/new 
-    cat $bundle >> /tmp/new
-    sort /tmp/new > $bundle
-    rm /tmp/new
+    cargo test -- --ignored $prefix::optim:: | grep '::optim::.* ok'  | cut -d ' ' -f 2 | sed "s/::/:/g" | cut -d : -f 3 > /tmp/$bundle
+done
+
+for bundle in *.txt
+do
+    cat $bundle >> /tmp/$bundle
+    sort /tmp/$bundle > $bundle
+    rm /tmp/$bundle
+done
+
+
+for bundle in *.txt
+do
+    cargo test -- --ignored $prefix::plain:: | grep '::plain::.* ok'  | cut -d ' ' -f 2 | sed "s/::/:/g" | cut -d : -f 3 | sed 's/$/ dynsize/'> /tmp/$bundle
+done
+
+for bundle in *.txt
+do
+    cat $bundle >> /tmp/$bundle
+    sort /tmp/$bundle > $bundle
+    rm /tmp/$bundle
 done
