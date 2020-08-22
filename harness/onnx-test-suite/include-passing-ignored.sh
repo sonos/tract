35a3dbef1165e84cf2cf48a7d41cafe1b377e233
Mathieu Poumeyrol <kali@zoy.org>
2020-08-22 17:35:52 +0200

    enable working tests

--- harness/onnx-test-suite/include-passing-ignored.sh
+++ harness/onnx-test-suite/include-passing-ignored.sh
@@ -1,29 +1,3 @@
 #!/bin/sh
 
-set -x
-
-for bundle in *.txt
-do
-    prefix=$(echo $bundle | sed s/\.txt// | tr '.-' '__')
-    cargo test -- --ignored $prefix::optim:: | grep '::optim::.* ok'  | cut -d ' ' -f 2 | sed "s/::/:/g" | cut -d : -f 3 > /tmp/$bundle
-done
-
-for bundle in *.txt
-do
-    cat $bundle >> /tmp/$bundle
-    sort /tmp/$bundle > $bundle
-    rm /tmp/$bundle
-done
-
-
-for bundle in *.txt
-do
-    cargo test -- --ignored $prefix::plain:: | grep '::plain::.* ok'  | cut -d ' ' -f 2 | sed "s/::/:/g" | cut -d : -f 3 | sed 's/$/ dynsize/'> /tmp/$bundle
-done
-
-for bundle in *.txt
-do
-    cat $bundle >> /tmp/$bundle
-    sort /tmp/$bundle > $bundle
-    rm /tmp/$bundle
-done
+cargo run
