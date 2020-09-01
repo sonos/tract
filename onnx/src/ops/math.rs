cb1eeb83f7d39f9b506f4e9ae48a0d0a0fc85b3c
Mathieu Poumeyrol <kali@zoy.org>
2020-09-01 14:34:31 +0200

    split onnx in two too

--- onnx/src/ops/math.rs
+++ onnx/src/ops/math.rs
@@ -43,13 +43,13 @@ pub fn register_all_ops(reg: &mut OnnxOpRegister) {
     reg.insert("Asinh", |_, _| Ok((Box::new(ops::math::asinh()), vec![])));
     reg.insert("Atanh", |_, _| Ok((Box::new(ops::math::atanh()), vec![])));
 
-    reg.insert("Erf", |_, _| Ok((Box::new(erf()), vec![])));
+    reg.insert("Erf", |_, _| Ok((Box::new(tract_onnx_opl::erf::erf()), vec![])));
     reg.insert("Exp", |_, _| Ok((Box::new(ops::math::exp()), vec![])));
     reg.insert("Log", |_, _| Ok((Box::new(ops::math::ln()), vec![])));
     reg.insert("Sqrt", |_, _| Ok((Box::new(ops::math::sqrt()), vec![])));
     reg.insert("Rsqrt", |_, _| Ok((Box::new(ops::math::rsqrt()), vec![])));
 
-    reg.insert("IsNaN", |_, _| Ok((Box::new(is_nan()), vec![])));
+    reg.insert("IsNaN", |_, _| Ok((Box::new(tract_onnx_opl::is_nan::is_nan()), vec![])));
     reg.insert("IsInf", isinf);
     reg.insert("Neg", |_, _| Ok((Box::new(ops::math::neg()), vec![])));
     reg.insert("Sign", |_, _| Ok((Box::new(ops::math::sign()), vec![])));
@@ -63,59 +63,13 @@ pub fn register_all_ops(reg: &mut OnnxOpRegister) {
     reg.insert("Gemm", gemm::gemm);
 }
 
-element_wise!(erf, Erf,
-    [f32] => |_, xs| {
-        xs.iter_mut().for_each(|x| *x = erf_f32(*x));
-        Ok(())
-    };
-    prefix: "onnx."
-);
-
-element_wise_oop!(is_nan, IsNan,
-    [f32] => bool |_, xs, ys| {
-        xs.iter().zip(ys.iter_mut()).for_each(|(x,y)| *y = x.is_nan());
-        Ok(())
-    };
-    prefix: "onnx."
-);
-
 fn isinf(
     _ctx: &ParsingContext,
     node: &NodeProto,
 ) -> TractResult<(Box<dyn InferenceOp>, Vec<String>)> {
     let detect_positive = node.get_attr_opt("detect_positive")?.unwrap_or(1) != 0;
     let detect_negative = node.get_attr_opt("detect_negative")?.unwrap_or(1) != 0;
-    Ok((Box::new(is_inf(detect_positive, detect_negative)), vec![]))
+    Ok((Box::new(tract_onnx_opl::is_inf::is_inf(detect_positive, detect_negative)), vec![]))
 }
 
-element_wise_oop!(is_inf, IsInf { detect_positive: bool, detect_negative: bool },
-    [f32] => bool |op, xs, ys| {
-        xs.iter().zip(ys.iter_mut()).for_each(|(x,y)|
-            *y = (op.detect_positive && *x == std::f32::INFINITY) || (op.detect_negative && *x == std::f32::NEG_INFINITY)
-        );
-        Ok(())
-    };
-    prefix: "onnx."
-);
 
-#[allow(non_upper_case_globals)]
-fn erf_f32(x: f32) -> f32 {
-    const a1: f32 = 0.0705230784;
-    const a2: f32 = 0.0422820123;
-    const a3: f32 = 0.0092705272;
-    const a4: f32 = 0.0001520143;
-    const a5: f32 = 0.0002765672;
-    const a6: f32 = 0.0000430638;
-
-    let signum = x.signum();
-    let x = x.abs();
-    let y = a6 * x;
-    let y = (a5 + y) * x;
-    let y = (a4 + y) * x;
-    let y = (a3 + y) * x;
-    let y = (a2 + y) * x;
-    let y = (a1 + y) * x;
-    let y = 1.0 - (y + 1.0).powi(16).recip();
-
-    y.copysign(signum)
-}
