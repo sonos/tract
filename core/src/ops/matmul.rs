9a9806d90e4a66bfbad2ead4c019d82425b6a3ef
Mathieu Poumeyrol <kali@zoy.org>
2020-09-22 16:47:17 +0200

    split pack_b from lir

--- core/src/ops/matmul.rs
+++ core/src/ops/matmul.rs
@@ -1,6 +1,8 @@
 pub mod lir;
 pub mod mir;
 pub mod mmm_wrapper;
+pub mod pack_b;
 
+use self::pack_b::MatMatMulPackB;
 pub use self::mir::{compute_shape, MatMul, MatMulUnary};
 pub use mmm_wrapper::MMMWrapper;
