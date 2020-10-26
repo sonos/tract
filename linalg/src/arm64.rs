19cf21d1fdeb96905954ccb262c68c12fe104798
Mathieu Poumeyrol <kali@zoy.org>
2020-10-26 17:17:04 +0100

    fix arm archs

--- linalg/src/arm64.rs
+++ linalg/src/arm64.rs
@@ -3,7 +3,6 @@ mod arm64simd;
 use crate::Ops;
 
 use crate::frame::MatMatMulImpl;
-use crate::frame::QMatMatMulImpl;
 use crate::frame::SigmoidImpl;
 use crate::frame::TanhImpl;
 
@@ -30,18 +29,10 @@ pub fn plug(ops: &mut Ops) {
         })
     }
     ops.qmmm_i8_i8 = Box::new(|m, k, n| {
-        Box::new(QMatMatMulImpl::from(
-            MatMatMulImpl::<arm64simd::MatMatMulI8x8x8, i8, i8, i8, i32>::new(m, k, n),
-        ))
+        Box::new(MatMatMulImpl::<arm64simd::MatMatMulI8x8x8, i8, i8, i8, i32>::new(m, k, n))
     });
     ops.qmmm_i8_i32 = Box::new(|m, k, n| {
-        Box::new(QMatMatMulImpl::from(MatMatMulImpl::<
-            arm64simd::MatMatMulI8xI32x8x8,
-            i8,
-            i8,
-            i32,
-            i32,
-        >::new(m, k, n)))
+        Box::new(MatMatMulImpl::<arm64simd::MatMatMulI8xI32x8x8, i8, i8, i32, i32>::new(m, k, n))
     });
     ops.sigmoid_f32 = Box::new(|| Box::new(SigmoidImpl::<arm64simd::SigmoidF32x4n, f32>::new()));
     ops.tanh_f32 = Box::new(|| Box::new(TanhImpl::<arm64simd::TanhF32x4n, f32>::new()));
