a946e6f2640b80bbc0d812bb003ed629369a49e9
Mathieu Poumeyrol <kali@zoy.org>
2020-11-03 10:15:26 +0100

    add script to update onnx harness

---
+++ harness/onnx-test-suite/src/bin/reset-test-list.rs
@@ -0,0 +1,85 @@
+use std::collections::HashMap;
+use std::io::{BufRead, Write};
+
+const SETS: &[&str] = &["node", "real", "simple", "pytorch-operator", "pytorch-converted"];
+const VERSIONS: &[&str] = &["1.4.1", "1.5.0", "1.6.0", "1.7.0"];
+
+// const SETS: &[&str] = &["node"];
+// const VERSIONS: &[&str] = &["1.4.1"];
+
+fn run_set(set: &str, ver: &str) -> HashMap<String, usize> {
+    let filter = format!("{}_{}::", set, ver).replace(['.', '-'].as_ref(), "_");
+    let mut command = std::process::Command::new("cargo");
+    command.arg("test");
+    if set == "real" {
+        command.arg("--release");
+    }
+    command.arg("--").arg("--ignored").arg(filter);
+    let output = command.output().unwrap();
+    let mut unexpected: HashMap<String, usize> = HashMap::default();
+    for line in std::io::BufReader::new(&mut &*output.stdout).lines() {
+        let line = line.unwrap();
+        if line.ends_with("ok") {
+            let test_id =
+                line.split_whitespace().nth(1).unwrap().split("::").nth(2).unwrap().to_string();
+            let level = line.split_whitespace().nth(1).unwrap().split("::").nth(1).unwrap();
+            let level = match level {
+                "nnef" => 3,
+                "optim" => 2,
+                "plain" => 1,
+                _ => panic!(),
+            };
+            let entry = unexpected.entry(test_id.clone()).or_insert(0);
+            *entry = (*entry).max(level);
+        }
+    }
+    unexpected
+}
+
+fn process_unexpected(set: &str, ver: &str, unexpected: HashMap<String, usize>) {
+    let file = format!("{}-{}.txt", set, ver);
+    eprintln!("## {} ##", file);
+    let mut specs: HashMap<String, String> = HashMap::new();
+    for line in std::fs::read_to_string(&file).unwrap().lines() {
+        let test = line.split_whitespace().nth(0).unwrap().to_string();
+        let entry = specs.entry(test).or_default();
+        if entry.len() < line.len() {
+            *entry = line.to_string();
+        }
+    }
+    for (test_id, level) in unexpected.into_iter() {
+        eprintln!("* {} level: {}", test_id, level);
+        let spec = specs
+            .entry(test_id.to_string())
+            .or_insert_with(|| format!("{} not-nnef not-typable", test_id));
+        if level >= 3 {
+            *spec =
+                spec.split_whitespace().filter(|t| t != &"not-nnef").collect::<Vec<_>>().join(" ");
+        }
+        if level >= 2 {
+            *spec = spec
+                .split_whitespace()
+                .filter(|t| t != &"not-typable")
+                .collect::<Vec<_>>()
+                .join(" ");
+        }
+    }
+    let mut file = std::fs::OpenOptions::new().write(true).truncate(true).open(file).unwrap();
+    let mut specs: Vec<String> = specs.into_iter().map(|e| e.1).collect();
+    specs.sort();
+    let buffer = specs.join("\n");
+    file.write_all(buffer.as_bytes()).unwrap();
+}
+
+fn main() {
+    let mut sets: HashMap<(&str, &str), _> = HashMap::default();
+    for &set in SETS {
+        for &ver in VERSIONS {
+            eprintln!("Running {} {}", set, ver);
+            sets.insert((set, ver), run_set(set, ver));
+        }
+    }
+    for ((set, ver), unexpected) in sets.into_iter() {
+        process_unexpected(set, ver, unexpected);
+    }
+}
