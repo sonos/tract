7b46c28cddf78a8bb0accbe615c02565613161ad
Mathieu Poumeyrol <kali@zoy.org>
2020-12-03 10:41:44 +0100

    cleanup bench with caching

--- linalg/benches/mm_for_asr_am.rs
+++ linalg/benches/mm_for_asr_am.rs
@@ -1,29 +1,73 @@
 use criterion::*;
 use tract_data::internal::*;
+use tract_linalg::frame::MatMatMul;
 
 use DatumType::*;
 
-fn mat_mul_f32(be: &mut Bencher, &(m, k, n): &(usize, usize, usize)) {
-    let mm = tract_linalg::ops().mmm(F32, F32, F32, m, k, n).unwrap();
-    let pa = Tensor::zero_aligned::<f32>(&[mm.a_pack().len(m)], mm.a_pack().alignment()).unwrap();
-    let pb = Tensor::zero_aligned::<f32>(&[mm.b_pack().len(n)], mm.b_pack().alignment()).unwrap();
-    let mut c = Tensor::zero::<f32>(&[m, n]).unwrap();
-    be.iter(move || unsafe { mm.run(&pa.view(), &pb.view(), &mut c.view_mut(), &[]) });
+fn ruin_cache() {
+    let _a = (0..1000000).collect::<Vec<i32>>();
+}
+fn run(
+    be: &mut Bencher,
+    mm: Box<dyn MatMatMul>,
+    pa: &Tensor,
+    pb: &Tensor,
+    c: &mut Tensor,
+    cold: bool,
+) {
+    unsafe {
+        be.iter_custom(move |iters| {
+            let mut dur = std::time::Duration::default();
+            for _ in 0..iters {
+                if cold {
+                    ruin_cache();
+                }
+                let instant = std::time::Instant::now();
+                mm.run(&pa.view(), &pb.view(), &mut c.view_mut(), &[]).unwrap();
+                let time = instant.elapsed();
+                dur += time;
+            }
+            dur
+        });
+    }
 }
 
-fn mat_mul_i8(be: &mut criterion::Bencher, &(m, k, n): &(usize, usize, usize)) {
-    let mm = tract_linalg::ops().mmm(I8, I8, I8, m, k, n).unwrap();
-    let pa = Tensor::zero_aligned::<i8>(&[mm.a_pack().len(m)], mm.a_pack().alignment()).unwrap();
-    let pb = Tensor::zero_aligned::<i8>(&[mm.b_pack().len(n)], mm.b_pack().alignment()).unwrap();
-    let mut c = Tensor::zero::<i8>(&[m, n]).unwrap();
-    be.iter(move || unsafe { mm.run(&pa.view(), &pb.view(), &mut c.view_mut(), &[]) });
+fn mat_mat(be: &mut Bencher, &(dt, m, k, n, cold): &(DatumType, usize, usize, usize, bool)) {
+    let mm = tract_linalg::ops().mmm(dt, dt, dt, m, k, n).unwrap();
+    let pa = Tensor::zero_aligned_dt(dt, &[mm.a_pack().len(m)], mm.a_pack().alignment()).unwrap();
+    let pb = Tensor::zero_aligned_dt(dt, &[mm.b_pack().len(n)], mm.b_pack().alignment()).unwrap();
+    let mut c = Tensor::zero_dt(dt, &[m, n]).unwrap();
+    run(be, mm, &pa, &pb, &mut c, cold);
+}
+
+fn mat_vec(be: &mut Bencher, &(dt, m, k, n, cold): &(DatumType, usize, usize, usize, bool)) {
+    let mut mm = tract_linalg::ops().mmm(dt, dt, dt, m, k, n).unwrap();
+    let pa = Tensor::zero_aligned_dt(dt, &[mm.a_pack().len(m)], mm.a_pack().alignment()).unwrap();
+    let pb = Tensor::zero_dt(dt, &[k, 1]).unwrap();
+    let mut c = Tensor::zero_dt(dt, &[m, n]).unwrap();
+    unsafe {
+        mm.b_vec_from_data();
+    }
+    run(be, mm, &pa, &pb, &mut c, cold);
 }
 
 fn packed_packed(c: &mut Criterion, m: usize, k: usize, n: usize) {
     let mut group = c.benchmark_group("packed_packed");
     let id = format!("{}x{}x{}", m, k, n);
-    group.bench_with_input(BenchmarkId::new("f32", &id), &(m, k, n), mat_mul_f32);
-    group.bench_with_input(BenchmarkId::new("i8", &id), &(m, k, n), mat_mul_i8);
+    group.bench_with_input(BenchmarkId::new("f32/cold", &id), &(F32, m, k, n, true), mat_mat);
+    group.bench_with_input(BenchmarkId::new("f32/hot", &id), &(F32, m, k, n, false), mat_mat);
+    group.bench_with_input(BenchmarkId::new("i8/cold", &id), &(I8, m, k, n, true), mat_mat);
+    group.bench_with_input(BenchmarkId::new("i8/hot", &id), &(I8, m, k, n, false), mat_mat);
+}
+
+fn packed_vec(c: &mut Criterion, m: usize, k: usize, n: usize) {
+    assert_eq!(n, 1);
+    let mut group = c.benchmark_group("packed_vec");
+    let id = format!("{}x{}x{}", m, k, n);
+    group.bench_with_input(BenchmarkId::new("f32/cold", &id), &(F32, m, k, n, true), mat_vec);
+    group.bench_with_input(BenchmarkId::new("f32/hot", &id), &(F32, m, k, n, false), mat_vec);
+    group.bench_with_input(BenchmarkId::new("i8/cold", &id), &(I8, m, k, n, true), mat_vec);
+    group.bench_with_input(BenchmarkId::new("i8/hot", &id), &(I8, m, k, n, false), mat_vec);
 }
 
 type ConvGeo = (usize, usize, usize, usize, usize);
@@ -56,7 +100,8 @@ fn direct_conv_i8(be: &mut Bencher, geo: &ConvGeo) {
     unsafe {
         let (m, k, n, rows_offsets, cols_offsets, b_len) = direct_conv_geo(geo);
         let mut mm = tract_linalg::ops().mmm(I8, I8, I8, m, k, n).unwrap();
-        let pa = Tensor::zero_aligned::<i8>(&[mm.a_pack().len(m)], mm.a_pack().alignment()).unwrap();
+        let pa =
+            Tensor::zero_aligned::<i8>(&[mm.a_pack().len(m)], mm.a_pack().alignment()).unwrap();
         let pb = Tensor::zero_aligned::<i8>(&[b_len], mm.b_pack().alignment()).unwrap();
         let mut c = Tensor::zero::<i8>(&[m, n]).unwrap();
         mm.b_from_data_and_offsets(&rows_offsets, &cols_offsets);
@@ -85,6 +130,11 @@ fn all(c: &mut Criterion) {
     packed_packed(c, 256, 256, 1); // fastlstm1 and 2 (rp) x 16 (2 layers x 8 loops)
     direct_conv(c, 8, 3, 256, 256, 1); // tdnn4, tdd5 (x2)
     packed_packed(c, 1690, 256, 8); // output
+
+    // 8M
+    packed_packed(c, 512, 200, 24); // tdnn1
+    packed_packed(c, 512, 256, 1); // fastlstm1 and 2 (four parts, rec mat*vec)
+    packed_vec(c, 512, 256, 1); // fastlstm1 and 2 (four parts, rec mat*vec)
 }
 
 criterion_group!(benches, all);
