4daf1c7afc5d1cc099e54a0d0778d7ca3cf16594
Mathieu Poumeyrol <kali@zoy.org>
2021-01-04 11:25:41 +0100

    reintegrate categorymapper

--- onnx-opl/src/ml/mod.rs
+++ onnx-opl/src/ml/mod.rs
@@ -1,2 +1,9 @@
+use tract_nnef::internal::*;
+
+pub mod category_mapper;
 pub mod tree;
 pub mod tree_ensemble_classifier;
+
+pub fn register(registry: &mut Registry) {
+    category_mapper::register(registry);
+}
