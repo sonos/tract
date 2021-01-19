2cd42934af0722d4fee3e09e0311ad40a5cbe665
Mathieu Poumeyrol <kali@zoy.org>
2021-01-19 11:56:19 +0100

    towards ND deconv

--- onnx/src/ops/nn/conv_transpose.rs
+++ onnx/src/ops/nn/conv_transpose.rs
@@ -1,5 +1,6 @@
 use crate::model::ParsingContext;
 use crate::pb::NodeProto;
+use tract_core::ops::cnn::KernelFormat;
 use tract_hir::internal::*;
 use tract_hir::ops::cnn::PaddingSpec;
 use tract_hir::ops::nn::DataFormat;
@@ -8,11 +9,14 @@ pub fn conv_transpose(
     _ctx: &ParsingContext,
     node: &NodeProto,
 ) -> TractResult<(Box<dyn InferenceOp>, Vec<String>)> {
-    Ok((expand(ConvTranspose::default()), vec!()))
+    let padding_spec = super::pad(node)?;
+    Ok((expand(ConvTranspose::new(padding_spec)), vec![]))
 }
 
 #[derive(Debug, Clone, new, Default, Hash)]
-pub struct ConvTranspose {}
+pub struct ConvTranspose {
+    padding_spec: PaddingSpec,
+}
 
 impl_dyn_hash!(ConvTranspose);
 
@@ -33,14 +37,22 @@ impl Expansion for ConvTranspose {
         check_output_arity(outputs, 1)?;
         s.equals(&inputs[0].datum_type, &outputs[0].datum_type)?;
         s.equals(&inputs[0].datum_type, &inputs[1].datum_type)?;
-        s.equals(&inputs[0].rank, 4)?;
-        s.equals(&inputs[1].rank, 4)?;
-        s.equals(&outputs[0].rank, 4)?;
+        s.equals(&inputs[0].rank, &inputs[1].rank)?;
+        s.equals(&inputs[0].rank, &outputs[0].rank)?;
         s.equals(&outputs[0].shape[0], &inputs[0].shape[0])?; // N
         s.equals(&inputs[0].shape[1], &inputs[1].shape[0])?; // O
         s.equals(&outputs[0].shape[1], &inputs[1].shape[1])?; // I
-        s.equals(&outputs[0].shape[2], inputs[0].shape[2].bex() + &inputs[1].shape[2] - 1.to_dim())?; // H
-        s.equals(&outputs[0].shape[3], inputs[0].shape[3].bex() + &inputs[1].shape[3] - 1.to_dim())?; // W
+
+        /* FIXME
+        s.equals(
+            &outputs[0].shape[2],
+            inputs[0].shape[2].bex() + &inputs[1].shape[2] - 1.to_dim(),
+        )?; // H
+        s.equals(
+            &outputs[0].shape[3],
+            inputs[0].shape[3].bex() + &inputs[1].shape[3] - 1.to_dim(),
+        )?; // W
+        */
         Ok(())
     }
 
@@ -53,7 +65,12 @@ impl Expansion for ConvTranspose {
         if let Some(k) = &target.outlet_fact(inputs[1])?.konst {
             target.wire_node(
                 prefix,
-                tract_core::ops::cnn::DeconvUnary::new(PaddingSpec::Valid, k.clone()),
+                tract_core::ops::cnn::DeconvUnary::new(
+                    DataFormat::NCHW,
+                    KernelFormat::OIHW,
+                    self.padding_spec.clone(),
+                    k.clone(),
+                ),
                 &[inputs[0]],
             )
         } else {
