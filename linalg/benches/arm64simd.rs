a57843bd70eed01be9bfc10d931d4cd60baf2a3a
Mathieu Poumeyrol <kali@zoy.org>
2021-03-15 10:27:17 +0100

    12x8

--- linalg/benches/arm64simd.rs
+++ linalg/benches/arm64simd.rs
@@ -671,245 +671,6 @@ pub fn ld_64F32(c: &mut Criterion) {
     });
 }
 
-fn packed_packed_12x8_loop1(c: &mut Criterion) {
-    let mut group = c.benchmark_group("packed_packed_12x8_loop1");
-    group.bench_function("ldr_w_no_preload", |b| {
-        b.iter(|| unsafe {
-            let mut p = F32;
-            let mut q = F32;
-            asm!("
-                    fmla        v8.4s, v0.4s, v4.s[0]
-                        ldr         w4, [{0}], #4
-                    fmla        v9.4s, v1.4s, v4.s[0]
-                            ldr         w19, [{1}], #4
-                    fmla        v10.4s, v2.4s, v4.s[0]
-                        ldr         w5, [{0}], #4
-
-                    fmla        v11.4s, v0.4s, v4.s[1]
-                            ldr         w20, [{1}], #4
-                    fmla        v12.4s, v1.4s, v4.s[1]
-                        ldr         w6, [{0}], #4
-                    fmla        v13.4s, v2.4s, v4.s[1]
-                            ldr         w21, [{1}], #4
-
-                    fmla        v14.4s, v0.4s, v4.s[2]
-                        ldr         w7, [{0}], #4
-                    fmla        v15.4s, v1.4s, v4.s[2]
-                            ldr         w22, [{1}], #4
-                    fmla        v16.4s, v2.4s, v4.s[2]
-                        ldr         w8, [{0}], #4
-                    fmla        v17.4s, v0.4s, v4.s[3]
-                            ldr         w23, [{1}], #4
-                    fmla        v18.4s, v1.4s, v4.s[3]
-                        ldr         w9, [{0}], #4
-                    fmla        v19.4s, v2.4s, v4.s[3]
-                            ldr         w24, [{1}], #4
-
-                    fmla        v20.4s, v0.4s, v5.s[0]
-                        ldr         w10, [{0}], #4
-                    fmla        v21.4s, v1.4s, v5.s[0]
-                            ldr         w25, [{1}], #4
-                    fmla        v22.4s, v2.4s, v5.s[0]
-                        ldr         w11, [{0}], #4
-                    fmla        v23.4s, v0.4s, v5.s[1]
-                            ldr         w26, [{1}], #4
-                    fmla        v24.4s, v1.4s, v5.s[1]
-                        ldr         w12, [{0}], #4
-                    fmla        v25.4s, v2.4s, v5.s[1]
-
-                    fmla        v26.4s, v0.4s, v5.s[2]
-                        ldr         w13, [{0}], #4
-                    fmla        v27.4s, v1.4s, v5.s[2]
-                    fmla        v28.4s, v2.4s, v5.s[2]
-                        ldr         w14, [{0}], #4
-                    fmla        v29.4s, v0.4s, v5.s[3]
-                    fmla        v30.4s, v1.4s, v5.s[3]
-                        ldr         w15, [{0}], #4
-                    fmla        v31.4s, v2.4s, v5.s[3]
-
-                        ins         v0.s[0], w4
-                        ins         v1.s[0], w8
-                        ins         v2.s[0], w12
-                            ins         v4.s[0], w19
-                            ins         v5.s[0], w23
-                        ins         v0.s[1], w5
-                        ins         v1.s[1], w9
-                        ins         v2.s[1], w13
-                            ins         v4.s[1], w20
-                            ins         v5.s[1], w24
-                        ins         v0.s[2], w6
-                        ins         v1.s[2], w10
-                        ins         v2.s[2], w14
-                            ins         v4.s[2], w21
-                            ins         v5.s[2], w25
-                        ins         v0.s[3], w7
-                        ins         v1.s[3], w11
-                        ins         v2.s[3], w15
-                            ins         v4.s[3], w22
-                            ins         v5.s[3], w26 ",
-            inout(reg) p, inout(reg) q,
-            out("x4") _, out("x5") _, out("x6") _, out("x7") _,
-            out("x8") _, out("x9") _, out("x10") _, out("x11") _,
-            out("x12") _, out("x13") _, out("x14") _, out("x15") _,
-            out("x19") _, out("x20") _, out("x21") _, out("x22") _,
-            out("x23") _, out("x24") _, out("x25") _, out("x26") _,
-            out("v0") _, out("v1") _, out("v2") _, out("v4") _, out("v5") _,
-            out("v16") _, out("v17") _, out("v18") _, out("v19") _,
-            out("v20") _, out("v21") _, out("v22") _, out("v23") _,
-            out("v24") _, out("v25") _, out("v26") _, out("v27") _,
-            out("v28") _, out("v29") _, out("v30") _, out("v31") _,
-            );
-        })
-    });
-    group.bench_function("ldr_w_with_preload", |b| {
-        b.iter(|| unsafe {
-            let mut p = F32;
-            let mut q = F32;
-            asm!("
-                    fmla        v8.4s, v0.4s, v4.s[0]
-                        ldr         w4, [{0}]
-                    fmla        v9.4s, v1.4s, v4.s[0]
-                            ldr         w19, [{1}], #4
-                    fmla        v10.4s, v2.4s, v4.s[0]
-                        ldr         w5, [{0}, #4]
-
-                    fmla        v11.4s, v0.4s, v4.s[1]
-                            ldr         w20, [{1}], #4
-                    fmla        v12.4s, v1.4s, v4.s[1]
-                        ldr         w6, [{0}, #8]
-                    fmla        v13.4s, v2.4s, v4.s[1]
-                            ldr         w21, [{1}], #4
-
-                    fmla        v14.4s, v0.4s, v4.s[2]
-                        ldr         w7, [{0}, #12]
-                    fmla        v15.4s, v1.4s, v4.s[2]
-                            ldr         w22, [{1}], #4
-                    fmla        v16.4s, v2.4s, v4.s[2]
-                        ldr         w8, [{0}, #16]
-                    fmla        v17.4s, v0.4s, v4.s[3]
-                            ldr         w23, [{1}], #4
-                    fmla        v18.4s, v1.4s, v4.s[3]
-                        ldr         w9, [{0}, #20]
-                    fmla        v19.4s, v2.4s, v4.s[3]
-                            ldr         w24, [{1}], #4
-
-                    fmla        v20.4s, v0.4s, v5.s[0]
-                        ldr         w10, [{0}, #24]
-                    fmla        v21.4s, v1.4s, v5.s[0]
-                            ldr         w25, [{1}], #4
-                    fmla        v22.4s, v2.4s, v5.s[0]
-                        ldr         w11, [{0}, #28]
-                    fmla        v23.4s, v0.4s, v5.s[1]
-                            ldr         w26, [{1}], #4
-                    fmla        v24.4s, v1.4s, v5.s[1]
-                        ldr         w12, [{0}, #32]
-                    fmla        v25.4s, v2.4s, v5.s[1]
-                        ldr         w13, [{0}, #36]
-
-                    fmla        v26.4s, v0.4s, v5.s[2]
-                        ldr         w14, [{0}, #40]
-                    fmla        v27.4s, v1.4s, v5.s[2]
-                        ldr         w15, [{0}, #44]
-                    fmla        v28.4s, v2.4s, v5.s[2]
-                        prfm        pldl1keep, [{0}, #512]
-                    fmla        v29.4s, v0.4s, v5.s[3]
-                        add         {0}, {0}, #48
-                    fmla        v30.4s, v1.4s, v5.s[3]
-                        prfm        pldl1keep, [{1}, #384]
-                    fmla        v31.4s, v2.4s, v5.s[3]
-
-                        ins         v0.s[0], w4
-
-                        ins         v1.s[0], w8
-                        ins         v2.s[0], w12
-
-                            ins         v4.s[0], w19
-                            ins         v5.s[0], w23
-
-                        ins         v0.s[1], w5
-                        ins         v1.s[1], w9
-
-                        ins         v2.s[1], w13
-                            ins         v4.s[1], w20
-
-                            ins         v5.s[1], w24
-                        ins         v0.s[2], w6
-
-                        ins         v1.s[2], w10
-                        ins         v2.s[2], w14
-
-                            ins         v4.s[2], w21
-                            ins         v5.s[2], w25
-
-                        ins         v0.s[3], w7
-                        ins         v1.s[3], w11
-
-                        ins         v2.s[3], w15
-                            ins         v4.s[3], w22
-                            ins         v5.s[3], w26
-             ", inout(reg) p, inout(reg) q,
-            out("x4") _, out("x5") _, out("x6") _, out("x7") _,
-            out("x8") _, out("x9") _, out("x10") _, out("x11") _,
-            out("x12") _, out("x13") _, out("x14") _, out("x15") _,
-            out("x19") _, out("x20") _, out("x21") _, out("x22") _,
-            out("x23") _, out("x24") _, out("x25") _, out("x26") _,
-            out("v0") _, out("v1") _, out("v2") _, out("v4") _, out("v5") _,
-            out("v16") _, out("v17") _, out("v18") _, out("v19") _,
-            out("v20") _, out("v21") _, out("v22") _, out("v23") _,
-            out("v24") _, out("v25") _, out("v26") _, out("v27") _,
-            out("v28") _, out("v29") _, out("v30") _, out("v31") _,
-            );
-        })
-    });
-    group.bench_function("naive", |b| {
-        b.iter(|| unsafe {
-            let mut p = F32;
-            let mut q = F32;
-            asm!("
-                    fmla        v8.4s, v0.4s, v4.s[0]
-                    fmla        v9.4s, v1.4s, v4.s[0]
-                    fmla        v10.4s, v2.4s, v4.s[0]
-                    fmla        v11.4s, v0.4s, v4.s[1]
-                    fmla        v12.4s, v1.4s, v4.s[1]
-                    fmla        v13.4s, v2.4s, v4.s[1]
-
-                    fmla        v14.4s, v0.4s, v4.s[2]
-                    fmla        v15.4s, v1.4s, v4.s[2]
-                    fmla        v16.4s, v2.4s, v4.s[2]
-                    fmla        v17.4s, v0.4s, v4.s[3]
-                    fmla        v18.4s, v1.4s, v4.s[3]
-                    fmla        v19.4s, v2.4s, v4.s[3]
-
-                    fmla        v20.4s, v0.4s, v5.s[0]
-                    fmla        v21.4s, v1.4s, v5.s[0]
-                    fmla        v22.4s, v2.4s, v5.s[0]
-                    fmla        v23.4s, v0.4s, v5.s[1]
-                    fmla        v24.4s, v1.4s, v5.s[1]
-                    fmla        v25.4s, v2.4s, v5.s[1]
-
-                    fmla        v26.4s, v0.4s, v5.s[2]
-                    fmla        v27.4s, v1.4s, v5.s[2]
-                    fmla        v28.4s, v2.4s, v5.s[2]
-                    fmla        v29.4s, v0.4s, v5.s[3]
-                    fmla        v30.4s, v1.4s, v5.s[3]
-                    fmla        v31.4s, v2.4s, v5.s[3]
-
-                    ld1         {{ v0.4s, v1.4s, v2.4s }}, [{0}]
-                    ld1         {{ v4.4s, v5.4s }}, [{1}] ",
-            inout(reg) p, inout(reg) q,
-            out("v0") _, out("v1") _, out("v2") _,
-            out("v4") _, out("v5") _,
-            out("v8") _, out("v9") _, out("v10") _, out("v11") _,
-            out("v12") _, out("v13") _, out("v14") _, out("v15") _,
-            out("v16") _, out("v17") _, out("v18") _, out("v19") _,
-            out("v20") _, out("v21") _, out("v22") _, out("v23") _,
-            out("v24") _, out("v25") _, out("v26") _, out("v27") _,
-            out("v28") _, out("v29") _, out("v30") _, out("v31") _,
-            );
-        })
-    });
-}
-
 fn packed_packed_8x8_loop1(c: &mut Criterion) {
     let mut group = c.benchmark_group("packed_packed_8x8_loop1");
     group.throughput(criterion::Throughput::Elements(8 * 8 * 4));
@@ -941,6 +702,40 @@ fn packed_packed_8x8_loop1(c: &mut Criterion) {
 
     bench_4_loop_1!(naive);
     bench_4_loop_1!(ldr_x_no_preload);
+    bench_4_loop_1!(ldr_w_no_preload);
+    bench_4_loop_1!(ldr_w_preload);
+}
+
+fn packed_packed_12x8_loop1(c: &mut Criterion) {
+    let mut group = c.benchmark_group("packed_packed_12x8_loop1");
+    group.throughput(criterion::Throughput::Elements(12 * 8 * 4));
+
+    macro_rules! bench_4_loop_1 {
+        ($id:ident) => {
+            group.bench_function(stringify!($id), |b| {
+                b.iter(|| unsafe {
+                    let mut p = F32;
+                    let mut q = F32;
+                    r4!(asm!(include_str!(concat!("../arm64/arm64simd/arm64simd_mmm_f32_12x8/packed_packed_loop1/", stringify!($id), ".tmpli")),
+                    inout("x1") p, inout("x2") q,
+                    out("x4") _, out("x5") _, out("x6") _, out("x7") _,
+                    out("x8") _, out("x9") _, out("x10") _, out("x11") _,
+                    out("x12") _, out("x13") _, out("x14") _, out("x15") _,
+                    out("x19") _, out("x20") _, out("x21") _, out("x22") _,
+                    out("x23") _, out("x24") _, out("x25") _, out("x26") _,
+                    out("v0") _, out("v1") _, out("v2") _,
+                    out("v4") _, out("v5") _,
+                    out("v16") _, out("v17") _, out("v18") _, out("v19") _,
+                    out("v20") _, out("v21") _, out("v22") _, out("v23") _,
+                    out("v24") _, out("v25") _, out("v26") _, out("v27") _,
+                    out("v28") _, out("v29") _, out("v30") _, out("v31") _,
+                    ));
+                })
+            });
+        }
+    }
+
+    bench_4_loop_1!(naive);
     bench_4_loop_1!(ldr_w_no_preload);
     bench_4_loop_1!(ldr_w_preload);
 }
