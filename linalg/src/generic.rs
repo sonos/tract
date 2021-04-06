d4d59174ec1cfa4f5de03ab0d72e2df4ebbda8c4
Mathieu Poumeyrol <kali@zoy.org>
2021-04-06 09:26:42 +0200

    plugin generic 4x1 everywhere

--- linalg/src/generic.rs
+++ linalg/src/generic.rs
@@ -4,6 +4,7 @@ pub mod sigmoid;
 pub mod tanh;
 
 pub use self::lut::GenericLut8;
+pub use self::mmm::GenericMmm4x1;
 pub use self::mmm::GenericMmm4x4;
 pub use self::sigmoid::SSigmoid4;
 pub use self::tanh::STanh4;
