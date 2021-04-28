273db4e78410feabd9324bb41d6e3199be53718c
TimotheeLeBerreSonos <timothee.leberre@sonos.com>
2021-04-28 10:29:10 +0200

    remove dbg

--- core/src/ops/matmul/lir_unary.rs
+++ core/src/ops/matmul/lir_unary.rs
@@ -273,10 +273,10 @@ impl TypedOp for LirMatMulUnary {
         let merge = |fused_micro_op: &ArrayD<Vec<ProtoFusedSpec>>,
                      additional_inputs: &[OutletId]|
          -> TractResult<Option<TypedModelPatch>> {
-            let mut new_op = dbg!(self).clone();
+            let mut new_op = self.clone();
             new_op
                 .micro_ops
-                .zip_mut_with(dbg!(fused_micro_op), |lhs, rhs| lhs.1.extend(rhs.iter().cloned()));
+                .zip_mut_with(fused_micro_op, |lhs, rhs| lhs.1.extend(rhs.iter().cloned()));
             let mut patch = TypedModelPatch::new(format!("fusing {}", succ));
             patch.dont_apply_twice = Some(format!("Fuse {} into {}", succ.name, node.name));
             let inputs = node
