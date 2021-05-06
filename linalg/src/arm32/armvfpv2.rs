080c6fcb5c6e43072cb0f4f94f46a98f7194342e
Mathieu Poumeyrol <kali@zoy.org>
2021-05-06 13:40:27 +0200

    fix armvfp2 bindings

--- linalg/src/arm32/armvfpv2.rs
+++ linalg/src/arm32/armvfpv2.rs
@@ -2,6 +2,6 @@ use crate::frame::mmm::*;
 
 extern_kernel!(fn armvfpv2_mmm_f32_4x4(op: *const MatMatMulKerSpec<f32>) -> isize);
 
-MMMKernel!(MatMatMulF32x4x4<f32>, "vfpv2" armvfpv2_mmm_f32_4x4; 4, 4; 4, 4; 0; 0);
+MMMKernel!(MatMatMulF32x4x4<f32>, "vfpv2", armvfpv2_mmm_f32_4x4; 4, 4; 4, 4; 0, 0);
 
 test_mmm_kernel_f32!(crate::arm32::armvfpv2::MatMatMulF32x4x4, test_MatMatMulF32x4x4, true);
