4d7d6585fa5d664d100988f658a6f7c6b1028b32
Mathieu Poumeyrol <kali@zoy.org>
2021-05-06 13:23:08 +0200

    macroify mmm kernel bindings

--- linalg/src/arm32/armvfpv2.rs
+++ linalg/src/arm32/armvfpv2.rs
@@ -2,38 +2,6 @@ use crate::frame::mmm::*;
 
 extern_kernel!(fn armvfpv2_mmm_f32_4x4(op: *const MatMatMulKerSpec<f32>) -> isize);
 
-#[derive(Copy, Clone, Debug)]
-pub struct MatMatMulF32x4x4;
-
-impl MatMatMulKer<f32> for MatMatMulF32x4x4 {
-    #[inline(always)]
-    fn name() -> &'static str {
-        "vfpv2"
-    }
-    #[inline(always)]
-    fn mr() -> usize {
-        4
-    }
-    #[inline(always)]
-    fn nr() -> usize {
-        4
-    }
-    fn alignment_bytes_packed_a() -> usize {
-        4
-    }
-    fn alignment_bytes_packed_b() -> usize {
-        4
-    }
-    fn end_padding_packed_a() -> usize {
-        0
-    }
-    fn end_padding_packed_b() -> usize {
-        0
-    }
-    #[inline(never)]
-    fn kernel(spec: &MatMatMulKerSpec<f32>) -> isize {
-        unsafe { armvfpv2_mmm_f32_4x4(spec) }
-    }
-}
+MMMKernel!(MatMatMulF32x4x4<f32>, "vfpv2" armvfpv2_mmm_f32_4x4; 4, 4; 4, 4; 0; 0);
 
 test_mmm_kernel_f32!(crate::arm32::armvfpv2::MatMatMulF32x4x4, test_MatMatMulF32x4x4, true);
--- linalg/src/frame/mmm.rs
+++ linalg/src/frame/mmm.rs
@@ -18,3 +18,42 @@ pub use storage::*;
 
 #[cfg(test)]
 pub use tests::*;
+
+macro_rules! MMMKernel {
+    ($typ:ident<$ti:ident>, $name:expr, $func:ident; $mr: expr, $nr: expr; $alignment_bytes_packed_a: expr, $alignment_bytes_packed_b: expr; $end_padding_packed_a: expr, $end_padding_packed_b: expr) => {
+        #[derive(Copy, Clone, Debug, new)]
+        pub struct $typ;
+
+        impl MatMatMulKer<$ti> for $typ {
+            #[inline(always)]
+            fn name() -> &'static str {
+                $name
+            }
+            #[inline(always)]
+            fn mr() -> usize {
+                $mr
+            }
+            #[inline(always)]
+            fn nr() -> usize {
+                $nr
+            }
+            fn alignment_bytes_packed_a() -> usize {
+                $alignment_bytes_packed_b
+            }
+            fn alignment_bytes_packed_b() -> usize {
+                $alignment_bytes_packed_b
+            }
+            fn end_padding_packed_a() -> usize {
+                $end_padding_packed_a
+            }
+            fn end_padding_packed_b() -> usize {
+                $end_padding_packed_b
+            }
+            #[inline(never)]
+            fn kernel(spec: &MatMatMulKerSpec<$ti>) -> isize {
+                unsafe { $func(spec) }
+            }
+        }
+    }
+}
+
