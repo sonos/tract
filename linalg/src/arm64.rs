cbc2b2ce5fa02fbd335be27360494662ecccc9c8
Mathieu Poumeyrol <kali@zoy.org>
2021-06-14 17:10:06 +0200

    tweak choosing rules

--- linalg/src/arm64.rs
+++ linalg/src/arm64.rs
@@ -70,7 +70,10 @@ fn best_of(
     if let (Some(m), Some(n)) = (m, n) {
         let k = kernels
             .iter()
-            .min_by_key(|k| (m.div_ceil(k.mr()) * n.div_ceil(k.nr())) * (50 + k.mr() * k.nr()))
+            .min_by_key(|k| {
+                (m.div_ceil(k.mr()) * n.div_ceil(k.nr()))
+                    * (50 + k.mr() * k.nr() + 5 * k.mr().abs_sub(k.nr()))
+            })
             .unwrap()
             .clone();
         k
