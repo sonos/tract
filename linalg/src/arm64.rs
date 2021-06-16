2beff3e7e0fdcdb9f57205052669f295273c8709
Mathieu Poumeyrol <kali@zoy.org>
2021-06-16 13:44:54 +0200

    improve kernel choice

--- linalg/src/arm64.rs
+++ linalg/src/arm64.rs
@@ -1,4 +1,5 @@
 mod arm64simd;
+use arm64simd::*;
 
 use crate::Ops;
 
@@ -18,16 +19,15 @@ fn is_cortex_a53() -> std::io::Result<bool> {
 pub fn plug(ops: &mut Ops) {
     if is_cortex_a53().unwrap_or(false) {
         log::info!("arm64simd activated for smmm (cortex A53)");
-        ops.mmv_f32 =
-            Box::new(|_, _| Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x64x1A53, f32>::new()));
+        ops.mmv_f32 = Box::new(|_, _| Box::new(MatMatMulImpl::<MatMatMulF32x64x1A53, f32>::new()));
         ops.mmm_f32 = Box::new(|m, _, n| {
             best_of(
                 m,
                 n,
                 &[
-                    Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x12x8A53, f32>::new()),
-                    Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x8x8A53, f32>::new()),
-                    Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x16x4A53, f32>::new()),
+                    Box::new(MatMatMulImpl::<MatMatMulF32x12x8A53, f32>::new()),
+                    Box::new(MatMatMulImpl::<MatMatMulF32x8x8A53, f32>::new()),
+                    Box::new(MatMatMulImpl::<MatMatMulF32x16x4A53, f32>::new()),
                 ],
             )
         })
@@ -40,26 +40,22 @@ pub fn plug(ops: &mut Ops) {
                 m,
                 n,
                 &[
-                    Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x12x8, f32>::new()),
-                    Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x8x8, f32>::new()),
-                    Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x16x4, f32>::new()),
+                    Box::new(MatMatMulImpl::<MatMatMulF32x12x8, f32>::new()),
+                    Box::new(MatMatMulImpl::<MatMatMulF32x8x8, f32>::new()),
+                    Box::new(MatMatMulImpl::<MatMatMulF32x16x4, f32>::new()),
                 ],
             )
         })
     }
 
-    ops.qmmm_i8_i8 =
-        Box::new(|_, _, _| Box::new(MatMatMulImpl::<arm64simd::MatMatMulI8x8x8, i32>::new()));
-    ops.qmmv_i8_i8 =
-        Box::new(|_, _| Box::new(MatMatMulImpl::<arm64simd::MatMatMulI8x64x1, i32>::new()));
+    ops.qmmm_i8_i8 = Box::new(|_, _, _| Box::new(MatMatMulImpl::<MatMatMulI8x8x8, i32>::new()));
+    ops.qmmv_i8_i8 = Box::new(|_, _| Box::new(MatMatMulImpl::<MatMatMulI8x64x1, i32>::new()));
     ops.qmmm_i8_i32 =
-        Box::new(|_, _, _| Box::new(MatMatMulImpl::<arm64simd::MatMatMulI8xI32x8x8, i32>::new()));
-    ops.qmmv_i8_i32 =
-        Box::new(|_, _| Box::new(MatMatMulImpl::<arm64simd::MatMatMulI8xI32x64x1, i32>::new()));
+        Box::new(|_, _, _| Box::new(MatMatMulImpl::<MatMatMulI8xI32x8x8, i32>::new()));
+    ops.qmmv_i8_i32 = Box::new(|_, _| Box::new(MatMatMulImpl::<MatMatMulI8xI32x64x1, i32>::new()));
 
-    ops.sigmoid_f32 =
-        Box::new(|| Box::new(ElementWiseImpl::<arm64simd::SigmoidF32x4n, f32>::new()));
-    ops.tanh_f32 = Box::new(|| Box::new(ElementWiseImpl::<arm64simd::TanhF32x4n, f32>::new()));
+    ops.sigmoid_f32 = Box::new(|| Box::new(ElementWiseImpl::<SigmoidF32x4n, f32>::new()));
+    ops.tanh_f32 = Box::new(|| Box::new(ElementWiseImpl::<TanhF32x4n, f32>::new()));
 }
 
 fn best_of(
@@ -71,15 +67,41 @@ fn best_of(
         let k = kernels
             .iter()
             .min_by_key(|k| {
-                (m.div_ceil(k.mr()) * n.div_ceil(k.nr()))
-                    * (50
-                        + k.mr() * k.nr()
-                        + 5 * (k.mr() as isize - k.nr() as isize).abs() as usize)
+                let rows = m.div_ceil(k.mr());
+                let cols = n.div_ceil(k.nr());
+                (rows * cols) * (50 + k.mr() * k.nr() + 10 * (k.nr() + k.mr()))
             })
             .unwrap()
             .clone();
         k
     } else {
         kernels.iter().max_by_key(|k| k.mr() * k.nr()).unwrap().clone()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn best(m: usize, n: usize) -> (usize, usize) {
+        let k = best_of(
+            Some(m),
+            Some(n),
+            &[
+                Box::new(MatMatMulImpl::<MatMatMulF32x12x8, f32>::new()),
+                Box::new(MatMatMulImpl::<MatMatMulF32x8x8, f32>::new()),
+                Box::new(MatMatMulImpl::<MatMatMulF32x16x4, f32>::new()),
+            ],
+        );
+        (k.mr(), k.nr())
+    }
+
+    #[test]
+    fn kernel_choice() {
+        assert_eq!(best(128, 40), (12, 8)); // hey_snips_v1 layer_0_2
+        assert_eq!(best(200, 12), (12, 8)); // 15M h_new
+        assert_eq!(best(768, 12), (12, 8)); // 15M h_new
+        assert_eq!(best(768, 4), (16, 4)); // 15M h_new
+        assert_eq!(best(2000, 4), (16, 4)); // 15M output
     }
 }
