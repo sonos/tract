536f0ee3c374609bd994d8a8e74f1e4b9af2a2f2
TimotheeLeBerreSonos <timothee.leberre@sonos.com>
2021-06-30 18:01:59 +0200

    qmul.. and qshift in kernels

--- linalg/src/generic/mmm.rs
+++ linalg/src/generic/mmm.rs
@@ -276,7 +276,13 @@ where
                         }
                     }
                     FusedKerSpec::AddUnicast(tile) => add_unicast::<TC, TI, _>(&tile, &mut ab),
-                    FusedKerSpec::QWrappingMulHighDoubling(_) => (),
+                    FusedKerSpec::QWrappingMulHighDoubling(mult) => {
+                        for i in 0..4 {
+                            for j in 0..4 {
+                                ab[i][j] = ab[i][j].doubling_mul(mult);
+                            }
+                        }
+                    }
                     FusedKerSpec::QShiftRightRounding(shift, pol) => {
                         for i in 0..4 {
                             for j in 0..4 {
@@ -532,7 +538,11 @@ where
                             std::slice::from_raw_parts_mut(ab.as_ptr().offset(3) as _, 1),
                         ],
                     ),
-                    FusedKerSpec::QWrappingMulHighDoubling(_) => (),
+                    FusedKerSpec::QWrappingMulHighDoubling(mult) => {
+                        for i in 0..4 {
+                            ab[i] = ab[i].doubling_mul(mult);
+                        }
+                    }
                     FusedKerSpec::QShiftRightRounding(shift, pol) => {
                         for i in 0..4 {
                             ab[i] = ab[i].shift(shift, pol)
@@ -790,7 +800,13 @@ where
                         }
                     }
                     FusedKerSpec::AddUnicast(tile) => add_unicast::<TC, TI, _>(&tile, &mut ab),
-                    FusedKerSpec::QWrappingMulHighDoubling(_) => (),
+                    FusedKerSpec::QWrappingMulHighDoubling(mult) => {
+                        for i in 0..3 {
+                            for j in 0..2 {
+                                ab[i][j] = ab[i][j].doubling_mul(mult);
+                            }
+                        }
+                    }
                     FusedKerSpec::QShiftRightRounding(shift, pol) => {
                         for i in 0..3 {
                             for j in 0..2 {
