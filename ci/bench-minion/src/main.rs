f9349b24283b4bed10b359d743cdc5f5969d7098
Mathieu Poumeyrol <kali@zoy.org>
2021-07-22 09:58:50 +0200

    riir runner script

--- ci/bench-minion/src/main.rs
+++ ci/bench-minion/src/main.rs
@@ -1,16 +1,21 @@
 use anyhow::Context;
 use anyhow::Result;
+use flate2::read::GzEncoder;
 use fs2::FileExt;
 use s3::creds::Credentials;
+use s3::serde_types::Object;
 use s3::Bucket;
 use s3::Region;
-use s3::serde_types::Object;
 use serde::{Deserialize, Deserializer};
+use std::collections::HashMap;
+use std::io::Write;
+use std::net::TcpStream;
 use std::path::{Path, PathBuf};
 use std::time::Duration;
 
 #[derive(Deserialize, Debug)]
 struct Config {
+    id: String,
     #[serde(default = "default_workdir")]
     workdir: PathBuf,
     #[serde(default = "default_region", deserialize_with = "deser_region")]
@@ -19,7 +24,17 @@ struct Config {
     s3_bucket: String,
     #[serde(default = "default_tasks")]
     s3_tasks: String,
+    #[serde(default = "default_logs")]
+    s3_logs: String,
     platform: String,
+    graphite: Option<Graphite>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Graphite {
+    host: String,
+    port: u16,
+    prefix: String,
 }
 
 fn default_workdir() -> PathBuf {
@@ -35,7 +50,11 @@ fn default_bucket() -> String {
 }
 
 fn default_tasks() -> String {
-    "tasks/".to_string()
+    "tasks".to_string()
+}
+
+fn default_logs() -> String {
+    "logs".to_string()
 }
 
 fn deser_region<'de, D>(d: D) -> Result<Region, D::Error>
@@ -61,12 +80,94 @@ fn read_config(path: impl AsRef<Path>) -> Result<Config> {
     Ok(config)
 }
 
-fn consider_task(config: &Config, bucket: &Bucket, task: &Object) -> Result<()> {
-    eprintln!("{}", task.key);
+fn do_task(config: &Config, bucket: &Bucket, task: &Object, task_name: &str) -> Result<()> {
+    log::info!("Performing {}", task.key);
+    let task_dir = config.workdir.join("current");
+    if task_dir.exists() {
+        std::fs::remove_dir_all(&task_dir)?;
+    }
+    log::info!("Retrieving task...");
+    std::fs::create_dir_all(&task_dir)?;
+    let (reader, mut writer) = pipe::pipe_buffered();
+    let task_dir_2 = task_dir.clone();
+    let thr = std::thread::spawn(move || {
+        let uncompressed = flate2::read::GzDecoder::new(reader);
+        tar::Archive::new(uncompressed).unpack(task_dir_2)
+    });
+    bucket.get_object_stream_blocking(&task.key, &mut writer)?;
+    if thr.join().is_err() {
+        anyhow::bail!("Failed to untar")
+    }
+    let vars_file = task_dir.join(task_name).join("vars");
+    log::info!("Reading vars...");
+    let mut vars: HashMap<String, String> = HashMap::default();
+    for line in std::fs::read_to_string(vars_file)?.lines() {
+        if line.starts_with("export ") {
+            let mut pair = line.split_whitespace().nth(1).unwrap().split("=");
+            vars.insert(pair.next().unwrap().to_string(), pair.next().unwrap().to_string());
+        }
+    }
+    log::info!("Vars: {:?}", vars);
+    let status = std::process::Command::new("sh")
+        .current_dir(task_dir.join(task_name))
+        .arg("-c")
+        .arg("pwd ; . ./vars ; ./entrypoint.sh 2> stderr.log > stdout.log")
+        .status()?;
+    log::info!("Script ran: {:?}", status);
+    for log in &["stderr.log", "stdout.log"] {
+        let local_path = task_dir.join(task_name).join(log);
+        if local_path.exists() {
+            let s3name = Path::new(&config.s3_logs)
+                .join(&config.id)
+                .join(task_name)
+                .join(log)
+                .with_extension("gz");
+            log::info!("Uploading {} to {:?}", log, s3name);
+            let mut gz =
+                GzEncoder::new(std::fs::File::open(&local_path)?, flate2::Compression::default());
+            let mut content = vec![];
+            gz.write_all(&mut content)?;
+            bucket.put_object_blocking(s3name.to_str().unwrap(), &content)?;
+        } else {
+            log::info!("Could not find {}", log);
+        }
+    }
+    let metrics_files = task_dir.join(task_name).join("metrics");
+    if metrics_files.exists() {
+        if let Some(gr) = &config.graphite {
+            let prefix = format!("{}.{}.{}.{}", gr.prefix, config.platform, config.id, vars["TRAVIS_BRANCH_SANE"]);
+            let mut socket = TcpStream::connect((gr.host.clone(), gr.port))?;
+            let ts = &vars["TIMESTAMP"];
+            for line in std::fs::read_to_string(metrics_files)?.lines() {
+                writeln!(socket, "{}{} {}", prefix, line, ts)?;
+            }
+        }
+    }
     Ok(())
 }
 
-fn run(config: &Config) -> Result<()> {
+fn consider_task(config: &Config, bucket: &Bucket, task: &Object) -> Result<bool> {
+    let task_path = Path::new(&task.key);
+    let task_name = task_path
+        .file_stem()
+        .context("Filename can not be splitted")?
+        .to_str()
+        .unwrap()
+        .to_string();
+    let done_file = config.workdir.join("taskdone").join(&task_name);
+    if done_file.exists() {
+        return Ok(false);
+    }
+    if let Err(e) = do_task(config, bucket, task, &task_name)
+        .with_context(|| format!("Running task {}", task.key))
+    {
+        eprintln!("{:?}", e);
+    }
+    std::fs::File::create(done_file)?;
+    Ok(true)
+}
+
+fn run(config: &Config) -> Result<bool> {
     log::info!("Running...");
     std::thread::sleep(Duration::from_secs(10));
     let credentials = Credentials::default().unwrap();
@@ -74,13 +175,13 @@ fn run(config: &Config) -> Result<()> {
     let tasks_prefix = std::path::PathBuf::from(&config.s3_tasks).join(&config.platform).join("");
     let objects_parts =
         bucket.list_blocking(tasks_prefix.to_str().unwrap().to_string(), Some("/".to_string()))?;
+    let mut done_anything = false;
     for parts in objects_parts {
         for item in parts.0.contents {
-            consider_task(config, &bucket, &item)?;
+            done_anything = consider_task(config, &bucket, &item)? || done_anything;
         }
-        //        anyhow::bail!("Non-200 code got from S3 on listing tasks");
     }
-    Ok(())
+    Ok(done_anything)
 }
 
 fn main_loop() -> Result<()> {
@@ -91,13 +192,22 @@ fn main_loop() -> Result<()> {
     let lock = config.workdir.join("lock");
     log::info!("Locking {:?}", lock);
     std::fs::create_dir_all(&config.workdir)?;
+    std::fs::create_dir_all(&config.workdir.join("taskdone"))?;
     let lock =
         std::fs::File::create(&lock).with_context(|| format!("Creating lock file {:?}", lock))?;
     loop {
         if let Ok(_) = lock.try_lock_exclusive() {
             log::info!("Lock obtained, starting for good.");
-            if let Err(e) = run(&config) {
-                eprintln!("{:?}", e);
+            match run(&config) {
+                Ok(done_something) => {
+                    if !done_something {
+                        log::info!("Nothing to do, sleeping a while");
+                        std::thread::sleep(Duration::from_secs(5 * 60));
+                    }
+                }
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                }
             }
         } else {
             log::info!("Already locked, bailing out...");
