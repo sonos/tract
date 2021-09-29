f242c9e0ddd908f5cb6feceb5a3714aed727a775
Mathieu Poumeyrol <kali@zoy.org>
2021-09-29 10:11:44 +0200

    introduce a thread local for alignment management buffer

--- linalg/src/frame/element_wise.rs
+++ linalg/src/frame/element_wise.rs
@@ -1,8 +1,48 @@
+use std::alloc::*;
 use std::fmt::Debug;
 use std::marker::PhantomData;
 use tract_data::anyhow;
 
-use tract_data::prelude::Tensor;
+struct TempBuffer {
+    layout: Layout,
+    buffer: *mut u8,
+}
+
+impl Default for TempBuffer {
+    fn default() -> Self {
+        TempBuffer { layout: Layout::new::<()>(), buffer: std::ptr::null_mut() }
+    }
+}
+
+impl TempBuffer {
+    fn ensure(&mut self, size: usize, alignment: usize) {
+        unsafe {
+            if size > self.layout.size() || alignment > self.layout.align() {
+                let size = size.max(self.layout.size());
+                let alignment = alignment.max(self.layout.align());
+                if !self.buffer.is_null() {
+                    std::alloc::dealloc(self.buffer, self.layout);
+                }
+                self.layout = Layout::from_size_align_unchecked(size, alignment);
+                self.buffer = std::alloc::alloc(self.layout);
+            }
+        }
+    }
+}
+
+impl Drop for TempBuffer {
+    fn drop(&mut self) {
+        unsafe {
+            if !self.buffer.is_null() {
+                std::alloc::dealloc(self.buffer, self.layout);
+            }
+        }
+    }
+}
+
+std::thread_local! {
+    static TMP:  std::cell::RefCell<TempBuffer> = std::cell::RefCell::new(TempBuffer::default());
+}
 
 pub trait ElementWise<T>: Send + Sync + Debug + dyn_clone::DynClone
 where
@@ -32,25 +72,27 @@ where
             return Ok(());
         }
         unsafe {
-            let mut tmp_buffer =
-                Tensor::uninitialized_aligned::<T>(&[K::nr()], K::alignment_bytes()).unwrap();
-            let mut tmp = tmp_buffer.as_slice_mut_unchecked::<T>();
-            let mut compute_via_temp_buffer = |slice: &mut [T]| {
-                tmp[..slice.len()].copy_from_slice(slice);
-                K::run(&mut tmp);
-                slice.copy_from_slice(&tmp[..slice.len()])
-            };
-            let prefix_len = vec.as_ptr().align_offset(K::alignment_bytes()).min(vec.len());
-            if prefix_len > 0 {
-                compute_via_temp_buffer(&mut vec[..prefix_len]);
-            }
-            let aligned_len = (vec.len() - prefix_len) / K::nr() * K::nr();
-            if aligned_len > 0 {
-                K::run(&mut vec[prefix_len..][..aligned_len]);
-            }
-            if prefix_len + aligned_len < vec.len() {
-                compute_via_temp_buffer(&mut vec[prefix_len + aligned_len..]);
-            }
+            TMP.with(|buffer| {
+                let mut buffer = buffer.borrow_mut();
+                buffer.ensure(K::nr() * T::datum_type().size_of(), K::alignment_bytes());
+                let mut tmp = std::slice::from_raw_parts_mut(buffer.buffer as *mut T, K::nr());
+                let mut compute_via_temp_buffer = |slice: &mut [T]| {
+                    tmp[..slice.len()].copy_from_slice(slice);
+                    K::run(&mut tmp);
+                    slice.copy_from_slice(&tmp[..slice.len()])
+                };
+                let prefix_len = vec.as_ptr().align_offset(K::alignment_bytes()).min(vec.len());
+                if prefix_len > 0 {
+                    compute_via_temp_buffer(&mut vec[..prefix_len]);
+                }
+                let aligned_len = (vec.len() - prefix_len) / K::nr() * K::nr();
+                if aligned_len > 0 {
+                    K::run(&mut vec[prefix_len..][..aligned_len]);
+                }
+                if prefix_len + aligned_len < vec.len() {
+                    compute_via_temp_buffer(&mut vec[prefix_len + aligned_len..]);
+                }
+            })
         }
         Ok(())
     }
