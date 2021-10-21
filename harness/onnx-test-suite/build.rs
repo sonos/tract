24abf9914159cd7d8e9d5b6e58c2edd547ed0d7e
Mathieu Poumeyrol <kali@zoy.org>
2021-10-21 14:22:33 +0200

    onnx bump, readme update

--- harness/onnx-test-suite/build.rs
+++ harness/onnx-test-suite/build.rs
@@ -20,6 +20,9 @@ fn versions() -> Vec<&'static str> {
     if cfg!(feature = "onnx_1_9_0") {
         versions.push("1.9.0");
     }
+    if cfg!(feature = "onnx_1_10_1") {
+        versions.push("1.10.1");
+    }
     versions
 }
 
