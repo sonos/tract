64dd325a95009b41b62538d254593f737c72a073
Mathieu Poumeyrol <kali@zoy.org>
2021-11-04 09:27:14 +0100

    introduce a 8x8 for fun

--- linalg/src/x86_64_fma/mmm.rs
+++ linalg/src/x86_64_fma/mmm.rs
@@ -1,12 +1,20 @@
 use crate::frame::mmm::*;
 
+extern_kernel!(fn fma_mmm_f32_8x8(op: *const FusedKerSpec<f32>) -> isize);
 extern_kernel!(fn fma_mmm_f32_16x6(op: *const FusedKerSpec<f32>) -> isize);
 extern_kernel!(fn fma_mmm_f32_64x1(op: *const FusedKerSpec<f32>) -> isize);
 extern_kernel!(fn avx2_mmm_i32_8x8(op: *const FusedKerSpec<i32>) -> isize);
 
+MMMKernel!(MatMatMulF32x8x8<f32>, fma_mmm_f32_8x8; 8, 8; 32, 4; 0, 0);
 MMMKernel!(MatMatMulF32x16x6<f32>,  fma_mmm_f32_16x6; 16, 6; 32, 4; 0, 0);
 MMMKernel!(MatMatMulF32x64x1<f32>,  fma_mmm_f32_64x1; 64, 1; 32, 4; 0, 0);
 MMMKernel!(MatMatMulI32x8x8<i32>, avx2_mmm_i32_8x8; 8, 8; 32, 4; 0, 0);
+
+test_mmm_kernel_f32!(
+    crate::x86_64_fma::mmm::MatMatMulF32x8x8,
+    test_MatMatMulF32x8x8,
+    is_x86_feature_detected!("fma")
+);
 
 test_mmm_kernel_f32!(
     crate::x86_64_fma::mmm::MatMatMulF32x16x6,
