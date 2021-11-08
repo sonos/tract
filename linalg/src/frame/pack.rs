9cb332b23f3879d1c81b8ec2ffe0e6513a873f79
Mathieu Poumeyrol <kali@zoy.org>
2021-11-08 17:37:44 +0100

    packing k and mn ranges

--- linalg/src/frame/pack.rs
+++ linalg/src/frame/pack.rs
@@ -45,21 +45,22 @@ impl Packer {
             pb.iter_mut().for_each(|v| *v = T::default());
         }
         if self.r == 1 && k_stride == 1 && mn == 1 {
-            pb[k_range.clone()].copy_from_slice(&b[k_range]);
+            pb[..k_range.len()].copy_from_slice(&b[k_range]);
         } else if mn_stride == 1 {
             let size_of = T::datum_type().size_of();
             let rbytes = self.r * size_of;
             let bp = b.as_ptr() as _;
             let pp = pb.as_mut_ptr() as _;
             let mn_range_bytes = mn_range.start * size_of..mn_range.end * size_of;
+            let k_stride_bytes = k_stride * size_of as isize;
             match rbytes {
-                16 => pack_mn_major::<[u8; 16]>(bp, pp, mn_range_bytes, k_range),
-                24 => pack_mn_major::<[u8; 24]>(bp, pp, mn_range_bytes, k_range),
-                32 => pack_mn_major::<[u8; 32]>(bp, pp, mn_range_bytes, k_range),
-                48 => pack_mn_major::<[u8; 32]>(bp, pp, mn_range_bytes, k_range),
-                64 => pack_mn_major::<[u8; 64]>(bp, pp, mn_range_bytes, k_range),
+                16 => pack_mn_major::<[u8; 16]>(bp, pp, k_stride_bytes, mn_range_bytes, k_range),
+                24 => pack_mn_major::<[u8; 24]>(bp, pp, k_stride_bytes, mn_range_bytes, k_range),
+                32 => pack_mn_major::<[u8; 32]>(bp, pp, k_stride_bytes, mn_range_bytes, k_range),
+                48 => pack_mn_major::<[u8; 48]>(bp, pp, k_stride_bytes, mn_range_bytes, k_range),
+                64 => pack_mn_major::<[u8; 64]>(bp, pp, k_stride_bytes, mn_range_bytes, k_range),
                 _ => {
-                    let mut packer = self.write_with_k_outer(pb, k_range.len(), mn);
+                    let mut packer = self.write_with_k_outer(pb, k_range.len(), mn_range.len());
                     for k in k_range {
                         for x in mn_range.clone() {
                             packer.write(
@@ -272,30 +273,34 @@ where
 
 #[inline(never)]
 pub unsafe fn pack_mn_major<Chunk: Copy>(
-    mut b: *const u8,
-    p: *mut u8,
-    mn_range: Range<usize>,
+    b: *const u8,
+    packed: *mut u8,
+    k_stride_bytes: isize,
+    mn_range_bytes: Range<usize>,
     k_range: Range<usize>,
 ) {
     let mnr = std::mem::size_of::<Chunk>();
-    let full_panes = mn_range.len() / mnr;
-    let partial_pane = mn_range.len() % mnr;
-    for k in k_range.clone() {
-        let mut p_row = p.offset((k * mnr) as isize);
+    let full_panes = mn_range_bytes.len() / mnr;
+    let partial_pane = mn_range_bytes.len() % mnr;
+    for k in 0..k_range.len() {
+        let mut p_row = packed.offset((k * mnr) as isize);
+        let mut b_row =
+            b.offset((k_range.start + k) as isize * k_stride_bytes + mn_range_bytes.start as isize);
         for _ in 0..full_panes {
-            p_row.copy_from_nonoverlapping(b, mnr);
+            p_row.copy_from_nonoverlapping(b_row, mnr);
             p_row = p_row.offset((k_range.len() * mnr) as isize);
-            b = b.offset(mnr as isize);
+            b_row = b_row.offset(mnr as isize);
         }
         if partial_pane > 0 {
-            p_row.copy_from_nonoverlapping(b, partial_pane);
-            b = b.offset(partial_pane as isize);
+            p_row.copy_from_nonoverlapping(b_row, partial_pane);
         }
     }
 }
 
 #[cfg(test)]
 mod test {
+    use std::ops::Range;
+
     use proptest::prelude::*;
     use tract_data::internal::*;
     use tract_ndarray::prelude::*;
@@ -306,42 +311,58 @@ mod test {
         mn: usize,
         is_a: bool,
         r: usize,
-        input: Array2<u32>,
+        k_range: Range<usize>,
+        mn_range: Range<usize>,
     }
 
     impl PackProblem {
-        fn packer(&self) -> Vec<u32> {
+        fn input(&self) -> Array2<u32> {
+            let shape = if self.is_a { (self.mn, self.k) } else { (self.k, self.mn) };
+            let data = (0..(self.k * self.mn) as u32).collect();
+            Array2::from_shape_vec(shape, data).unwrap()
+        }
+
+        fn packer(&self) -> Array3<u32> {
+            let panels = self.mn_range.len().divceil(self.r);
             let packer = super::Packer::new(self.r, 1, 0);
-            let input = self.input.clone().into_tensor();
-            let mut output = Tensor::zero::<u32>(&[packer.len(self.k, self.mn)]).unwrap();
+            let input = self.input().into_tensor();
+            let mut output =
+                Tensor::zero::<u32>(&[packer.len(self.k_range.len(), self.mn_range.len())])
+                    .unwrap();
             unsafe {
-                packer.pack(
+                packer.pack_segment(
                     output.view_mut(),
                     input.view(),
                     self.is_a as usize,
                     !self.is_a as usize,
+                    self.k_range.clone(),
+                    self.mn_range.clone(),
                 )
             };
-            output.as_slice::<u32>().unwrap().to_vec()
+            output
+                .into_array::<u32>()
+                .unwrap()
+                .into_shape((panels, self.k_range.len(), self.r))
+                .unwrap()
         }
 
-        fn reference(&self) -> Vec<u32> {
-            let panels = self.mn.divceil(self.r);
-            let len = panels * self.k * self.r;
-            let mut vec = vec![0; len];
-            for panel in 0..panels {
-                for k in 0..self.k {
-                    for x in 0..self.r {
-                        let ix = panel * self.r + x;
-                        let v = *self
-                            .input
-                            .get(if self.is_a { (ix, k) } else { (k, ix) })
-                            .unwrap_or(&0);
-                        vec[panel * self.k * self.r + k * self.r + x] = v;
-                    }
+        fn reference(&self) -> Array3<u32> {
+            let input = self.input();
+            let panels = self.mn_range.len().divceil(self.r);
+            Array3::from_shape_fn([panels, self.k_range.len(), self.r], |(panel, k, x)| {
+                if self.mn_range.start + panel * self.r + x >= self.mn_range.end {
+                    0
+                } else {
+                    let mn = panel * self.r + x + self.mn_range.start;
+                    let k = k + self.k_range.start;
+                    let coords = if self.is_a { (mn, k) } else { (k, mn) };
+                    *input.get(coords).unwrap_or(&0)
                 }
-            }
-            vec
+            })
+        }
+
+        fn check(&self) {
+            assert_eq!(self.packer(), self.reference())
         }
     }
 
@@ -349,50 +370,93 @@ mod test {
         type Parameters = ();
         type Strategy = BoxedStrategy<PackProblem>;
         fn arbitrary_with(_args: ()) -> Self::Strategy {
-            (any::<bool>(), 1usize..4, 1usize..8, 1usize..8)
-                .prop_flat_map(|(is_a, r, mn, k)| {
-                    (Just((is_a, r, mn, k)), proptest::collection::vec(0u32..40, mn * k..=mn * k))
+            (any::<bool>(), 1usize..9, 1usize..20, 1usize..20)
+                .prop_flat_map(|(is_a, r, k, mn)| {
+                    (Just((is_a, r, k, mn)), sub_range_strat(0..k), sub_range_strat(0..mn))
                 })
-                .prop_map(|((is_a, r, mn, k), input)| PackProblem {
+                .prop_map(|((is_a, r, k, mn), k_range, mn_range)| PackProblem {
                     k,
                     mn,
                     is_a,
                     r,
-                    input: arr1(&*input).into_shape(if is_a { (mn, k) } else { (k, mn) }).unwrap(),
+                    k_range,
+                    mn_range,
                 })
                 .boxed()
         }
     }
 
+    fn sub_range_strat(range: Range<usize>) -> BoxedStrategy<Range<usize>> {
+        (0..range.len())
+            .prop_flat_map(|cropped| (Just(cropped), 0..=cropped))
+            .prop_map(move |(cropped, left)| range.start + left..range.end - (cropped - left))
+            .boxed()
+    }
+
     proptest::proptest! {
         #[test]
         fn prop(pb in any::<PackProblem>()) {
-            assert_eq!(pb.reference(), pb.packer());
+            pb.check();
         }
+
+        #[test]
+        fn subrange_prop(_range in sub_range_strat(0..20)) {
+        }
+
     }
 
     #[test]
     fn simple_b_1() {
-        let pb = PackProblem { k: 2, mn: 1, is_a: false, r: 1, input: arr2(&[[0], [1]]) };
-        assert_eq!(pb.reference(), pb.packer());
+        PackProblem { k: 2, mn: 1, is_a: false, r: 1, k_range: 0..2, mn_range: 0..1 }.check();
     }
 
     #[test]
     fn simple_b_2() {
-        let pb = PackProblem { k: 2, mn: 2, is_a: false, r: 1, input: arr2(&[[0, 0], [0, 1]]) };
-        assert_eq!(pb.reference(), pb.packer());
+        PackProblem { k: 2, mn: 2, is_a: false, r: 1, k_range: 0..2, mn_range: 0..2 }.check()
+    }
+
+    #[test]
+    fn simple_b_3() {
+        PackProblem { k: 2, mn: 1, is_a: false, r: 4, k_range: 0..2, mn_range: 0..1 }.check();
     }
 
     #[test]
     fn simple_a_1() {
-        let pb = PackProblem { k: 2, mn: 2, is_a: true, r: 1, input: arr2(&[[0, 0], [0, 1]]) };
-        assert_eq!(pb.reference(), pb.packer());
+        PackProblem { k: 2, mn: 2, is_a: true, r: 1, k_range: 0..2, mn_range: 0..2 }.check();
     }
 
     #[test]
     fn simple_a_2() {
-        let pb =
-            PackProblem { k: 2, mn: 3, is_a: true, r: 2, input: arr2(&[[0, 0], [0, 0], [0, 1]]) };
-        assert_eq!(pb.reference(), pb.packer());
+        PackProblem { k: 2, mn: 3, is_a: true, r: 2, k_range: 0..2, mn_range: 0..3 }.check();
+    }
+
+    #[test]
+    fn range_k_0() {
+        PackProblem { k: 2, mn: 1, is_a: false, r: 1, k_range: 1..2, mn_range: 0..1 }.check();
+    }
+
+    #[test]
+    fn range_k_1() {
+        PackProblem { k: 2, mn: 2, is_a: false, r: 1, k_range: 0..2, mn_range: 0..1 }.check();
+    }
+
+    #[test]
+    fn range_k_2() {
+        PackProblem { k: 2, mn: 1, is_a: false, r: 6, k_range: 1..2, mn_range: 0..1 }.check();
+    }
+
+    #[test]
+    fn range_mn_0() {
+        PackProblem { k: 1, mn: 2, is_a: false, r: 2, k_range: 0..1, mn_range: 0..1 }.check();
+    }
+
+    #[test]
+    fn range_b_4() {
+        PackProblem { k: 1, mn: 2, is_a: false, r: 6, k_range: 0..1, mn_range: 1..2 }.check();
+    }
+
+    #[test]
+    fn range_b_5() {
+        PackProblem { k: 1, mn: 7, is_a: false, r: 6, k_range: 0..1, mn_range: 1..7 }.check();
     }
 }
