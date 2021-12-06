ad0d26825980a98207fe25f367edd1afcf5e6a0c
Mathieu Poumeyrol <kali@zoy.org>
2021-12-06 15:01:06 +0100

    refine cpu kind selection logic

--- linalg/src/arm64.rs
+++ linalg/src/arm64.rs
@@ -9,7 +9,18 @@ use crate::frame::MatMatMulImpl;
 
 use tract_data::internal::DimLike;
 
-const PART_ARM53: &str = "0xd03";
+lazy_static::lazy_static! {
+    static ref KIND: Kind = Kind::choose();
+}
+
+// https://en.wikipedia.org/wiki/Comparison_of_ARMv8-A_cores
+const PART_A53: &str = "0xd03";
+const PART_A55: &str = "0xd05";
+const PART_A72: &str = "0xd08";
+#[allow(dead_code)]
+const PART_A73: &str = "0xd09";
+#[allow(dead_code)]
+const PART_A75: &str = "0xd0a";
 
 fn max_cpuid() -> std::io::Result<String> {
     let cpu_info = std::fs::read_to_string("/proc/cpuinfo")?;
@@ -21,44 +32,78 @@ fn max_cpuid() -> std::io::Result<String> {
     Ok(max.unwrap_or("").to_string())
 }
 
-fn is_cortex_a53() -> std::io::Result<bool> {
-    Ok(max_cpuid()? == PART_ARM53)
+#[derive(Debug)]
+enum Kind {
+    Generic,
+    CortexA53Like,
+}
+
+impl Kind {
+    fn choose() -> Kind {
+        let kind = if let Ok(kind) = std::env::var("TRACT_CPU_AARCH64_KIND") {
+            log::info!("CPU kind forced with TRACT_CPU_AARCH64_KIND: {}", kind);
+            if kind.to_lowercase().contains("a53") {
+                Kind::CortexA53Like
+            } else {
+                Kind::Generic
+            }
+        } else {
+            let part = if let Ok(part) = std::env::var("TRACT_CPU_AARCH64_OVERRIDE_CPU_PART") {
+                log::info!("CPU part forced with TRACT_CPU_AARCH64_OVERRIDE_CPU_PART: {}", part);
+                part
+            } else {
+                let part = max_cpuid().unwrap_or("0x00".to_string());
+                log::info!("CPU part auto detected: {}", part);
+                part
+            };
+            if [PART_A72, PART_A55, PART_A53].contains(&&*part) {
+                Kind::CortexA53Like
+            } else {
+                Kind::Generic
+            }
+        };
+        log::info!("CPU optimisation: {:?}", kind);
+        kind
+    }
 }
 
 pub fn plug(ops: &mut Ops) {
-    if is_cortex_a53().unwrap_or(false) {
-        log::info!("arm64simd activated for smmv (cortex A53)");
-        ops.mmv_f32 = Box::new(|_, _| Box::new(MatMatMulImpl::<MatMatMulF32x64x1A53, f32>::new()));
-        ops.mmm_f32 = Box::new(|m, k, n| {
-            best_of(
-                m,
-                k,
-                n,
-                &[
-                    Box::new(MatMatMulImpl::<MatMatMulF32x12x8A53, f32>::new()),
-                    Box::new(MatMatMulImpl::<MatMatMulF32x8x8A53, f32>::new()),
-                    Box::new(MatMatMulImpl::<MatMatMulF32x16x4A53, f32>::new()),
-                ],
-            )
-        });
-    } else {
-        log::info!("arm64simd activated for smmv (generic)");
-        ops.mmv_f32 =
-            Box::new(|_, _| Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x64x1, f32>::new()));
-        ops.mmm_f32 = Box::new(|m, k, n| {
-            best_of(
-                m,
-                k,
-                n,
-                &[
-                    Box::new(MatMatMulImpl::<MatMatMulF32x12x8, f32>::new()),
-                    Box::new(MatMatMulImpl::<MatMatMulF32x8x8, f32>::new()),
-                    Box::new(MatMatMulImpl::<MatMatMulF32x16x4, f32>::new()),
-                ],
-            )
-        });
+    match *KIND {
+        Kind::CortexA53Like => {
+            ops.mmv_f32 =
+                Box::new(|_, _| Box::new(MatMatMulImpl::<MatMatMulF32x64x1A53, f32>::new()));
+            ops.mmm_f32 = Box::new(|m, k, n| {
+                best_of(
+                    m,
+                    k,
+                    n,
+                    &[
+                        Box::new(MatMatMulImpl::<MatMatMulF32x12x8A53, f32>::new()),
+                        Box::new(MatMatMulImpl::<MatMatMulF32x8x8A53, f32>::new()),
+                        Box::new(MatMatMulImpl::<MatMatMulF32x16x4A53, f32>::new()),
+                    ],
+                )
+            })
+        }
+        Kind::Generic => {
+            ops.mmv_f32 =
+                Box::new(
+                    |_, _| Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x64x1, f32>::new()),
+                );
+            ops.mmm_f32 = Box::new(|m, k, n| {
+                best_of(
+                    m,
+                    k,
+                    n,
+                    &[
+                        Box::new(MatMatMulImpl::<MatMatMulF32x12x8, f32>::new()),
+                        Box::new(MatMatMulImpl::<MatMatMulF32x8x8, f32>::new()),
+                        Box::new(MatMatMulImpl::<MatMatMulF32x16x4, f32>::new()),
+                    ],
+                )
+            });
+        }
     }
-
     ops.qmmm_i32 = Box::new(|_, _, _| Box::new(MatMatMulImpl::<MatMatMulI8x8x8, i32>::new()));
     ops.qmmv_i32 = Box::new(|_, _| Box::new(MatMatMulImpl::<MatMatMulI8x64x1, i32>::new()));
     ops.sigmoid_f32 = Box::new(|| Box::new(ElementWiseImpl::<SigmoidF32x4n, f32>::new()));
@@ -73,15 +118,14 @@ fn best_of(
 ) -> Box<dyn MatMatMul> {
     if let (Some(m), Some(k), Some(n)) = (m, k, n) {
         // eprintln!("{}x{}x{}", m, k, n);
-        let a53 = is_cortex_a53().unwrap_or(false);
         let ker = kernels
             .iter()
             .min_by_key(|ker| {
                 let rows = m.divceil(ker.mr());
                 let cols = n.divceil(ker.nr());
                 let tiles = rows * cols;
-                let cost = if a53 {
-                    match (ker.kernel_name(), ker.mr(), ker.nr()) {
+                let cost = match *KIND {
+                    Kind::CortexA53Like => match (ker.kernel_name(), ker.mr(), ker.nr()) {
                         ("arm64simd (generic)", 16, 4) => 31043 * k + 937000,
                         ("arm64simd (generic)", 12, 8) => 37448 * k + 990000,
                         ("arm64simd (generic)", 8, 8) => 28228 * k + 990000,
@@ -89,9 +133,8 @@ fn best_of(
                         ("arm64simd (cortex A53)", 12, 8) => 36823 * k + 990000,
                         ("arm64simd (cortex A53)", 8, 8) => 28333 * k + 990000,
                         _ => panic!("uncosted kernel"),
-                    }
-                } else {
-                    match (ker.kernel_name(), ker.mr(), ker.nr()) {
+                    },
+                    Kind::Generic => match (ker.kernel_name(), ker.mr(), ker.nr()) {
                         ("arm64simd (generic)", 16, 4) => 1500 * k + 83000,
                         ("arm64simd (generic)", 12, 8) => 2083 * k + 83000,
                         ("arm64simd (generic)", 8, 8) => 1458 * k + 83000,
@@ -99,21 +142,21 @@ fn best_of(
                         ("arm64simd (cortex A53)", 12, 8) => 5625 * k + 83000,
                         ("arm64simd (cortex A53)", 8, 8) => 4834 * k + 41000,
                         _ => panic!("uncosted kernel"),
-                    }
+                    },
                 };
                 let indirect_tiles =
                     (rows * ker.mr() > m) as usize * cols + (cols * ker.nr() > n) as usize * rows;
                 let score = tiles * cost + indirect_tiles * ker.nr() * ker.mr() * 25000;
                 /*
                 eprintln!(
-                    " {} {}x{} cost: {} tiles: {} indirect_tiles: {} score: {}",
-                    ker.kernel_name(),
-                    ker.mr(),
-                    ker.nr(),
-                    cost,
-                    tiles,
-                    indirect_tiles,
-                    score,
+                " {} {}x{} cost: {} tiles: {} indirect_tiles: {} score: {}",
+                ker.kernel_name(),
+                ker.mr(),
+                ker.nr(),
+                cost,
+                tiles,
+                indirect_tiles,
+                score,
                 );
                 */
                 score
