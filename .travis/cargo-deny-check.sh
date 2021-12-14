b878597808e9015081fe63ce808c5ca85520f191
Mathieu Poumeyrol <kali@users.noreply.github.com>
2021-12-14 16:33:44 +0100

    More cargo deny fixes (#600)
    * isolate deny script
    
    * allow multiple itao

---
+++ .travis/cargo-deny-check.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -e cargo-deny ]
+then
+    CARGO_DENY=`pwd`/cargo-deny
+else
+    CARGO_DENY="cargo deny"
+fi
+
+for crate in onnx pulse kaldi tensorflow
+do
+    echo $crate:
+    (cd $crate ; $CARGO_DENY check)
+done
