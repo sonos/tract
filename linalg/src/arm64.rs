6bff4ce2d01f2384e7eceeff98106cce473805f9
Mathieu Poumeyrol <kali@zoy.org>
2022-01-21 14:19:57 +0100

    add models for a55, a72, a73

--- linalg/src/arm64.rs
+++ linalg/src/arm64.rs
@@ -1,5 +1,8 @@
 mod arm64simd;
 mod cortex_a53;
+mod cortex_a55;
+mod cortex_a72;
+mod cortex_a73;
 pub use arm64simd::*;
 
 use crate::Ops;
@@ -35,15 +38,28 @@ fn max_cpuid() -> std::io::Result<String> {
 #[derive(Debug)]
 enum Kind {
     Generic,
-    CortexA53Like,
+    CortexA53,
+    CortexA55,
+    CortexA72,
+    CortexA73,
+    CortexA75,
 }
 
 impl Kind {
     fn choose() -> Kind {
         let kind = if let Ok(kind) = std::env::var("TRACT_CPU_AARCH64_KIND") {
             log::info!("CPU kind forced with TRACT_CPU_AARCH64_KIND: {}", kind);
-            if kind.to_lowercase().contains("a53") {
-                Kind::CortexA53Like
+            let kind = kind.to_lowercase();
+            if kind.contains("a53") {
+                Kind::CortexA53
+            } else if kind.contains("a55") {
+                Kind::CortexA55
+            } else if kind.contains("a72") {
+                Kind::CortexA72
+            } else if kind.contains("a73") {
+                Kind::CortexA73
+            } else if kind.contains("a75") {
+                Kind::CortexA75
             } else {
                 Kind::Generic
             }
@@ -56,10 +72,13 @@ impl Kind {
                 log::info!("CPU part auto detected: {}", part);
                 part
             };
-            if [PART_A55, PART_A53].contains(&&*part) {
-                Kind::CortexA53Like
-            } else {
-                Kind::Generic
+            match &*part {
+                PART_A53 => Kind::CortexA53,
+                PART_A55 => Kind::CortexA55,
+                PART_A72 => Kind::CortexA72,
+                PART_A73 => Kind::CortexA73,
+                PART_A75 => Kind::CortexA75,
+                _ => Kind::Generic,
             }
         };
         log::info!("CPU optimisation: {:?}", kind);
@@ -77,13 +96,13 @@ pub fn plug(ops: &mut Ops) {
         MatMatMulF32x16x4::mmm(),
     ] {
         ops.mmm_f32_impls.push((mm, None));
-    } 
+    }
     match *KIND {
-        Kind::CortexA53Like => {
+        Kind::CortexA53 => {
             ops.mmv_f32 =
                 Box::new(|_, _| Box::new(MatMatMulImpl::<MatMatMulF32x64x1A53, f32>::new()));
         }
-        Kind::Generic => {
+        _ => {
             ops.mmv_f32 =
                 Box::new(
                     |_, _| Box::new(MatMatMulImpl::<arm64simd::MatMatMulF32x64x1, f32>::new()),
@@ -94,5 +113,11 @@ pub fn plug(ops: &mut Ops) {
     ops.qmmv_i32 = Box::new(|_, _| Box::new(MatMatMulImpl::<MatMatMulI32x64x1, i32>::new()));
     ops.sigmoid_f32 = Box::new(|| Box::new(ElementWiseImpl::<SigmoidF32x4n, f32>::new()));
     ops.tanh_f32 = Box::new(|| Box::new(ElementWiseImpl::<TanhF32x4n, f32>::new()));
-    ops.set_cost_models(cortex_a53::models());
+    match *KIND {
+        Kind::CortexA53 => ops.set_cost_models(cortex_a53::models()),
+        Kind::CortexA55 => ops.set_cost_models(cortex_a55::models()),
+        Kind::CortexA72 => ops.set_cost_models(cortex_a72::models()),
+        Kind::CortexA73 => ops.set_cost_models(cortex_a72::models()),
+        _ => ops.set_cost_models(cortex_a53::models()),
+    }
 }
