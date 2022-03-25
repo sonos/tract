405c36e96bb9e78e5961af2601dc169bf3a12294
Mathieu Poumeyrol <kali@zoy.org>
2022-03-25 12:02:45 +0100

    re-disable threshold to figure out what is wrong

--- linalg/src/frame/mmm/cost_model.rs
+++ linalg/src/frame/mmm/cost_model.rs
@@ -77,10 +77,10 @@ impl<'a> CostModel<'a> {
         n: Option<usize>,
     ) -> Box<dyn MatMatMul> {
         if let (Some(m), Some(k), Some(n)) = (m, k, n) {
-            if (m as f32) * (k as f32) * (n as f32) < self.big_product_mkn_threshold {
+      //      if (m as f32) * (k as f32) * (n as f32) < self.big_product_mkn_threshold {
                 let choice = self.predict(m, k, n);
                 return impls.iter().find(|k| k.kernel_name() == choice).unwrap().clone();
-            }
+      //}
         }
         return impls
             .iter()
