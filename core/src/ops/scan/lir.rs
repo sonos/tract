76b26369a7e956e61c948008b785a8535a7ba95b
Mathieu Poumeyrol <kali@zoy.org>
2022-03-25 18:12:34 +0100

    fix serialization for backwards scans

--- core/src/ops/scan/lir.rs
+++ core/src/ops/scan/lir.rs
@@ -234,10 +234,10 @@ impl OpState for State {
                 .filter_map(|x| x)
                 .collect();
 
-            trace!("iter_inputs: {:?}", iter_inputs);
+            trace!("iter_inputs #{}: {:?}", i, iter_inputs);
             let iter_outputs =
                 mutable.model_state.run(iter_inputs).with_context(|| "Evaluating inner body")?;
-            trace!("iter_outputs: {:?}", iter_outputs);
+            trace!("iter_outputs #{}: {:?}", i, iter_outputs);
 
             for (v, mapping) in iter_outputs.into_iter().zip(&op.output_mapping) {
                 if let Some(slot) = mapping.full_slot {
