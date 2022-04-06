d1c45ad6188c3021c081531d02c5da0d7e65271e
gith <julien.balian@sonos.com>
2022-04-06 16:43:13 +0200

    fix: since 1.0.1 nnef spec tan, arc(tan|sin|cos) and hyperbolic equivalent are part of NNEF spec

--- nnef/src/ops/core.rs
+++ nnef/src/ops/core.rs
@@ -14,16 +14,6 @@ mod scatter;
 mod source;
 
 pub fn register(registry: &mut Registry) {
-    registry.register_unit_element_wise("tract_core_tan", &ops::math::Tan {});
-    registry.register_unit_element_wise("tract_core_acos", &ops::math::Acos {});
-    registry.register_unit_element_wise("tract_core_asin", &ops::math::Asin {});
-    registry.register_unit_element_wise("tract_core_atan", &ops::math::Atan {});
-    registry.register_unit_element_wise("tract_core_cosh", &ops::math::Cosh {});
-    registry.register_unit_element_wise("tract_core_sinh", &ops::math::Sinh {});
-    registry.register_unit_element_wise("tract_core_acosh", &ops::math::Acosh {});
-    registry.register_unit_element_wise("tract_core_asinh", &ops::math::Asinh {});
-    registry.register_unit_element_wise("tract_core_atanh", &ops::math::Atanh {});
-
     registry.register_unit_element_wise("tract_core_round_even", &ops::math::RoundHalfToEven {});
 
     registry.register_binary("tract_core_xor", &ops::logic::Xor {});
