2e2f473b427ec9bcd83dc0bb556d186fc5b7edf3
Julien Balian <julien.balian@sonos.com>
2022-04-11 17:03:23 +0200

    fix: missing serialization update for repeat "s"

--- nnef/src/ops/nnef/ser.rs
+++ nnef/src/ops/nnef/ser.rs
@@ -71,7 +71,7 @@ pub fn tile(
     op: &ops::array::Tile,
 ) -> TractResult<Option<Arc<RValue>>> {
     let wire = ast.mapping[&node.inputs[0]].clone();
-    Ok(Some(invocation("tile", &[wire], &[("repeat", ints(&op.multipliers))])))
+    Ok(Some(invocation("tile", &[wire], &[("repeats", ints(&op.multipliers))])))
 }
 
 pub fn pad(
