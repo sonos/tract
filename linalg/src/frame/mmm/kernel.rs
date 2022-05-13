6646b35c9c4befb36f0836cc008f17de661c6998
Mathieu Poumeyrol <kali@zoy.org>
2022-05-13 16:28:20 +0200

    fix shift right as arithmetic shift

--- linalg/src/frame/mmm/kernel.rs
+++ linalg/src/frame/mmm/kernel.rs
@@ -52,7 +52,14 @@ macro_rules! test_mmm_kernel_i32 {
                 mmm_kernel_tests!($cond, $k, i8, i8, i8, i32);
                 mmm_kernel_fuse_tests!($cond, $k, i8, i32);
                 mmm_frame_tests!($cond, $k, i8, i8, i8, i32);
+            }
+            #[cfg(test)]
+            mod [<test_qi8_ $k>] {
                 qmmm_kernel_fuse_tests!($cond, $k, i8, i8, i8, i32);
+            }
+            #[cfg(test)]
+            mod [<test_qi32_ $k>] {
+                qmmm_kernel_fuse_tests!($cond, $k, i8, i8, i32, i32);
             }
         }
     };
