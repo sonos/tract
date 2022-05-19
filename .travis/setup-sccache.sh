83038f00e192889838fe7819915aa372fbafb2b2
Mathieu Poumeyrol <kali@zoy.org>
2022-05-19 16:48:44 +0200

    shell rules

---
+++ .travis/setup-sccache.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo $HOME
+if [ `uname` = "Linux" ]
+then
+  SCCACHE_FILE=sccache-$SCCACHE_VERSION-x86_64-unknown-linux-musl
+else
+  SCCACHE_FILE=sccache-$SCCACHE_VERSION-x86_64-apple-darwin
+fi
+mkdir -p $SCCACHE_DIR
+mkdir -p $HOME/.local/bin
+curl -L "$LINK/$SCCACHE_VERSION/$SCCACHE_FILE.tar.gz" | tar xz
+mv -f $SCCACHE_FILE/sccache $HOME/.local/bin/sccache
+chmod +x $HOME/.local/bin/sccache
+echo "$HOME/.local/bin" >> $GITHUB_PATH
+
