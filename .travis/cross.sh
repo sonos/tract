55c6a03442d6712c9d46ccfd0d0d6fc18b32a9b9
Mathieu Poumeyrol <kali@zoy.org>
2022-05-23 15:20:00 +0200

    typo

--- .travis/cross.sh
+++ .travis/cross.sh
@@ -103,7 +103,7 @@ case "$PLATFORM" in
             "aarch64-unknown-linux-gnu")
                 export ARCH=aarch64
                 export QEMU_ARCH=aarch64
-                export LIBC_ARCH=atm64
+                export LIBC_ARCH=arm64
                 export RUSTC_TRIPLE=$ARCH-unknown-linux-gnu
                 export DEBIAN_TRIPLE=$ARCH-linux-gnu
                 ;;
