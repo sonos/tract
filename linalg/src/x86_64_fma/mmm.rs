e6d4c8f5d870ea9d26036757ec407082348f7e02
Tom Solberg <me@sbg.dev>
2022-06-09 13:22:02 +0200

    fix test 40x2

--- linalg/src/x86_64_fma/mmm.rs
+++ linalg/src/x86_64_fma/mmm.rs
@@ -5,7 +5,7 @@ MMMKernel!(f32, fma_mmm_f32_16x6; 16, 6; 32, 4; 0, 0; no_prefetch, is_x86_featur
 MMMKernel!(f32, fma_mmm_f32_16x5; 16, 5; 32, 4; 0, 0; no_prefetch, is_x86_feature_detected!("fma"));
 MMMKernel!(f32, fma_mmm_f32_24x4; 24, 4; 32, 4; 0, 0; no_prefetch, is_x86_feature_detected!("fma"));
 MMMKernel!(f32, fma_mmm_f32_32x3; 32, 3; 32, 4; 0, 0; no_prefetch, is_x86_feature_detected!("fma"));
-MMMKernel!(f32, fma_mmm_f32_40x2; 32, 3; 32, 4; 0, 0; no_prefetch, is_x86_feature_detected!("fma"));
+MMMKernel!(f32, fma_mmm_f32_40x2; 40, 2; 32, 4; 0, 0; no_prefetch, is_x86_feature_detected!("fma"));
 MMMKernel!(f32, fma_mmm_f32_64x1; 64, 1; 32, 4; 0, 0; no_prefetch, is_x86_feature_detected!("fma"));
 
 MMMKernel!(i32, avx2_mmm_i32_8x8; 8, 8; 32, 4; 0, 0; no_prefetch, is_x86_feature_detected!("avx2"));
