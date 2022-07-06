d1bc4390610d13b47bbec82179f85912b557ebb3
Mathieu Poumeyrol <kali@zoy.org>
2022-07-06 09:52:20 +0200

    new tanh and sigmoid (intel + generic)

--- linalg/src/generic/tanh.rs
+++ linalg/src/generic/tanh.rs
@@ -1,25 +1,30 @@
 use crate::frame::element_wise::ElementWiseKer;
 
-const LOW: f32 = -9.0;
-const HIGH: f32 = 9.0;
-const ALPHA_13: f32 = -2.76076847742355e-16;
-const ALPHA_11: f32 = 2.00018790482477e-13;
-const ALPHA_9: f32 = -8.60467152213735e-11;
-const ALPHA_7: f32 = 5.12229709037114e-08;
-const ALPHA_5: f32 = 1.48572235717979e-05;
-const ALPHA_3: f32 = 6.37261928875436e-04;
-const ALPHA_1: f32 = 4.89352455891786e-03;
-const BETA_6: f32 = 1.19825839466702e-06;
-const BETA_4: f32 = 1.18534705686654e-04;
-const BETA_2: f32 = 2.26843463243900e-03;
-const BETA_0: f32 = 4.89352518554385e-03;
+/* (x (1. + x^2 (0.129499 + x^2 (0.00295387 + x^2 (9.57366*10^-6 + x^2 (-1.37187*10^-8 + 2.07234*10^-11 x^2))))))
+ * ---------------------------------------------------------------------------------------------------------------
+ *          (1. + x^2 (0.462832 + x^2 (0.0238983 + 0.000232702 x^2)))
+ */
+
+const LOW: f32 = -8.5;
+const HIGH: f32 = 8.5;
+
+const ALPHA_11: f32 = 2.07234e-11;
+const ALPHA_9: f32 = -1.37187e-8;
+const ALPHA_7: f32 = 9.57366e-6;
+const ALPHA_5: f32 = 0.00295387;
+const ALPHA_3: f32 = 0.129499;
+const ALPHA_1: f32 = 1.0;
+const BETA_6: f32 = 0.000232702;
+const BETA_4: f32 = 0.0238983;
+const BETA_2: f32 = 0.462832;
+const BETA_0: f32 = 1.0;
 
 pub fn stanh(x: f32) -> f32 {
     let x = x.max(LOW).min(HIGH);
 
     let x2 = x * x;
 
-    let p = x2 * ALPHA_13 + ALPHA_11;
+    let p = ALPHA_11;
     let p = x2 * p + ALPHA_9;
     let p = x2 * p + ALPHA_7;
     let p = x2 * p + ALPHA_5;
@@ -27,7 +32,8 @@ pub fn stanh(x: f32) -> f32 {
     let p = x2 * p + ALPHA_1;
     let p = p * x;
 
-    let q = x2 * BETA_6 + BETA_4;
+    let q = BETA_6;
+    let q = x2 * q + BETA_4;
     let q = x2 * q + BETA_2;
     let q = x2 * q + BETA_0;
 
