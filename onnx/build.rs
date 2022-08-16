4f36bb9323f93c313a7401406de31ab6049a258d
Mathieu Poumeyrol <kali@zoy.org>
2022-08-16 19:45:03 +0200

    use protobuf-src

--- onnx/build.rs
+++ onnx/build.rs
@@ -3,6 +3,7 @@ use std::{env, fs, path};
 fn main() {
     let workdir = path::PathBuf::from(env::var("OUT_DIR").unwrap()).join("prost");
     let _ = fs::create_dir_all(&workdir);
+    std::env::set_var("PROTOC", protobuf_src::protoc());
     prost_build::Config::new()
         .out_dir(workdir)
         .compile_protos(&["protos/onnx/onnx.proto3"], &["protos/"])
