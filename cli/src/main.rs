81a8c5e8e2bf6073508a1e9490b387d901e3c500
Mathieu Poumeyrol <kali@users.noreply.github.com>
2022-09-07 20:33:48 +0200

    fix typo

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -102,7 +102,7 @@ fn main() -> tract_core::anyhow::Result<()> {
         .arg(arg!(--"onnx-test-data-set" [data_set] "Use onnx-test data-set as input (expect test_data_set_N dir with input_X.pb, etc. inside)"))
         .arg(arg!(--"onnx-ignore-output-shapes" "Ignore output shapes from model (workaround for pytorch export bug with mask axes)"))
 
-        .arg(arg!(--"input-node" [node] ... "Oveide input nodes names (auto-detects otherwise)."))
+        .arg(arg!(--"input-node" [node] ... "Override input nodes names (auto-detects otherwise)."))
         .arg(arg!(--"output-node" [node] ... "Override output nodes name (auto-detects otherwise)."))
         .arg(arg!(--"label-wires" "Propagate node labels to wires"))
 
