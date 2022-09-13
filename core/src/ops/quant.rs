5a0b2a20044be6de92fb0b30b1ccf376c253b825
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@sonos.com>
2022-09-13 14:13:56 +0200

    Remove println

--- core/src/ops/quant.rs
+++ core/src/ops/quant.rs
@@ -366,8 +366,6 @@ where
     let new_product = scaler.mult.map(|it| (it >> 16) as f32 * 2f32.powi(-15)).unwrap_or(1.0)
         * 2f32.powi(-scaler.shift as i32)
         * b;
-    let real_product = b * a;
-    println!("real: {}, new: {}, a: {}, scale: {}", real_product, new_product, b, a);
     round_ties_to_even(new_product).as_()
 }
 
