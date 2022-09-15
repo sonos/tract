b14140011d3ec4916178c8a37ee1b35fea0c9c16
Mathieu Poumeyrol <kali@zoy.org>
2022-09-15 15:50:16 +0200

    some einsum translation to matmul

---
+++ onnx-opl/src/einsum/mod.rs
@@ -0,0 +1,162 @@
+use tract_nnef::internal::*;
+use tract_nnef::prelude::tract_itertools::Itertools;
+use tract_nnef::tract_ndarray::{Axis, Dimension};
+use tract_nnef::tract_num_traits::{One, Zero};
+
+mod expr;
+use expr::Expr;
+mod to_matmul;
+
+#[derive(Debug, Clone, Hash)]
+pub struct EinSum {
+    pub expr: Expr,
+}
+
+impl_dyn_hash!(EinSum);
+
+impl Op for EinSum {
+    fn name(&self) -> Cow<str> {
+        "EinSum".into()
+    }
+
+    fn info(&self) -> TractResult<Vec<String>> {
+        Ok(vec![format!("{}", self.expr)])
+    }
+
+    op_onnx!();
+    op_as_typed_op!();
+}
+
+impl EinSum {
+    fn output_shape<D: DimLike>(&self, inputs: &[&[D]]) -> TVec<D> {
+        self.expr
+            .index
+            .iter()
+            .sorted_by_key(|axis| axis.result.unwrap())
+            .map(|axis| {
+                axis.inputs
+                    .iter()
+                    .enumerate()
+                    .flat_map(|(input_id, positions)| {
+                        positions.iter().map(move |p| inputs[input_id][*p].clone())
+                    })
+                    .find(|x| x != &1.into())
+                    .unwrap_or_else(|| 1.into())
+            })
+            .collect()
+    }
+
+    fn eval_t<T: Datum + Zero + One>(
+        &self,
+        inputs: TVec<Arc<Tensor>>,
+    ) -> TractResult<TVec<Arc<Tensor>>> {
+        let shapes: TVec<_> = inputs.iter().map(|t| t.shape()).collect();
+        let output_shape = self.output_shape(&shapes);
+        let inputs: TVec<tract_ndarray::ArrayViewD<T>> =
+            inputs.iter().map(|t| t.to_array_view::<T>()).collect::<TractResult<_>>()?;
+        let summing_shape: TVec<usize> = self
+            .expr
+            .sum
+            .iter()
+            .map(|axis| {
+                axis.inputs
+                    .iter()
+                    .enumerate()
+                    .find_map(|(input_id, positions)| {
+                        if positions.len() > 0 {
+                            Some(inputs[input_id].shape()[positions[0]])
+                        } else {
+                            None
+                        }
+                    })
+                    .unwrap()
+            })
+            .collect();
+        let output = tract_ndarray::ArrayD::<T>::from_shape_fn(&*output_shape, |coords| {
+            let coords = coords.as_array_view();
+            let mut views = inputs.clone();
+            for (axis, x) in
+                self.expr.index.iter().sorted_by_key(|axis| axis.result.unwrap()).zip(coords)
+            {
+                for (input_id, input_axis_positions) in axis.inputs.iter().enumerate() {
+                    for position in input_axis_positions {
+                        let x = if views[input_id].shape()[*position] == 1 { 0 } else { *x };
+                        views[input_id]
+                            .slice_axis_inplace(tract_ndarray::Axis(*position), (x..=x).into());
+                    }
+                }
+            }
+            let mut sum: T = T::zero();
+            for sum_coords in tract_ndarray::indices(&*summing_shape) {
+                let mut views = views.clone();
+                let sum_coords = sum_coords.as_array_view();
+                for (axis, x) in self.expr.sum.iter().zip(&sum_coords) {
+                    for (input_id, input_axis_positions) in axis.inputs.iter().enumerate() {
+                        for position in input_axis_positions {
+                            views[input_id].slice_axis_inplace(Axis(*position), (*x..=*x).into())
+                        }
+                    }
+                }
+                let mut product = T::one();
+                for v in &views {
+                    debug_assert_eq!(v.len(), 1);
+                    product = product * v.iter().next().unwrap().clone();
+                }
+                sum = sum + product;
+            }
+            sum
+        });
+        Ok(tvec!(output.into_arc_tensor()))
+    }
+}
+
+impl EvalOp for EinSum {
+    fn is_stateless(&self) -> bool {
+        true
+    }
+
+    fn eval(&self, inputs: TVec<Arc<Tensor>>) -> TractResult<TVec<Arc<Tensor>>> {
+        dispatch_numbers!(Self::eval_t(inputs[0].datum_type())(self, inputs))
+    }
+}
+
+impl TypedOp for EinSum {
+    fn output_facts(&self, inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
+        let shapes: TVec<&[TDim]> = inputs.iter().map(|t| &*t.shape).collect();
+        Ok(tvec!(TypedFact::dt_shape(inputs[0].datum_type, self.output_shape(&*shapes))))
+    }
+
+    fn declutter(
+        &self,
+        model: &TypedModel,
+        node: &TypedNode,
+    ) -> TractResult<Option<TypedModelPatch>> {
+        to_matmul::declutter(self, model, node)
+    }
+
+    as_op!();
+}
+
+pub fn parameters() -> Vec<Parameter> {
+    vec![TypeName::Scalar.tensor().array().named("inputs"), TypeName::String.named("expr")]
+}
+
+pub fn dump(ast: &mut IntoAst, node: &TypedNode) -> TractResult<Option<Arc<RValue>>> {
+    let einsum = node.op_as::<EinSum>().unwrap();
+    let inputs = node.inputs.iter().map(|i| (*ast.mapping[i]).clone()).collect();
+    Ok(Some(invocation(
+        "tract_onnx_einsum",
+        &[Arc::new(RValue::Array(inputs))],
+        &[("expr", string(einsum.expr.to_string()))],
+    )))
+}
+
+pub fn load(
+    builder: &mut ModelBuilder,
+    invocation: &ResolvedInvocation,
+) -> TractResult<TVec<OutletId>> {
+    let expr = invocation.named_arg_as::<String>(builder, "expr")?.parse::<Expr>()?;
+    let einsum = EinSum { expr };
+    let inputs: TVec<OutletId> = invocation.named_arg_as(builder, "inputs")?;
+    builder.wire(einsum, &inputs)
+}
---
+++ onnx-opl/src/einsum/to_matmul.rs
@@ -0,0 +1,149 @@
+use tract_nnef::internal::*;
+use tract_nnef::tract_core::ops::matmul::MatMul;
+
+pub fn declutter(
+    op: &super::EinSum,
+    model: &TypedModel,
+    node: &TypedNode,
+) -> TractResult<Option<TypedModelPatch>> {
+    // 2 input, exactly one axis sum
+    // FIXME: no projection (axis present more than once in one input)
+    if op.expr.n_inputs() != 2 && op.expr.sum.len() != 1 {
+        return Ok(None);
+    }
+    let k_axis = op.expr.sum[0].clone();
+    assert!(k_axis.result.is_none());
+    assert!(k_axis.inputs.iter().all(|pos| pos.len() == 1));
+    let inputs = model.node_input_facts(node.id)?;
+    // summing axis is either last or last before last
+    if k_axis.inputs[0][0] + 2 < inputs[0].rank() || k_axis.inputs[1][0] + 2 < inputs[1].rank() {
+        trace!("Not decluttering, k_axis");
+        return Ok(None);
+    }
+    eprintln!("k: {:?}", k_axis);
+    let a_rank = inputs[0].rank();
+    let b_rank = inputs[1].rank();
+    let c_rank = op.expr.output_rank();
+    let m_axis = op
+        .expr
+        .input_axis(0, if k_axis.inputs[0][0] == a_rank - 1 { a_rank - 2 } else { a_rank - 1 })
+        .unwrap();
+    let n_axis = op
+        .expr
+        .input_axis(1, if k_axis.inputs[1][0] == b_rank - 1 { b_rank - 2 } else { b_rank - 1 })
+        .unwrap();
+    if m_axis.result != Some(c_rank - 2) && m_axis.result != Some(c_rank - 1) {
+        trace!("Not decluttering, m_axis");
+        return Ok(None);
+    }
+    if n_axis.result != Some(c_rank - 2) && n_axis.result != Some(c_rank - 1) {
+        trace!("Not decluttering, n_axis");
+        return Ok(None);
+    }
+    /*
+    // add broadcasting axes if required
+    for axis in &op.expr.index {
+        if axis == n_axis || axis == m_axis {
+            continue
+        }
+        if axis.inputs[0].len() == 0 {
+            eprintln!("missing axis on input 0: {:?}", axis);
+            let mut new_expr = op.expr.clone();
+            new_expr.insert_input_axis(axis.repr, 0, 0);
+            eprintln!("{}", new_expr);
+        }
+    }
+    */
+    let op = MatMul {
+        a_trans: k_axis.inputs[0][0] == a_rank - 2,
+        b_trans: k_axis.inputs[1][0] == b_rank - 1,
+        c_trans: m_axis.result == Some(c_rank - 1),
+    };
+    Ok(Some(TypedModelPatch::replace_single_op(model, node, &node.inputs, op)?))
+}
+
+#[cfg(test)]
+mod test {
+    use super::super::EinSum;
+    use super::*;
+
+    struct EinSumProblem {
+        a: Tensor,
+        b: Tensor,
+        expr: String,
+    }
+
+    impl EinSumProblem {
+        fn new(a: Tensor, b: Tensor, expr: &str) -> EinSumProblem {
+            EinSumProblem { a, b, expr: expr.to_string() }
+        }
+
+        fn check(&self) {
+            let _ = env_logger::Builder::from_env("TRACT_LOG").try_init();
+            let mut model = TypedModel::default();
+            let a = model.add_source("a", TypedFact::from(&self.a).without_value()).unwrap();
+            let b = model.add_source("b", TypedFact::from(&self.b).without_value()).unwrap();
+            let c = model
+                .wire_node("c,", EinSum { expr: self.expr.parse().unwrap() }, &[a, b])
+                .unwrap();
+            model.set_output_outlets(&*c).unwrap();
+            let expect = model
+                .clone()
+                .into_runnable()
+                .unwrap()
+                .run(tvec!(self.a.clone(), self.b.clone()))
+                .unwrap();
+            let model = model.into_decluttered().unwrap();
+            assert!(model.nodes.iter().all(|n| !n.op_is::<EinSum>()));
+            let found =
+                model.into_runnable().unwrap().run(tvec!(self.a.clone(), self.b.clone())).unwrap();
+            assert_eq!(found, expect);
+        }
+    }
+
+    fn t(shape: &[usize]) -> Tensor {
+        let mut t = Tensor::zero::<f32>(shape).unwrap();
+        t.as_slice_mut::<f32>().unwrap().iter_mut().enumerate().for_each(|(i, x)| *x = i as f32);
+        t
+    }
+
+    #[test]
+    fn simple() {
+        EinSumProblem::new(t(&[2, 3]), t(&[3, 4]), "mk,kn->mn").check()
+    }
+
+    #[test]
+    fn a_trans() {
+        EinSumProblem::new(t(&[3, 2]), t(&[3, 4]), "km,kn->mn").check()
+    }
+
+    #[test]
+    fn b_trans() {
+        EinSumProblem::new(t(&[2, 3]), t(&[4, 3]), "mk,nk->mn").check()
+    }
+
+    #[test]
+    fn c_trans() {
+        EinSumProblem::new(t(&[2, 3]), t(&[3, 4]), "mk,kn->nm").check()
+    }
+
+    #[test]
+    fn prefix() {
+        EinSumProblem::new(t(&[5, 3, 2]), t(&[5, 3, 4]), "akm,akn->amn").check()
+    }
+
+    #[test]
+    fn prefix_broadcast_a() {
+        EinSumProblem::new(t(&[1, 3, 2]), t(&[5, 3, 4]), "akm,akn->amn").check()
+    }
+
+    #[test]
+    fn prefix_broadcast_b() {
+        EinSumProblem::new(t(&[5, 3, 2]), t(&[1, 3, 4]), "akm,akn->amn").check()
+    }
+
+    #[test]
+    fn broadcast_a() {
+        EinSumProblem::new(t(&[2, 3]), t(&[5, 3, 4]), "mk,akn->anm").check()
+    }
+}
