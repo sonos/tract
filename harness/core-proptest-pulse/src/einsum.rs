0744199d7541849f62c91f517d0d8b1696ea6dea
Hendrik Schroeter <h.schroeter@pm.me>
2022-10-24 14:00:06 +0200

    Add failing testcase for pulsed einsum

---
+++ harness/core-proptest-pulse/src/einsum.rs
@@ -0,0 +1,42 @@
+use tract_hir::internal::*;
+use tract_onnx_opl::einsum::*;
+
+use super::*;
+
+#[test]
+fn einsum_bmm() {
+    let mut model = TypedModel::default();
+    let s = 5;
+    let x = model.add_source("x", f32::fact(dims!(s, 8, 2))).unwrap();
+    let w = model.add_const("w", Tensor::zero::<f32>(&[8, 2, 4]).unwrap()).unwrap();
+
+    let expr = "sij,ijk->sik".parse::<Expr>().unwrap();
+    let einsum = EinSum { expr };
+
+    let einsum = model.wire_node("einsum", einsum, &[x, w]).unwrap();
+    model.set_output_outlets(&einsum).unwrap();
+    model.declutter().unwrap();
+
+    let mut input = Tensor::zero::<f32>(&[5, 8, 2]).unwrap();
+    input.as_slice_mut::<f32>().unwrap().iter_mut().enumerate().for_each(|(ix, x)| *x = ix as f32);
+    proptest_regular_against_pulse(model, 1, input.into_array().unwrap(), 0).unwrap()
+}
+
+#[test]
+fn einsum_pulsedmm() {
+    let mut model = TypedModel::default();
+    let s = stream_symbol();
+    let x = model.add_source("x", f32::fact(dims!(s, 8, 2))).unwrap();
+    let w = model.add_const("w", Tensor::zero::<f32>(&[8, 2, 4]).unwrap()).unwrap();
+
+    let expr = "sij,ijk->sik".parse::<Expr>().unwrap();
+    let einsum = EinSum { expr };
+
+    let einsum = model.wire_node("einsum", einsum, &[x, w]).unwrap();
+    model.set_output_outlets(&einsum).unwrap();
+    model.declutter().unwrap();
+
+    let mut input = Tensor::zero::<f32>(&[5, 8, 2]).unwrap();
+    input.as_slice_mut::<f32>().unwrap().iter_mut().enumerate().for_each(|(ix, x)| *x = ix as f32);
+    proptest_regular_against_pulse(model, 1, input.into_array().unwrap(), 0).unwrap()
+}
