6419e30158994f79c545f8f4805392819915feef
Mathieu Poumeyrol <kali@zoy.org>
2022-11-13 11:40:50 +0100

    bit of cleanup

--- examples/onnx-mobilenet-v2/ci.sh
+++ examples/onnx-mobilenet-v2/ci.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-wget -q https://github.com/onnx/models/raw/main/vision/classification/mobilenet/model/mobilenetv2-7.onnx
+wget -q https://github.com/onnx/models/raw/main/vision/classification/mobilenet/model/mobilenetv2-7.onnx -O mobilenetv2-7.onnx
 cargo run
