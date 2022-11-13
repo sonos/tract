256a4124c75d0978864b7fab547f095a1864a659
Mathieu Poumeyrol <kali@zoy.org>
2022-11-13 11:40:50 +0100

    bit of cleanup

--- examples/jupyter-keras-tract-tf2/ci.sh
+++ examples/jupyter-keras-tract-tf2/ci.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 
--- examples/nnef-mobilenet-v2/ci.sh
+++ examples/nnef-mobilenet-v2/ci.sh
@@ -2,5 +2,6 @@
 
 set -ex
 
-wget -q https://sfo2.digitaloceanspaces.com/nnef-public/mobilenet_v2_1.0.onnx.nnef.tgz
+wget -q https://sfo2.digitaloceanspaces.com/nnef-public/mobilenet_v2_1.0.onnx.nnef.tgz -O mobilenet_v2_1.0.onnx.nnef.tgz
+
 cargo run
