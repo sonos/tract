b618e4da600cc4788f7981fb8097174ed082fc72
Mathieu Poumeyrol <kali@zoy.org>
2022-11-22 09:36:41 +0100

    support dt from numpy

--- ffi/py/tract/__init__.py
+++ ffi/py/tract/__init__.py
@@ -20,21 +20,34 @@ lib.tract_get_last_error.restype = c_char_p
 TRACT_DATUM_TYPE_BOOL = 0x01
 TRACT_DATUM_TYPE_U8 = 0x11
 TRACT_DATUM_TYPE_U16 = 0x12
-TRACT_DATUM_TYPE_U32 = 0x13
-TRACT_DATUM_TYPE_U64 = 0x14
+TRACT_DATUM_TYPE_U32 = 0x14
+TRACT_DATUM_TYPE_U64 = 0x18
 TRACT_DATUM_TYPE_I8 = 0x21
 TRACT_DATUM_TYPE_I16 = 0x22
-TRACT_DATUM_TYPE_I32 = 0x23
-TRACT_DATUM_TYPE_I64 = 0x24
+TRACT_DATUM_TYPE_I32 = 0x24
+TRACT_DATUM_TYPE_I64 = 0x28
 TRACT_DATUM_TYPE_F16 = 0x32
-TRACT_DATUM_TYPE_F32 = 0x33
-TRACT_DATUM_TYPE_F64 = 0x34
+TRACT_DATUM_TYPE_F32 = 0x34
+TRACT_DATUM_TYPE_F64 = 0x38
 TRACT_DATUM_TYPE_COMPLEX_I16 = 0x42
-TRACT_DATUM_TYPE_COMPLEX_I32 = 0x43
-TRACT_DATUM_TYPE_COMPLEX_I64 = 0x44
+TRACT_DATUM_TYPE_COMPLEX_I32 = 0x44
+TRACT_DATUM_TYPE_COMPLEX_I64 = 0x48
 TRACT_DATUM_TYPE_COMPLEX_F16 = 0x52
-TRACT_DATUM_TYPE_COMPLEX_F32 = 0x53
-TRACT_DATUM_TYPE_COMPLEX_F64 = 0x54
+TRACT_DATUM_TYPE_COMPLEX_F32 = 0x54
+TRACT_DATUM_TYPE_COMPLEX_F64 = 0x58
+
+def dt_numpy_to_tract(dt):
+    if dt.kind == 'b':
+        return TRACT_DATUM_TYPE_BOOL
+    if dt.kind == 'u':
+        return 0x1 + dt.item_size
+    if dt.kind == 'i':
+        return 0x2 + dt.item_size
+    if dt.kind == 'f':
+        return 0x3 + dt.item_size
+    if dt.kind == 'c':
+        return 0x5 + dt.item_size / 2
+    raise TractError("Unsupported Numpy dtype: " + dt)
 
 def version():
     return str(lib.tract_version(), "utf-8")
@@ -155,7 +168,8 @@ class Value:
         shape = shape_t()
         for ix in range(0, array.ndim):
             shape[ix] = array.shape[ix]
-        check(lib.tract_value_create(TRACT_DATUM_TYPE_F32, c_size_t(array.ndim), shape, data, byref(ptr)))
+        dt = dt_numpy_to_tract(array.dtype)
+        check(lib.tract_value_create(dt, c_size_t(array.ndim), shape, data, byref(ptr)))
         return Value(ptr)
 
     def to_numpy(self):
