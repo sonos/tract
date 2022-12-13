402ab06904e11278d403261dce0e9d0c965722a5
Mathieu Poumeyrol <kali@zoy.org>
2022-12-13 17:58:58 +0100

    clippy stuff

--- core/src/ops/cnn/deconv/deconv_sum.rs
+++ core/src/ops/cnn/deconv/deconv_sum.rs
@@ -98,7 +98,7 @@ impl DeconvSum {
         let mut tensor = if let Some(b) = &self.bias {
             if output_shape.shape[0..output_shape.c_axis()].iter().all(|d| *d == 1) {
                 unsafe {
-                    let mut tensor = Tensor::uninitialized::<T>(&*output_shape.shape)?;
+                    let mut tensor = Tensor::uninitialized::<T>(&output_shape.shape)?;
                     let values = b.as_ptr::<T>()?;
                     let slice = tensor.as_ptr_mut::<T>()?;
                     let stride = *output_shape.c_stride();
@@ -111,7 +111,7 @@ impl DeconvSum {
                     tensor
                 }
             } else {
-                let mut tensor = Tensor::zero::<T>(&*output_shape.shape)?;
+                let mut tensor = Tensor::zero::<T>(&output_shape.shape)?;
                 let mut output = tensor.to_array_view_mut::<T>()?;
                 let mut bias_shape = tvec!(1; output_shape.rank());
                 bias_shape[output_shape.c_axis()] = b.len();
@@ -120,7 +120,7 @@ impl DeconvSum {
                 tensor
             }
         } else {
-            Tensor::zero::<T>(&*output_shape.shape)?
+            Tensor::zero::<T>(&output_shape.shape)?
         };
         let mut output = tensor.to_array_view_mut::<T>()?;
         let hw = *gemm.shape().last().unwrap();
