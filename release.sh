6783f21ca22d00f950faa81b331456874da5a0a6
Mathieu Poumeyrol <kali@zoy.org>
2022-12-18 14:07:46 +0100

    release 0.19.0-alpha.17

--- release.sh
+++ release.sh
@@ -1,50 +1,10 @@
 #!/bin/bash
 
-# From: https://gist.github.com/sj26/88e1c6584397bb7c13bd11108a579746
-
-# Retry a command up to a specific numer of times until it exits successfully,
-# with exponential back off.
-#
-#  $ retry 5 echo Hello
-#  Hello
-#
-#  $ retry 5 false
-#  Retry 1/5 exited 1, retrying in 1 seconds...
-#  Retry 2/5 exited 1, retrying in 2 seconds...
-#  Retry 3/5 exited 1, retrying in 4 seconds...
-#  Retry 4/5 exited 1, retrying in 8 seconds...
-#  Retry 5/5 exited 1, no more retries left.
-#  
-function retry {
-  local retries=$1
-  shift
-
-  local count=0
-  until "$@"; do
-    exit=$?
-    wait=$((2 ** $count))
-    count=$(($count + 1))
-    if [ $count -lt $retries ]; then
-      echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
-      sleep $wait
-    else
-      echo "Retry $count/$retries exited $exit, no more retries left."
-      return $exit
-    fi
-  done
-  return 0
-}
+cargo install tomato-toml
 
 CRATE=$1
 VERSION=$2
 CRATES="data linalg core nnef pulse-opl pulse hir tensorflow onnx-opl onnx kaldi libcli ffi cli"
-
-# if [ `uname` = "Darwin" ]
-# then
-#     SED=gsed
-# else
-#     SED=sed
-# fi
 
 if [ -z "$VERSION" ]
 then
@@ -64,19 +24,7 @@ then
     exit 0
 fi
 
-# set_version cargo-dinghy/Cargo.toml 0.3.0
-set_version() {
-    FILE=$1
-    VERSION=$2
-    tomato set package.version $VERSION $FILE
-}
-
-# for crate in $CRATES
-# do
-#     cargo update -p tract-$crate || true
-# done
-
-set_version $CRATE/Cargo.toml $VERSION
+tomato set package.version $VERSION $CRATE/Cargo.toml
 (cd $CRATE ; cargo publish --allow-dirty)
 
 for manifest in `find * -mindepth 1 -a -name Cargo.toml`
