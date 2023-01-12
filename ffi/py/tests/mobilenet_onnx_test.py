6735a727d21ff9b4729735dc92c67a8392f08818
Mathieu Poumeyrol <kali@zoy.org>
2023-01-12 17:57:42 +0100

    python/nnef test

--- ffi/py/tests/mobilenet_onnx_test.py
+++ ffi/py/tests/mobilenet_onnx_test.py
@@ -9,11 +9,29 @@ def setup_module(module):
             "https://github.com/onnx/models/raw/main/vision/classification/mobilenet/model/mobilenetv2-7.onnx",
             "mobilenetv2-7.onnx",
         )
+    if not path.exists(""):
+        urllib.request.urlretrieve(
+            "https://sfo2.digitaloceanspaces.com/nnef-public/mobilenet_v2_1.0.onnx.nnef.tgz",
+            "mobilenet_v2_1.0.onnx.nnef.tgz"
+        )
 
 def test_onnx():
     model = (
         tract.onnx()
         .model_for_path("./mobilenetv2-7.onnx")
+        .into_optimized()
+        .into_runnable()
+    )
+    img = numpy.load("grace_hopper_1x3x224x244.npy")
+
+    result = model.run([img])
+    confidences = result[0].to_numpy()
+    assert numpy.argmax(confidences) == 652
+
+def test_nnef():
+    model = (
+        tract.nnef()
+        .model_for_path("mobilenet_v2_1.0.onnx.nnef.tgz")
         .into_optimized()
         .into_runnable()
     )
