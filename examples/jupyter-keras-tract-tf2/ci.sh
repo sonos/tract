3d7843c5e227acfef1f9ee2bcfcdbbd715caede6
Mathieu Poumeyrol <kali@zoy.org>
2023-01-12 15:30:30 +0100

    try and fix tf mesa setup

--- examples/jupyter-keras-tract-tf2/ci.sh
+++ examples/jupyter-keras-tract-tf2/ci.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+sudo apt-get update
 sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 
 if [ ! -d $HOME/anaconda3 ]
