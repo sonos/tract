ba7a486f3e392e9fe677d9034aec0f53c7ba9fd0
Mathieu Poumeyrol <kali@zoy.org>
2023-01-14 17:08:56 +0100

    generic load and store

---
+++ linalg/src/arm64/apple_amx.rs
@@ -0,0 +1,6 @@
+//use crate::frame::element_wise::ElementWiseKer;
+use crate::frame::mmm::*;
+// #[cfg(not(feature="no_fp16"))]
+// use tract_data::half::f16;
+
+MMMKernel!(f32, apple_amx_mmm_f32_32x32; 32, 32; 128, 128; 1, 1; no_prefetch, true);
