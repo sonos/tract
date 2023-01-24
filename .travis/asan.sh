d14553e1399ae52f31b73e15c3758cbce8cca786
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2023-01-24 17:17:20 +0100

    auto default target disco

--- .travis/asan.sh
+++ .travis/asan.sh
@@ -2,14 +2,16 @@
 
 set -ex
 
+TARGET=$(rustc -vV | sed -n 's|host: ||p')
+
 rustup toolchain add nightly
-rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
+rustup component add rust-src --toolchain nightly-$TARGET
 
 export RUSTFLAGS=-Zsanitizer=address 
 export RUSTDOCFLAGS=$RUSTFLAGS
 export RUSTUP_TOOLCHAIN=nightly
 export RUST_VERSION=nightly
-export CARGO_EXTRA="-Zbuild-std --target x86_64-unknown-linux-gnu"
+export CARGO_EXTRA="-Zbuild-std --target $TARGET"
 
 ./.travis/regular-tests.sh
 ./.travis/onnx-tests.sh
