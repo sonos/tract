52e90b10e2e829d1ea14d6db236199dcf2609239
Mathieu Poumeyrol <kali@zoy.org>
2023-02-27 23:18:54 +0100

    wip. todo:non-i64, optim, nnef

--- onnx/src/ops/array/trilu.rs
+++ onnx/src/ops/array/trilu.rs
@@ -7,12 +7,14 @@ pub fn trilu(
     node: &NodeProto,
 ) -> TractResult<(Box<dyn InferenceOp>, Vec<String>)> {
     let upper: i64 = node.get_attr_opt("upper")?.unwrap_or(1);
-    Ok((Box::new(Trilu { upper: upper == 1 }), vec![]))
+    let has_k = node.input.len() == 2;
+    Ok((Box::new(Trilu { upper: upper == 1, has_k }), vec![]))
 }
 
 #[derive(Debug, Clone)]
 struct Trilu {
     upper: bool,
+    has_k: bool,
 }
 
 impl Op for Trilu {
@@ -29,17 +31,24 @@ impl EvalOp for Trilu {
     }
 
     fn eval(&self, mut inputs: TVec<TValue>) -> TractResult<TVec<TValue>> {
-        let mut input = args_1!(inputs).into_tensor();
+        let (mut input, k) = if self.has_k {
+            let (input, k) = args_2!(inputs);
+            let k = *k.to_scalar::<i64>()?;
+            (input.into_tensor(), k)
+        } else {
+            (args_1!(inputs).into_tensor(), 0)
+        };
+        dbg!(k);
         let mut view = input.to_array_view_mut::<i64>()?;
         for coords in tract_ndarray::indices(view.shape()) {
-            let row = coords[view.ndim() - 2];
-            let col = coords[view.ndim() - 1];
+            let row = coords[view.ndim() - 2] as i64;
+            let col = coords[view.ndim() - 1] as i64;
             if self.upper {
-                if col < row {
+                if (col as i64) < row as i64 + k {
                     view[coords] = 0;
                 }
             } else {
-                if col > row {
+                if (col as i64) > row as i64 + k {
                     view[coords] = 0;
                 }
             }
@@ -55,10 +64,14 @@ impl InferenceRulesOp for Trilu {
         inputs: &'p [TensorProxy],
         outputs: &'p [TensorProxy],
     ) -> InferenceResult {
-        check_input_arity(inputs, 1)?;
+        check_input_arity(inputs, 1 + self.has_k as usize)?;
         check_output_arity(outputs, 1)?;
         solver.equals(&inputs[0].datum_type, &outputs[0].datum_type)?;
         solver.equals(&inputs[0].shape, &outputs[0].shape)?;
+        if self.has_k {
+            solver.equals(&inputs[1].datum_type, i64::datum_type())?;
+            solver.equals(&inputs[1].rank, 0)?;
+        }
         Ok(())
     }
 
