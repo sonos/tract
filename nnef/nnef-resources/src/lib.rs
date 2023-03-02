0a399f0e452a5b01beedb61cdedc9e9a70c69840
Hubert de La Jonquiere <hubert.delajonquiere@sonos.com>
2023-03-02 15:10:51 -0500

    Add typedModel loader

--- nnef/nnef-resources/src/lib.rs
+++ nnef/nnef-resources/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod json_loader;
+pub mod typed_model_loader;
 
 pub mod internal {
     pub use crate::json_loader::{JsonLoader, JsonResource};
+    pub use crate::typed_model_loader::{TypedModelLoader, TypedModelResource};
 }
