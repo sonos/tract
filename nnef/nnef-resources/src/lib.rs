bcfb700a09a074558d310c7ebfb4e3639e924529
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@sonos.com>
2023-03-06 16:48:35 +0100

    remove typed_model_loader (moved to nnef crate)

--- nnef/nnef-resources/src/lib.rs
+++ nnef/nnef-resources/src/lib.rs
@@ -1,7 +1,5 @@
 pub mod json_loader;
-pub mod typed_model_loader;
 
 pub mod internal {
     pub use crate::json_loader::{JsonLoader, JsonResource};
-    pub use crate::typed_model_loader::{TypedModelLoader, TypedModelResource};
 }
