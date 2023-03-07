5085951b846135261b13544adbf4928bd38cbdbf
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@sonos.com>
2023-03-07 10:19:34 +0100

    Update test (no more prefix removal)

--- harness/pre-optimized-graphes/mdl-en-2019-Q3-librispeech/runme.sh
+++ harness/pre-optimized-graphes/mdl-en-2019-Q3-librispeech/runme.sh
@@ -14,6 +14,6 @@ fi
 $TRACT_RUN $CACHEDIR/mdl-en-2019-Q3-librispeech.onnx -i S,40 --output-node output --pulse 24 --nnef-tract-pulse --nnef-extended-identifier dump -q --nnef-graph found
 
 version=`cargo metadata --format-version 1 | jq -r '.packages | map(select( (.name) == "tract-core") | .version) | .[] '`
-perl -pi -e "s/$version/0.16.10-pre/" found
+perl -pi -e "s/$version/0.19.3-pre/" found
 
 diff -u expected found
