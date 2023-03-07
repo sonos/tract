c0c3b2c9e03764b77d27c9721abd3f975d392fc7
Mathieu Poumeyrol <kali@zoy.org>
2023-03-07 22:42:01 +0100

    clippy

--- core/src/ops/array/trilu.rs
+++ core/src/ops/array/trilu.rs
@@ -20,7 +20,7 @@ impl EvalOp for Trilu {
 
     fn eval(&self, mut inputs: TVec<TValue>) -> TractResult<TVec<TValue>> {
         let (input, k) = args_2!(inputs);
-        let mut input = input.clone().into_tensor();
+        let mut input = input.into_tensor();
         let k = *k.to_scalar::<i64>()?;
         fn eval_t<T: Datum>(tensor: &mut Tensor, upper: bool, k: i64) -> TractResult<()> {
             let mut view = tensor.to_array_view_mut::<T>()?;
@@ -31,10 +31,8 @@ impl EvalOp for Trilu {
                     if col < row + k {
                         view[coords] = T::default();
                     }
-                } else {
-                    if col > row + k {
-                        view[coords] = T::default();
-                    }
+                } else if col > row + k {
+                    view[coords] = T::default();
                 }
             }
             Ok(())
@@ -47,7 +45,6 @@ impl EvalOp for Trilu {
 impl TypedOp for Trilu {
     fn output_facts(&self, inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
         Ok(tvec!(inputs[0].without_value()))
-        
     }
 
     as_op!();
