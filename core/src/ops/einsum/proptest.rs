2216ac3e2cf21d9bb07efb6b5325b7d91ee30e95
Mathieu Poumeyrol <kali@zoy.org>
2023-03-11 09:35:22 +0100

    more test around einsum codegen

--- core/src/ops/einsum/proptest.rs
+++ core/src/ops/einsum/proptest.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 
 use crate::internal::*;
+use crate::ops::matmul::lir_unary::LirMatMulUnary;
 use proptest::prelude::*;
 use proptest::strategy::BoxedStrategy;
 use tract_ndarray::ArrayD;
@@ -32,10 +33,17 @@ impl Arbitrary for BinEinsumProblem {
                 let m_axes: String = ('a'..).take(m_axes).collect();
                 let n_axes: String = ('g'..).take(n_axes).collect();
                 let iter_axes: String = ('w'..).take(iter_axes).collect();
-                let a_axes: String = m_axes.clone() + &iter_axes + "k";
-                let b_axes: String = n_axes.clone() + &iter_axes + "k";
-                let c_axes: String = m_axes.clone() + &n_axes + &iter_axes;
-                let expr: AxesMapping = format!("{a_axes},{b_axes}->{c_axes}").parse().unwrap();
+                let a_axes: Vec<char> = (m_axes.clone() + &iter_axes + "k").chars().collect();
+                let b_axes: Vec<char> = (n_axes.clone() + &iter_axes + "k").chars().collect();
+                let c_axes: Vec<char> = (m_axes.clone() + &n_axes + &iter_axes).chars().collect();
+                (Just(a_axes), Just(b_axes), Just(c_axes))
+            })
+            .prop_flat_map(|(a, b, c)| (a.prop_shuffle(), b.prop_shuffle(), c.prop_shuffle()))
+            .prop_map(|(a, b, c)| {
+                let a: String = a.into_iter().collect();
+                let b: String = b.into_iter().collect();
+                let c: String = c.into_iter().collect();
+                let expr: AxesMapping = format!("{a},{b}->{c}").parse().unwrap();
                 expr
             })
             .prop_flat_map(|expr| {
@@ -62,7 +70,7 @@ impl Arbitrary for BinEinsumProblem {
 
 pub fn tensor(shape: &[usize]) -> BoxedStrategy<Tensor> {
     let len = shape.iter().product::<usize>();
-    let shape:Vec<usize> = shape.into();
+    let shape: Vec<usize> = shape.into();
     proptest::collection::vec((-10i8..=10i8).prop_map(|i| i as f32), len..=len)
         .prop_map(move |vec| ArrayD::from_shape_vec(shape.clone(), vec).unwrap().into_tensor())
         .boxed()
@@ -85,6 +93,7 @@ impl BinEinsumProblem {
             .run(tvec![self.a.clone().into(), self.b.clone().into()])?
             .remove(0);
         let optimised = model.clone().into_optimized()?;
+        ensure!(optimised.nodes[optimised.nodes.len() - 1].op_is::<LirMatMulUnary>());
         let found = optimised
             .into_runnable()?
             .run(tvec![self.a.clone().into(), self.b.clone().into()])?
