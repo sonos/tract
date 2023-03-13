21b64242dfe3e9a275b8b10c9bae3f3c0c35d3ff
Mathieu Poumeyrol <kali@zoy.org>
2023-03-13 14:39:48 +0100

    more testig einsum->lir translation

--- core/src/ops/einsum/proptest.rs
+++ core/src/ops/einsum/proptest.rs
@@ -17,11 +17,16 @@ struct BinEinsumProblem {
     b: Tensor,
     a_constant: bool,
     b_constant: bool,
+    unicast_add_constant: Option<Tensor>,
 }
 
 impl std::fmt::Debug for BinEinsumProblem {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{} A:{:?} B:{:?}", self.expr, self.a, self.b)
+        write!(
+            f,
+            "{} A:{:?} B:{:?} a_constant:{:?} b_constant:{:?} unicast_add_constant:{:?}",
+            self.expr, self.a, self.b, self.a_constant, self.b_constant, self.unicast_add_constant
+        )
     }
 }
 
@@ -63,12 +68,18 @@ impl Arbitrary for BinEinsumProblem {
                     .map(|axis| expr.iter_all_axes().position(|x| x == axis).unwrap())
                     .map(|dim| axis_dims[dim])
                     .collect();
-                (Just(expr), tensor(&shape_a), tensor(&shape_b), 0..3usize)
+                let shape_output: TVec<usize> = expr
+                    .output_axes(0)
+                    .map(|axis| expr.iter_all_axes().position(|x| x == axis).unwrap())
+                    .map(|dim| axis_dims[dim])
+                    .collect();
+                let unicast_add_constant = proptest::option::of(tensor(&*shape_output));
+                (Just(expr), tensor(&shape_a), tensor(&shape_b), 0..3usize, unicast_add_constant)
             })
-            .prop_map(|(expr, a, b, a_b_constant)| {
+            .prop_map(|(expr, a, b, a_b_constant, unicast_add_constant)| {
                 let a_constant = (a_b_constant & 0x1) != 0;
                 let b_constant = (a_b_constant & 0x2) != 0;
-                BinEinsumProblem { expr, a, b, a_constant, b_constant }
+                BinEinsumProblem { expr, a, b, a_constant, b_constant, unicast_add_constant }
             })
             .boxed()
     }
@@ -98,12 +109,17 @@ impl BinEinsumProblem {
             inputs.push(self.b.clone().into_tvalue());
             model.add_source("b", TypedFact::from(&self.b).without_value())?
         };
-        let sum = model.wire_node(
+        let mut output = model.wire_node(
             "einsum",
             EinSum { expr: self.expr.clone(), operating_dt: f32::datum_type(), q_params: None },
             &[a, b],
         )?;
-        model.set_output_outlets(&sum)?;
+        if let Some(c) = &self.unicast_add_constant {
+            let c = model.add_const("c", c.clone())?;
+            output = model.wire_node("add", crate::ops::math::add(), &[output[0], c])?;
+        }
+        model.set_output_outlets(&output)?;
+        model = model.into_decluttered()?;
         let expected = model.clone().into_runnable()?.run(inputs.clone())?.remove(0);
         let optimised = model.clone().into_optimized()?;
         ensure!(optimised.nodes[optimised.nodes.len() - 1].op_is::<LirMatMulUnary>());
@@ -117,4 +133,32 @@ proptest::proptest! {
     fn prop(pb in any::<BinEinsumProblem>()) {
         pb.check().unwrap();
     }
+}
+
+#[test]
+fn unicast_0() {
+    BinEinsumProblem {
+        expr: "ak,gk->ag".parse().unwrap(),
+        a: Tensor::zero::<f32>(&[1, 2]).unwrap(),
+        b: Tensor::zero::<f32>(&[1, 2]).unwrap(),
+        a_constant: false,
+        b_constant: false,
+        unicast_add_constant: Some(Tensor::zero::<f32>(&[1, 1]).unwrap()),
+    }
+    .check()
+    .unwrap()
+}
+
+#[test]
+fn unicast_1() {
+    BinEinsumProblem {
+        expr: "ak,gk->ag".parse().unwrap(),
+        a: Tensor::zero::<f32>(&[2, 1]).unwrap(),
+        b: Tensor::zero::<f32>(&[2, 1]).unwrap(),
+        a_constant: false,
+        b_constant: false,
+        unicast_add_constant: Some(tensor2(&[[0f32, 0.], [0., 1.]])),
+    }
+    .check()
+    .unwrap()
 }
