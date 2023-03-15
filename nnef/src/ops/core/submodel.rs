cc720ea47a750748d1fc6d121b0b997e37decbc8
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@sonos.com>
2023-03-15 11:46:44 +0100

    Make submodel generic on the inner model

--- nnef/src/ops/core/submodel.rs
+++ nnef/src/ops/core/submodel.rs
@@ -1,4 +1,4 @@
-use tract_core::ops::{submodel::SubmodelOp, self};
+use tract_core::ops::{self, submodel::SubmodelOp};
 
 use crate::internal::*;
 
@@ -6,10 +6,7 @@ pub fn register(registry: &mut Registry) {
     registry.register_dumper(TypeId::of::<ops::submodel::SubmodelOp>(), ser_submodel);
     registry.register_primitive(
         "tract_core_submodel",
-        &[
-            TypeName::Scalar.tensor().array().named("input"),
-            TypeName::String.named("label"),
-        ],
+        &[TypeName::Scalar.tensor().array().named("input"), TypeName::String.named("label")],
         &[("outputs", TypeName::Any.tensor().array())],
         de_submodel,
     );
@@ -22,36 +19,22 @@ fn de_submodel(builder: &mut ModelBuilder, invocation: &ResolvedInvocation) -> T
         .proto_model
         .resources
         .get(label.as_str())
-        .with_context(|| {
-            anyhow!(
-                "{} not found in model builder loaded resources",
-                label.as_str()
-            )
-        })?
+        .with_context(|| anyhow!("{} not found in model builder loaded resources", label.as_str()))?
         .clone()
         .downcast_arc::<TypedModelResource>()
         .map_err(|_| anyhow!("Error while downcasting typed model resource"))
         .map(|r| r.0.clone())
         .with_context(|| anyhow!("Error while loading typed model resource"))?;
 
-    let op: Box<dyn TypedOp> = Box::new(SubmodelOp::new(model, &label)?);
+    let op: Box<dyn TypedOp> = Box::new(SubmodelOp::new(Box::new(model), &label)?);
 
-    builder
-        .model
-        .wire_node(label, op, &wires)
-        .map(Value::from)
+    builder.model.wire_node(label, op, &wires).map(Value::from)
 }
 
 fn ser_submodel(ast: &mut IntoAst, node: &TypedNode) -> TractResult<Option<Arc<RValue>>> {
     let op = node.op_as::<SubmodelOp>().unwrap();
     let input = tvec![ast.mapping[&node.inputs[0]].clone()];
-    let invoke = invocation(
-        "tract_core_submodel",
-        &input,
-        &[
-            ("label", string(op.label())),
-        ],
-    );
+    let invoke = invocation("tract_core_submodel", &input, &[("label", string(op.label()))]);
     ast.resources.insert(op.label().to_string(), Arc::new(TypedModelResource(op.model().clone())));
     Ok(Some(invoke))
 }
