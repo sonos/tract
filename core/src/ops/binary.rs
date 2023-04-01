755ac526517a0c3153dc08dd766c8e4cabd69fa3
Mathieu Poumeyrol <kali@zoy.org>
2023-04-01 16:09:13 +0200

    uniform is back (in some cases)

--- core/src/ops/binary.rs
+++ core/src/ops/binary.rs
@@ -81,12 +81,11 @@ pub trait BinMiniOp: fmt::Debug + dyn_clone::DynClone + Send + Sync + 'static + 
     fn generic_eval(&self, axes: &AxesMapping, a: TValue, b: TValue) -> TractResult<Tensor> {
         let c_dt = self.result_datum_type(a.datum_type(), b.datum_type())?;
         let c_shape = output_shape(axes, a.shape(), b.shape())?;
-        /*
-        if c_dt == b.datum_type() && a.len() == 1 {
+        if c_dt == b.datum_type() && a.len() == 1 && axes.direct(InOut::In(1), InOut::Out(0)) {
             let mut b = b.into_tensor();
             self.eval_uniform_in_place(&a, &mut b)?;
             Ok(b)
-        } else */if a.shape() == b.shape()
+        } else if a.shape() == b.shape()
             && c_dt == b.datum_type()
             && axes.direct(InOut::In(0), InOut::In(1))
             && axes.direct(InOut::In(0), InOut::Out(0))
