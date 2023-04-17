fb4fc350aa6c018a01a9f5abe48cf51718b96bf6
Mathieu Poumeyrol <kali@zoy.org>
2023-04-17 14:33:16 +0200

    hardswish

--- core/src/ops/nn/mod.rs
+++ core/src/ops/nn/mod.rs
@@ -8,12 +8,16 @@ pub use self::softmax::Softmax;
 
 pub use crate::internal::*;
 
-element_wise!(sigmoid, Sigmoid, 
-    [f16] => |_, xs| { (tract_linalg::ops().sigmoid_f16)().run(xs) },
-    [f32] => |_, xs| { (tract_linalg::ops().sigmoid_f32)().run(xs) };
-    cost: |dt| {tvec!((Cost::FMA(dt), 11), (Cost::Div(dt), 1))}
+element_wise!(sigmoid, Sigmoid,
+ [f16] => |_, xs| { (tract_linalg::ops().sigmoid_f16)().run(xs) },
+ [f32] => |_, xs| { (tract_linalg::ops().sigmoid_f32)().run(xs) };
+ cost: |dt| {tvec!((Cost::FMA(dt), 11), (Cost::Div(dt), 1))}
 );
 
-element_wise!(leaky_relu, LeakyRelu { #[educe(Hash(method = "hash_f32"))] alpha: f32 },
-    [f32] => |op, xs| { xs.iter_mut().for_each(|x| *x *= if *x < 0. { op.alpha } else { 1.0 }); Ok(()) }
+element_wise!(hard_swish, HardSwish,
+ [f32] => |_, xs| { xs.iter_mut().for_each(|x| *x = *x * 0f32.max(1f32.min((1. / 6.) * *x + 0.5))); Ok(()) }
+);
+
+element_wise!(leaky_relu, LeakyRelu { alpha: f32 },
+ [f32] => |op, xs| { xs.iter_mut().for_each(|x| *x *= if *x < 0. { op.alpha } else { 1.0 }); Ok(()) }
 );
