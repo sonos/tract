bae612ba6282f4edd970afa5e739b2251d69fc27
fgh <fanguanghong1999@163.com>
2023-05-06 18:37:34 +0800

    test: add wasm runtime check in onnx example ci

--- examples/onnx-mobilenet-v2/ci.sh
+++ examples/onnx-mobilenet-v2/ci.sh
@@ -3,6 +3,13 @@
 set -ex
 
 wget -q https://github.com/onnx/models/raw/main/vision/classification/mobilenet/model/mobilenetv2-7.onnx -O mobilenetv2-7.onnx
+
+# on win/linux
 cargo run
+# on wasm
+wasmtime -V || curl https://wasmtime.dev/install.sh -sSf | bash # install wasmtime
+cargo build --target wasm32-wasi
+wasmtime ../../target/wasm32-wasi/debug/example-onnx-mobilenet-v2.wasm --dir=.
+
 cargo clean
 rm  mobilenetv2-7.onnx
