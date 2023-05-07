383109a28259e2512dcf5fb1f506ec787e3be67a
Mathieu Poumeyrol <kali@zoy.org>
2023-05-07 13:20:50 +0200

    install wasm target

--- examples/onnx-mobilenet-v2/ci.sh
+++ examples/onnx-mobilenet-v2/ci.sh
@@ -8,6 +8,7 @@ wget -q https://github.com/onnx/models/raw/main/vision/classification/mobilenet/
 cargo run
 # on wasm
 wasmtime -V || curl https://wasmtime.dev/install.sh -sSf | bash # install wasmtime
+rustup target install wasm32-wasi
 cargo build --target wasm32-wasi
 wasmtime ../../target/wasm32-wasi/debug/example-onnx-mobilenet-v2.wasm --dir=.
 
