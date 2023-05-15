244bd95b7cadf7cdee20415362e9d2b1a71488d9
Mathieu Poumeyrol <kali@zoy.org>
2023-05-15 21:47:02 +0200

    hardswish ok

--- linalg/src/frame/activations/tests.rs
+++ linalg/src/frame/activations/tests.rs
@@ -16,10 +16,10 @@ pub fn run_kernel_test<TI: LADatum, K: ActivationKer<TI>>(
     input: &[TI],
     prog: &[Op<TI>],
     refer: impl Fn(TI) -> TI,
-) {
+    ) {
     let mut tensor =
         tract_data::prelude::Tensor::zero_aligned::<TI>(&[input.len()], K::alignment_bytes())
-            .unwrap();
+        .unwrap();
     tensor.as_slice_mut::<TI>().unwrap().copy_from_slice(input);
     let expected: Vec<TI> = input.iter().cloned().map(|x| refer(x)).collect();
     let expected = tract_data::prelude::tensor1(&expected);
@@ -94,7 +94,7 @@ macro_rules! act_tests {
                             &x,
                             &[Load(RegisterId::B, konst), Move(RegisterId::A, RegisterId::B)],
                             |_| konst
-                        );
+                            );
                     }
                 }
 
@@ -125,97 +125,124 @@ macro_rules! act_tests {
                         run_kernel_test::<$ti, $ker>(&x, &[MinConst(alpha)], |x| x.min(alpha));
                     }
                 }
-            }
 
-            #[test]
-            fn max_const_zero() {
-                if $cond {
-                    run_kernel_test::<$ti, $ker>(
-                        &vec![<$ti>::zero(); <$ker>::nr()],
-                        &[MaxConst(<$ti>::zero())],
-                        |x| x.max(<$ti>::zero()),
-                    );
-                }
-            }
-
-            #[test]
-            fn max_const_big_alpha() {
-                if $cond {
-                    run_kernel_test::<$ti, $ker>(
-                        &vec![<$ti>::zero(); <$ker>::nr()],
-                        &[MaxConst(7.567773e37.into())],
-                        |x| x.max(7.567773e37.into()),
-                    );
-                }
-            }
-
-            #[test]
-            fn move_b_to_a_0() {
-                if $cond {
-                    run_kernel_test::<$ti, $ker>(
-                        &*vec![<$ti>::zero(); <$ker>::nr()],
-                        &[Load(RegisterId::B, 1.0 as _), Move(RegisterId::A, RegisterId::B)],
-                        |_| 1.0 as _,
-                    );
-                }
-            }
-
-            proptest::proptest! {
                 #[test]
-                fn relu_prop(x in x_strat()) {
+                fn mul_prop(x in x_strat(), v in any::<$ti>()) {
                     if $cond {
-                        run_kernel_test::<$ti, $ker>(
-                            &x,
-                            &$crate::frame::activations::definitions::relu().ops,
-                            |x| x.max(<$ti>::zero())
-                        );
+                        run_kernel_test::<$ti, $ker>(&x, &[Load(RegisterId::B, v), Mul], |x| x * v);
                     }
                 }
 
                 #[test]
-                fn affine_prop(x in x_strat(), alpha in any::<$ti>(), beta in any::<$ti>()) {
+                fn ifposte_prop(x in x_strat()) {
                     if $cond {
-                        run_kernel_test::<$ti, $ker>(
-                            &x,
-                            &$crate::frame::activations::definitions::affine(alpha, beta).ops,
-                            |x| x * alpha + beta
-                        );
+                        run_kernel_test::<$ti, $ker>(&x,
+                            &[Load(RegisterId::B, 2 as _), Load(RegisterId::C, 3 as _), IfPosTE], 
+                            |x| if x >= <$ti>::zero() { 2 as _ } else { 3 as _ });
+                        }
                     }
                 }
 
                 #[test]
-                fn hard_sigmoid(x in x_strat(), alpha in any::<$ti>(), beta in any::<$ti>()) {
+                fn max_const_zero() {
                     if $cond {
                         run_kernel_test::<$ti, $ker>(
-                            &x,
-                            &$crate::frame::activations::definitions::hard_sigmoid(alpha, beta).ops,
-                            |x| (x * alpha + beta).min(<$ti>::one()).max(<$ti>::zero())
-                        );
+                            &vec![<$ti>::zero(); <$ker>::nr()],
+                            &[MaxConst(<$ti>::zero())],
+                            |x| x.max(<$ti>::zero()),
+                            );
                     }
                 }
 
                 #[test]
-                fn hard_swish(x in x_strat()) {
+                fn max_const_big_alpha() {
                     if $cond {
                         run_kernel_test::<$ti, $ker>(
-                            &x,
-                            &$crate::frame::activations::definitions::hard_swish().ops,
-                            |x| (x * 1./6. + 0.5).min(<$ti>::one()).max(<$ti>::zero()) * x
-                        );
+                            &vec![<$ti>::zero(); <$ker>::nr()],
+                            &[MaxConst(7.567773e37.into())],
+                            |x| x.max(7.567773e37.into()),
+                            );
                     }
                 }
+
+                #[test]
+                fn move_b_to_a_0() {
+                    if $cond {
+                        run_kernel_test::<$ti, $ker>(
+                            &*vec![<$ti>::zero(); <$ker>::nr()],
+                            &[Load(RegisterId::B, 1.0 as _), Move(RegisterId::A, RegisterId::B)],
+                            |_| 1.0 as _,
+                            );
+                    }
+                }
+
+                proptest::proptest! {
+                    #[test]
+                    fn relu_prop(x in x_strat()) {
+                        if $cond {
+                            run_kernel_test::<$ti, $ker>(
+                                &x,
+                                &$crate::frame::activations::definitions::relu().ops,
+                                |x| x.max(<$ti>::zero())
+                                );
+                        }
+                    }
+
+                    #[test]
+                    fn affine_prop(x in x_strat(), alpha in any::<$ti>(), beta in any::<$ti>()) {
+                        if $cond {
+                            run_kernel_test::<$ti, $ker>(
+                                &x,
+                                &$crate::frame::activations::definitions::affine(alpha, beta).ops,
+                                |x| x * alpha + beta
+                                );
+                        }
+                    }
+
+                    #[test]
+                    fn leaky_relu_prop(x in x_strat(), alpha in any::<$ti>()) {
+                        if $cond {
+                            run_kernel_test::<$ti, $ker>(
+                                &x,
+                                &$crate::frame::activations::definitions::leaky_relu(alpha).ops,
+                                |x| if x >= <$ti>::zero() { x } else { alpha * x }
+                                );
+                        }
+                    }
+
+                    #[test]
+                    fn hard_sigmoid(x in x_strat(), alpha in any::<$ti>(), beta in any::<$ti>()) {
+                        if $cond {
+                            run_kernel_test::<$ti, $ker>(
+                                &x,
+                                &$crate::frame::activations::definitions::hard_sigmoid(alpha, beta).ops,
+                                |x| (x * alpha + beta).min(<$ti>::one()).max(<$ti>::zero())
+                                );
+                        }
+                    }
+
+                    #[test]
+                    fn hard_swish(x in x_strat()) {
+                        if $cond {
+                            run_kernel_test::<$ti, $ker>(
+                                &x,
+                                &$crate::frame::activations::definitions::hard_swish().ops,
+                                |x| (x * 1./6. + 0.5).min(<$ti>::one()).max(<$ti>::zero()) * x
+                                );
+                        }
+                    }
+                }
+                /*
+                   prop_act_e2e!($cond, $ti, $ker, affine(alpha, beta));
+                   prop_act_e2e!($cond, $ti, $ker, leaky_relu(alpha));
+                   prop_act_e2e!($cond, $ti, $ker, threshold_relu(alpha));
+                   prop_act_e2e!($cond, $ti, $ker, softsign());
+                   prop_act_e2e!($cond, $ti, $ker, hardswish());
+                /*
+                prop_activation!($cond, $ti, $ker, sigmoid());
+                prop_activation!($cond, $ti, $ker, exp2f());
+                */
+                */
             }
-            /*
-            prop_act_e2e!($cond, $ti, $ker, affine(alpha, beta));
-            prop_act_e2e!($cond, $ti, $ker, leaky_relu(alpha));
-            prop_act_e2e!($cond, $ti, $ker, threshold_relu(alpha));
-            prop_act_e2e!($cond, $ti, $ker, softsign());
-            prop_act_e2e!($cond, $ti, $ker, hardswish());
-            /*
-               prop_activation!($cond, $ti, $ker, sigmoid());
-               prop_activation!($cond, $ti, $ker, exp2f());
-               */
-            */
-        }
-    };
-}
+        };
+    }
