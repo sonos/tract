009069df3fcc353a9a7fae008a601f000aaaa6a2
Mathieu Poumeyrol <kali@zoy.org>
2023-06-05 16:59:41 +0200

    missing -sys crate

---
+++ api/proxy/sys/src/lib.rs
@@ -0,0 +1,8 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(improper_ctypes)]
+#![allow(deref_nullptr)]
+#![allow(clippy::redundant_static_lifetimes)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
