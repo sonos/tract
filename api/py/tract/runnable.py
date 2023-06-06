f644864b0071286dd593cb4ec3f3755d9a987901
Mathieu Poumeyrol <kali@zoy.org>
2023-06-06 10:10:46 +0200

    align python api impl

--- api/py/tract/runnable.py
+++ api/py/tract/runnable.py
@@ -2,6 +2,7 @@ from ctypes import *
 from typing import Dict, List, Union # after ctypes so that Union is overriden
 import numpy
 from .value import Value
+from .state import State
 from .bindings import check, lib
 
 class Runnable:
@@ -10,11 +11,6 @@ class Runnable:
     """
     def __init__(self, ptr):
         self.ptr = ptr
-        i = c_size_t()
-        o = c_size_t()
-        check(lib.tract_runnable_nbio(self.ptr, byref(i), byref(o)))
-        self.inputs = i.value
-        self.outputs = o.value
 
     def __del__(self):
         check(lib.tract_runnable_release(byref(self.ptr)))
@@ -23,25 +19,28 @@ class Runnable:
         if self.ptr == None:
             raise TractError("invalid runnable (maybe already consumed ?)")
 
+    def input_count(self) -> int:
+        """Return the number of inputs of the underlying model"""
+        self._valid()
+        i = c_size_t()
+        check(lib.tract_runnable_input_count(self.ptr, byref(i)))
+        return i.value
+
+    def output_count(self) -> int:
+        """Return the number of outputs of the underlying model"""
+        self._valid()
+        i = c_size_t()
+        check(lib.tract_runnable_output_count(self.ptr, byref(i)))
+        return i.value
+
     def run(self, inputs: List[Union[Value, numpy.ndarray]]) -> List[Value]:
         """
         Runs the model over the provided input list, and returns the model outputs.
         """
+        return self.spawn_state().run(inputs)
+
+    def spawn_state(self):
         self._valid()
-        input_values = []
-        for v in inputs:
-            if isinstance(v, Value):
-                input_values.append(v)
-            elif isinstance(v, numpy.ndarray):
-                input_values.append(Value.from_numpy(v))
-            else:
-                raise TractError(f"Inputs must be of type tract.Value or numpy.Array, got {v}")
-        input_ptrs = (c_void_p * self.inputs)()
-        output_ptrs = (c_void_p * self.outputs)()
-        for ix, v in enumerate(input_values):
-            input_ptrs[ix] = v.ptr
-        check(lib.tract_runnable_run(self.ptr, input_ptrs, output_ptrs))
-        result = []
-        for v in output_ptrs:
-            result.append(Value(c_void_p(v)))
-        return result
+        state = c_void_p()
+        check(lib.tract_runnable_spawn_state(self.ptr, byref(state)))
+        return State(state)
