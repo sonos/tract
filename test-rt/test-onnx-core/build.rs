2141acbbb398270e2f5bd20cca684a1f4a154e5e
Mathieu Poumeyrol <kali@zoy.org>
2023-08-08 16:13:21 +0200

    lower precision expectation on onnx test

--- test-rt/test-onnx-core/build.rs
+++ test-rt/test-onnx-core/build.rs
@@ -1,10 +1,10 @@
 fn main() {
     let suite = suite_onnx::suite();
-    suite.test_runtime("default", "suite_onnx::suite()", "default()", "Approximation::Close");
+    suite.test_runtime("default", "suite_onnx::suite()", "default()", "Approximation::Approximate");
     suite.test_runtime(
         "unoptimized",
         "suite_onnx::suite()",
         "unoptimized()",
-        "Approximation::Close",
+        "Approximation::Approximate",
     );
 }
