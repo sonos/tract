c4511f0a443bd7e60aa4a5d44fff40a1c12df3de
Mathieu Poumeyrol <kali@zoy.org>
2023-08-14 13:32:27 +0200

    investigate disk usg

--- .travis/asan.sh
+++ .travis/asan.sh
@@ -7,13 +7,19 @@ TARGET=$(rustc -vV | sed -n 's|host: ||p')
 rustup toolchain add nightly
 rustup component add rust-src --toolchain nightly-$TARGET
 
+
 export RUSTFLAGS=-Zsanitizer=address 
 export RUSTDOCFLAGS=$RUSTFLAGS
 export RUSTUP_TOOLCHAIN=nightly
 export RUST_VERSION=nightly
 export CARGO_EXTRA="-Zbuild-std --target $TARGET"
 
+df -h
+
 ./.travis/regular-tests.sh
-./.travis/onnx-tests.sh
-./.travis/cli-tests.sh
+df -h
+# ./.travis/onnx-tests.sh
+# df -h
+# ./.travis/cli-tests.sh
+# df -h
 
