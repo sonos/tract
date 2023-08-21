7e30315f663380c33df7d3e774d7c90a19e3089a
Mathieu Poumeyrol <kali@zoy.org>
2023-08-21 13:07:15 +0200

    debug again tflite snafu

--- .travis/native.sh
+++ .travis/native.sh
@@ -6,17 +6,17 @@ rustup update
 
 cargo check --all-targets
 
-./.travis/regular-tests.sh
-
-cargo test -q -p tract-core --features paranoid_assertions
-
-# useful as debug_asserts will come into play
-cargo -q test -q -p test-onnx-core
-cargo -q test -q -p test-onnx-nnef-cycle
-
-cargo check -p tract-nnef --features complex
-cargo check -p tract-tflite
-cargo check -p tract --no-default-features
+# ./.travis/regular-tests.sh
+# 
+# cargo test -q -p tract-core --features paranoid_assertions
+# 
+# # useful as debug_asserts will come into play
+# cargo -q test -q -p test-onnx-core
+# cargo -q test -q -p test-onnx-nnef-cycle
+# 
+# cargo check -p tract-nnef --features complex
+# cargo check -p tract-tflite
+# cargo check -p tract --no-default-features
 
 ./.travis/onnx-tests.sh
-./.travis/cli-tests.sh
+# ./.travis/cli-tests.sh
--- .travis/onnx-tests.sh
+++ .travis/onnx-tests.sh
@@ -39,8 +39,9 @@ fi
 export CACHEDIR
 
 opset=onnx_"${1:-1_13_0}"
-
-cargo -q test -p test-onnx-core $CARGO_EXTRA -q --no-default-features --features $opset
-cargo -q test -p test-onnx-nnef-cycle $CARGO_EXTRA -q --no-default-features
-cargo -q test -p test-conv-core $CARGO_EXTRA -q 
+# 
+# cargo -q test -p test-onnx-core $CARGO_EXTRA -q --no-default-features --features $opset
+# cargo -q test -p test-onnx-nnef-cycle $CARGO_EXTRA -q --no-default-features
+# cargo -q test -p test-conv-core $CARGO_EXTRA -q 
+cargo tree
 cargo -q test -p test-tflite $CARGO_EXTRA -q 
