f5901a9634ec2d3e3769146d8799e776f0d79f15
Mathieu Poumeyrol <kali@zoy.org>
2023-08-22 13:59:47 +0200

    cargo clean on CI

--- .travis/native.sh
+++ .travis/native.sh
@@ -10,6 +10,11 @@ cargo check --all-targets
 
 cargo test -q -p tract-core --features paranoid_assertions
 
+if [ -n "$CI" ]
+then
+    cargo clean
+fi
+
 # useful as debug_asserts will come into play
 cargo -q test -q -p test-onnx-core
 cargo -q test -q -p test-onnx-nnef-cycle
@@ -18,5 +23,14 @@ cargo check -p tract-nnef --features complex
 cargo check -p tract-tflite
 cargo check -p tract --no-default-features
 
+if [ -n "$CI" ]
+then
+    cargo clean
+fi
+
 ./.travis/onnx-tests.sh
+if [ -n "$CI" ]
+then
+    cargo clean
+fi
 ./.travis/cli-tests.sh
