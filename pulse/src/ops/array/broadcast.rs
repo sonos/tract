ac1aa8db315b229aa01b9ec23b56c4a5ea620e25
Mathieu Poumeyrol <kali@zoy.org>
2023-08-23 17:01:03 +0200

    wip pulsing broadcast

--- pulse/src/ops/array/broadcast.rs
+++ pulse/src/ops/array/broadcast.rs
@@ -1,5 +1,6 @@
+use crate::fact::StreamInfo;
 use crate::internal::*;
-use crate::model::NonPulsingWrappingOp;
+use crate::model::PulseWrappingOp;
 use tract_pulse_opl::tract_core::ops::array::MultiBroadcastTo;
 
 register_all!(MultiBroadcastTo: pulsify);
@@ -13,10 +14,60 @@ fn pulsify(
     symbol: &Symbol,
     pulse: &TDim,
 ) -> TractResult<Option<TVec<OutletId>>> {
-    let new_op = MultiBroadcastTo {
-        shape: op.shape.iter().map(|d| d.substitute(symbol, pulse)).collect::<TVec<_>>().into(),
-    };
-    target
-        .wire_node(&node.name, NonPulsingWrappingOp(Box::new(new_op)), &[mapping[&node.inputs[0]]])
-        .map(Some)
+    if let Some(axis) = op.shape.iter().position(|dim| dim.symbols().contains(symbol)) {
+        let full_dim = op.shape[axis].clone();
+        let fact = PulsedFact {
+            datum_type: _source.outlet_fact(node.inputs[0])?.datum_type,
+            shape: op.shape.iter().map(|dim| dim.substitute(symbol, pulse)).collect(),
+            stream: Some(StreamInfo { axis, dim: full_dim, delay: 0 }),
+        };
+        let new_op = PulsedMultibroadcastTo { fact };
+        target
+            .wire_node(&node.name, PulseWrappingOp(Box::new(new_op)), &[mapping[&node.inputs[0]]])
+            .map(Some)
+    } else {
+        Ok(None)
+    }
+}
+
+/// Concat with pulse along concat axis
+#[derive(Debug, Clone, Hash)]
+struct PulsedMultibroadcastTo {
+    fact: PulsedFact,
+}
+
+impl Op for PulsedMultibroadcastTo {
+    fn name(&self) -> Cow<str> {
+        "PulsedMultibroadcastTo".into()
+    }
+
+    op_as_typed_op!();
+}
+
+impl TypedOp for PulsedMultibroadcastTo {
+    fn output_facts(&self, inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
+        Ok(tvec!(inputs[0].datum_type.fact(self.fact.to_pulse_fact().shape)))
+    }
+    as_op!();
+}
+
+impl EvalOp for PulsedMultibroadcastTo {
+    fn is_stateless(&self) -> bool {
+        true
+    }
+    fn eval(&self, inputs: TVec<TValue>) -> TractResult<TVec<TValue>> {
+        self.to_typed().eval(inputs)
+    }
+}
+
+impl PulsedOp for PulsedMultibroadcastTo {
+    fn pulsed_output_facts(&self, inputs: &[&PulsedFact]) -> TractResult<TVec<PulsedFact>> {
+        Ok(tvec!(self.fact.clone()))
+    }
+
+    fn to_typed(&self) -> Box<dyn TypedOp> {
+        Box::new(MultiBroadcastTo { shape: self.fact.to_pulse_fact().shape })
+    }
+
+    as_op!();
 }
