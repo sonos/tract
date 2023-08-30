b5df93116a05bc30446bf35591b3d5572f417c5e
Mathieu Poumeyrol <kali@zoy.org>
2023-08-30 14:59:28 +0200

    release 0.20.18

--- api/proxy/sys/build.rs
+++ api/proxy/sys/build.rs
@@ -4,14 +4,14 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rerun-if-changed=../../tract.h");
+    println!("cargo:rerun-if-changed=tract.h");
     println!("cargo:rustc-link-lib=tract");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        .header("../../tract.h")
+        .header("tract.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .generate()
         .expect("Unable to generate bindings");
--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ cargo install tomato-toml
 
 CRATE_PATH=$1
 VERSION=$2
-ALL_CRATES_PATH="data linalg core nnef nnef/nnef-resources pulse-opl pulse hir tflite tensorflow onnx-opl onnx libcli api api/rs api/proxy/sys api/proxy cli"
+ALL_CRATES_PATH="data linalg core nnef nnef/nnef-resources pulse-opl pulse hir tflite tensorflow onnx-opl onnx libcli api api/rs api/ffi api/proxy/sys api/proxy cli"
 
 if [ -z "$VERSION" ]
 then
@@ -35,11 +35,11 @@ cargo publish --allow-dirty -p $crate
 
 for other_cargo_toml in `find . -name Cargo.toml`
 do
-    if tomato get dependencies.$crate $other_cargo_toml | grep -F .
+    if tomato get dependencies.$crate $other_cargo_toml | grep .
     then
         tomato set "dependencies.$crate.version" "=$VERSION" $other_cargo_toml
     fi
-    if tomato get dev-dependencies.$crate $other_cargo_toml | grep -F .
+    if tomato get dev-dependencies.$crate $other_cargo_toml | grep .
     then
         tomato set "dev-dependencies.$crate.version" "=$VERSION" $other_cargo_toml
     fi
