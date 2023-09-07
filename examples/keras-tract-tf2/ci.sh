5ef5165a5a14e4a1066fd2a5b051aeaf995410dc
Mathieu Poumeyrol <kali@zoy.org>
2023-09-07 14:30:57 +0200

    jupyter-less example

---
+++ examples/keras-tract-tf2/ci.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+sudo apt-get update
+sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 
+
+virtualenv venv
+. venv/bin/activate
+
+pip install -r requirements.txt
+python example.py
+cargo run
