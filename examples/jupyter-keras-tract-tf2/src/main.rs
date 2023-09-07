9b833e9a79eac9a3dcfd05cfb72a4bf0829148f0
Mathieu Poumeyrol <kali@zoy.org>
2023-09-07 13:31:12 +0200

    fix example

--- examples/jupyter-keras-tract-tf2/src/main.rs
+++ examples/jupyter-keras-tract-tf2/src/main.rs
@@ -1,23 +1,24 @@
-use rand::*;
+use tract_core::internal::Approximation;
+use tract_core::ndarray::{IxDyn, OwnedRepr};
 use tract_onnx::prelude::*;
 
 fn main() -> TractResult<()> {
     let model = tract_onnx::onnx()
     // load the model
-    .model_for_path("./example.onnx")?
+    .model_for_path("example.onnx")?
     // optimize graph
     .into_optimized()?
     // make the model runnable and fix its inputs and outputs
     .into_runnable()?;
 
-    // Generate some input data for the model
-    let mut rng = thread_rng();
-    let vals: Vec<_> = (0..1000).map(|_| rng.gen::<f32>()).collect();
-    let input = tract_ndarray::arr1(&vals).into_shape((10, 100)).unwrap().into_tensor();
+    // load input and expected output from npz file into tract tensors
+    let io = std::fs::File::open("io.npz").unwrap();
+    let mut npz = ndarray_npy::NpzReader::new(io)?;
+    let input = npz.by_name::<OwnedRepr<f32>, IxDyn>("input.npy").unwrap().into_tensor();
+    let expected = npz.by_name::<OwnedRepr<f32>, IxDyn>("output.npy").unwrap().into_tensor();
 
     // Input the generated data into the model
-    let result = model.run(tvec![input.into()]).unwrap();
-    let to_show = result[0].to_array_view::<f32>()?;
-    println!("result: {to_show:?}");
+    let found = model.run(tvec![input.into()]).unwrap().remove(0);
+    assert!(found.close_enough(&expected, Approximation::Approximate).is_ok());
     Ok(())
 }
