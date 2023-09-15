9d17ce6794c136a24c58bb09982f2979e8741d4b
Mathieu Poumeyrol <kali@zoy.org>
2023-09-15 08:35:50 +0200

    setup venv

--- examples/keras-tract-tf2/ci.sh
+++ examples/keras-tract-tf2/ci.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo apt-get update
-sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 
+sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 python3-virtualenv
 
 virtualenv venv
 . venv/bin/activate
