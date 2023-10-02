ecc951698d55bb3c521510c07e1367ad53bc0d17
Mathieu Poumeyrol <kali@zoy.org>
2023-10-02 14:40:02 +0200

    fix feature detection

--- linalg/src/arm64.rs
+++ linalg/src/arm64.rs
@@ -37,9 +37,24 @@ fn max_cpuid() -> std::io::Result<String> {
     Ok(max.unwrap_or("").to_string())
 }
 
+lazy_static::lazy_static! {
+    static ref CPU_FEATURES: Vec<String> = {
+        let cpu_info = std::fs::read_to_string("/proc/cpuinfo").unwrap();
+        let line = cpu_info
+            .lines()
+            .filter(|line| line.starts_with("Features"))
+            .next().unwrap();
+        line.split_once(":").unwrap().1.split_whitespace().map(|s| s.to_string()).collect()
+    };
+
+    static ref HAS_FP16: bool = {
+        CPU_FEATURES.iter().find(|s| &**s == "asimdhp").is_some()
+    };
+}
+
 #[inline]
 pub fn has_fp16() -> bool {
-    cfg!(feature_cpu = "fp16")
+    *HAS_FP16
 }
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
@@ -150,6 +165,7 @@ pub fn plug(ops: &mut Ops) {
     #[cfg(not(feature = "no_fp16"))]
     if has_fp16() {
         if *KIND == Kind::CortexA55 {
+            log::info!("Cortex-A55 mmm_f16 and mmv_f16 activated");
             ops.mmm_f16 = Box::new(|_, _, n| {
                 use tract_data::internal::DimLike;
                 if n.unwrap_or(1024).divceil(4) * 4 < n.unwrap_or(1024).divceil(8) * 8 {
@@ -160,6 +176,7 @@ pub fn plug(ops: &mut Ops) {
             });
             ops.mmv_f16 = Box::new(|_, _| arm64fp16_mmm_f16_128x1_a55::mmm());
         } else {
+            log::info!("ARMv8.2 mmm_f16 and mmv_f16 activated");
             ops.mmm_f16 = Box::new(|_, _, n| {
                 use tract_data::internal::DimLike;
                 if n.unwrap_or(1024).divceil(4) * 4 < n.unwrap_or(1024).divceil(8) * 8 {
@@ -175,6 +192,7 @@ pub fn plug(ops: &mut Ops) {
     ops.tanh_f32 = Box::new(|| arm64simd_tanh_f32_4n::ew());
     #[cfg(not(feature = "no_fp16"))]
     if has_fp16() {
+        log::info!("ARMv8.2 tanh_f16 and sigmoid_f16 activated");
         ops.tanh_f16 = Box::new(|| arm64fp16_tanh_f16_8n::ew());
         ops.sigmoid_f16 = Box::new(|| arm64fp16_sigmoid_f16_8n::ew());
     }
