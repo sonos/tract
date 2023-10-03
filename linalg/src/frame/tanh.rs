785aa3c00186c1b08a84e93429d9c1a062beaf4e
Mathieu Poumeyrol <kali@zoy.org>
2023-10-03 10:46:24 +0200

    add params with usize as default

--- linalg/src/frame/tanh.rs
+++ linalg/src/frame/tanh.rs
@@ -1,6 +1,6 @@
 macro_rules! tanh_impl {
     ($ti: ident, $func: ident, $nr: expr, $alignment_items: expr, $cond: expr) => {
-        ew_impl!($ti, $func, $nr, $alignment_items);
+        ew_impl!($ti, $func, $nr, $alignment_items, usize);
         #[cfg(test)]
         paste! {
             mod [<test_ $func>] {
@@ -81,7 +81,7 @@ pub mod test {
                         .iter()
                         .map(|x| <f32 as num_traits::AsPrimitive<$t>>::as_(*x))
                         .collect();
-                    <$ker>::ew().run(&mut input).unwrap();
+                    <$ker>::ew().run(&mut input, 0).unwrap();
                     tensor1(&input)
                         .close_enough(&tensor1(&expected), Approximation::Close)
                         .unwrap();
