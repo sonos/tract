89027a5943873b1a83f4be3a6a4eb5bec77204cf
Mathieu Poumeyrol <kali@zoy.org>
2023-10-31 16:22:54 +0100

    run these on CI too

--- .travis/native.sh
+++ .travis/native.sh
@@ -4,24 +4,7 @@ set -ex
 
 rustup update
 
-cargo check --all-targets
-
 ./.travis/regular-tests.sh
-
-cargo test -q -p tract-core --features paranoid_assertions
-
-if [ -n "$CI" ]
-then
-    cargo clean
-fi
-
-# useful as debug_asserts will come into play
-cargo -q test -q -p test-onnx-core
-cargo -q test -q -p test-onnx-nnef-cycle
-
-cargo check -p tract-nnef --features complex
-cargo check -p tract-tflite
-cargo check -p tract --no-default-features
 
 if [ -n "$CI" ]
 then
