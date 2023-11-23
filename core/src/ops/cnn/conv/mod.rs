fce9ec85d0c7bb3c7d0edbce4780dfc087590542
Mathieu Poumeyrol <kali@zoy.org>
2023-11-23 13:08:53 +0100

    simpler code for kernel reformatting

--- core/src/ops/cnn/conv/mod.rs
+++ core/src/ops/cnn/conv/mod.rs
@@ -54,46 +54,42 @@ impl KernelFormat {
         &full_shape[self.o_axis(full_shape)]
     }
 
-    pub fn input_channels<'s, D: DimLike>(&self, full_kernel_shape: &'s [D], group: usize) -> Cow<'s, D> {
+    pub fn input_channels<'s, D: DimLike>(
+        &self,
+        full_kernel_shape: &'s [D],
+        group: usize,
+    ) -> Cow<'s, D> {
         match self {
             KernelFormat::OIHW => Cow::Owned(self.i(full_kernel_shape).clone() * group),
             KernelFormat::HWIO | KernelFormat::OHWI => Cow::Borrowed(self.i(full_kernel_shape)),
         }
     }
 
-    pub fn output_channels<'s, D: DimLike>(&self, full_kernel_shape: &'s [D], group: usize) -> Cow<'s, D> {
+    pub fn output_channels<'s, D: DimLike>(
+        &self,
+        full_kernel_shape: &'s [D],
+        group: usize,
+    ) -> Cow<'s, D> {
         match self {
             KernelFormat::OIHW => Cow::Borrowed(self.o(full_kernel_shape)),
-            KernelFormat::HWIO | KernelFormat::OHWI => Cow::Owned(self.o(full_kernel_shape).clone() * group),
+            KernelFormat::HWIO | KernelFormat::OHWI => {
+                Cow::Owned(self.o(full_kernel_shape).clone() * group)
+            }
         }
     }
 
     pub fn kernel_as_group_o_i_hw(&self, kernel: &Tensor, group: usize) -> TractResult<Tensor> {
-        let input_channels = self.input_channels(kernel.shape(), group);
-        let output_channels = self.output_channels(kernel.shape(), group);
-        let shape_g_o_i_hw = [
-            group,
-            output_channels.into_owned() / group,
-            input_channels.into_owned() / group,
-            self.hw(kernel.shape()).iter().product(),
-        ];
-        trace!("kernel shape (group, output, rest) = {:?}", shape_g_o_i_hw);
-        let hw_rank = kernel.rank() - 2;
+        let mut kernel = kernel.clone();
+        while kernel.rank() > 3 {
+            kernel = kernel.collapse_axis_with_next(self.h_axis());
+        }
         match self {
-            KernelFormat::HWIO => {
-                let mut hw_gi_o = kernel.clone();
-                for _ in 0..hw_rank - 1 {
-                    hw_gi_o = hw_gi_o.collapse_axis_with_next(0);
-                }
-                let hw_g_i_o = hw_gi_o.split_axis(1, group)?;
-                let g_o_i_hw = hw_g_i_o.move_axis(0, 3)?.move_axis(1, 2)?;
-                Ok(g_o_i_hw)
-            }
-            KernelFormat::OIHW => Ok(kernel.clone().into_shape(&shape_g_o_i_hw)?),
-            KernelFormat::OHWI => {
-                // move I to OIHW, then same as OIHW
-                Ok(kernel.clone().move_axis(kernel.rank() - 1, 1)?.into_shape(&shape_g_o_i_hw)?)
-            }
+            // g is on i
+            KernelFormat::HWIO => kernel.split_axis(1, group)?.move_axis(0, 3)?.move_axis(1, 2),
+            // g is on o
+            KernelFormat::OIHW => kernel.split_axis(0, group),
+            // g is on i
+            KernelFormat::OHWI => kernel.move_axis(1, 2)?.split_axis(1, group)?.move_axis(1, 0),
         }
     }
 
