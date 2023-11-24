5948f314aa9459d6b08fd2e6b9ba9071d8558d65
Mathieu Poumeyrol <kali@zoy.org>
2023-11-24 09:18:45 +0100

    wip tractifying kernel prep

--- core/src/ops/cnn/conv/mod.rs
+++ core/src/ops/cnn/conv/mod.rs
@@ -78,19 +78,57 @@ impl KernelFormat {
         }
     }
 
-    pub fn kernel_as_group_o_i_hw(&self, kernel: &Tensor, group: usize) -> TractResult<Tensor> {
-        let mut kernel = kernel.clone();
-        while kernel.rank() > 3 {
-            kernel = kernel.collapse_axis_with_next(self.h_axis());
-        }
+    pub fn kernel_as_group_o_i_hw_ops(
+        &self,
+        full_shape: &[impl DimLike],
+        group: usize,
+    ) -> TractResult<TVec<AxisOp>> {
+        let mut ops = tvec!();
+        ops.push(AxisOp::Reshape(
+            self.h_axis(),
+            self.hw(full_shape).iter().map(|t| t.to_dim()).collect(),
+            tvec!(self.hw(full_shape).iter().map(|t| t.to_dim()).product()),
+        ));
         match self {
             // g is on i
-            KernelFormat::HWIO => kernel.split_axis(1, group)?.move_axis(0, 3)?.move_axis(1, 2),
+            KernelFormat::HWIO => {
+                ops.push(AxisOp::Reshape(
+                    1,
+                    tvec!(self.i(full_shape).to_dim()),
+                    tvec!(group.to_dim(), self.i(full_shape).to_dim() / group),
+                ));
+                ops.push(AxisOp::Move(0, 3));
+                ops.push(AxisOp::Move(1, 2));
+            }
             // g is on o
-            KernelFormat::OIHW => kernel.split_axis(0, group),
+            KernelFormat::OIHW => {
+                ops.push(AxisOp::Reshape(
+                    0,
+                    tvec!(self.o(full_shape).to_dim()),
+                    tvec!(group.to_dim(), self.o(full_shape).to_dim() / group),
+                ));
+            }
             // g is on i
-            KernelFormat::OHWI => kernel.move_axis(1, 2)?.split_axis(1, group)?.move_axis(1, 0),
+            KernelFormat::OHWI => {
+                ops.push(AxisOp::Reshape(
+                    2,
+                    tvec!(self.i(full_shape).to_dim()),
+                    tvec!(group.to_dim(), self.i(full_shape).to_dim() / group),
+                )); // o hw g i
+                ops.push(AxisOp::Move(2, 0)); // g o hw i
+                ops.push(AxisOp::Move(2, 3));
+            }
         }
+        Ok(ops)
+    }
+
+    pub fn kernel_as_group_o_i_hw(&self, kernel: &Tensor, group: usize) -> TractResult<Tensor> {
+        let mut kernel = kernel.clone();
+        let ops = self.kernel_as_group_o_i_hw_ops(&kernel.shape(), group)?;
+        for op in &ops {
+            op.change_tensor(&mut kernel, false)?;
+        }
+        Ok(kernel)
     }
 
     pub fn kernel_as_group_o_ihw(&self, kernel: &Tensor, group: usize) -> TractResult<Tensor> {
