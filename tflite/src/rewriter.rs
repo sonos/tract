fe417221c1c9870a6ba64024962e446b8231004e
Mathieu Poumeyrol <kali@zoy.org>
2023-12-08 16:51:33 +0100

    q8 casts tests

--- tflite/src/rewriter.rs
+++ tflite/src/rewriter.rs
@@ -6,6 +6,7 @@ use tract_core::ops::cnn::{ConvUnary, PaddingSpec};
 use tract_core::ops::einsum::BasicMatMul;
 use tract_core::ops::element_wise::ElementWiseOp;
 use tract_core::ops::math::{add, sub, Recip};
+use tract_core::ops::matmul::mir_quant::wire_ensure_q8_flavour;
 use tract_core::ops::nn::{DataFormat, Softmax};
 use tract_core::tract_data::itertools::Itertools;
 
@@ -15,7 +16,7 @@ pub fn rewrite_for_tflite(model: &mut TypedModel) -> TractResult<()> {
         .with_rule_for("trivial_axes_around_matmul", trivial_axes_around_matmul)
         .with_rule_for("kernel_in_ohwi", kernel_in_ohwi)
         .with_rule_for("bias_as_vector", bias_as_vector)
-        .with_rule_for("homogeneous-convolution", homogeneous_convolution)
+        .with_rule_for("per_layer_in_u8", per_layer_in_u8)
         .with_rule_for("make_1d_2d", make_1d_2d)
         .with_rule_for("force_n_axis", force_n_axis)
         .with_rule_for("nchw-to-nhwc", nchw_to_nhwc)
@@ -127,7 +128,7 @@ fn bias_as_vector(
     Ok(Some(patch))
 }
 
-fn homogeneous_convolution(
+fn per_layer_in_u8(
     _ctx: &(),
     model: &TypedModel,
     node: &TypedNode,
@@ -138,69 +139,22 @@ fn homogeneous_convolution(
     let idt = input_fact.datum_type;
     let kernel_fact = model.outlet_fact(node.inputs[1])?;
     let kdt = kernel_fact.datum_type;
-    match (kdt.unquantized(), idt.unquantized()) {
-        (DatumType::I8, DatumType::U8) => {
-            todo!()
-            /*
-            let new_qp =
-            QParams::ZpScale { zero_point: kdt.zp_scale().0 + 128, scale: kdt.zp_scale().1 };
-            let new_dt = u8::datum_type().quantize(new_qp);
-            let mut new_kernel = conv.kernel.clone().into_tensor();
-            unsafe {
-            new_kernel
-            .as_slice_mut_unchecked::<u8>()
-            .iter_mut()
-            .for_each(|x| *x = x.wrapping_add(128));
-            new_kernel.set_datum_type(new_dt)
-            }
-            let mut new = conv.clone();
-            new.kernel = new_kernel.into_arc_tensor();
-            let mut patch = TypedModelPatch::default();
-            let mut wire = patch.taps(model, &node.inputs)?;
-            let k0_fix = patch.add_const(format!("{name}.128"), tensor0(128i32))?;
-            wire[1] = wire_with_rank_broadcast(
-            format!("{name}.fix_k0"),
-            &mut patch,
-            add(),
-            &[wire[1], k0_fix],
-            )?[0];
-            wire = patch.wire_node(name, new, &wire)?;
-            patch.shunt_outside(model, node.id.into(), wire[0])?;
-            Ok(Some(patch))
-            */
-        }
-        (DatumType::U8, DatumType::I8) => {
-            todo!()
-            /*
-            let new_qp =
-            QParams::ZpScale { zero_point: kdt.zp_scale().0 - 128, scale: kdt.zp_scale().1 };
-            let new_dt = i8::datum_type().quantize(new_qp);
-            let mut new_kernel = conv.kernel.clone().into_tensor();
-            unsafe {
-            new_kernel
-            .as_slice_mut_unchecked::<u8>()
-            .iter_mut()
-            .for_each(|x| *x = x.wrapping_sub(128));
-            new_kernel.set_datum_type(new_dt)
-            }
-            let mut new = conv.clone();
-            new.kernel = new_kernel.into_arc_tensor();
-            let mut patch = TypedModelPatch::default();
-            let mut wire = patch.taps(model, &node.inputs)?;
-            let k0_fix = patch.add_const(format!("{name}.128"), tensor0(128i32))?;
-            wire[1] = wire_with_rank_broadcast(
-            format!("{name}.fix_k0"),
-            &mut patch,
-            sub(),
-            &[wire[1], k0_fix],
-            )?[0];
-            wire = patch.wire_node(name, new, &wire)?;
-            patch.shunt_outside(model, node.id.into(), wire[0])?;
-            Ok(Some(patch))
-            */
-        }
-        _ => Ok(None),
+    if idt.is_float() || model.outlet_fact(node.inputs[6])?.shape.len() > 1 {
+        return Ok(None);
     }
+    if idt.unquantized() == u8::datum_type() && kdt.unquantized() == u8::datum_type() {
+        return Ok(None);
+    }
+    let mut patch = TypedModelPatch::default();
+    let wire = patch.taps(model, &node.inputs)?;
+    let [mut i, mut k, b, mut i0, is, mut k0, ks, o0, os] = &*wire else {
+        bail!("Unexpected number of inputs")
+    };
+    wire_ensure_q8_flavour(&mut patch, name, &mut i, "input", &mut i0, DatumType::U8)?;
+    wire_ensure_q8_flavour(&mut patch, name, &mut k, "kernel", &mut k0, DatumType::U8)?;
+    let output = patch.wire_node(name, conv.clone(), &[i, k, *b, i0, *is, k0, *ks, *o0, *os])?;
+    patch.shunt_outside(model, node.id.into(), output[0])?;
+    Ok(Some(patch))
 }
 
 fn force_n_axis(
