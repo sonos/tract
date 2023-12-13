2c6c2323118a4c0a2b8f4b3703779278a3ee57de
Mathieu Poumeyrol <kali@zoy.org>
2023-12-13 11:29:37 +0100

    dbg & ci script fix

--- examples/nnef-dump-mobilenet-v2/ci.sh
+++ examples/nnef-dump-mobilenet-v2/ci.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-wget https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz
+[ -e mobilenet_v2_1.4_224.tgz ] || wget https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz
 tar zxf mobilenet_v2_1.4_224.tgz
 
 cargo run -p tract -- mobilenet_v2_1.4_224_frozen.pb -i 1,224,224,3,f32 dump --nnef mobilenet.nnef.tgz
