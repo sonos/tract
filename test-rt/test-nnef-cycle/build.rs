2c63e7871f07fa2b8fe7c228218e0a92b9437dd0
Mathieu Poumeyrol <kali@zoy.org>
2023-12-13 09:21:24 +0100

    predump check for nnef

--- test-rt/test-nnef-cycle/build.rs
+++ test-rt/test-nnef-cycle/build.rs
@@ -2,6 +2,6 @@
 mod suite;
 
 fn main() {
-    suite::suite().test_runtime("nnef_cycle", "suite::suite()", "nnef_cycle()", "Approximation::Approximate");
+    suite::suite().test_runtime("nnef_cycle", "suite::suite()", "runtime()", "Approximation::Approximate");
 }
 
--- test-rt/test-nnef-cycle/src/lib.rs
+++ test-rt/test-nnef-cycle/src/lib.rs
@@ -8,34 +8,74 @@ use tract_onnx_opl::*;
 #[path="../suite.rs"]
 mod suite;
 
-struct NnefCyclingRuntime(Nnef);
+mod nnef_predump {
+    use super::*;
 
-impl Debug for NnefCyclingRuntime {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "NnefCyclingRuntime")
-    }
-}
+    struct NnefPredumpRuntime(Nnef);
 
-impl Runtime for NnefCyclingRuntime {
-    fn name(&self) -> Cow<str> {
-        "nnef_cycle".into()
+    impl Debug for NnefPredumpRuntime {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "NnefPredumpRuntime")
+        }
     }
 
-    fn prepare(&self, model: TypedModel) -> TractResult<Box<dyn Runnable>> {
-        info!("Store to NNEF");
-        let mut buffer = vec![];
-        self.0.write_to_tar(&model, &mut buffer)?;
-        info!("Reload from NNEF");
-        let reloaded = self.0.model_for_read(&mut &*buffer)?;
-        Ok(Box::new(Arc::new(reloaded.into_optimized()?.into_runnable()?)))
+    impl Runtime for NnefPredumpRuntime {
+        fn name(&self) -> Cow<str> {
+            "nnef_predump".into()
+        }
+
+        fn prepare(&self, model: TypedModel) -> TractResult<Box<dyn Runnable>> {
+            let mut model = model.clone();
+            tract_nnef::ser::rewrite_model(&mut model)?;
+            Ok(Box::new(Arc::new(model.into_optimized()?.into_runnable()?)))
+        }
     }
+
+    fn runtime() -> &'static NnefPredumpRuntime {
+        lazy_static::lazy_static! {
+            static ref RT: NnefPredumpRuntime = NnefPredumpRuntime(tract_nnef::nnef().with_onnx());
+        };
+        &RT
+    }
+
+    include!(concat!(env!("OUT_DIR"), "/tests/nnef_cycle.rs"));
 }
 
-fn nnef_cycle() -> &'static NnefCyclingRuntime {
-    lazy_static::lazy_static! {
-        static ref RT: NnefCyclingRuntime = NnefCyclingRuntime(tract_nnef::nnef().with_onnx());
-    };
-    &RT
-}
 
-include!(concat!(env!("OUT_DIR"), "/tests/nnef_cycle.rs"));
+mod nnef_cycle {
+    use super::*;
+
+    struct NnefCyclingRuntime(Nnef);
+
+    impl Debug for NnefCyclingRuntime {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "NnefCyclingRuntime")
+        }
+    }
+
+    impl Runtime for NnefCyclingRuntime {
+        fn name(&self) -> Cow<str> {
+            "nnef_cycle".into()
+        }
+
+        fn prepare(&self, model: TypedModel) -> TractResult<Box<dyn Runnable>> {
+            info!("Store to NNEF");
+            let mut buffer = vec![];
+            eprintln!("{model}");
+            self.0.write_to_tar(&model, &mut buffer)?;
+            info!("Reload from NNEF");
+            let reloaded = self.0.model_for_read(&mut &*buffer)?;
+            eprintln!("{reloaded}");
+            Ok(Box::new(Arc::new(reloaded.into_optimized()?.into_runnable()?)))
+        }
+    }
+
+    fn runtime() -> &'static NnefCyclingRuntime {
+        lazy_static::lazy_static! {
+            static ref RT: NnefCyclingRuntime = NnefCyclingRuntime(tract_nnef::nnef().with_onnx());
+        };
+        &RT
+    }
+
+    include!(concat!(env!("OUT_DIR"), "/tests/nnef_cycle.rs"));
+}
