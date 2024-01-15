db6ffd34c0731c7e2535fbc0c96fdc5affa07d3d
Mathieu Poumeyrol <kali@zoy.org>
2024-01-15 15:01:51 +0100

    fast max impl

--- linalg/src/frame/element_wise_helper.rs
+++ linalg/src/frame/element_wise_helper.rs
@@ -82,6 +82,49 @@ where
     Ok(red)
 }
 
+pub(crate) fn map_reduce_slice_with_alignment<T>(
+    vec: &mut [T],
+    f: impl Fn(&mut [T]) -> T,
+    nr: usize,
+    alignment_bytes: usize,
+    neutral: T,
+    reduce: impl Fn(T, T) -> T,
+) -> TractResult<T>
+where
+    T: LADatum,
+{
+    if vec.is_empty() {
+        return Ok(neutral);
+    }
+    let mut red = neutral;
+    unsafe {
+        TMP.with(|buffer| {
+            let mut buffer = buffer.borrow_mut();
+            buffer.ensure(nr * T::datum_type().size_of(), alignment_bytes);
+            let tmp = std::slice::from_raw_parts_mut(buffer.buffer as *mut T, nr);
+            let mut compute_via_temp_buffer = |slice: &mut [T], red: &mut T| {
+                tmp[..slice.len()].copy_from_slice(slice);
+                tmp[slice.len()..].fill(neutral);
+                *red = reduce(*red, f(tmp));
+                slice.copy_from_slice(&tmp[..slice.len()]);
+            };
+            let prefix_len = vec.as_ptr().align_offset(alignment_bytes).min(vec.len());
+            if prefix_len > 0 {
+                compute_via_temp_buffer(&mut vec[..prefix_len], &mut red);
+            }
+            let aligned_len = (vec.len() - prefix_len) / nr * nr;
+            if aligned_len > 0 {
+                let t = f(&mut vec[prefix_len..][..aligned_len]);
+                red = reduce(red, t);
+            }
+            if prefix_len + aligned_len < vec.len() {
+                compute_via_temp_buffer(&mut vec[prefix_len + aligned_len..], &mut red);
+            }
+        })
+    }
+    Ok(red)
+}
+
 std::thread_local! {
     static TMP: std::cell::RefCell<TempBuffer> = std::cell::RefCell::new(TempBuffer::default());
 }
