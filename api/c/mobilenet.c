7bc86cab7802d17469cd83ae4b7b18b36636ad66
Konstantina Papafragkaki <papafragakikonstantina@gmail.com>
2024-02-09 20:58:04 +0200

    Change code to return all fields of prediction.

--- api/c/mobilenet.c
+++ api/c/mobilenet.c
@@ -10,15 +10,45 @@
         fprintf(stderr, "Error calling tract: %s", tract_get_last_error());     \
         exit(1) ;                                                               \
     }                                                                           \
-}                                                                               \
+}
 
-TractValue *
-inference(char *model_name, TractValue *input, TractValue *input2)
+typedef struct prediction {
+    double pred;
+    int category;
+    TractValue *output;
+}prediction;
+
+prediction **
+init_predictions(int arg)
+{
+    prediction **inf = malloc(arg * sizeof(prediction));
+    for (int i = 0; i < arg; i++) {
+        inf[i] = malloc(sizeof(prediction));
+        inf[i]->pred = 0.0;
+        inf[i]->category = 0;
+        inf[i]->output = NULL;
+    }
+    return inf;
+}
+
+void
+free_predictions(prediction **inf, int arg)
+{
+    for (int i = 0; i < arg; i++) {
+        check(tract_value_destroy(&inf[i]->output));
+        assert(!inf[i]->output);
+        free(inf[i]);
+    }
+    free(inf);
+}
+
+prediction *
+inference(char *model_name, TractValue *input, TractValue *input2, prediction *inf)
 {
     struct timeval t1, t2;
     double elapsedTime;
 
-    // Initialize nnef parser
+    // Initialize onnx parser
     TractOnnx *onnx = NULL;
     check(tract_onnx_create(&onnx));
     assert(onnx);
@@ -30,7 +60,6 @@ inference(char *model_name, TractValue *input, TractValue *input2)
     assert(inference_model);
     assert(onnx);
 
-    // once the model is build, the framework is not necessary anymore
     check(tract_onnx_destroy(&onnx));
     assert(!onnx);
 
@@ -38,7 +67,6 @@ inference(char *model_name, TractValue *input, TractValue *input2)
     check(tract_inference_model_into_typed(&inference_model, &model));
     assert(model);
 
-    //Optimize the model
     check(tract_model_optimize(model));
     assert(model);
 
@@ -68,61 +96,60 @@ inference(char *model_name, TractValue *input, TractValue *input2)
             argmax = i;
         }
     }
+    assert(data[argmax] == max);
     printf("\nModel: %s\nMax is %f for category %d\n", model_name, max, argmax);
-    check(tract_value_destroy(&output));
 
-    // or spawn a state to run the model
+    check(tract_runnable_release(&runnable));
+    assert(!runnable);
+
+    /* // or spawn a state to run the model
     TractState *state = NULL;
     check(tract_runnable_spawn_state(runnable, &state));
     assert(state);
 
-    // runnable is refcounted by the spawned states, so we can release it now.
-    check(tract_runnable_release(&runnable));
-    assert(!runnable);
-
     check(tract_state_run(state, &input, &output));
 
     check(tract_value_as_bytes(output, NULL, NULL, NULL, (const void**) &data));
-    assert(data[argmax] == max);
+    check(tract_state_destroy(&state));
+    assert(!state);
+
+    printf("\nModel: %s\nMax is %f for category %d\n", model_name, max, argmax); */
 
     gettimeofday(&t2, NULL);
     elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
     elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
     printf("Model run in %f ms.\n", elapsedTime);
 
-    return output;
+    inf->output = output;
+    inf->pred = max;
+    inf->category = argmax;
+
+    return inf;
 }
 
 
-int main(int argc, char **argv) {
+int
+main(int argc, char **argv)
+{
     size_t shape[4] = {1, 3, 224, 224};
 
-    int calculated_shape = shape[1] * shape[2] * shape[3];
+    int calculated_shape = shape[0] * shape[1] * shape[2] * shape[3];
     float *image = malloc(calculated_shape*sizeof(float));
-    FILE *fd = fopen(argv[4], "rb");
+    FILE *fd = fopen(argv[argc - 1], "rb");
     assert(fread(image, sizeof(float), calculated_shape, fd) == calculated_shape);
     fclose(fd);
 
-    TractValue* input = NULL;
-    check(tract_value_from_bytes(TRACT_DATUM_TYPE_F32, 4, shape, image, &input));
+    prediction** preds = init_predictions(argc-1);
+
+    check(tract_value_from_bytes(TRACT_DATUM_TYPE_F32, 4, shape, image, &preds[0]->output));
     free(image);
 
-    TractValue* input1 = inference(argv[1], input, NULL);
+    //If model is cut inside a circle, the inference of the last model is gonna take the output of the 2 previous models, like input2, input3
+    for (int i = 1; i < argc-1; i++) {
+        preds[i] = inference(argv[i], preds[i-1]->output, NULL, preds[i]);
+    }
 
-    TractValue* input2 = inference(argv[2], input, NULL);
-    check(tract_value_destroy(&input));
-
-    TractValue* input3 = inference(argv[3], input2, NULL);
-
-    //third model
-    //TractValue* input4 = inference("../../../cONNXr/test/tiny_yolov2/model_2.onnx", input3, NULL);
-    //TractValue* input4 = inference("../../../cONNXr/test/mobilenetv2-1.0/mobilenetv2-1.0_test_22.onnx", input2, input3);
-    //check(tract_value_destroy(&input4));
-    
-
-    check(tract_value_destroy(&input1));
-    check(tract_value_destroy(&input3));
-    check(tract_value_destroy(&input2));
+    free_predictions(preds, argc-1);
 
     return 0;
 }
