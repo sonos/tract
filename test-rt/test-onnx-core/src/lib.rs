2e572106d0d8698e003f2682bca31984ceb28618
Mathieu Poumeyrol <kali@zoy.org>
2024-02-28 17:09:42 +0100

    remnants

--- test-rt/test-onnx-core/src/lib.rs
+++ test-rt/test-onnx-core/src/lib.rs
@@ -31,25 +31,3 @@ mod unoptimized {
     include!(concat!(env!("OUT_DIR"), "/tests/unoptimized.rs"));
 }
 
-mod as_blas {
-    use super::*;
-
-    pub fn as_blas() -> &'static AsBlasRuntime {
-        &AsBlasRuntime
-    }
-
-    #[derive(Debug)]
-    pub struct AsBlasRuntime;
-
-    impl Runtime for AsBlasRuntime {
-        fn name(&self) -> Cow<str> {
-            Cow::Borrowed("as_blas")
-        }
-        fn prepare(&self, mut model: TypedModel) -> TractResult<Box<dyn Runnable>> {
-            tract_core::transform::get_transformer("as-blas").unwrap().transform(&mut model)?;
-            Ok(Box::new(Arc::new(model.into_runnable()?)))
-        }
-    }
-
-    include!(concat!(env!("OUT_DIR"), "/tests/as_blas.rs"));
-}
