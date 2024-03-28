975bd4abc29822ef07c306bbb71dfd9635c4c656
Mathieu Poumeyrol <kali@zoy.org>
2024-03-28 15:29:55 +0100

    typo

--- nnef/src/deser.rs
+++ nnef/src/deser.rs
@@ -457,7 +457,7 @@ impl RValue {
                     let sym = builder.model.symbol_table.sym(&id.0);
                     Ok(Value::Dim(sym.into()))
                 } else {
-                    bail!("Can not resolve {:?}. Not a known identifier, and symbol introduction is forbiddent out of \"external\" shape field", id);
+                    bail!("Can not resolve {:?}. Not a known identifier, and symbol introduction is forbidden out of \"external\" shape field", id);
                 }
             }
             RValue::Invocation(inv) => builder
