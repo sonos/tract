a8820296a4f92261c3824bc28338342094436bb1
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-03-29 11:58:09 +0100

    model detection for fp16

--- linalg/src/arm64.rs
+++ linalg/src/arm64.rs
@@ -40,38 +40,6 @@ fn max_cpuid() -> std::io::Result<String> {
     Ok(max.unwrap_or("").to_string())
 }
 
-#[cfg(any(target_os = "macos"))]
-fn has_amx() -> bool {
-    true
-}
-
-#[cfg(any(target_os = "ios"))]
-fn has_amx() -> bool {
-    use std::ffi::{c_char, c_void, CStr, CString};
-    use std::ptr::null_mut;
-
-    extern "C" {
-        fn sysctlbyname(
-            name: *const c_char,
-            oldp: *mut c_void,
-            oldlenp: *mut isize,
-            newp: *mut c_void,
-            newlen: isize,
-        );
-    }
-
-    unsafe {
-        let mut len: isize = 0;
-        let name = CString::new("hw.machine").unwrap();
-        sysctlbyname(name.as_ptr(), null_mut(), &mut len, null_mut(), 0);
-        let mut buf = vec![0u8; len as _];
-        sysctlbyname(name.as_ptr(), buf.as_mut_ptr() as _, &mut len, null_mut(), 0);
-        let version = CStr::from_bytes_with_nul(&buf).unwrap().to_string_lossy().into_owned();
-        let major = version.trim_start_matches("iPhone").split_once(",").unwrap().0;
-        // iPhone12,1 is the one branded "iPhone 11", with Apple A13 bionic, first CPU featuring amx
-        major.parse::<usize>().unwrap() >= 12
-    }
-}
 
 lazy_static::lazy_static! {
     static ref KIND: Kind = Kind::choose();
@@ -98,7 +66,55 @@ lazy_static::lazy_static! {
     };
 }
 
+#[cfg(target_os = "ios")]
+lazy_static::lazy_static! {
+    static ref IPHONE_MODEL_MAJOR:Option<usize> = {
+        use std::ffi::{c_char, c_void, CStr, CString};
+        use std::ptr::null_mut;
+
+        extern "C" {
+            fn sysctlbyname(
+                name: *const c_char,
+                oldp: *mut c_void,
+                oldlenp: *mut isize,
+                newp: *mut c_void,
+                newlen: isize,
+            );
+        }
+
+        unsafe {
+            let mut len: isize = 0;
+            let name = CString::new("hw.machine").unwrap();
+            sysctlbyname(name.as_ptr(), null_mut(), &mut len, null_mut(), 0);
+            let mut buf = vec![0u8; len as _];
+            sysctlbyname(name.as_ptr(), buf.as_mut_ptr() as _, &mut len, null_mut(), 0);
+            let version = CStr::from_bytes_with_nul(&buf).unwrap().to_string_lossy().into_owned();
+            let Some((major, _)) = version.trim_start_matches("iPhone").split_once(",") else { return None };
+            major.parse::<usize>().ok()
+        }
+    };
+}
+
+#[cfg(target_os = "macos")]
+fn has_amx() -> bool {
+    true
+}
+
+#[cfg(target_os = "ios")]
+fn has_amx() -> bool {
+    // iPhone12,1 is the one branded "iPhone 11", with Apple A13 bionic, first CPU featuring amx
+    IPHONE_MODEL_MAJOR.map(|it| it >= 12).unwrap_or(false)
+}
+
 #[inline]
+#[cfg(target_os = "ios")]
+pub fn has_fp16() -> bool {
+    // iPhone10,1 is the one branded "iPhone 8", with Apple A11 bionic, first CPU featuring fp16
+    IPHONE_MODEL_MAJOR.map(|it| it >= 10).unwrap_or(false)
+}
+
+#[inline]
+#[cfg(not(target_os = "ios"))]
 pub fn has_fp16() -> bool {
     cfg!(target_os = "macos")
         || cfg!(feature_cpu = "fp16")
@@ -284,7 +300,10 @@ pub fn plug(ops: &mut Ops) {
     #[cfg(any(target_os = "macos", target_os = "ios"))]
     {
         if has_amx() {
+            log::info!("AMX optimisation activated");
             ops.mmm_f32 = Box::new(|_, _, _| apple_amx::apple_amx_mmm_f32_32x32::mmm());
+        } else {
+            log::info!("Noe AMX optimisation");
         }
     }
 }
