824a6bde90e219a8d5c5078e80ea56f3da165f99
Julien Balian <julien.balian@sonos.com>
2024-04-15 18:32:03 +0200

    fix: cast k to i64 in trilu ops deserialization

--- nnef/src/ops/core/trilu.rs
+++ nnef/src/ops/core/trilu.rs
@@ -1,6 +1,7 @@
 use crate::internal::*;
 use crate::ser::*;
 use tract_core::ops::array::Trilu;
+use tract_core::ops::cast::cast;
 
 pub fn register(registry: &mut Registry) {
     registry.register_dumper(ser_trilu);
@@ -8,7 +9,7 @@ pub fn register(registry: &mut Registry) {
         "tract_core_trilu",
         &[
             TypeName::Scalar.tensor().named("input"),
-            TypeName::Scalar.tensor().named("k"),
+            TypeName::Integer.tensor().named("k"),
             TypeName::Logical.named("upper"),
         ],
         &[("output", TypeName::Scalar.tensor())],
@@ -26,5 +27,6 @@ fn de_trilu(builder: &mut ModelBuilder, invocation: &ResolvedInvocation) -> Trac
     let input = invocation.named_arg_as(builder, "input")?;
     let k = invocation.named_arg_as(builder, "k")?;
     let upper = invocation.named_arg_as(builder, "upper")?;
-    builder.wire(Trilu { upper }, &[input, k])
+    let k_casted = builder.wire_as_outlets(cast(DatumType::I64), &[k])?[0];
+    builder.wire(Trilu { upper }, &[input, k_casted])
 }
