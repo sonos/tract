130a67b0088db3583f4828194f54c78612a58fe3
Mathieu Poumeyrol <kali@zoy.org>
2024-04-16 09:04:09 +0200

    wip, heuristics

--- core/src/model/order.rs
+++ core/src/model/order.rs
@@ -1,10 +1,7 @@
 //! Evaluation order for nodes.
 use crate::internal::*;
 use bit_set::BitSet;
-use std::collections::BinaryHeap;
-use std::default;
 use std::fmt::{Debug, Display};
-use std::iter::once;
 use tract_data::itertools::Itertools;
 
 /// Find an evaluation order for a model, using its default inputs and outputs
@@ -20,6 +17,7 @@ where
 }
 
 /// Find a working evaluation order for a list of nodes.
+/// This algorithm starts from the outputs, so it will only compute what is necessary.
 pub fn eval_order_for_nodes<F, O>(
     nodes: &[Node<F, O>],
     model_inputs: &[usize],
@@ -98,7 +96,7 @@ where
 /// Find an evaluation order for a list of nodes trying to minimize memory occupation.
 pub fn eval_order_opt_ram_for_nodes<F, O>(
     nodes: &[Node<F, O>],
-    _model_inputs: &[usize],
+    model_inputs: &[usize],
     model_outputs: &[usize],
     more_dependencies: &[(usize, usize)],
 ) -> TractResult<Vec<usize>>
@@ -106,10 +104,15 @@ where
     F: Fact + Clone + 'static,
     O: Debug + Display + AsRef<dyn Op> + AsMut<dyn Op> + Clone + 'static,
 {
+    let tocompute: BitSet =
+        eval_order_for_nodes(nodes, model_inputs, model_outputs, more_dependencies)?
+            .into_iter()
+            .collect();
+
     let mut ups = vec![tvec!(); nodes.len()];
     let mut downs = vec![tvec!(); nodes.len()];
-    for (ix, node) in nodes.iter().enumerate() {
-        for input in &node.inputs {
+    for ix in tocompute.iter() {
+        for input in &nodes[ix].inputs {
             if !ups[ix].contains(&input.node) {
                 ups[ix].push(input.node);
                 downs[input.node].push(ix);
@@ -122,192 +125,74 @@ where
             downs[*up].push(*down);
         }
     }
-    let costs: Vec<usize> = nodes
-        .iter()
-        .map(|node| {
-            node.outputs
-                .iter()
-                .map(|o| {
-                    o.fact
-                        .to_typed_fact()
-                        .map(|f| {
-                            f.datum_type.size_of()
-                                * f.shape
-                                    .as_concrete()
-                                    .map(|dims| dims.iter().product())
-                                    .unwrap_or(1)
-                        })
-                        .unwrap_or(1)
-                })
-                .sum()
-        })
-        .collect_vec();
 
-    struct DFS {
-        nodes: usize,
-        costs: Vec<usize>,
+    struct Dfs {
         ups: Vec<TVec<usize>>,
         downs: Vec<TVec<usize>>,
-        outputs: Vec<usize>,
     }
 
-    let dfs = DFS { nodes: nodes.len(), costs, ups, downs, outputs: model_outputs.to_vec() };
+    let dfs = Dfs { ups, downs };
 
     #[derive(Debug, Default, Clone, PartialEq, Eq)]
     struct Path {
         order: Vec<usize>,
+        done: BitSet,
         alive: Vec<usize>,
-        space_now: usize,
-        space_max: usize,
-        score: usize,
     }
 
     impl Path {
-        /*
-        fn candidates(&self, dfs: &DFS) -> Vec<usize> {
-        (0..dfs.nodes)
-        .filter(|node| {
-        !self.order.contains(node)
-        && dfs.ups[*node].iter().all(|up| self.alive.contains(up))
-        })
-        .collect()
+        fn follow_one(&mut self, dfs: &Dfs, next: usize) {
+            assert!(!self.done.contains(next));
+            self.order.push(next);
+            self.done.insert(next);
+            self.alive.push(next);
+            self.alive.retain(|n| dfs.downs[*n].iter().any(|down| !self.done.contains(*down)))
         }
+    }
 
-        fn follow(&self, dfs: &DFS, next: usize) -> Path {
-        let it = self.follow_one(dfs, next);
-        if let [one] = it.candidates(dfs)[..] {
-        it.follow(dfs, one)
+    let mut done: Path = Path::default();
+    for i in model_inputs {
+        if tocompute.contains(*i) {
+            done.follow_one(&dfs, *i);
+        }
+    }
+
+    while !model_outputs.iter().all(|o| done.done.contains(*o)) {
+        let candidates: Vec<usize> = done
+            .alive
+            .iter()
+            .copied()
+            .flat_map(|n| dfs.downs[n].iter())
+            .copied()
+            .filter(|n| !done.done.contains(*n))
+            .sorted()
+            .unique()
+            .collect_vec();
+        if let Some(next) =
+            candidates.iter().copied().find(|n| dfs.ups[*n].iter().all(|n| done.done.contains(*n)))
+        {
+            done.follow_one(&dfs, next);
         } else {
-        it
-        }
-        }
-        */
-        fn follow_one(&self, dfs: &DFS, next: usize) -> Path {
-            let order = self.order.iter().copied().chain(once(next)).collect_vec();
-            let alive = self
-                .alive
-                .iter()
-                .copied()
-                .chain(once(next))
-                .filter(|n| dfs.downs[*n].iter().any(|down| !order.contains(down)))
-                .sorted()
-                .collect_vec();
-            let space_now = alive.iter().map(|a| dfs.costs[*a]).sum::<usize>();
-            let space_max = space_now.max(self.space_max);
-            let score = self.score + space_now;
-            Path { order, alive, space_now, space_max, score }
-        }
-    }
-
-    impl PartialOrd for Path {
-        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-            Some(self.cmp(other))
-        }
-    }
-
-    impl Ord for Path {
-        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-            self.space_max
-                .cmp(&other.space_max)
-                .then(self.score.cmp(&other.score))
-                .then(self.order.cmp(&other.order))
-        }
-    }
-
-    type Cache = HashMap<BitSet, Path>;
-
-    /*
-    impl DFS {
-    fn reach<'c>(&self, cache: &'c mut Cache, to: &BitSet) -> &'c Path {
-    if !cache.contains_key(to) {
-    let path = if to.len() == 0 {
-    Path::default()
-    } else {
-    to.iter()
-    .map(|last| {
-    let mut previous = to.clone();
-    previous.remove(last);
-    previous.extend(self.ups[last].iter().copied());
-    let mut path = self.reach(cache, &previous).clone();
-    if !path.order.contains(&last) {
-    path = path.follow_one(self, last);
-    }
-    path
-    })
-    .min()
-    .unwrap()ow
-    };
-    cache.insert(to.clone(), path);
-    }
-    &cache[to]
-    }
-    }
-    Ok(dfs.reach(&mut Default::default(), &dfs.outputs.iter().copied().collect()).order.clone())
-    */
-
-    let mut done = HashMap::<BitSet, Path>::default();
-    let mut todo: Vec<BitSet> = vec![];
-    let target:BitSet = dfs.outputs.iter().copied().collect();
-    done.insert(BitSet::default(), Path::default());
-    todo.push(target.clone());
-    while let Some(current) = todo.pop() {
-  //      println!("Stack: {todo:?}");
-        if done.contains_key(&current) {
-            continue;
-        }
- //       println!("Computing: {current:?}");
-        let mut best: Option<Path> = None;
-        let mut incomplete = false;
-        for last in current.iter() {
-            let mut previous = current.clone();
-            previous.remove(last);
-            previous.extend(dfs.ups[last].iter().copied());
-            if let Some(prec) = done.get(&previous) {
-                let path = prec.follow_one(&dfs, last);
-                if let Some(b) = best {
-                    best = Some(b.min(path));
-                } else {
-                    best = Some(path)
+            let next = candidates.first().copied().unwrap_or_else(|| {
+                tocompute.iter().find(|n| !done.done.contains(*n)).unwrap()
+            });
+            let mut stack: Vec<usize> = vec![next];
+            while stack.len() > 0 {
+                let target = *stack.last().unwrap();
+                for up in &dfs.ups[target] {
+                    if !done.done.contains(*up) {
+                        stack.push(*up);
+                    }
                 }
-            } else {
-                if !incomplete {
-//                    println!("  Pushing {current:?} again");
-                    todo.push(current.clone());
+                if *stack.last().unwrap() == target {
+                    done.follow_one(&dfs, target);
+                    stack.pop();
                 }
- //               println!("  Pushing {previous:?}");
-                todo.push(previous);
-                incomplete = true;
             }
         }
-        if !incomplete {
-//            println!("######## {current:?} ====> {best:?}");
-            done.insert(current, best.unwrap());
-        }
     }
 
-    Ok(done[&target].order.clone())
-
-    /*
-    let mut to_explore = BinaryHeap::<Path>::new();
-    to_explore.push(Path::default());
-    let mut best = eval_order_for_nodes(nodes, _model_inputs, model_outputs, more_dependencies)?
-    .iter()
-    .fold(Path::default(), |path, next| path.follow_one(&dfs, *next));
-    while let Some(from) = to_explore.pop() {
-    println!("best {} ::: [{}] ::: {:?}", best.space_max, to_explore.len(), from);
-    if dfs.outputs.iter().all(|t| from.order.contains(t)) {
-    best = from;
-    continue;
-    }
-    for c in from.candidates(&dfs) {
-    let next = from.follow(&dfs, c);
-    if next.space_max < best.space_max {
-    to_explore.push(next);
-    }
-    }
-    }
-    Ok(best.order)
-    */
+    Ok(done.order.clone())
 }
 
 #[cfg(test)]
