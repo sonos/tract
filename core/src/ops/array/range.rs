93c5cee49d66e8bf6bad65b3b78adb5f92e7592c
Julien Balian <julien.balian@sonos.com>
2024-04-17 18:04:53 +0200

    fix: ensure step is scalar compatible with .shape.volume().is_one()

--- core/src/ops/array/range.rs
+++ core/src/ops/array/range.rs
@@ -92,9 +92,9 @@ impl TypedOp for Range {
         };
         ensure!(start.datum_type() == end.datum_type());
         ensure!(start.datum_type() == step.datum_type());
-        ensure!(start.is_scalar_compatible());
-        ensure!(end.is_scalar_compatible());
-        ensure!(step.is_scalar_compatible());
+        ensure!(start.shape.volume().is_one());
+        ensure!(end.shape.volume().is_one());
+        ensure!(step.shape.volume().is_one());
         if let (Some(start), Some(end), Some(step)) = (&start.konst, &end.konst, &step.konst) {
             let len = if start.datum_type() == TDim::datum_type() {
                 let start = start.to_scalar::<TDim>()?;
