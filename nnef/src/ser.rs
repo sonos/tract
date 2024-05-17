c9f9812af433eca77e1223c96fec2fc23f93ddad
Mathieu Poumeyrol <kali@zoy.org>
2024-05-17 10:19:01 +0200

    introduce broadcast tdim op

--- nnef/src/ser.rs
+++ nnef/src/ser.rs
@@ -446,6 +446,7 @@ pub fn tdim(dim: &TDim) -> RValue {
             .unwrap(),
         TDim::MulInt(x, y) => RValue::Binary(numeric(x).boxed(), "*".to_string(), tdim(y).boxed()),
         TDim::Div(x, y) => RValue::Binary(tdim(x).boxed(), "/".to_string(), numeric(y).boxed()),
+        TDim::Broadcast(_, _) => todo!(),
     }
 }
 
