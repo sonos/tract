8399baf2a61eb5e6144468edec487f4c19c8bda9
Mathieu Poumeyrol <kali@zoy.org>
2024-06-11 14:23:11 +0200

    neon arm32 i8/i32 fixes

--- linalg/src/arm32/armv7neon.rs
+++ linalg/src/arm32/armv7neon.rs
@@ -5,8 +5,6 @@ pub fn prefetch(start: *const u8, len: usize) {
     unsafe { armv7neon_prefetch(start, start.offset(len as isize)) }
 }
 
-MMMExternKernel!(i32, armv7neon_mmm_i32_8x4; 8, 4; 32, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(i32, armv7neon_mmm_i32_32x1; 32,1 ; 32, 4; 0, 0; prefetch, crate::arm32::has_neon());
 MMMExternKernel!(f32, armv7neon_mmm_f32_8x4_cortexa7; 8, 4; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
 MMMExternKernel!(f32, armv7neon_mmm_f32_8x4_cortexa9; 8, 4; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
 MMMExternKernel!(f32, armv7neon_mmm_f32_8x4_generic; 8, 4; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
@@ -16,6 +14,26 @@ MMMExternKernel!(f32, armv7neon_mmm_f32_8x6_generic; 8, 6; 4, 4; 0, 0; prefetch,
 MMMExternKernel!(f32, armv7neon_mmm_f32_32x1_cortexa7; 32, 1; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
 MMMExternKernel!(f32, armv7neon_mmm_f32_32x1_cortexa9; 32, 1; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
 MMMExternKernel!(f32, armv7neon_mmm_f32_32x1_generic; 32, 1; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
+
+MMMExternKernel!(i32, armv7neon_mmm_i32_8x4; 8, 4; 32, 4; 0, 0; prefetch, crate::arm32::has_neon(),
+     packing_defs: {
+         const I8_A: Packer = Packer::new(DatumType::I8, 8, 32, 0);
+         const I8_B: Packer = Packer::new(DatumType::I8, 4, 4, 0);
+         const I8_I8: (&dyn MMMInputFormat, &dyn MMMInputFormat) = (&I8_A, &I8_B);
+     },
+     packings: I8_I8,
+     test: mmm_kernel_tests!{ true, armv7neon_mmm_i32_8x4, i8i8:1, i8, i8, i32, i32 }
+);
+
+MMMExternKernel!(i32, armv7neon_mmm_i32_32x1; 32,1 ; 32, 4; 0, 0; prefetch, crate::arm32::has_neon(),
+     packing_defs: {
+         const I8_A: Packer = Packer::new(DatumType::I8, 32, 32, 0);
+         const I8_B: Packer = Packer::new(DatumType::I8, 1, 4, 0);
+         const I8_I8: (&dyn MMMInputFormat, &dyn MMMInputFormat) = (&I8_A, &I8_B);
+     },
+     packings: I8_I8,
+     test: mmm_kernel_tests!{ true, armv7neon_mmm_i32_32x1, i8i8:1, i8, i8, i32, i32 }
+);
 
 sigmoid_impl!(f32, armv7neon_sigmoid_f32_4n, 4, 4, crate::arm32::has_neon());
 tanh_impl!(f32, armv7neon_tanh_f32_4n, 4, 4, crate::arm32::has_neon());
