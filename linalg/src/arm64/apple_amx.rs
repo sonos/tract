a7db76be039416ba2673f662e132b3d174dbb443
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-06-11 13:50:57 +0200

    fix amx declaration file

--- linalg/src/arm64/apple_amx.rs
+++ linalg/src/arm64/apple_amx.rs
@@ -1,3 +1,6 @@
+use crate::mmm::*;
+use tract_data::prelude::*;
+
 MMMExternKernel!(f32, apple_amx_mmm_f32_32x32; 32, 32; 128, 128; 1, 1; no_prefetch, crate::arm64::has_amx(),
            can_fuse: |f| !matches!(f, &FusedSpec::LeakyRelu(_))
 );
