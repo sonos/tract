47f320baadbb4e0aa3bfe1803d2286b1c88ce836
Hubert de La Jonquiere <hubert.delajonquiere@sonos.com>
2024-06-24 16:42:08 +0200

    Axis OP implementation WIP

--- metal/src/ops/mod.rs
+++ metal/src/ops/mod.rs
@@ -1,5 +1,6 @@
 pub mod binary;
 pub mod cast;
+pub mod change_axes;
 pub mod element_wise;
 pub mod gemm;
 pub mod konst;
@@ -7,6 +8,7 @@ pub mod sync;
 
 pub use binary::MetalBinOp;
 pub use cast::MetalCast;
+pub use change_axes::MetalAxisOp;
 pub use element_wise::MetalElementWiseOp;
 pub use gemm::MetalGemm;
 pub use konst::MetalConst;
