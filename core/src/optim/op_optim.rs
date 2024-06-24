7b5cda2be1b3339b41f91c09c1d6837d8232d1a9
Mathieu Poumeyrol <kali@zoy.org>
2024-06-24 15:55:43 +0200

    optimize optimizer

--- core/src/optim/op_optim.rs
+++ core/src/optim/op_optim.rs
@@ -1,4 +1,5 @@
 use crate::internal::*;
+use crate::ops::dummy::Dummy;
 
 use super::OptimizerSession;
 
@@ -18,15 +19,17 @@ impl OpOptim {
     fn full_pass(
         &mut self,
         session: &mut OptimizerSession,
-        new: &TypedModel,
+        model: &TypedModel,
     ) -> TractResult<Option<TypedModelPatch>> {
-        for (ix, &id) in new.eval_order()?.iter().enumerate().skip(self.2) {
-            let node = &new.nodes()[id];
-            let patch = (self.1)(node.op.as_ref(), session, new, node)
+        for node in &model.nodes[self.2..] {
+            if node.op_is::<Dummy>() {
+                continue;
+            }
+            let patch = (self.1)(node.op.as_ref(), session, model, node)
                 .with_context(|| format!("{self:?} node {node}"))?;
             if let Some(mut p) = patch {
                 p.push_context(format!("{self:?} {node}"));
-                self.2 = ix + p.dont_apply_twice.is_some() as usize;
+                self.2 = node.id + p.dont_apply_twice.is_some() as usize;
                 return Ok(Some(p));
             }
         }
