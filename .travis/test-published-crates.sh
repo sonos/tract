3bdd7a1b584ebdf2754db080f09b9f2251af5fbc
Mathieu Poumeyrol <kali@zoy.org>
2024-06-26 14:03:18 +0200

    split full

---
+++ .travis/test-published-crates.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
+if [ -e /proc/cpuinfo ]
+then
+    grep "^flags" /proc/cpuinfo | head -1 | \
+        grep --color=always '\(s\?sse[0-9_]*\|fma\|f16c\|avx[^ ]*\)'
+fi
+
+set -x
+
+. $(dirname $0)/ci-system-setup.sh
+
+set -e
+
+if [ `arch` = "x86_64" -a "$RUST_VERSION" = "stable" ]
+then
+    ALL_FEATURES=--all-features
+fi
+
+set +x
+
+for c in data linalg core nnef hir onnx pulse onnx-opl pulse-opl rs proxy
+do
+    echo
+    echo "$WHITE ### $c ### $NC"
+    echo
+    cargo -q test $CARGO_EXTRA -q -p tract-$c
+done
+
+# doc test are not finding libtensorflow.so
+if ! cargo -q test $CARGO_EXTRA -q -p tract-tensorflow --lib $ALL_FEATURES
+then
+    # this crate triggers an incremental bug on nightly.
+    cargo clean -p tract-tensorflow
+    cargo -q test $CARGO_EXTRA -q -p tract-tensorflow --lib $ALL_FEATURES
+fi
