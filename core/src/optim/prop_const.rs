f21a220958f164e155d6c6ab4b6c42714f274ef3
Mathieu Poumeyrol <kali@zoy.org>
2024-06-27 10:18:57 +0200

    faster (?) converence for prop-const

--- core/src/optim/prop_const.rs
+++ core/src/optim/prop_const.rs
@@ -20,14 +20,14 @@ impl super::TypedPass for PropConst {
         model: &TypedModel,
     ) -> TractResult<Option<TypedModelPatch>> {
         for node in &model.nodes[self.0..] {
-            let inputs = model.node_input_facts(node.id)?;
             if node.op_is::<Const>() && node.outputs[0].fact.konst.is_none() {
                 self.0 = node.id;
                 let mut patch = TypedModelPatch::default();
                 let wire = patch.add_const(&node.name, node.op_as::<Const>().unwrap().0.clone())?;
                 patch.shunt_outside(model, node.id.into(), wire)?;
-                return Ok(Some(patch))
+                return Ok(Some(patch));
             }
+            let inputs = model.node_input_facts(node.id)?;
             if !node.op_is::<Const>()
                 && !node.op_is::<Dummy>()
                 && !node.op_is::<TypedSource>()
@@ -37,13 +37,29 @@ impl super::TypedPass for PropConst {
                 let inputs =
                     inputs.iter().map(|f| f.konst.clone().unwrap().into_tvalue()).collect();
                 match node.op.eval_with_session(&SessionState::default(), inputs) {
-                    Ok(res) => {
+                    Ok(mut res) => {
+                        self.0 = node.id;
+                        let mut node = node;
+                        loop {
+                            let Some(succ) = model.single_succ(node.id)? else { break };
+                            if succ.inputs.len() > 1 || !succ.op.is_stateless() {
+                                break;
+                            }
+                            let Ok(succ_res) =
+                                succ.op.eval_with_session(&SessionState::default(), res.clone())
+                            else {
+                                break;
+                            };
+                            res = succ_res;
+                            node = succ;
+                        }
                         let mut patch = TypedModelPatch::default();
                         for (ix, output) in res.into_iter().enumerate() {
-                            let mut name = node.name.clone();
-                            if ix > 0 {
-                                name = format!("{name}.{ix}");
-                            }
+                            let name = if ix > 0 {
+                                format!("{}.{ix}", node.name)
+                            } else {
+                                node.name.clone()
+                            };
                             let wire = patch.add_const(name, output.into_arc_tensor())?;
                             patch.shunt_outside(model, (node.id, ix).into(), wire)?;
                         }
