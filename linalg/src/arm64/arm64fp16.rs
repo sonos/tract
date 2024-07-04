f3b5f3ea36aab9c964a9360332981803fff3b1f1
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-07-04 12:58:44 +0200

    introduce arm64fp16 64x1

--- linalg/src/arm64/arm64fp16.rs
+++ linalg/src/arm64/arm64fp16.rs
@@ -18,5 +18,7 @@ MMMExternKernel!(f16, arm64fp16_mmm_f16_32x4_a55; 32, 4; 16, 16; 1, 1; no_prefet
 MMMExternKernel!(f16, arm64fp16_mmm_f16_128x1_gen; 128, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
 MMMExternKernel!(f16, arm64fp16_mmm_f16_128x1_a55; 128, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
 
+MMMExternKernel!(f16, arm64fp16_mmm_f16_64x1_gen; 64, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
+
 tanh_impl!(f16, arm64fp16_tanh_f16_8n, 8, 8, crate::arm64::has_fp16());
 sigmoid_impl!(f16, arm64fp16_sigmoid_f16_8n, 8, 8, crate::arm64::has_fp16());
