7bce598e6c8de0a65d7ebac656bdfe58b90e9969
Hubert de La Jonquiere <hubert.delajonquiere@sonos.com>
2024-07-17 14:42:17 +0200

    Rewrite Model with dedicated RMS norm op

---
+++ metal/src/kernels/nn/rms_norm.rs
@@ -0,0 +1,289 @@
+use crate::{LibraryName, MetalContext, MetalTensor};
+use anyhow::Result;
+use metal::MTLSize;
+use tract_core::internal::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct RmsNorm;
+
+impl RmsNorm {
+    pub fn is_supported_dt(dt: DatumType) -> bool {
+        Self::tname(dt).is_ok()
+    }
+
+    pub fn tname(dt: DatumType) -> Result<&'static str> {
+        let tname = match dt {
+            DatumType::F32 => "f32",
+            DatumType::F16 => "f16",
+            _ => bail!("Unsupport dt {:?} for reducer op", dt),
+        };
+        Ok(tname)
+    }
+
+    pub fn kernel_name(&self, dt: DatumType) -> Result<String> {
+        let tname = Self::tname(dt)?;
+        Ok(format!("nn_ops::rms_norm_nd3_{tname}"))
+    }
+
+    pub fn reshape_to_rank_3(shape: &[usize], axis: usize) -> Vec<usize> {
+        let dim_axis_0 = shape[0..axis].iter().product::<usize>();
+        let dim_axis_1 = shape[axis];
+        let dim_axis_2 = shape[axis + 1..].iter().product::<usize>();
+
+        vec![dim_axis_0, dim_axis_1, dim_axis_2]
+    }
+
+    pub fn eval(
+        &self,
+        context: &MetalContext,
+        input: &MetalTensor,
+        axis: usize,
+        eps: &Tensor,
+    ) -> Result<MetalTensor> {
+        let o = self.dispatch_eval(context, input, axis, eps)?;
+        context.wait_until_completed()?;
+        Ok(o)
+    }
+
+    pub fn dispatch_eval(
+        &self,
+        context: &MetalContext,
+        input: &MetalTensor,
+        axis: usize,
+        eps: &Tensor,
+    ) -> Result<MetalTensor> {
+        input.retained_until_completion();
+
+        let o_dt = input.datum_type();
+        let o_shape = input.shape().to_vec();
+
+        let shape_nd3 = Self::reshape_to_rank_3(input.shape(), axis);
+        let strides_nd3 = Tensor::natural_strides(&shape_nd3);
+
+        let output = unsafe { MetalTensor::uninitialized_dt(o_dt, &o_shape)? };
+        output.retained_until_completion();
+
+        let pipeline = context
+            .shared_context()
+            .load_pipeline(LibraryName::NNOps, &self.kernel_name(input.datum_type())?)?;
+
+        let command_buffer = context.command_buffer();
+        let encoder = command_buffer.new_compute_command_encoder();
+        encoder.set_compute_pipeline_state(&pipeline);
+        encoder.set_buffer(0, Some(input.metal()), 0);
+        encoder
+            .set_bytes(1, o_dt.size_of() as _, unsafe { eps.as_ptr_unchecked::<u8>() } as *const _);
+        encoder.set_buffer(2, Some(output.metal()), 0);
+        encoder.set_bytes(
+            3,
+            (shape_nd3.len() * std::mem::size_of::<usize>()) as _,
+            shape_nd3.as_ptr() as *const _,
+        );
+        encoder.set_bytes(
+            4,
+            (strides_nd3.len() * std::mem::size_of::<usize>()) as _,
+            strides_nd3.as_ptr() as *const _,
+        );
+
+        let grid_size = MTLSize { width: shape_nd3[2] as _, height: 1, depth: shape_nd3[0] as _ };
+        let group_size = MTLSize { width: usize::min(32, shape_nd3[1]) as _, height: 1, depth: 1 };
+
+        encoder.use_resource(input.metal(), metal::MTLResourceUsage::Read);
+        encoder.use_resource(output.metal(), metal::MTLResourceUsage::Write);
+        encoder.dispatch_thread_groups(grid_size, group_size);
+        encoder.end_encoding();
+
+        Ok(output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::IntoMetal;
+    use derive_new::new;
+    use num_traits::AsPrimitive;
+    use num_traits::Float;
+    use proptest::collection::vec;
+    use proptest::prelude::*;
+    use tract_core::internal::Tensor;
+    // use tract_core::ops::nn::RmsNorm as TractRmsNorm;
+    // use tract_core::ops::nn::RmsNormExp;
+
+    // #[test]
+    // fn test_softmax_f32() -> Result<()> {
+    //     objc::rc::autoreleasepool(|| {
+    //         crate::METAL_CONTEXT.with_borrow(|context| {
+    //             let m = 4;
+    //             let k = 4;
+    //             let axis = 1;
+
+    //             let a =
+    //                 Tensor::from_shape(&[m, k], &(0..m * k).map(|f| f as f32).collect::<Vec<_>>())?
+    //                     .into_metal()?;
+
+    //             let cpu_softmax = TractRmsNorm {
+    //                 axes: tvec![axis],
+    //                 quant_output_dt: None,
+    //                 exp: RmsNormExp::Libc,
+    //             };
+
+    //             let cpu_output =
+    //                 cpu_softmax.eval(tvec![a.to_cpu().into_tvalue()])?[0].clone().into_tensor();
+    //             let metal_output = RmsNorm.eval(context, &a, axis)?;
+    //             cpu_output.close_enough(&metal_output.to_cpu(), Approximation::Approximate)?;
+    //             Ok(())
+    //         })
+    //     })
+    // }
+
+    // #[test]
+    // fn test_softmax_f32_2() -> Result<()> {
+    //     objc::rc::autoreleasepool(|| {
+    //         crate::METAL_CONTEXT.with_borrow(|context| {
+    //             let shape = [8, 4, 3];
+    //             let num_elements = shape.iter().product();
+    //             let axis = 0;
+
+    //             let a = Tensor::from_shape(
+    //                 &shape,
+    //                 &(0..num_elements).map(|f| f as f32 / 1000.0).collect::<Vec<_>>(),
+    //             )?
+    //             .into_metal()?;
+
+    //             let cpu_softmax = TractRmsNorm {
+    //                 axes: tvec![axis],
+    //                 quant_output_dt: None,
+    //                 exp: RmsNormExp::Libc,
+    //             };
+
+    //             let cpu_output =
+    //                 cpu_softmax.eval(tvec![a.to_cpu().into_tvalue()])?[0].clone().into_tensor();
+    //             let metal_output = RmsNorm.eval(context, &a, axis)?;
+    //             cpu_output.close_enough(&metal_output.to_cpu(), Approximation::Approximate)?;
+    //             Ok(())
+    //         })
+    //     })
+    // }
+
+    // #[test]
+    // fn test_softmax_f16() -> Result<()> {
+    //     objc::rc::autoreleasepool(|| {
+    //         crate::METAL_CONTEXT.with_borrow(|context| {
+    //             let m = 4;
+    //             let k = 4;
+    //             let axis = 1;
+
+    //             let a = Tensor::from_shape(
+    //                 &[m, k],
+    //                 &(0..m * k).map(|f| -> f16 { f.as_() }).collect::<Vec<_>>(),
+    //             )?
+    //             .into_metal()?;
+
+    //             let cpu_softmax = TractRmsNorm {
+    //                 axes: tvec![axis],
+    //                 quant_output_dt: None,
+    //                 exp: RmsNormExp::Libc,
+    //             };
+
+    //             let cpu_output =
+    //                 cpu_softmax.eval(tvec![a.to_cpu().into_tvalue()])?[0].clone().into_tensor();
+    //             let metal_output = RmsNorm.eval(context, &a, axis)?;
+    //             cpu_output.close_enough(&metal_output.to_cpu(), Approximation::Approximate)?;
+    //             Ok(())
+    //         })
+    //     })
+    // }
+
+    // proptest::proptest! {
+    //     #[test]
+    //     fn softmax_prop_f32(pb in any::<RmsNormProblem<f32>>()) {
+    //         fn run(pb: RmsNormProblem<f32>) -> TractResult<()> {
+    //             let out = pb.run()?;
+    //             let reference = pb.reference()?;
+
+    //             out.close_enough(&reference, Approximation::Approximate)
+    //                .with_context(|| anyhow!("Cpu: {:?}, Metal: {:?}", reference.dump(true), out.dump(true)))
+    //         }
+    //         run(pb).map_err(|e| TestCaseError::Fail(format!("{:?}", e).into()))?;
+    //     }
+
+    //     #[test]
+    //     fn softmax_prop_f16(pb in any::<RmsNormProblem<f16>>()) {
+    //         fn run(pb: RmsNormProblem<f16>) -> TractResult<()> {
+    //             let out = pb.run()?;
+    //             let reference = pb.reference()?;
+
+    //             out.close_enough(&reference, Approximation::Approximate)
+    //                .with_context(|| anyhow!("Cpu: {:?}, Metal: {:?}", reference.dump(true), out.dump(true)))
+    //         }
+
+    //         run(pb).map_err(|e| TestCaseError::Fail(format!("{:?}", e).into()))?;
+    //     }
+    // }
+
+    // #[derive(Debug, new)]
+    // pub struct RmsNormProblem<F: Datum + Float>
+    // where
+    //     F: Datum + Float,
+    //     usize: AsPrimitive<F>,
+    // {
+    //     pub shape: Vec<usize>,
+    //     pub axis: usize,
+    //     pub input: Vec<F>,
+    // }
+
+    // impl<F> Arbitrary for RmsNormProblem<F>
+    // where
+    //     F: Datum + Float,
+    //     usize: AsPrimitive<F>,
+    // {
+    //     type Parameters = ();
+    //     type Strategy = BoxedStrategy<Self>;
+
+    //     fn arbitrary_with(_: ()) -> Self::Strategy {
+    //         (0usize..3, 0usize..3)
+    //             .prop_flat_map(|(left, right)| {
+    //                 let axis = left;
+    //                 let shape_len = usize::min(left + right + 1, 4);
+    //                 let shape = 1usize..10;
+    //                 (vec(shape, shape_len..=shape_len), Just(axis))
+    //             })
+    //             .prop_map(|(shape, axis)| {
+    //                 let input = (0..shape.iter().product::<usize>())
+    //                     .map(|f| f.as_() / 1000.as_())
+    //                     .collect::<Vec<_>>();
+    //                 Self { shape, axis, input }
+    //             })
+    //             .boxed()
+    //     }
+    // }
+
+    // impl<F> RmsNormProblem<F>
+    // where
+    //     F: Datum + Float + std::ops::AddAssign,
+    //     usize: AsPrimitive<F>,
+    // {
+    //     pub fn reference(&self) -> Result<Tensor> {
+    //         let a = Tensor::from_shape(self.shape.as_slice(), &self.input)?;
+
+    //         let cpu_softmax = TractRmsNorm {
+    //             axes: tvec![self.axis],
+    //             quant_output_dt: None,
+    //             exp: RmsNormExp::Libc,
+    //         };
+    //         let cpu_output = cpu_softmax.eval(tvec![a.into_tvalue()])?[0].clone().into_tensor();
+    //         Ok(cpu_output)
+    //     }
+
+    //     pub fn run(&self) -> Result<Tensor> {
+    //         objc::rc::autoreleasepool(|| {
+    //             crate::METAL_CONTEXT.with_borrow(|context| {
+    //                 let a = Tensor::from_shape(self.shape.as_slice(), &self.input)?.into_metal()?;
+    //                 let metal_output = RmsNorm.eval(context, &a, self.axis)?;
+    //                 Ok(metal_output.to_cpu())
+    //             })
+    //         })
+    //     }
+    // }
+}
---
+++ metal/src/rewrite_rules.rs
@@ -0,0 +1,121 @@
+use std::sync::Arc;
+use tract_core::internal::*;
+use tract_core::ops::binary::TypedBinOp;
+use tract_core::ops::element_wise::ElementWiseOp;
+use tract_core::ops::konst::Const;
+use tract_core::ops::math::{Add, Mul, Rsqrt};
+use tract_core::ops::nn::{Reduce, Reducer};
+
+#[derive(Clone, Debug, Hash)]
+pub struct RewrittenRmsNorm {
+    pub axis: usize,
+    pub eps: Arc<Tensor>,
+}
+
+impl Op for RewrittenRmsNorm {
+    fn name(&self) -> Cow<str> {
+        format!("RewrittenRmsNorm").into()
+    }
+    fn info(&self) -> TractResult<Vec<String>> {
+        Ok(vec![format!("axis: {:?}, eps: {:?}", self.axis, self.eps)])
+    }
+    op_as_typed_op!();
+}
+
+impl EvalOp for RewrittenRmsNorm {
+    fn is_stateless(&self) -> bool {
+        true
+    }
+}
+
+impl TypedOp for RewrittenRmsNorm {
+    fn output_facts(&self, inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
+        let dt = inputs[0].datum_type;
+        let fact = dt.fact(inputs[0].shape.clone());
+        Ok(tvec!(fact))
+    }
+
+    as_op!();
+}
+
+macro_rules! rule_ensure {
+    ($cond:expr) => {
+        if !$cond {
+            return Ok(None);
+        }
+    };
+}
+
+fn next_node<'a>(model: &'a TypedModel, node: &TypedNode) -> Option<&'a TypedNode> {
+    if node.outputs.iter().map(|of| of.successors.len()).sum::<usize>() != 1 {
+        return None;
+    }
+    let succ = node.outputs[0].successors[0];
+    Some(&model.nodes()[succ.node])
+}
+
+fn collect_node_const_inputs<'a>(model: &'a TypedModel, node: &TypedNode) -> TVec<&'a Const> {
+    node.inputs
+        .iter()
+        .filter_map(|i| {
+            let prec = &model.nodes()[i.node];
+            prec.op_as::<Const>()
+        })
+        .collect::<TVec<_>>()
+}
+
+pub fn as_rms_norm_rule(
+    _ctx: &(),
+    model: &TypedModel,
+    node: &TypedNode,
+    node_name: &str,
+    op: &Reduce,
+) -> TractResult<Option<TypedModelPatch>> {
+    // Search pattern => A = A * RSQRT(MEAN_OF_SQUARES(A) + EPS);
+
+    rule_ensure!(op.reducer == Reducer::MeanOfSquares);
+    rule_ensure!(op.axes.len() == 1);
+    let axis = op.axes[0];
+
+    let in_fact = model.node_input_facts(node.id)?[0];
+    let dt = in_fact.datum_type;
+
+    // Only F16 and F32 is supported.
+    rule_ensure!(matches!(dt, DatumType::F32 | DatumType::F16));
+
+    let mut patch = TypedModelPatch::default();
+    let rsm_input = patch.taps(model, &node.inputs)?;
+
+    // Identify Add operator
+    let Some(add_succ) = next_node(model, node) else { return Ok(None) };
+    let Some(add_succ_op) = add_succ.op_as::<TypedBinOp>() else { return Ok(None) };
+    rule_ensure!(add_succ_op.0.is::<Add>());
+
+    // Retrieve epsilon
+    let add_consts = collect_node_const_inputs(model, add_succ);
+    rule_ensure!(add_consts.len() == 1);
+    let eps = add_consts[0].0.clone();
+    rule_ensure!(eps.len() == 1);
+    rule_ensure!(eps.datum_type() == dt);
+
+    // Identify Rsqrt
+    let Some(rsqrt_succ) = next_node(model, add_succ) else { return Ok(None) };
+    let Some(rsqrt_succ_op) = rsqrt_succ.op_as::<ElementWiseOp>() else { return Ok(None) };
+    rule_ensure!(rsqrt_succ_op.0.is::<Rsqrt>());
+
+    // Identify Mul
+    let Some(mul_succ) = next_node(model, rsqrt_succ) else { return Ok(None) };
+    let Some(mul_succ_op) = mul_succ.op_as::<TypedBinOp>() else { return Ok(None) };
+    rule_ensure!(mul_succ_op.0.is::<Mul>());
+    rule_ensure!(mul_succ.inputs.contains(&node.inputs[0]));
+
+    let out = patch.wire_node(
+        format!("{node_name}.rms_norm"),
+        RewrittenRmsNorm { axis, eps },
+        &rsm_input,
+    )?;
+
+    patch.shunt_outside(model, mul_succ.id.into(), out[0])?;
+
+    Ok(Some(patch))
+}
