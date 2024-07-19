af2e22368208e03871800610261f2ebf21cda38f
Hubert de La Jonquiere <hubert.delajonquiere@sonos.com>
2024-07-19 14:49:40 +0200

    Proptest Silu and RMS norm and fixes

--- metal/src/kernels/nn/silu.rs
+++ metal/src/kernels/nn/silu.rs
@@ -57,3 +57,162 @@ impl Silu {
         Ok(output)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::IntoMetal;
+    use derive_new::new;
+    use num_traits::AsPrimitive;
+    use num_traits::Float;
+    use proptest::collection::vec;
+    use proptest::prelude::*;
+    use tract_core::internal::Tensor;
+    use crate::rewrite_rules::BasicSilu;
+
+
+    fn test_case<F>(
+        shape: &[usize],
+        offset: f32,
+        scale: f32,
+        appriximate: Approximation,
+    ) -> Result<()>
+    where
+        F: Float + Datum,
+        usize: AsPrimitive<f32>,
+        f32: AsPrimitive<F>,
+    {
+        objc::rc::autoreleasepool(|| {
+            crate::METAL_CONTEXT.with_borrow(|context| {
+                let len = shape.iter().product::<usize>();
+
+                let a = Tensor::from_shape(
+                    shape,
+                    &(0..len)
+                        .map(|f| -> F {
+                            let v: f32 = f.as_();
+                            (v * scale + offset).as_()
+                        })
+                        .collect::<Vec<_>>(),
+                )?
+                .into_metal()?;
+
+                let cpu_output =
+                    BasicSilu.eval(tvec![a.to_cpu().into_tvalue()])?[0].clone().into_tensor();
+                let metal_output = Silu.eval(context, &a)?;
+
+                cpu_output
+                    .close_enough(&metal_output.to_cpu(), appriximate)
+                    .with_context(|| {
+                        anyhow!(
+                            "Input: {:?}, scale: {:?} Cpu: {:?}, Metal: {:?}",
+                            a.to_cpu().dump(true),
+                            scale,
+                            cpu_output.dump(true),
+                            metal_output.to_cpu().dump(true)
+                        )
+                    })?;
+                Ok(())
+            })
+        })
+    }
+
+
+    #[test]
+    fn test_silu() -> Result<()> {
+        test_case::<f32>(&[4, 4], -0.0, 1.0/100.0, Approximation::Approximate)?;
+        test_case::<f16>(&[4, 4], -6.0, 1.0/1000.0, Approximation::SuperApproximate)?;
+        Ok(())
+    }
+
+    proptest::proptest! {
+        #[test]
+        fn silu_prop_f32(pb in any::<SiluProblem<f32>>()) {
+            fn run(pb: SiluProblem<f32>) -> TractResult<()> {
+                let out = pb.run()?;
+                let reference = pb.reference()?;
+
+                out.close_enough(&reference, Approximation::Approximate)
+                   .with_context(|| anyhow!("Cpu: {:?}, Metal: {:?}", reference.dump(true), out.dump(true)))
+            }
+            run(pb).map_err(|e| TestCaseError::Fail(format!("{:?}", e).into()))?;
+        }
+
+        #[test]
+        fn silu_prop_f16(pb in any::<SiluProblem<f16>>()) {
+            fn run(pb: SiluProblem<f16>) -> TractResult<()> {
+                let out = pb.run()?;
+                let reference = pb.reference()?;
+
+                out.close_enough(&reference, Approximation::Approximate)
+                   .with_context(|| anyhow!("Cpu: {:?}, Metal: {:?}", reference.dump(true), out.dump(true)))
+            }
+
+            run(pb).map_err(|e| TestCaseError::Fail(format!("{:?}", e).into()))?;
+        }
+    }
+
+    #[derive(Debug, new)]
+    pub struct SiluProblem<F: Datum + Float>
+    where
+        F: Datum + Float,
+        usize: AsPrimitive<F>,
+        f32: AsPrimitive<F>,
+    {
+        pub shape: Vec<usize>,
+        pub input: Vec<F>,
+    }
+
+    impl<F> Arbitrary for SiluProblem<F>
+    where
+        F: Datum + Float,
+        usize: AsPrimitive<F>,
+        f32: AsPrimitive<F>,
+    {
+        type Parameters = ();
+        type Strategy = BoxedStrategy<Self>;
+
+        fn arbitrary_with(_: ()) -> Self::Strategy {
+            (0usize..3, 0usize..3)
+                .prop_flat_map(|(left, right)| {
+                    let shape_len = usize::min(left + right + 1, 4);
+                    let shape = 1usize..10;
+                    vec(shape, shape_len..=shape_len)
+                })
+                .prop_map(|shape| {
+                    let input = (0..shape.iter().product::<usize>())
+                        .map(|f| f.as_() / 1000.as_())
+                        .collect::<Vec<_>>();
+                    Self { shape, input }
+                })
+                .boxed()
+        }
+    }
+
+    impl<F> SiluProblem<F>
+    where
+        F: Datum + Float + std::ops::AddAssign,
+        usize: AsPrimitive<F>,
+        f32: AsPrimitive<F>,
+    {
+        pub fn reference(&self) -> Result<Tensor> {
+            let a = Tensor::from_shape(self.shape.as_slice(), &self.input)?;
+
+            let cpu_output =
+                    BasicSilu.eval(tvec![a.into_tvalue()])?[0].clone().into_tensor();
+
+            Ok(cpu_output)
+        }
+
+        pub fn run(&self) -> Result<Tensor> {
+            objc::rc::autoreleasepool(|| {
+                crate::METAL_CONTEXT.with_borrow(|context| {
+                    let a = Tensor::from_shape(self.shape.as_slice(), &self.input)?.into_metal()?;
+                    let metal_output = Silu.eval(context, &a)?;
+                    Ok(metal_output.to_cpu())
+                })
+            })
+        }
+    }
+
+}
