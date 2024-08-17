fa2c9941bcff060ff3c9eecf925552dbffcabd1f
Akbar Banitalebi <banitalebi.akbar@gmail.com>
2024-08-17 23:34:26 +0330

    fix keras example

--- examples/keras-tract-tf2/no-ci.sh
+++ examples/keras-tract-tf2/no-ci.sh
@@ -2,11 +2,16 @@
 
 set -e
 
-sudo apt-get update
-sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 python3-virtualenv
 
-virtualenv venv
-. venv/bin/activate
+sudo apt-get update
+
+# Install required libraries
+sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 python3.8 python3.8-venv
+
+# Create a Python 3.8 virtual environmen
+python3.8 -m venv venv
+source venv/bin/activate
+
 
 pip install -r requirements.txt
 python example.py
