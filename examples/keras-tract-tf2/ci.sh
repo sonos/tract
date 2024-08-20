74e5b66478c8310bbd56a0163e96d79f2634843e
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-08-20 09:12:51 +0200

    leave python version alone

--- examples/keras-tract-tf2/ci.sh
+++ examples/keras-tract-tf2/ci.sh
@@ -6,10 +6,9 @@ set -e
 sudo apt-get update
 
 # Install required libraries
-sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 python3.8 python3.8-venv
+sudo apt-get install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 python python-venv
 
-# Create a Python 3.8 virtual environmen
-python3.8 -m venv venv
+python -m venv venv
 source venv/bin/activate
 
 
