3652edcf7a1c50a0e9beda2f9d161fb76488a36a
heabeounMKTO <heabeoun69zin@gmail.com>
2024-09-03 17:45:05 +0700

    cosine distance too far on the same image?

--- examples/face_similarity_arcface_onnx/src/arc_face.rs
+++ examples/face_similarity_arcface_onnx/src/arc_face.rs
@@ -1,21 +1,23 @@
-use image::{imageops, DynamicImage};
+use image::{imageops, DynamicImage, GenericImageView};
 use tract_core::plan::SimplePlan;
 use tract_onnx::prelude::*;
 use anyhow::{Result, Error};
 use tract_ndarray::{ArrayBase, OwnedRepr};
-use tract_ndarray::Array1;
+use tract_ndarray::linalg::Dot;
+use tract_ndarray::{Array1, Array3};
 
 pub struct ArcFace {
     model: SimplePlan<TypedFact, Box<dyn TypedOp>, Graph<TypedFact, Box<dyn TypedOp>>>,
 }
 
-
 impl ArcFace {
-    // returns a vec size of [1, 512] representing a face
+    /// returns a ndarray of length 512 representing a face
+    /// please crop face before using !
     pub fn get_face_embedding(&self, input_image: &DynamicImage) -> Result<ArrayBase<OwnedRepr<f32>, tract_core::ndarray::Dim<[usize; 1]>>, Error> {
-        let preprocess_image = preprocess_arcface(input_image, 112);
+        let preprocess_image = preprocess_arcface(input_image, 112)?;
         let forward = self.model.run(tvec![preprocess_image.to_owned().into()])?; 
-        let results =  forward[0].to_array_view::<f32>()?.to_shape((512))?.to_owned();
+        println!("FORWARD {:?}", forward);
+        let results =  forward[0].to_array_view::<f32>()?.to_shape(512)?.to_owned();
         Ok(results)
     }
 }
@@ -23,6 +25,7 @@ impl ArcFace {
 pub fn load_arcface_model(model_path: &str, input_size: i32) -> ArcFace {
     let load_model = tract_onnx::onnx().model_for_path(model_path).unwrap()
         .with_input_fact(0, f32::fact([1,3,input_size, input_size]).into()).unwrap()
+        .incorporate().unwrap()
         .into_optimized().unwrap()
         .into_runnable().unwrap();
     ArcFace {
@@ -30,26 +33,32 @@ pub fn load_arcface_model(model_path: &str, input_size: i32) -> ArcFace {
     }
 }
 
+fn image_to_tract_tensor(img: &DynamicImage) -> Array3<f32> {
+    let height = img.height();
+    let width = img.width();
+    let img_buffer = img.to_rgb8();
+    Array3::from_shape_vec((height as usize, width as usize, 3), img_buffer.into_raw()).expect("cannot convert image to ndarray").mapv(|x| x as f32)
+}
+
 
 pub fn preprocess_arcface(
     input_image: &DynamicImage,
     target_size: u32 
-) -> Tensor {
-    let resized = image::imageops::resize(&input_image.to_rgb8(), target_size, target_size, image::imageops::FilterType::Triangle);
-    let mut ndarray_image = image::RgbImage::new(target_size , target_size);
-    image::imageops::replace(&mut ndarray_image, &resized, target_size as i64,target_size as i64);
-    let image: Tensor = tract_ndarray::Array4::from_shape_fn((1, 3, target_size as usize, target_size as usize), |(_, c, y, x)| {
-            let mut _a = ndarray_image.get_pixel(x as u32, y as u32)[c] as f32; 
-            _a -= 127.5;
-            _a *= 1.0 / 128.0;
-            _a
-    }).into();
-    image
+) -> Result<Tensor, Error> {
+    let resize = input_image.resize_exact(target_size, target_size, imageops::FilterType::Triangle);
+    let mut ndarray_img = image_to_tract_tensor(&resize);
+    ndarray_img -= 127.5;
+    ndarray_img *= 1.0 / 128.0;
+    let mut _final: Tensor = ndarray_img.permuted_axes((2,0,1)).into();
+    _final.insert_axis(0).unwrap();
+    println!("FINAL SHAPE: {:?}", _final.shape());
+    Ok(_final)
 }
 
 pub fn cosine_similarity(a: &Array1<f32>, b: &Array1<f32>) -> f32 {
     let dotprod = a.dot(b);
     let norm_a = a.dot(a).sqrt();
     let norm_b = b.dot(b).sqrt();
+    println!("dotprod {:?} norm_a {:?} norm_b {:?}", dotprod, norm_a, norm_b);
     dotprod / (norm_a * norm_b)
 }
--- examples/face_similarity_arcface_onnx/src/main.rs
+++ examples/face_similarity_arcface_onnx/src/main.rs
@@ -1,31 +1,53 @@
 mod yolo_face;
 mod arc_face;
-use arc_face::{load_arcface_model, cosine_similarity};
+use arc_face::{load_arcface_model, ArcFace};
 use yolo_face::{load_yolo_model, YoloFace, sort_conf_bbox};
 use clap::Parser;
 use anyhow::{Error, Result};
+use semanticsimilarity_rs::{cosine_similarity, euclidean_distance};
 
 #[derive(Parser)]
 struct CliArgs {
     #[arg(long)]
-    input_image: String,
-    
+    face1: String,
+
     #[arg(long)]
-    weights: String,
+    face2: String,
 }
 
 fn main() -> Result<(), Error>{
-    let test_image = image::open("/home/hbdesk/Downloads/cwigl2.jpeg")?;
+    let args = CliArgs::parse();
+    println!("face1 {:#?}, \n face2 {:?}", &args.face1, &args.face2);
+    let face1 = image::open(&args.face1)?;
+    let face2 = image::open(&args.face2)?;
+    
     let yolo_model: YoloFace = load_yolo_model("/home/hbdesk/face_api_opencv/face_api/models/yolov8n-face.onnx", (640,640));
-    let arcface_model = load_arcface_model("/home/hbdesk/face_api_opencv/face_api/models/arcfaceresnet100-8.onnx", 112);
+    let arcface_model: ArcFace = load_arcface_model("/home/hbdesk/face_api_opencv/face_api/models/arcfaceresnet100-8.onnx", 112);
     
-    let mut test_bbox= yolo_model.get_faces_bbox(&test_image, 0.5, 0.5)?; 
-    let sorted_bbox = sort_conf_bbox(&mut test_bbox);
+    let mut face1_bbox = yolo_model.get_faces_bbox(&face1, 0.5,0.5)?;
+    let mut face2_bbox = yolo_model.get_faces_bbox(&face2, 0.5, 0.5)?;
+
+
+    let f1_sorted_bbox = sort_conf_bbox(&mut face1_bbox);
+    let f2_sorted_bbox = sort_conf_bbox(&mut face2_bbox);
     
-    let crop_face = sorted_bbox[0].crop_bbox(&test_image)?;
-    let embedding = arcface_model.get_face_embedding(&crop_face)?;
-    let similarity = cosine_similarity(&embedding, &embedding);
-    println!("SIMILARITY {:?}", similarity);
+    println!("face1 {:?}, \n face2 {:?}", f1_sorted_bbox, f2_sorted_bbox);
+    
+    let f1_crop = f1_sorted_bbox[0].crop_bbox(&face1)?;
+    let f2_crop = f2_sorted_bbox[0].crop_bbox(&face2)?;
+
+    let f1_embed = arcface_model.get_face_embedding(&f1_crop)?;
+    let f2_embed = arcface_model.get_face_embedding(&f2_crop)?;
+
+    println!("F1 {:?}, \n\n\n\n F2 {:?}", f1_embed, f2_embed);
+    let f1_f64: Vec<f64> = {
+        f1_embed.into_raw_vec().iter().map(|x| x.to_owned() as f64).collect()
+    };
+    let f2_f64: Vec<f64> = {
+        f2_embed.into_raw_vec().iter().map(|x| x.to_owned() as f64).collect()
+    };
+    let similarity = cosine_similarity(&f1_f64, &f2_f64,false);
+    println!("SIMILARITY {:#?}", similarity);
     Ok(())
 }
 
