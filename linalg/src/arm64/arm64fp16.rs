cb71f519a7d2560a56331d1c1cea491897986b47
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-09-04 15:25:38 +0200

    introduce mmm_f16_64x3

--- linalg/src/arm64/arm64fp16.rs
+++ linalg/src/arm64/arm64fp16.rs
@@ -20,6 +20,8 @@ MMMExternKernel!(f16, arm64fp16_mmm_f16_32x4_a55; 32, 4; 16, 16; 1, 1; no_prefet
 MMMExternKernel!(f16, arm64fp16_mmm_f16_128x1_gen; 128, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
 MMMExternKernel!(f16, arm64fp16_mmm_f16_128x1_a55; 128, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
 
+MMMExternKernel!(f16, arm64fp16_mmm_f16_64x3_gen; 64, 3; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
+
 MMMExternKernel!(f16, arm64fp16_mmm_f16_64x1_gen; 64, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16(),
      packing_defs: {
          use crate::frame::block_quant::*;
