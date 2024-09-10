5aab0019c8b6e4a137e41b2e5c79c3f1c11d6116
Mathieu Poumeyrol <kali@zoy.org>
2024-09-10 09:47:05 +0200

    use release build for wasm

--- examples/face_detection_yolov8onnx_example/ci.sh
+++ examples/face_detection_yolov8onnx_example/ci.sh
@@ -11,8 +11,8 @@ cargo run -- --input-image grace_hopper.jpg --weights yolov8n-face.onnx
 wasmtime -V || curl https://wasmtime.dev/install.sh -sSf | bash # install wasmtime
 PATH=$PATH:$HOME/.wasmtime/bin
 rustup target install wasm32-wasi
-cargo build --target wasm32-wasi
-wasmtime --dir . ../../target/wasm32-wasi/debug/face_detection_yolov8onnx_example.wasm --input-image grace_hopper.jpg --weights yolov8n-face.onnx
+cargo build --target wasm32-wasi --release
+wasmtime --dir . ../../target/wasm32-wasi/release/face_detection_yolov8onnx_example.wasm --input-image grace_hopper.jpg --weights yolov8n-face.onnx
 
 cargo clean 
 rm yolov8n-face.onnx
--- examples/face_similarity_arcface_onnx/ci.sh
+++ examples/face_similarity_arcface_onnx/ci.sh
@@ -12,8 +12,8 @@ cargo run -- --face1 grace_hopper.jpeg --face2 grace_hopper2.jpeg
 wasmtime -V || curl https://wasmtime.dev/install.sh -sSf | bash # install wasmtime
 PATH=$PATH:$HOME/.wasmtime/bin
 rustup target install wasm32-wasi
-cargo build --target wasm32-wasi
-wasmtime --dir . ../../target/wasm32-wasi/debug/face_similarity_arcface_onnx.wasm --face1 grace_hopper.jpeg --face2 grace_hopper2.jpeg
+cargo build --target wasm32-wasi --release
+wasmtime --dir . ../../target/wasm32-wasi/release/face_similarity_arcface_onnx.wasm --face1 grace_hopper.jpeg --face2 grace_hopper2.jpeg
 
 cargo clean 
 rm yolov8n-face.onnx
