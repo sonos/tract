c7dc700c90d550734d6c4dd2f5b272e19c4373c4
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-09-12 17:10:40 +0200

    filter out metal on CI

--- .travis/test-rt.sh
+++ .travis/test-rt.sh
@@ -29,7 +29,7 @@ do
     if [ "$c" = "test-rt/test-tflite" ]
     then
         echo "$WHITE ### $c ### IGNORED $NC"
-    elif [ "$c" = "test-rt/test-metal" -a  `uname` != "Darwin" ]
+    elif [ "$c" = "test-rt/test-metal" -a  ( `uname` != "Darwin" -o -n "$CI" ) ]
     then
         echo "$WHITE ### $c ### IGNORED $NC"
     else
