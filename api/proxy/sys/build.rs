adca59269ce4ce99a481fa9a75acc6d1ccd12ee0
Mathieu Poumeyrol <kali@zoy.org>
2024-09-16 11:38:29 +0200

    rebuild on env var change

--- api/proxy/sys/build.rs
+++ api/proxy/sys/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo:rerun-if-env-changed=TRACT_DYLIB_SEARCH_PATH");
     if let Ok(path) = std::env::var("TRACT_DYLIB_SEARCH_PATH") {
         println!("cargo:rustc-link-search={path}");
     }
