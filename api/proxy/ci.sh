f73ec8ae629d0449a45bee5cd9e8607a75e9daf7
Mathieu Poumeyrol <kali@zoy.org>
2024-09-17 09:22:34 +0200

    fix asan ci test

--- api/proxy/ci.sh
+++ api/proxy/ci.sh
@@ -4,9 +4,9 @@ ROOT=$(dirname $(realpath $0))/../..
 
 set -ex
 
-cargo build --release -p tract-ffi
+cargo build --release -p tract-ffi $CARGO_EXTRA
 export TRACT_DYLIB_SEARCH_PATH=$ROOT/target/release
 export LD_LIBRARY_PATH=$ROOT/target/release
 
 cd $(dirname $(realpath $0))
-cargo test
+cargo test $CARGO_EXTRA
