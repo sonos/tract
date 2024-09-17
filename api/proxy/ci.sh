d6ab77d37f8fd123df82fd4134f41049e08b0d38
Mathieu Poumeyrol <kali@zoy.org>
2024-09-17 19:27:07 +0200

    try again to fix jq

--- api/proxy/ci.sh
+++ api/proxy/ci.sh
@@ -5,7 +5,7 @@ ROOT=$(dirname $(realpath $0))/../..
 set -ex
 
 cargo build --release -p tract-ffi $CARGO_EXTRA
-SO=$(cargo build  --message-format=json --release -p tract-ffi $CARGO_EXTRA | grep cdylib | jq -r '.filenames.[0]')
+SO=$(cargo build  --message-format=json --release -p tract-ffi $CARGO_EXTRA | grep cdylib | jq -r '.filenames .[0]')
 SO_PATH=$(dirname $SO)
 export TRACT_DYLIB_SEARCH_PATH=$SO_PATH
 export LD_LIBRARY_PATH=$SO_PATH
