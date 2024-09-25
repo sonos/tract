137ce335cd7899f76f4d32f68efba08df49a4091
Mathieu Poumeyrol <kali@zoy.org>
2024-09-25 16:02:15 +0200

    convert arm32 kernels

--- linalg/src/arm32/armv7neon.rs
+++ linalg/src/arm32/armv7neon.rs
@@ -1,38 +1,24 @@
-extern_kernel!(fn armv7neon_prefetch(start: *const u8, end: *const u8) -> ());
+use crate::frame::PackedFormat;
+use crate::DatumType;
 
-#[inline(always)]
-pub fn prefetch(start: *const u8, len: usize) {
-    unsafe { armv7neon_prefetch(start, start.offset(len as isize)) }
-}
+const NEON: fn() -> bool = || crate::arm32::has_neon();
 
-MMMExternKernel!(f32, armv7neon_mmm_f32_8x4_cortexa7; 8, 4; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(f32, armv7neon_mmm_f32_8x4_cortexa9; 8, 4; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(f32, armv7neon_mmm_f32_8x4_generic; 8, 4; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(f32, armv7neon_mmm_f32_8x6_cortexa7; 8, 6; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(f32, armv7neon_mmm_f32_8x6_cortexa9; 8, 6; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(f32, armv7neon_mmm_f32_8x6_generic; 8, 6; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(f32, armv7neon_mmm_f32_32x1_cortexa7; 32, 1; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(f32, armv7neon_mmm_f32_32x1_cortexa9; 32, 1; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
-MMMExternKernel!(f32, armv7neon_mmm_f32_32x1_generic; 32, 1; 4, 4; 0, 0; prefetch, crate::arm32::has_neon());
+MMMExternKernel2!(armv7neon_mmm_f32_8x4_cortexa7 <f32>( 8, 4 )@(4, 4 ) where(NEON));
+MMMExternKernel2!(armv7neon_mmm_f32_8x4_cortexa9 <f32>( 8, 4 )@(4, 4 ) where(NEON));
+MMMExternKernel2!(armv7neon_mmm_f32_8x4_generic  <f32>( 8, 4 )@(4, 4 ) where(NEON));
+MMMExternKernel2!(armv7neon_mmm_f32_8x6_cortexa7 <f32>( 8, 6 )@(4, 4 ) where(NEON));
+MMMExternKernel2!(armv7neon_mmm_f32_8x6_cortexa9 <f32>( 8, 6 )@(4, 4 ) where(NEON));
+MMMExternKernel2!(armv7neon_mmm_f32_8x6_generic  <f32>( 8, 6 )@(4, 4 ) where(NEON));
+MMMExternKernel2!(armv7neon_mmm_f32_32x1_cortexa7<f32>( 32, 1)@( 4, 4) where(NEON));
+MMMExternKernel2!(armv7neon_mmm_f32_32x1_cortexa9<f32>( 32, 1)@( 4, 4) where(NEON));
+MMMExternKernel2!(armv7neon_mmm_f32_32x1_generic <f32>(32, 1 )@(4, 4 ) where(NEON));
 
-MMMExternKernel!(i32, armv7neon_mmm_i32_8x4; 8, 4; 32, 4; 0, 0; prefetch, crate::arm32::has_neon(),
-     packing_defs: {
-         const I8_A: PackedFormat = PackedFormat::new(DatumType::I8, 8, 32);
-         const I8_B: PackedFormat = PackedFormat::new(DatumType::I8, 4, 4);
-         const I8_I8: (&dyn MMMInputFormat, &dyn MMMInputFormat) = (&I8_A, &I8_B);
-     },
-     packings: I8_I8,
-     test: mmm_packed_packed_tests!{ true, armv7neon_mmm_i32_8x4, i8i8:1 }
+MMMExternKernel2!(armv7neon_mmm_i32_8x4<i32>(8, 4)@(32, 4) where(NEON)
+  packing[1] = i8i8 => |k| k.with_packing(PackedFormat::new(DatumType::I8, 8, 32), PackedFormat::new(DatumType::I8, 4, 32))
 );
 
-MMMExternKernel!(i32, armv7neon_mmm_i32_32x1; 32,1 ; 32, 4; 0, 0; prefetch, crate::arm32::has_neon(),
-     packing_defs: {
-         const I8_A: PackedFormat = PackedFormat::new(DatumType::I8, 32, 32);
-         const I8_B: PackedFormat = PackedFormat::new(DatumType::I8, 1, 4);
-         const I8_I8: (&dyn MMMInputFormat, &dyn MMMInputFormat) = (&I8_A, &I8_B);
-     },
-     packings: I8_I8,
-     test: mmm_packed_packed_tests!{ true, armv7neon_mmm_i32_32x1, i8i8:1 }
+MMMExternKernel2!(armv7neon_mmm_i32_32x1<i32>(32, 1)@(32, 4) where(NEON)
+  packing[1] = i8i8 => |k| k.with_packing(PackedFormat::new(DatumType::I8, 32, 32), PackedFormat::new(DatumType::I8, 1, 4))
 );
 
 sigmoid_impl!(f32, armv7neon_sigmoid_f32_4n, 4, 4, crate::arm32::has_neon());
