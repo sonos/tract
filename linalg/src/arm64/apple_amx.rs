7bdf9aa46f3b97465b917f5ca531d13fd080a309
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-09-25 17:30:18 +0200

    clip

--- linalg/src/arm64/apple_amx.rs
+++ linalg/src/arm64/apple_amx.rs
@@ -1,7 +1,7 @@
 use crate::mmm::*;
 use tract_data::prelude::*;
 
-const AMX: fn() -> bool = || crate::arm64::has_amx();
+const AMX: fn() -> bool = crate::arm64::has_amx;
 const CAN_FUSE: fn(&FusedSpec) -> bool = |f| !matches!(f, &FusedSpec::LeakyRelu(_));
 
 MMMExternKernel!(apple_amx_mmm_f32_32x32<f32>(32, 32)@(128, 128) where(AMX) can_fuse(CAN_FUSE));
