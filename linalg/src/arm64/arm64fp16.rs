e06f68c07fec8e48efbaafec007d2a54a8a47df3
Mathieu Poumeyrol <kali@zoy.org>
2024-09-25 15:50:42 +0200

    converting arm64 kernels

--- linalg/src/arm64/arm64fp16.rs
+++ linalg/src/arm64/arm64fp16.rs
@@ -11,31 +11,25 @@ pub use max::*;
 pub use mul::*;
 pub use sum::*;
 
-use crate::mmm::no_prefetch;
+use crate::frame::block_quant::Q4_0;
+use crate::frame::block_quant::PackedBlockQuantFormat;
 
-MMMExternKernel!(f16, arm64fp16_mmm_f16_16x8_gen; 16, 8; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
-MMMExternKernel!(f16, arm64fp16_mmm_f16_16x8_a55; 16, 8; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
-MMMExternKernel!(f16, arm64fp16_mmm_f16_32x4_gen; 32, 4; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
-MMMExternKernel!(f16, arm64fp16_mmm_f16_32x4_a55; 32, 4; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
-MMMExternKernel!(f16, arm64fp16_mmm_f16_128x1_gen; 128, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
-MMMExternKernel!(f16, arm64fp16_mmm_f16_128x1_a55; 128, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
+const FP16: fn() -> bool = crate::arm64::has_fp16;
 
-MMMExternKernel!(f16, arm64fp16_mmm_f16_64x3_gen; 64, 3; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
-MMMExternKernel!(f16, arm64fp16_mmm_f16_32x6_gen; 32, 6; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16());
+MMMExternKernel2!(arm64fp16_mmm_f16_16x8_gen<f16>(16, 8)@(16, 16) where(FP16));
+MMMExternKernel2!(arm64fp16_mmm_f16_16x8_a55<f16>(16, 8)@(16, 16) where(FP16));
+MMMExternKernel2!(arm64fp16_mmm_f16_32x4_gen<f16>(32, 4)@(16, 16) where(FP16));
+MMMExternKernel2!(arm64fp16_mmm_f16_32x4_a55<f16>(32, 4)@(16, 16) where(FP16));
+MMMExternKernel2!(arm64fp16_mmm_f16_128x1_gen<f16>(128,1)@(16, 16) where(FP16));
+MMMExternKernel2!(arm64fp16_mmm_f16_128x1_a55<f16>(128,1)@(16, 16) where(FP16));
 
-MMMExternKernel!(f16, arm64fp16_mmm_f16_64x1_gen; 64, 1; 16, 16; 1, 1; no_prefetch, crate::arm64::has_fp16(),
-     packing_defs: {
-         use crate::frame::block_quant::*;
-         const PQ40_R64_Z16_SE: PackedBlockQuantFormat = PackedBlockQuantFormat::new(&Q4_0, 64, 16, true);
-         const PQ40_R64_Z16: PackedBlockQuantFormat = PackedBlockQuantFormat::new(&Q4_0, 64, 16, false);
-         const F16_B: PackedFormat = PackedFormat::new(DatumType::F16, 1, 2);
-         const PQ40_F16_Z16_SE: (&dyn MMMInputFormat, &dyn MMMInputFormat) = (&PQ40_R64_Z16_SE, &F16_B);
-         const PQ40_F16_Z16: (&dyn MMMInputFormat, &dyn MMMInputFormat) = (&PQ40_R64_Z16, &F16_B);
-     },
- packings: PQ40_F16_Z16_SE PQ40_F16_Z16,
- test: mmm_packed_packed_tests!{ crate::arm64::has_fp16(), arm64fp16_mmm_f16_64x1_gen, q40f16z16se:1 },
- test: mmm_packed_packed_tests!{ crate::arm64::has_fp16(), arm64fp16_mmm_f16_64x1_gen, q40f16z16:2 }
-);
+MMMExternKernel2!(arm64fp16_mmm_f16_64x3_gen<f16>(64, 3)@(16, 16) where(FP16));
+MMMExternKernel2!(arm64fp16_mmm_f16_32x6_gen<f16>(32, 6)@(16, 16) where(FP16));
+
+MMMExternKernel2! { arm64fp16_mmm_f16_64x1_gen<f16>(64, 1)@(16, 16) where(FP16)
+    packing[1] = q40f16z16se => |k| k.with_packing_a(PackedBlockQuantFormat::new(&Q4_0, 64, 16, true))
+    packing[2] = q40f16z16 => |k| k.with_packing_a(PackedBlockQuantFormat::new(&Q4_0, 64, 16, false))
+}
 
 tanh_impl!(f16, arm64fp16_tanh_f16_8n, 8, 8, crate::arm64::has_fp16());
 sigmoid_impl!(f16, arm64fp16_sigmoid_f16_8n, 8, 8, crate::arm64::has_fp16());
--- linalg/src/arm64/arm64simd.rs
+++ linalg/src/arm64/arm64simd.rs
@@ -1,3 +1,5 @@
+use tract_data::internal::*;
+
 mod by_scalar;
 mod leaky_relu;
 mod max;
@@ -5,53 +7,40 @@ mod mul;
 mod softmax;
 mod sum;
 
-use crate::mmm::no_prefetch;
+use crate::frame::PackedFormat;
 
 pub use by_scalar::arm64simd_mul_by_scalar_f32_16n;
 pub use leaky_relu::arm64simd_leaky_relu_f32_8n;
 pub use max::arm64simd_max_f32_16n;
 pub use mul::arm64simd_unicast_mul_f32_16n;
-pub use sum::arm64simd_sum_f32_16n;
 pub use softmax::arm64simd_softmax2_fastcompact_f32_16n;
+pub use sum::arm64simd_sum_f32_16n;
 
-MMMExternKernel!(f32, arm64simd_mmm_f32_8x8_a55; 8, 8; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_12x8_a55; 12, 8; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_16x4_a55; 16, 4; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_24x4_a55; 24, 4; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_64x1_a55; 64, 1; 16, 16; 1, 1; no_prefetch, true);
+MMMExternKernel2!(arm64simd_mmm_f32_8x8_a55 <f32>(8,  8)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_12x8_a55<f32>(12, 8)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_16x4_a55<f32>(16, 4)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_24x4_a55<f32>(24, 4)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_64x1_a55<f32>(64, 1)@(16, 16));
 
-MMMExternKernel!(f32, arm64simd_mmm_f32_16x4_a53; 16, 4; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_24x4_a53; 24, 4; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_8x8_a53; 8, 8; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_12x8_a53; 12, 8; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_64x1_a53; 64, 1; 16, 16; 1, 1; no_prefetch, true);
+MMMExternKernel2!(arm64simd_mmm_f32_16x4_a53<f32>(16, 4)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_24x4_a53<f32>(24, 4)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_8x8_a53 <f32>(8,  8)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_12x8_a53<f32>(12, 8)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_64x1_a53<f32>(64, 1)@(16, 16));
 
-MMMExternKernel!(f32, arm64simd_mmm_f32_16x4_gen; 16, 4; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_24x4_gen; 24, 4; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_8x8_gen; 8, 8; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_12x8_gen; 12, 8; 16, 16; 1, 1; no_prefetch, true);
-MMMExternKernel!(f32, arm64simd_mmm_f32_64x1_gen; 64, 1; 16, 16; 1, 1; no_prefetch, true);
+MMMExternKernel2!(arm64simd_mmm_f32_16x4_gen<f32>(16, 4)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_24x4_gen<f32>(24, 4)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_8x8_gen <f32>(8,  8)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_12x8_gen<f32>(12, 8)@(16, 16));
+MMMExternKernel2!(arm64simd_mmm_f32_64x1_gen<f32>(64, 1)@(16, 16));
 
-MMMExternKernel!(i32, arm64simd_mmm_i32_8x8; 8, 8; 16, 16; 0,0; no_prefetch, true,
- packing_defs: {
-     const I8_A: PackedFormat = PackedFormat::new(DatumType::I8, 8, 16);
-     const I8_B: PackedFormat = PackedFormat::new(DatumType::I8, 8, 16);
-     const I8_I8: (&dyn MMMInputFormat, &dyn MMMInputFormat) = (&I8_A, &I8_B);
- },
- packings: I8_I8,
- test: mmm_packed_packed_tests!{ true, arm64simd_mmm_i32_8x8, i8i8:1 }
+MMMExternKernel2!(arm64simd_mmm_i32_8x8<i32>(8, 8)@(16, 16)
+   packing[1] = i8i8 => |k| k.with_packing(PackedFormat::new(DatumType::I8, 8, 16), PackedFormat::new(DatumType::I8, 8, 16))
 );
 
-MMMExternKernel!(i32, arm64simd_mmm_i32_64x1; 64, 1; 16, 1; 0,0; no_prefetch, true,
- packing_defs: {
-     const I8_A: PackedFormat = PackedFormat::new(DatumType::I8, 64, 16);
-     const I8_B: PackedFormat = PackedFormat::new(DatumType::I8, 1, 1);
-     const I8_I8: (&dyn MMMInputFormat, &dyn MMMInputFormat) = (&I8_A, &I8_B);
- },
- packings: I8_I8,
- test: mmm_packed_packed_tests!{ true, arm64simd_mmm_i32_64x1, i8i8:1 }
+MMMExternKernel2!(arm64simd_mmm_i32_64x1<i32>(64, 1)@(16, 1)
+   packing[1] = i8i8 => |k| k.with_packing(PackedFormat::new(DatumType::I8, 64,16), PackedFormat::new(DatumType::I8, 1, 1))
 );
 
 tanh_impl!(f32, arm64simd_tanh_f32_4n, 4, 4, true);
 sigmoid_impl!(f32, arm64simd_sigmoid_f32_4n, 4, 4, true);
-
