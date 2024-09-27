015e93a6fc9e2a020c7e408eb6f1d419c55303ee
Mathieu Poumeyrol <kali@zoy.org>
2024-09-27 16:45:57 +0200

    make mention of i8 storage on i32 kernels

--- linalg/src/arm64/arm64simd.rs
+++ linalg/src/arm64/arm64simd.rs
@@ -36,10 +36,12 @@ MMMExternKernel!(arm64simd_mmm_f32_64x1_gen<f32>(64, 1)@(16, 16));
 
 MMMExternKernel!(arm64simd_mmm_i32_8x8<i32>(8, 8)@(16, 16)
    packing[1] = i8i8 => |k| k.with_packing(PackedFormat::new(DatumType::I8, 8, 16), PackedFormat::new(DatumType::I8, 8, 16));
+   store(i8)
 );
 
 MMMExternKernel!(arm64simd_mmm_i32_64x1<i32>(64, 1)@(16, 1)
    packing[1] = i8i8 => |k| k.with_packing(PackedFormat::new(DatumType::I8, 64,16), PackedFormat::new(DatumType::I8, 1, 1));
+   store(i8)
 );
 
 tanh_impl!(f32, arm64simd_tanh_f32_4n, 4, 4, true);
