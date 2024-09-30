3184942ccfe4cd8405f94e9d67ae552d986d3c32
Mathieu Poumeyrol <kali@zoy.org>
2024-09-30 17:14:08 +0200

    wip multi scenario in matmul

--- core/src/ops/einsum/kernel_selection.rs
+++ core/src/ops/einsum/kernel_selection.rs
@@ -1,4 +1,5 @@
-use tract_linalg::frame::block_quant::{BlockQuant, PackedBlockQuantFormat};
+use dyn_clone::clone_box;
+use tract_linalg::frame::block_quant::{BlockQuant, PackedBlockQuantFormat, StaticBlockQuant};
 use tract_linalg::frame::PackedFormat;
 use tract_linalg::mmm::{MMMInputValue, MatMatMul};
 
@@ -25,13 +26,11 @@ pub fn wire_packing(
         && a_fact.opaque_fact.as_ref().is_some_and(|of| of.is::<BlockQuantFact>())
         && op.operating_dt.is_float()
     {
-        dbg!("block quant");
         let weights =
             a_fact.opaque_fact.as_ref().unwrap().downcast_ref::<BlockQuantFact>().unwrap();
-        return wire_block_quant_matmul(model, patch, prefix, op, operands, &*weights.format, b_dt);
+        return with_block_quant(model, patch, prefix, op, operands, &*weights.format, b_dt);
     }
 
-    dbg!("simple one");
     // "simple" kernel selection
     let mmm = tract_linalg::ops()
         .mmm(op.operating_dt, op.m.to_usize().ok(), op.k.to_usize().ok(), op.n.to_usize().ok())
@@ -60,7 +59,7 @@ pub fn wire_packing(
     Ok((pa, pb, vec![(mmm, packing)]))
 }
 
-fn wire_block_quant_matmul(
+fn with_block_quant(
     model: &TypedModel,
     patch: &mut TypedModelPatch,
     prefix: &str,
@@ -72,13 +71,13 @@ fn wire_block_quant_matmul(
     let m = op.m.to_usize().expect("m is expected to be an integer");
     if model.symbols.all_scenarios().into_iter().count() < 2 {
         if op.n.is_one() {
-            return wire_block_quant_matvec(patch, prefix, op, operands, m, a_bq, b_dt);
+            return with_block_quant_matvec(patch, prefix, op, operands, m, a_bq, b_dt);
         } else {
-            todo!("Missing strategy for squarish matmat with blockquant input");
+            return with_block_quant_matmat(patch, prefix, op, operands, a_bq, b_dt);
         }
     } else {
         let (pa, pb, mut mmv) =
-            wire_block_quant_matvec(patch, prefix, op, operands, m, a_bq, b_dt)?;
+            with_block_quant_matvec(patch, prefix, op, operands, m, a_bq, b_dt)?;
         let mr = mmv[0].0.mr();
         let matching_matmat = tract_linalg::ops()
             .mmm_impls()
@@ -104,7 +103,54 @@ fn wire_block_quant_matmul(
     }
 }
 
-fn wire_block_quant_matvec(
+fn with_block_quant_matmat(
+    patch: &mut TypedModelPatch,
+    prefix: &str,
+    op: &EinSumAnnotatedAsMatMul,
+    operands: &[OutletId],
+    a_bq: &dyn BlockQuant,
+    b_dt: DatumType,
+) -> TractResult<(OutletId, OutletId, Vec<(Box<dyn MatMatMul>, usize)>)> {
+    // just pick a kernel with regular packing
+    let mmm = tract_linalg::ops()
+        .mmm(op.operating_dt, op.m.to_usize().ok(), op.k.to_usize().ok(), op.n.to_usize().ok())
+        .unwrap();
+    let (packing, pa, pb) = mmm
+        .packings()
+        .into_iter()
+        .enumerate()
+        .filter_map(|(ix, p)| {
+            Some((ix, p.0.downcast_ref::<PackedFormat>()?, p.1.downcast_ref::<PackedFormat>()?))
+        })
+        .find(|(_ix, pa, pb)| pa.dt == b_dt.unquantized() && pb.dt == b_dt.unquantized())
+        .with_context(|| format!("No packing for {mmm:?} with inputs {b_dt:?} and {b_dt:?}"))?;
+
+    // just do a block quant aware packing for now
+    let packer = PackedBlockQuantFormat {
+        bq: StaticBlockQuant::Owned(clone_box(a_bq)),
+        r: pa.r,
+        zip: 0,
+        scales_at_end: false,
+    };
+    let value = patch.outlet_fact(operands[0])?.konst.as_ref().context("A should be a const")?;
+    let value = value
+        .to_scalar::<Opaque>()?
+        .downcast_ref::<BlockQuantValue>()
+        .context("A should be a BlockQuantValue")?;
+    let packed = packer.pack(&value.value, op.k.to_usize()?)?;
+    let konst = tensor0(Opaque::from(Box::new(packed) as Box<dyn MMMInputValue>));
+    let pa = patch.add_const(format!("{prefix}.pack_a"), konst)?;
+
+    let pb = patch.wire_node(
+        format!("{prefix}.pack_b"),
+        MatMatMulPack { k_axis: op.b_k(), mn_axis: op.b_n(), packers: vec![pb.clone()] },
+        &[operands[1]],
+    )?[0];
+
+    Ok((pa, pb, vec![(mmm.clone(), packing)]))
+}
+
+fn with_block_quant_matvec(
     patch: &mut TypedModelPatch,
     prefix: &str,
     op: &EinSumAnnotatedAsMatMul,
@@ -142,8 +188,8 @@ fn wire_block_quant_matvec(
         .context("A should be a BlockQuantValue")?;
     let packed = pa.pack(&value.value, op.k.to_usize()?)?;
     let konst = tensor0(Opaque::from(Box::new(packed) as Box<dyn MMMInputValue>));
-
     let pa = patch.add_const(format!("{prefix}.pack_a"), konst)?;
+
     let pb = patch.wire_node(
         format!("{prefix}.pack_b"),
         MatMatMulPack { k_axis: op.b_k(), mn_axis: op.b_n(), packers: vec![pb.clone()] },
