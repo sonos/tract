baff1367288d42484c71a7641ca3fcd6df57de0f
Mathieu Poumeyrol <kali@zoy.org>
2024-10-18 10:59:39 +0200

    runnable script >> yaml

--- .travis/cache_file.sh
+++ .travis/cache_file.sh
@@ -8,11 +8,16 @@ then
 fi
 
 mkdir -p $CACHEDIR
-
 cd $CACHEDIR
 for file in $@
 do
-     [ -e $file ] || wget https://s3.amazonaws.com/tract-ci-builds/tests/$file
+    mkdir -p $(dirname $file)
+    if [ -n $AWS_ACCESS_KEY_ID ]
+    then
+        [ -e $file ] || aws s3 cp s3://tract-ci-builds/tests/$file $file
+    else
+        [ -e $file ] || wget https://s3.amazonaws.com/tract-ci-builds/tests/$file -O $file
+    fi
 done
 
 exit 0
