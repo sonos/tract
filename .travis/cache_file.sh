f5e3219bf284317f1e870644f63005ac9d25c022
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-11-22 11:37:16 +0100

    tweaks for tinyllama

--- .travis/cache_file.sh
+++ .travis/cache_file.sh
@@ -12,9 +12,12 @@ cd $CACHEDIR
 for file in $@
 do
     mkdir -p $(dirname $file)
-    [ -e $file ] \
-        || wget --no-verbose https://s3.amazonaws.com/tract-ci-builds/tests/$file -O $file \
-        || aws s3 cp s3://tract-ci-builds/tests/$file $file
+    if [ ! -e $file ]
+    then
+        wget --no-verbose https://s3.amazonaws.com/tract-ci-builds/tests/$file -O $file.tmp \
+        || aws s3 cp s3://tract-ci-builds/tests/$file $file.tmp
+        mv $file.tmp $file
+    fi
 done
 
 exit 0
