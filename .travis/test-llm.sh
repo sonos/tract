9c89d19bbfa527b4ab3df0d8e22a1e9cd7ef1a42
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2024-11-22 11:37:16 +0100

    tweaks for tinyllama

--- .travis/test-llm.sh
+++ .travis/test-llm.sh
@@ -64,6 +64,9 @@ do
         TinyLlama--TinyLlama_v1.1-f16f16.p0s100) approx="--approx-custom 0.2,0.1,0.001";;
         TinyLlama--TinyLlama_v1.1-f16f16.p50s50) approx="--approx-custom 0.2,0.1,0.005";;
         TinyLlama--TinyLlama_v1.1-f16f16.p99s1) approx="--approx-custom 0.2,0.1,0.004";;
+        TinyLlama--TinyLlama_v1.1-q40f16.p0s100) approx="--approx-custom 0.2,0.1,0.004";;
+        TinyLlama--TinyLlama_v1.1-q40f16.p99s1) approx="--approx-custom 0.2,0.1,0.002";;
+        TinyLlama--TinyLlama_v1.1-q40f16.p50s50) approx="--approx-custom 0.2,0.1,0.004";;
     esac
 
 
