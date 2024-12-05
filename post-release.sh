3df1f743a7746710f4635d9f4510a9d4b6025837
Mathieu Poumeyrol <kali@zoy.org>
2024-12-05 13:14:48 +0100

    post-release 0.21.9-pre

--- post-release.sh
+++ post-release.sh
@@ -19,8 +19,9 @@ fi
 for path in $ALL_CRATES_PATH
 do
     crate=$(tomato get package.name $path/Cargo.toml)
+    echo $crate
     tomato set package.version $VERSION $path/Cargo.toml > /dev/null
-    ./.change_crate_dep.sh $crate $VERSION
+    tomato set workspace.dependencies.$crate.version $VERSION Cargo.toml
 done
 
 git commit . -m "post-release $VERSION"
