c33b2665fa8d014dfad13476ab2c2a5fd69a8306
LouisChourakiSonos <louis.chouraki@sonos.com>
2024-12-09 16:41:09 +0100

    add mem pool mgmt to bench command

--- cli/src/bench.rs
+++ cli/src/bench.rs
@@ -37,11 +37,22 @@ pub fn handle(
     let model =
         params.tract_model.downcast_ref::<TypedModel>().context("Can only bench TypedModel")?;
     let inputs = tract_libcli::tensor::retrieve_or_make_inputs(model, &run_params)?.remove(0);
-    let plan = SimplePlan::new_with_options(model, &plan_options)?;
+    let mut plan = SimplePlan::new_with_options(model, &plan_options)?;
+    let state = TypedSimpleState::new_from_inputs(&plan, inputs.clone())?;
 
     limits.warmup(model, &inputs)?;
 
-    let mut state = SimpleState::new(plan)?;
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    {
+        let session_handler = tract_metal::MetalSessionHandler::from_plan(
+            &plan,
+            &state.session_state.resolved_symbols,
+            )?;
+        
+        plan = plan.with_session_handler(session_handler);
+    }
+
+    let mut state = SimpleState::new(Arc::new(plan))?;
     let mut iters = 0;
 
     let progress = probe.and_then(|m| m.get_i64("progress"));
