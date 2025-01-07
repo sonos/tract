70109f20f4bcf0951889743897e4c6905cd64e25
Mathieu Poumeyrol <kali@zoy.org>
2025-01-07 15:48:52 +0100

    exception for Tiny f32

--- .travis/test-llm.sh
+++ .travis/test-llm.sh
@@ -62,6 +62,7 @@ do
 
     case "$id.$t" in 
         apple--OpenELM-270M-f16f16.p50s50) approx="--approx-custom 0.2,0.1,0.003";;
+        TinyLlama--TinyLlama_v1.1-f32f32.p50s50) approx="--approx-custom 0.2,0.1,0.001";;
         TinyLlama--TinyLlama_v1.1-f16f16.p0s100) approx="--approx-custom 0.2,0.1,0.001";;
         TinyLlama--TinyLlama_v1.1-f16f16.p50s50) approx="--approx-custom 0.2,0.1,0.005";;
         TinyLlama--TinyLlama_v1.1-f16f16.p99s1) approx="--approx-custom 0.2,0.1,0.004";;
