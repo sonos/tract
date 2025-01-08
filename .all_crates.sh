77b6c212913a3e05ef561922e1fb3af9763515be
Mathieu Poumeyrol <kali@zoy.org>
2025-01-08 16:59:53 +0100

    release 0.21.9

--- .all_crates.sh
+++ .all_crates.sh
@@ -1,2 +1,2 @@
 
-ALL_CRATES_PATH="data linalg core nnef nnef/nnef-resources pulse-opl pulse extra hir tflite tensorflow onnx-opl onnx libcli metal api api/rs api/ffi api/proxy/sys api/proxy cli"
+ALL_CRATES_PATH="data linalg core nnef nnef/nnef-resources pulse-opl pulse extra hir tflite tensorflow onnx-opl onnx metal libcli api api/rs api/ffi api/proxy/sys api/proxy cli"
