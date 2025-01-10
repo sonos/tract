506f6212bd66353cb3ad068f06644f7d610a1e7a
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2025-01-10 15:41:49 +0100

    llama tweaks

--- .travis/test-llm.sh
+++ .travis/test-llm.sh
@@ -84,7 +84,8 @@ do
         TinyLlama--TinyLlama_v1.1-q40ef16.p0s100) approx="--approx-custom 0.2,0.1,0.002";;
         TinyLlama--TinyLlama_v1.1-q40ef16.p50s50) approx="--approx-custom 0.2,0.1,0.002";;
 
-        meta-llama--Llama-3.2-3B-f16f16.p50s50) approx="--approx-custom 0.2,0.1,0.002"
+        meta-llama--Llama-3.2-3B-f16f16.p0s100) approx="--approx-custom 0.2,0.1,0.004";;
+        meta-llama--Llama-3.2-3B-f16f16.p50s50) approx="--approx-custom 0.2,0.1,0.002";;
     esac
 
 
