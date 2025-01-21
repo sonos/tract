888be7972ab29a1cbf7cabece649b3e1b1759392
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-01-21 15:55:40 +0100

    Refactor core runtimes

--- test-rt/test-unit-core/src/lib.rs
+++ test-rt/test-unit-core/src/lib.rs
@@ -1,34 +1,56 @@
 #![cfg(test)]
 use tract_core::internal::*;
 
-mod default {
+mod raw {
     use super::*;
 
-    pub fn default() -> &'static DefaultRuntime {
-        &DefaultRuntime
-    }
-    include!(concat!(env!("OUT_DIR"), "/tests/default.rs"));
-}
-
-mod unoptimized {
-    use super::*;
-
-    pub fn unoptimized() -> &'static UnoptimizedRuntime {
-        &UnoptimizedRuntime
+    pub fn raw() -> &'static RawRuntime {
+        &RawRuntime
     }
 
     #[derive(Debug)]
-    pub struct UnoptimizedRuntime;
+    pub struct RawRuntime;
 
-    impl Runtime for UnoptimizedRuntime {
+    impl Runtime for RawRuntime {
         fn name(&self) -> Cow<str> {
-            Cow::Borrowed("unoptimized")
+            Cow::Borrowed("raw")
         }
         fn prepare(&self, model: TypedModel) -> TractResult<Box<dyn Runnable>> {
             Ok(Box::new(Arc::new(model.into_runnable()?)))
         }
     }
 
-    include!(concat!(env!("OUT_DIR"), "/tests/unoptimized.rs"));
+    include!(concat!(env!("OUT_DIR"), "/tests/raw.rs"));
 }
 
+mod decluttered {
+    use super::*;
+
+    pub fn decluttered() -> &'static DeclutteredRuntime {
+        &DeclutteredRuntime
+    }
+
+    #[derive(Debug)]
+    pub struct DeclutteredRuntime;
+
+    impl Runtime for DeclutteredRuntime {
+        fn name(&self) -> Cow<str> {
+            Cow::Borrowed("decluttered")
+        }
+        fn prepare(&self, model: TypedModel) -> TractResult<Box<dyn Runnable>> {
+            Ok(Box::new(Arc::new(model.into_decluttered()?.into_runnable()?)))
+        }
+    }
+
+    include!(concat!(env!("OUT_DIR"), "/tests/decluttered.rs"));
+}
+
+
+mod optimized {
+    use super::*;
+
+    pub fn optimized() -> &'static DefaultRuntime {
+        &DefaultRuntime
+    }
+    include!(concat!(env!("OUT_DIR"), "/tests/optimized.rs"));
+}
