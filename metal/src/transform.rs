8551e7287522871bda956835b9c22dd02eeb34ef
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-01-31 14:03:01 +0100

    permute axes during transform for ggml

--- metal/src/transform.rs
+++ metal/src/transform.rs
@@ -257,11 +257,11 @@ impl Translate<TypedFact, Box<dyn TypedOp>, TypedFact, Box<dyn TypedOp>> for Met
         let translatable = can_translate_to_metal_op(source, node, self.gemm_impl)?;
 
         if translatable {
-            let gpu_inputs =
+            let mut gpu_inputs =
                 self.sync_inputs_if_required(target, node, mapping, MetalSyncKind::ToGpu)?;
 
             let outlet_ids: TVec<OutletId> = if let Some(op) = node.op_as::<BasicMatMul>() {
-                convert_matmul_to_metal(source, node, target, &gpu_inputs, op, self.gemm_impl)?
+                convert_matmul_to_metal(source, node, target, &mut gpu_inputs, op, self.gemm_impl)?
             } else {
                 let op: Box<dyn TypedOp> = if let Some(op) = node.op_as::<ElementWiseOp>() {
                     Box::new(map_element_wise_ops_to_metal(op).unwrap())
@@ -348,7 +348,7 @@ fn convert_matmul_to_metal(
     model: &TypedModel,
     node: &TypedNode,
     target: &mut TypedModel,
-    inputs: &[OutletId],
+    inputs: &mut [OutletId],
     op: &BasicMatMul,
     gemm_impl: MetalGemmImplKind,
 ) -> TractResult<TVec<OutletId>> {
@@ -363,7 +363,22 @@ fn convert_matmul_to_metal(
             Box::new(ops::MetalGemm::<MfaGemm>::new(op.transpose_a, op.transpose_b))
         }
         MetalGemmImplKind::Ggml => {
-            Box::new(ops::MetalGemm::<GgmlGemm>::new(op.transpose_a, op.transpose_b))
+            let input_facts: tract_smallvec::SmallVec<[&TypedFact; 4]> = model.node_input_facts(node.id)?;
+            
+            if op.transpose_a {
+                let rank = input_facts[0].rank();
+                let perm_a_op: Box<dyn TypedOp> = Box::new(ops::change_axes::MetalAxisOp::from_tract_core(AxisOp::Move(rank - 2, rank - 1)));
+                let perm_a_name = node.name.clone() + ".perm_a";
+                inputs[0] = target.wire_node(perm_a_name, perm_a_op, &[inputs[0]])?[0];
+            }
+
+            if !op.transpose_b {
+                let rank = input_facts[1].rank();
+                let perm_b_op: Box<dyn TypedOp> = Box::new(ops::change_axes::MetalAxisOp::from_tract_core(AxisOp::Move(rank - 2, rank - 1)));
+                let perm_b_name = node.name.clone() + ".perm_b";
+                inputs[1] = target.wire_node(perm_b_name, perm_b_op, &[inputs[1]])?[0];
+            }
+            Box::new(ops::MetalGemm::<GgmlGemm>::new(false, true))
         }
     };
 
