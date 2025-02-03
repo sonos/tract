7d0838d17ca9f3f2c124ac7c33fd12cdb13f8c22
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-02-03 15:09:22 +0100

    Fixed  matmul output_facts condition

--- metal/src/ops/gemm.rs
+++ metal/src/ops/gemm.rs
@@ -45,7 +45,11 @@ impl<K: GemmKernel> MetalGemm<K> {
         };
         ensure!(a.rank() == b.rank());
         ensure!(a.rank() >= 2);
-
+        ensure!(
+            a.shape[a.rank() - 2 + !self.transpose_a() as usize]
+                == b.shape[b.rank() - 2 + self.transpose_b() as usize]
+        );
+        
         self.kernel.output_facts(a, b)
     }
 }
