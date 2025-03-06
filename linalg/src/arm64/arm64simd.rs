113842cf743be1030340250478292e948dda2289
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2025-03-06 16:41:10 +0100

    32x1 q40f32

--- linalg/src/arm64/arm64simd.rs
+++ linalg/src/arm64/arm64simd.rs
@@ -37,6 +37,7 @@ MMMExternKernel!(arm64simd_mmm_f32_64x1_gen<f32>(64, 1)@(16, 16) quality(Manuall
 
 MMMExternKernel!(arm64simd_mmm_f32_32x1_gen<f32>(32, 1)@(16, 16)
     packing[1] = q40f16z16se => |k| k.with_packing(PackedBlockQuantFormat::new(&Q4_0, 32, 16, true), f16::packing(1));
+    packing[2] = q40f32z16se => |k| k.with_packing(PackedBlockQuantFormat::new(&Q4_0, 32, 16, true), f32::packing(1));
     quality(ManuallyOptimized)
 );
 
