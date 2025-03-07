aef2d78441a2be52137ba670e17d7c0a23bc1b02
Mathieu Poumeyrol <kali@zoy.org>
2025-03-07 13:56:41 +0100

    more robust size extraction

--- .travis/test-llm.sh
+++ .travis/test-llm.sh
@@ -38,7 +38,6 @@ then
     fi
 fi
 
-set -x
 
 nnef=llm/$generation/$id/$id.nnef.tgz
 
@@ -49,7 +48,7 @@ if [ -e $MODELS/$nnef ]
 then
     size=$(stat -c %s $MODELS/$nnef)
 else
-    size=$(curl -I $MODELS/$nnef | grep Content-Length | cut -d " " -f 2)
+    size=$(curl -s -I $MODELS/$nnef | grep Content-Length | cut -d " " -f 2 | tr -cd 0123456789)
 fi
 
 alloc_max=$(cat readings.out | tail -n +2 | awk '{print $10-$11}' | sort -n | tail -1)
