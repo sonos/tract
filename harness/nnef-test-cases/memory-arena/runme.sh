09336e7411d9b99648a280c38ffdafc7d3105491
Aaron Jones <aaron.jones@sonos.com>
2025-03-18 15:05:21 -0400

    Add CLI regression test 'runme.sh' to execute 'check-mem-arena' on Metal, and check for exact match with past result JSON

---
+++ harness/nnef-test-cases/memory-arena/runme.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ `uname` = "Darwin" ] && ! grep -q "(Virtual)" <<< "$(sysctl -n machdep.cpu.brand_string)"
+then
+
+  ROOT=$(dirname $(realpath $0))/../../..
+  . $ROOT/.travis/ci-system-setup.sh
+
+  cd `dirname $0`
+  set -ex
+
+  : ${TRACT_RUN:=cargo run -p tract $CARGO_OPTS --}
+
+  model=OpenELM-270M
+  q=q40f16
+  id="apple--$model-$q"
+  generation=current
+  nnef="llm/$generation/$id/$id.nnef.tgz"
+  $CACHE_FILE $nnef
+
+  $TRACT_RUN -v --nnef-tract-core --metal ggml $MODELS/$nnef dump --set S=1024 --set P=0 --memory-arena found.json
+
+  diff -u expected.json found.json
+else
+  echo "Skipped (memory arena test requires apple hardware)"
+fi
