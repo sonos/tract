ea62bd260cb3b3213667029418cbc6cdf1d403d5
Mathieu Poumeyrol <kali@zoy.org>
2025-03-21 10:49:47 +0100

    conv q40 with groups

--- core/src/ops/cnn/conv/block_quant.rs
+++ core/src/ops/cnn/conv/block_quant.rs
@@ -56,3 +56,64 @@ impl TypedOp for BlockQuantIntoShape {
     }
     as_op!();
 }
+
+#[derive(Debug, Clone, new, Hash)]
+pub struct SplitGroupBlockQuant {
+    pub group: usize,
+}
+
+impl Op for SplitGroupBlockQuant {
+    fn name(&self) -> Cow<str> {
+        "SplitGroupBlockQuant".into()
+    }
+
+    op_as_typed_op!();
+}
+
+impl EvalOp for SplitGroupBlockQuant {
+    fn is_stateless(&self) -> bool {
+        true
+    }
+
+    fn state(
+        &self,
+        _session: &mut SessionState,
+        _node_id: usize,
+    ) -> TractResult<Option<Box<dyn OpState>>> {
+        Ok(None)
+    }
+
+    fn eval(&self, inputs: TVec<TValue>) -> TractResult<TVec<TValue>> {
+        let input = args_1!(inputs);
+        let bqv = input
+            .to_scalar::<Opaque>()?
+            .downcast_ref::<BlockQuantValue>()
+            .context("Expect BlockQuantValue")?;
+        let rows_per_group = bqv.fact.m() / self.group;
+        let splits = (0..self.group)
+            .map(|g| {
+                Ok(Opaque(Arc::new(bqv.split_rows(g * rows_per_group..(g + 1) * rows_per_group)?)))
+            })
+            .collect::<TractResult<Vec<_>>>()?;
+        let output = tensor1(&splits);
+        Ok(tvec!(output.into_tvalue()))
+    }
+}
+
+impl TypedOp for SplitGroupBlockQuant {
+    fn output_facts(&self, inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
+        let input = inputs[0];
+        ensure!(input.shape.rank() == 0);
+        let bqf = input
+            .opaque_fact
+            .as_ref()
+            .and_then(|of| of.downcast_ref::<BlockQuantFact>())
+            .context("Expect BlockQuantFact")?;
+        let mut shape: TVec<usize> = bqf.shape().into();
+        shape[0] /= self.group;
+        let opaque_fact = BlockQuantFact::new(bqf.format.clone(), shape);
+        let fact = Opaque::fact(&[self.group]).with_opaque_fact(opaque_fact);
+        Ok(tvec!(fact))
+    }
+    as_op!();
+}
