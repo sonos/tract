c5f52625d121db7ea4975cf71b98243664da784b
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-03-24 15:41:38 +0100

    always run std profiling

--- cli/src/dump.rs
+++ cli/src/dump.rs
@@ -128,22 +128,24 @@ pub fn handle(
             .context("Can only profile typed models")?;
         let inputs = retrieve_or_make_inputs(model, &run_params)?;
 
-        if matches.is_present("metal") {
-            #[cfg(any(target_os = "macos", target_os = "ios"))]
-            {
-                tract_libcli::profile::profile_metal(
-                    model,
-                    bench_limits,
-                    &mut annotations,
-                    &plan_options,
-                    &inputs[0],
-                )?;
-            }
-            #[cfg(not(any(target_os = "macos", target_os = "ios")))]
-            {
-                bail!("Metal profiling called on non-Metal device");
-            }
-        } else {
+        // Metal Profiling is broken, just run standrad profiling
+
+        //if matches.is_present("metal") {
+        //    #[cfg(any(target_os = "macos", target_os = "ios"))]
+        //    {
+        //        tract_libcli::profile::profile_metal(
+        //            model,
+        //            bench_limits,
+        //            &mut annotations,
+        //            &plan_options,
+        //            &inputs[0],
+        //        )?;
+        //    }
+        //    #[cfg(not(any(target_os = "macos", target_os = "ios")))]
+        //    {
+        //        bail!("Metal profiling called on non-Metal device");
+        //    }
+        //} else {
             tract_libcli::profile::profile(
                 model,
                 bench_limits,
@@ -153,7 +155,7 @@ pub fn handle(
                 None,
                 options.folded,
             )?;
-        }
+        //}
     }
 
     if sub_matches.is_present("axes") || sub_matches.is_present("axes-names") {
