d3126f453363284386f1d83da83893795a372f25
Mathieu Poumeyrol <kali@zoy.org>
2025-03-28 08:59:09 +0100

    pull mobilenet from s3

--- examples/tensorflow-mobilenet-v2/ci.sh
+++ examples/tensorflow-mobilenet-v2/ci.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-wget -q https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.4_224.tgz -O mobilenet_v2_1.4_224.tgz
+wget -nc -q https://s3.amazonaws.com/tract-ci-builds/model/mobilenet_v2_1.4_224.tgz
 tar zxf mobilenet_v2_1.4_224.tgz
 cargo run
 rm -rf mobilenet*
