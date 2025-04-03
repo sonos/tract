baedd3dd88f2eef816060e139d13071eba943861
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-04-03 14:53:19 +0200

    simplify broadcast rule

--- metal/src/rewrite_rules/remove_matmul_broadcast.rs
+++ metal/src/rewrite_rules/remove_matmul_broadcast.rs
@@ -40,37 +40,43 @@ pub fn remove_ggml_broadcast_pre_matmul(
 
     // Check if optional Move before matmul is present
     let reshape_out_shape = reshape_node.outputs[0].fact.shape.dims();
-    let (mm_node, new_mm_input, prev_node_id) = match next_node(model, reshape_node) {
-        Some(n) if n.op_is::<BasicMatMul>() => (n, inputs[0], reshape_node.id),
-        Some(n)
-            if n.op_as::<AxisOp>() == Some(&AxisOp::Move(0, 1))
-                && reshape_out_shape[0] == TDim::Val(1)
-                && next_node(model, n).is_some_and(|m| m.op_is::<BasicMatMul>()) =>
+
+    let mut axis_op_chain: TVec<&AxisOp> = tvec![];
+    let mut running_node = reshape_node;
+    while let Some(node) = next_node(model, running_node).filter(|n| n.op_is::<AxisOp>()) {
+        axis_op_chain.push(node.op_as::<AxisOp>().unwrap());
+        running_node = node;
+    }
+    
+    rule_ensure!(next_node(model, running_node).is_some_and(|node| node.op_is::<BasicMatMul>()));
+
+    if let Some(mm_node) = next_node(model, running_node) {
+        let mut new_mm_input = inputs[0];
+        if (axis_op_chain == tvec![&AxisOp::Move(0,1)] && reshape_out_shape[0] == TDim::Val(1)) || 
+            axis_op_chain == tvec![&AxisOp::Rm(0), &AxisOp::Add(1)] {
+                new_mm_input =
+                    patch.wire_node(format!("{node_name}.reshape"), AxisOp::Move(0, 1), &inputs)?[0];
+        }
+
+        // Only Ggml kernels have internal broadcasting. If no kernel impl is specified,
+        // Ggml kernels will be used if and only activations are F32
+        let in_facts = model.node_input_facts(mm_node.id)?;
+        if ctx.gemm_impl != Some(MetalGemmImplKind::Ggml)
+            && (ctx.gemm_impl.is_some() || in_facts[0].datum_type != DatumType::F32)
         {
-            let swap_input =
-                patch.wire_node(format!("{node_name}.reshape"), AxisOp::Move(0, 1), &inputs)?[0];
-            (next_node(model, n).unwrap(), swap_input, n.id)
+            return Ok(None);
         }
-        _ => return Ok(None),
-    };
 
-    // Only Ggml kernels have internal broadcasting. If no kernel impl is specified,
-    // Ggml kernels will be used if and only activations are F32
-    let in_facts = model.node_input_facts(mm_node.id)?;
-    if ctx.gemm_impl != Some(MetalGemmImplKind::Ggml)
-        && (ctx.gemm_impl.is_some() || in_facts[0].datum_type != DatumType::F32)
-    {
-        return Ok(None);
-    }
-
-    let mut matmul_inputs = patch.taps(model, &mm_node.inputs)?;
-    for (idx, o) in mm_node.inputs.iter().enumerate() {
-        if o.node == prev_node_id {
-            matmul_inputs[idx] = new_mm_input;
+        let mut matmul_inputs = patch.taps(model, &mm_node.inputs)?;
+        for (idx, o) in mm_node.inputs.iter().enumerate() {
+            if o.node == running_node.id {
+                matmul_inputs[idx] = new_mm_input;
+            }
         }
-    }
 
-    let out = patch.wire_node(&mm_node.name, &mm_node.op, &matmul_inputs)?;
-    patch.shunt_outside(model, mm_node.id.into(), out[0])?;
-    Ok(Some(patch))
+        let out = patch.wire_node(&mm_node.name, &mm_node.op, &matmul_inputs)?;
+        patch.shunt_outside(model, mm_node.id.into(), out[0])?;
+        return Ok(Some(patch))
+    }
+    Ok(None)
 }
