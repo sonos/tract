4cb9d76656cf5f188fdd00397d9bb57dae1356eb
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-04-04 15:28:46 +0200

    moved rms norm to transformers

--- transformers/src/lib.rs
+++ transformers/src/lib.rs
@@ -1,4 +1,22 @@
+mod rewriter;
+mod rewrite_rules;
+pub mod ops;
+use rewriter::RmsNormTransform;
 use tract_nnef::internal::*;
+use tract_nnef::tract_core::transform::ModelTransform;
+
+pub fn get_transform(name: &str) -> Option<Box<dyn ModelTransform>> {
+    match name {
+        "as-rms-norm" => Some(Box::new(RmsNormTransform)),
+        _ => None,
+    }
+}
+ 
+pub fn register(registry: &mut Registry) {
+    registry.transforms = Box::new(|s| Ok(get_transform(s)));
+
+    ops::rms_norm::register(registry);
+}
 
 pub trait WithTractTransformers {
     fn enable_tract_transformers(&mut self);
@@ -18,7 +36,12 @@ impl WithTractTransformers for tract_nnef::framework::Nnef {
 }
 
 pub fn tract_transformers_registry() -> Registry {
-    let reg = Registry::new("tract_transformers");
+    let mut reg = Registry::new("tract_transformers")
+        .with_doc("Extension `tract_transformers` extends NNEF with operators")
+        .with_doc("for transformer networks.")
+        .with_doc("")
+        .with_doc("Add `extension tract_transformers` to `graph.nnef`");
+    register(&mut reg);
     reg
 }
 
