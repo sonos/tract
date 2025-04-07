fd6bd37f73d77e93a13349b088d0f55929aa9a55
Julien Balian <julien.balian@sonos.com>
2025-04-07 09:43:33 +0200

    fix: do not forget to accelerate

--- examples/pytorch-albert-v2/ci.sh
+++ examples/pytorch-albert-v2/ci.sh
@@ -4,7 +4,7 @@ set -e
 sudo apt-get install -y python3-virtualenv
 virtualenv venv
 . ./venv/bin/activate
-pip install -q torch transformers onnx
+pip install -q torch transformers onnx accelerate
 python export.py
 cargo run --release
 rm -rf venv
