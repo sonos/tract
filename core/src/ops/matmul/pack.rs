7f165b10c61a5c077af105c4c06ac99c9aa84ad2
Mathieu Poumeyrol <kali@zoy.org>
2025-04-15 17:42:06 +0200

    fix wiring on left packing

--- core/src/ops/matmul/pack.rs
+++ core/src/ops/matmul/pack.rs
@@ -44,6 +44,10 @@ impl EvalOp for OptMatMulPack {
 
 impl TypedOp for OptMatMulPack {
     fn output_facts(&self, inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
+        match self.mode_picker {
+            ModePicker::Single => ensure!(self.packers.len() == 1),
+            ModePicker::VecVsMat => ensure!(self.packers.len() == 2),
+        }
         let k = inputs[0].shape[self.k_axis].clone();
         let mn = inputs[0].shape[self.mn_axis].clone();
         let opaque_fact = DynPackedOpaqueFact { k, mn, packers: self.packers.clone() };
