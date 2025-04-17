df526f9719a8b6cb0402aa08d49c262a5447acb4
Mathieu Poumeyrol <kali@zoy.org>
2025-04-17 13:14:54 +0200

    lstm optim conflict

--- core/src/optim/concat_then_einsum.rs
+++ core/src/optim/concat_then_einsum.rs
@@ -38,9 +38,7 @@ impl super::TypedPass for ConcatThenEinsum {
             let inlet = self.0.unwrap();
             let outlet = model.nodes[inlet.node].inputs[inlet.slot];
             let concat_node = model.node(outlet.node);
-            if concat_node.outputs[0].successors.len() > 1
-                || model.outputs.contains(&concat_node.id.into())
-            {
+            if model.outputs.contains(&concat_node.id.into()) {
                 continue;
             }
             let einsum_node = &model.nodes[inlet.node];
