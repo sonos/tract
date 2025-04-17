d172f7d68ab2bd31d5687324bd27fbbb5bdfbfc6
Mathieu Poumeyrol <kali@zoy.org>
2025-04-17 12:00:55 +0200

    plus in delcuttering broadcast-einsum

--- core/src/optim/concat_then_einsum.rs
+++ core/src/optim/concat_then_einsum.rs
@@ -38,6 +38,11 @@ impl super::TypedPass for ConcatThenEinsum {
             let inlet = self.0.unwrap();
             let outlet = model.nodes[inlet.node].inputs[inlet.slot];
             let concat_node = model.node(outlet.node);
+            if concat_node.outputs[0].successors.len() > 1
+                || model.outputs.contains(&concat_node.id.into())
+            {
+                continue;
+            }
             let einsum_node = &model.nodes[inlet.node];
             if einsum_node.inputs.len() != 2 {
                 // should we try and apply this on quantized einsums ?
