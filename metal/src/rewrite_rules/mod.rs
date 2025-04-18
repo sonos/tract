d5b42f54049c2807f2f4e7fee593633156b03390
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-04-18 15:20:05 +0200

    fmt

--- metal/src/rewrite_rules/mod.rs
+++ metal/src/rewrite_rules/mod.rs
@@ -2,9 +2,9 @@ mod fuse_axis_op;
 mod remove_matmul_broadcast;
 mod untranspose_matmul_output;
 
-pub use tract_gpu::rewrite_rules::{next_node, previous_node, previous_nodes };
 pub use fuse_axis_op::{fuse_axis_op, fuse_move_axis};
 pub use remove_matmul_broadcast::remove_ggml_broadcast_pre_matmul;
+pub use tract_gpu::rewrite_rules::{next_node, previous_node, previous_nodes};
 pub use untranspose_matmul_output::untranspose_matmul_output;
 
 #[macro_export]
