88b478fecf3ecbc2a670b1dc2be9c67afcd9d4fa
Mathieu Poumeyrol <kali@zoy.org>
2025-05-15 15:54:09 +0200

    add gpu crate

--- .all_crates.sh
+++ .all_crates.sh
@@ -1,2 +1,2 @@
 
-ALL_CRATES_PATH="data linalg core nnef nnef/nnef-resources pulse-opl pulse extra transformers hir tflite tensorflow onnx-opl onnx metal libcli api api/rs api/ffi api/proxy/sys api/proxy cli"
+ALL_CRATES_PATH="data linalg core nnef nnef/nnef-resources pulse-opl pulse extra transformers hir tflite tensorflow onnx-opl onnx gpu metal libcli api api/rs api/ffi api/proxy/sys api/proxy cli"
