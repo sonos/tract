c049529b95fdb347715410843821ced9cb4773bc
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-05-26 09:42:32 +0200

    rmv debugs

--- metal/src/ops/concat.rs
+++ metal/src/ops/concat.rs
@@ -64,7 +64,11 @@ impl EvalOp for MetalConcat {
             inputs[0].datum_type(),
             &output_shape,
         )?;
-        with_borrowed_metal_stream(|stream| self.kernel.dispatch_eval(stream, &inputs, &output))?;
+
+        with_borrowed_metal_stream(|stream| {
+            self.kernel.dispatch_eval(stream, &inputs, &output)
+        })?;
+
         Ok(tvec!(output.into_opaque_tensor().into_tvalue()))
     }
 }
