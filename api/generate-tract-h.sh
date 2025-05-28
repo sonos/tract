0008f1770b6e1456c54e49f5e22f7491380dfdac
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-05-28 09:27:18 +0200

    remove bindgen.rs and its generation command from script

--- api/generate-tract-h.sh
+++ api/generate-tract-h.sh
@@ -2,9 +2,7 @@
 
 set -ex
 
-cargo install bindgen-cli
 cargo install cbindgen
 
 cbindgen ffi > tract.h
 cp tract.h c
-bindgen tract.h -o proxy/sys/bindings.rs --rust-target 1.85.0 --rust-edition 2024
