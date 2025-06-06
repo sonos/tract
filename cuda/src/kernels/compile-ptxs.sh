a49d911cd52f7e6a395a88b54d5c1367d2de3f96
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-06-06 15:29:12 +0200

    add script for kernel generation

---
+++ cuda/src/kernels/compile-ptxs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+set -u
+
+CU_DIR="cu"
+PTX_DIR="ptx"
+
+mkdir -p "$PTX_DIR"
+
+for cu_file in "$CU_DIR"/*.cu; do
+    base_name=$(basename "$cu_file" .cu)
+    ptx_file="$PTX_DIR/$base_name.ptx"
+
+    echo "Compiling $cu_file -> $ptx_file"
+    nvcc -arch=sm_75 -ptx "$cu_file" -o "$ptx_file"
+    sed -i 's/\.target sm_75/\.target compute_75/' "$ptx_file"
+done
