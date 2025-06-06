8f596ed5bd074ee0bcd254fe9b7204cbdd2302f8
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-06-06 13:51:57 +0200

    add util

--- cuda/src/kernels/mod.rs
+++ cuda/src/kernels/mod.rs
@@ -1,4 +1,14 @@
 mod silu;
+use cust::memory::DevicePointer;
 pub use silu::Silu;
+use tract_gpu::tensor::DeviceTensor;
 
-const NN_OPS: &str = include_str!("unary.ptx");
+use crate::tensor::CudaBuffer;
+
+const UNARY_OPS: &str = include_str!("unary.ptx");
+
+pub fn get_cuda_ptr(t: &DeviceTensor) -> DevicePointer<u8> {
+    let ptr = t.device_buffer().downcast_ref::<CudaBuffer>().unwrap().as_device_ptr();
+    let offset = t.buffer_offset::<isize>() * t.datum_type().size_of() as isize;
+    unsafe { ptr.offset(offset) }
+}
