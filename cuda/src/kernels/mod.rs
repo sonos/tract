eb7a5ea5f1f3999cc632e06ea0fe4a64325b3030
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-06-06 15:10:09 +0200

    cache functions

--- cuda/src/kernels/mod.rs
+++ cuda/src/kernels/mod.rs
@@ -7,6 +7,19 @@ use crate::tensor::CudaBuffer;
 
 const UNARY_OPS: &str = include_str!("unary.ptx");
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum LibraryName {
+    UnaryOps,
+}
+
+impl LibraryName {
+    pub fn content(&self) -> &str {
+        match self {
+            Self::UnaryOps => UNARY_OPS,
+        }
+    }
+}
+
 pub fn get_cuda_ptr(t: &DeviceTensor) -> DevicePointer<u8> {
     let ptr = t.device_buffer().downcast_ref::<CudaBuffer>().unwrap().as_device_ptr();
     let offset = t.buffer_offset::<isize>() * t.datum_type().size_of() as isize;
