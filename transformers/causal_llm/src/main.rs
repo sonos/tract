0eea25f9cd74b245f3d09da09fc0e4c90ff181dc
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2025-06-23 16:26:19 +0200

    api-ify

--- transformers/causal_llm/src/main.rs
+++ transformers/causal_llm/src/main.rs
@@ -1,64 +1,97 @@
+use std::error::Error;
+use std::path::Path;
 use std::str::FromStr;
 
+use crate::tract_core::transform::ModelTransform;
 use float_ord::FloatOrd;
+use tokenizers::Tokenizer;
 use tract_nnef::prelude::tract_itertools::Itertools;
 use tract_nnef::prelude::*;
 use tract_transformers::WithTractTransformers;
 
-fn main() -> TractResult<()> {
-    let nn_path = "OhanaTract-llama3_2_1B_f32/model/model.nnef.tgz";
+#[derive(Clone, Debug)]
+pub struct CausalLlmModel {
+    pub tokenizer: Tokenizer,
+    pub nn: Arc<TypedRunnableModel<TypedModel>>,
+}
 
-    eprintln!("Loading tokenizers");
-    let tokenizer_path = "OhanaTract-llama3_2_1B_f32/tokenizer/tokenizer.json";
-    let tokenizer = tokenizers::Tokenizer::from_file(tokenizer_path).unwrap();
-    eprintln!("Loaded tokenizers");
+impl CausalLlmModel {
+    fn from_paths(
+        tokenizer: impl AsRef<Path>,
+        nn: impl AsRef<Path>,
+    ) -> TractResult<Arc<CausalLlmModel>> {
+        let tokenizer = tokenizers::Tokenizer::from_file(tokenizer).unwrap();
 
-    eprintln!("Loading NN");
-    let nnef = tract_nnef::nnef().with_tract_transformers();
-    let mut nn = nnef.model_for_path(&nn_path)?.into_decluttered()?;
+        let nnef = tract_nnef::nnef().with_tract_transformers();
+        let mut nn = nnef.model_for_path(nn.as_ref())?.into_decluttered()?;
 
-    let transform = nnef.get_transform("transformers-detect-all").unwrap().unwrap();
-    nn.transform(&*transform)?;
-    #[cfg(any(target_os = "darwin", target_os = "ios"))]
-    tract_metal::MetalTransform::from_str("")?.transform(&mut nn).unwrap();
-    nn.optimize()?;
-    let nn = nn.into_runnable()?;
-    eprintln!("Loaded NN");
+        let transform = nnef.get_transform("transformers-detect-all").unwrap().unwrap();
+        nn.transform(&*transform)?;
+        #[cfg(any(target_os = "macos", target_os = "ios"))]
+        {
+            tract_metal::MetalTransform::from_str("")?.transform(&mut nn)?;
+        }
+        nn.optimize()?;
+        let nn = nn.into_runnable()?;
+        Ok(Arc::new(CausalLlmModel { tokenizer, nn: Arc::new(nn) }))
+    }
 
-    let prompt = "Paul McCartney is";
-    let tokenized = tokenizer.encode(prompt, false).unwrap();
+    fn spawn(self: &Arc<Self>) -> TractResult<CausalLlmState> {
+        let nn_state = SimpleState::new(self.nn.clone())?;
+        Ok(CausalLlmState { model: self.clone(), nn_state, seq: vec![] })
+    }
+}
 
-    let mut state = SimpleState::new(&nn)?;
-    dbg!(&state.model().inputs);
-    dbg!(state.model().input_fact(0))?;
+#[derive(Debug)]
+struct CausalLlmState {
+    pub model: Arc<CausalLlmModel>,
+    pub nn_state: TypedSimpleState<TypedModel, Arc<TypedRunnableModel<TypedModel>>>,
+    pub seq: Vec<u32>,
+}
 
-    let mut seq = tokenized.get_ids().iter().map(|id| *id as i64).collect_vec();
+impl CausalLlmState {
+    pub fn process_text(&mut self, prompt: &str) -> TractResult<u32> {
+        let seq = self.model.tokenizer.encode(prompt, false).unwrap();
+        self.process_tokens(&seq.get_ids())
+    }
 
-    let mut processed = 0; // this is P
-
-    loop {
-        let mut input = tensor1(&seq[processed..]);
+    pub fn process_tokens(&mut self, tokens: &[u32]) -> TractResult<u32> {
+        self.seq.extend(tokens);
+        let mut input = tensor1(&tokens.iter().map(|t| *t as i64).collect_vec());
         input.insert_axis(0)?;
-        dbg!(&input);
-        let output = state.run(tvec![input.into_tvalue()])?;
-        dbg!("ran");
+        let output = self.nn_state.run(tvec![input.into_tvalue()])?;
         let next_tok = output[0]
             .as_slice::<f32>()?
             .iter()
             .enumerate()
             .max_by_key(|(_ix, v)| FloatOrd(**v))
             .unwrap()
-            .0;
-        dbg!(next_tok);
-        processed = seq.len();
-        seq.push(next_tok as i64);
-        if seq.len() == 128 {
+            .0 as u32;
+        Ok(next_tok)
+    }
+
+    pub fn to_words(&self, tokens: &[u32]) -> TractResult<String> {
+        Ok(self.model.tokenizer.decode(&tokens, true).unwrap())
+    }
+}
+
+fn main() -> TractResult<()> {
+    let tokenizer_path = "OhanaTract-llama3_2_1B_f32/tokenizer/tokenizer.json";
+    let nn_path = "OhanaTract-llama3_2_1B_f32/model/model.nnef.tgz";
+    let model = CausalLlmModel::from_paths(tokenizer_path, nn_path)?;
+    let mut state = model.spawn()?;
+
+    // prompt processing
+    let mut current_token = state.process_text("Paul McCartney is")?;
+
+    // text gen loop
+    loop {
+        current_token = state.process_tokens(&[current_token])?;
+        if state.seq.len() == 128 {
             break;
         }
     }
+    println!("{}", state.to_words(&state.seq)?);
 
-    let text = tokenizer.decode(&seq.iter().map(|s| *s as u32).collect_vec(), true).unwrap();
-
-    dbg!(text);
     Ok(())
 }
