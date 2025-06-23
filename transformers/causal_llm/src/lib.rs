5fcf813d064ac4e08be1baff380d313f4c678076
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2025-06-23 16:40:28 +0200

    librarify example

--- transformers/causal_llm/src/lib.rs
+++ transformers/causal_llm/src/lib.rs
@@ -1,10 +1,10 @@
-use std::error::Error;
 use std::path::Path;
 use std::str::FromStr;
 
 use crate::tract_core::transform::ModelTransform;
 use float_ord::FloatOrd;
 use tokenizers::Tokenizer;
+use tract_nnef::internal::{anyhow, TractErrorContext};
 use tract_nnef::prelude::tract_itertools::Itertools;
 use tract_nnef::prelude::*;
 use tract_transformers::WithTractTransformers;
@@ -16,16 +16,18 @@ pub struct CausalLlmModel {
 }
 
 impl CausalLlmModel {
-    fn from_paths(
+    pub fn from_paths(
         tokenizer: impl AsRef<Path>,
         nn: impl AsRef<Path>,
     ) -> TractResult<Arc<CausalLlmModel>> {
-        let tokenizer = tokenizers::Tokenizer::from_file(tokenizer).unwrap();
+        let tokenizer = tokenizers::Tokenizer::from_file(tokenizer).map_err(|e| anyhow!(e))?;
 
         let nnef = tract_nnef::nnef().with_tract_transformers();
         let mut nn = nnef.model_for_path(nn.as_ref())?.into_decluttered()?;
 
-        let transform = nnef.get_transform("transformers-detect-all").unwrap().unwrap();
+        let transform = nnef
+            .get_transform("transformers-detect-all")?
+            .context("transformers-detect-all not found")?;
         nn.transform(&*transform)?;
         #[cfg(any(target_os = "macos", target_os = "ios"))]
         {
@@ -36,14 +38,14 @@ impl CausalLlmModel {
         Ok(Arc::new(CausalLlmModel { tokenizer, nn: Arc::new(nn) }))
     }
 
-    fn spawn(self: &Arc<Self>) -> TractResult<CausalLlmState> {
+    pub fn spawn(self: &Arc<Self>) -> TractResult<CausalLlmState> {
         let nn_state = SimpleState::new(self.nn.clone())?;
         Ok(CausalLlmState { model: self.clone(), nn_state, seq: vec![] })
     }
 }
 
 #[derive(Debug)]
-struct CausalLlmState {
+pub struct CausalLlmState {
     pub model: Arc<CausalLlmModel>,
     pub nn_state: TypedSimpleState<TypedModel, Arc<TypedRunnableModel<TypedModel>>>,
     pub seq: Vec<u32>,
@@ -51,7 +53,7 @@ struct CausalLlmState {
 
 impl CausalLlmState {
     pub fn process_text(&mut self, prompt: &str) -> TractResult<u32> {
-        let seq = self.model.tokenizer.encode(prompt, false).unwrap();
+        let seq = self.model.tokenizer.encode(prompt, false).map_err(|e| anyhow!(e))?;
         self.process_tokens(&seq.get_ids())
     }
 
@@ -65,33 +67,12 @@ impl CausalLlmState {
             .iter()
             .enumerate()
             .max_by_key(|(_ix, v)| FloatOrd(**v))
-            .unwrap()
+            .context("no tokens in output ?")?
             .0 as u32;
         Ok(next_tok)
     }
 
     pub fn to_words(&self, tokens: &[u32]) -> TractResult<String> {
-        Ok(self.model.tokenizer.decode(&tokens, true).unwrap())
+        self.model.tokenizer.decode(&tokens, true).map_err(|e| anyhow!(e))
     }
-}
-
-fn main() -> TractResult<()> {
-    let tokenizer_path = "OhanaTract-llama3_2_1B_f32/tokenizer/tokenizer.json";
-    let nn_path = "OhanaTract-llama3_2_1B_f32/model/model.nnef.tgz";
-    let model = CausalLlmModel::from_paths(tokenizer_path, nn_path)?;
-    let mut state = model.spawn()?;
-
-    // prompt processing
-    let mut current_token = state.process_text("Paul McCartney is")?;
-
-    // text gen loop
-    loop {
-        current_token = state.process_tokens(&[current_token])?;
-        if state.seq.len() == 128 {
-            break;
-        }
-    }
-    println!("{}", state.to_words(&state.seq)?);
-
-    Ok(())
 }
