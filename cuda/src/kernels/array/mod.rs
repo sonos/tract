a091227c8afbd9e99d8c3306c856c8c47f18fb4b
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-07-04 16:59:06 +0200

    preload array ops

--- cuda/src/kernels/array/mod.rs
+++ cuda/src/kernels/array/mod.rs
@@ -11,3 +11,32 @@ pub use concat::Concat;
 pub use copy::Memcpy;
 pub use permute_axes::PermuteAxes;
 pub use rotate_half::RotateHalf;
+
+pub fn all_functions() -> Vec<String> {
+    use std::collections::HashSet;
+    let mut functions = HashSet::<String>::new();
+
+    functions.extend(
+        tract_gpu::tensor::DeviceTensor::SUPPORTED_DT
+            .into_iter()
+            .flat_map(|dt| crate::kernels::BroadcastKind::ALL.into_iter().map(move |b| (dt, b)))
+            .flat_map(|(dt, b)| MultiBroadcast.kernel_name(dt, b).into_iter()),
+    );
+
+    functions.extend(
+        tract_gpu::tensor::DeviceTensor::SUPPORTED_DT
+            .into_iter()
+            .flat_map(|dt| Memcpy.kernel_name(dt).into_iter()),
+    );
+
+    functions.extend(
+        tract_gpu::tensor::DeviceTensor::SUPPORTED_DT
+            .into_iter()
+            .flat_map(|dt1| {
+                tract_gpu::tensor::DeviceTensor::SUPPORTED_DT.into_iter().map(move |dt2| (dt1, dt2))
+            })
+            .flat_map(|(dt1, dt2)| Cast.kernel_name(dt1, dt2).into_iter()),
+    );
+
+    functions.into_iter().collect()
+}
