25dd40d0caa93166b64c5bd594cd807d16d2fc6f
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-07-10 13:53:35 +0200

    add reduce

--- cuda/src/kernels/nn/mod.rs
+++ cuda/src/kernels/nn/mod.rs
@@ -3,8 +3,11 @@ mod apply_rope;
 mod rms_norm;
 mod scaled_masked_softmax;
 mod softmax;
+mod reduce;
 
 pub use rms_norm::RmsNorm;
 pub use scaled_masked_softmax::ScaledMaskedSoftmax;
 pub use softmax::Softmax;
 pub use gelu_approximate::GeluApproximate;
+pub use apply_rope::ApplyRope;
+pub use reduce::Reducer;
