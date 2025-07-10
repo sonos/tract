0fa50f7d4b7797c32e7fb6e9f3c3c94782ec387b
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-07-10 11:05:17 +0200

    avoid double-printing of opaque konsts

--- metal/src/tensor.rs
+++ metal/src/tensor.rs
@@ -127,7 +127,7 @@ pub struct MetalTensor {
 
 impl std::fmt::Debug for MetalTensor {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.inner)
+        write!(f, "MetalTensor: {:?}", self.inner)
     }
 }
 
