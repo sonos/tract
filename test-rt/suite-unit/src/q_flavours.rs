7ad752f3b23f55160ab759568d1bc50d3ae3f4aa
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-08-19 15:55:18 +0200

    format

--- test-rt/suite-unit/src/q_flavours.rs
+++ test-rt/suite-unit/src/q_flavours.rs
@@ -54,7 +54,7 @@ impl Test for QFlavoursProblem {
             .run(tvec![self.input.clone().into_tvalue()])?
             .remove(0)
             .into_tensor();
-        dbg!(&output);
+        //dbg!(&output);
         let reference = self.input.cast_to::<f32>()?;
         let comparison = output.cast_to::<f32>()?;
         comparison.close_enough(&reference, approx)
