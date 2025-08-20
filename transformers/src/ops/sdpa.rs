0bac792884223cfd463b0db94922660b28f8a1cb
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@sonos.com>
2025-08-20 12:36:45 +0200

    Fix softmax & add is_causal param

--- transformers/src/ops/sdpa.rs
+++ transformers/src/ops/sdpa.rs
@@ -1,5 +1,9 @@
+use std::str::FromStr;
+
+use tract_core::ops::array::Trilu;
 use tract_core::ops::binary::BinMiniOp;
 use tract_core::ops::einsum::EinSum;
+use tract_core::ops::logic::Iff;
 use tract_core::ops::math::{Add, Mul};
 use tract_nnef::internal::*;
 use tract_nnef::tract_core::ops::nn::{Softmax, SoftmaxExp, SoftmaxKind};
@@ -25,13 +29,24 @@ fn de_scaled_masked_softmax(
     let q = invocation.named_arg_as(builder, "q")?;
     let k = invocation.named_arg_as(builder, "k")?;
     let v = invocation.named_arg_as(builder, "v")?;
-    let mask = invocation.named_arg_as(builder, "mask")?;
-    builder.wire(Sdpa { scale: None }, &[q, k, v, mask])
+    let mut inputs = vec![q, k, v];
+    if let Some(mask) = invocation.get_named_arg_as(builder, "mask")? {
+        inputs.push(mask);
+    };
+    let scale: Option<f32> = invocation.get_named_arg_as(builder, "scale")?;
+    let dt = DatumType::from_str(&invocation.named_arg_as::<String>(builder, "d_type")?)?;
+    let inner_dt =
+        DatumType::from_str(&invocation.named_arg_as::<String>(builder, "inner_dtype")?)?;
+    let is_causal = invocation.named_arg_as(builder, "causal")?;
+    builder.wire(Sdpa { scale: scale.map(|it| tensor0(it)), dt, inner_dt, is_causal }, &inputs)
 }
 
 #[derive(Debug, Clone, Hash)]
 pub struct Sdpa {
     pub scale: Option<Tensor>,
+    pub dt: DatumType,
+    pub inner_dt: DatumType,
+    pub is_causal: bool,
 }
 
 impl Op for Sdpa {
@@ -53,40 +68,82 @@ impl EvalOp for Sdpa {
         let q = inputs.remove(0);
         let k = inputs.remove(0);
         let v = inputs.remove(0);
-        let mask = inputs.remove(0);
+        let mut mask = if !inputs.is_empty() { Some(inputs.remove(0)) } else { None };
 
         let rank = q.rank();
-        let d_k = k.shape()[rank - 1] as f32;
+        let k_shape = k.shape().iter().cloned().collect::<Vec<_>>();
+        let q_shape = q.shape().iter().cloned().collect::<Vec<_>>();
 
+        // Computing scaling factor for attention
+        let d_k = k_shape[rank - 1] as f32;
         let scale = if let Some(scale) = self.scale.as_ref() {
-            scale.cast_to_dt(DatumType::F32)?.into_owned()
+            scale.cast_to_dt(self.inner_dt)?.into_owned()
         } else {
-            tensor0(1.0 / d_k.sqrt())
+            tensor0(1.0 / d_k.sqrt()).cast_to_dt(self.inner_dt)?.into_owned()
         };
 
+        // Construct causal mask if needed
+        if self.is_causal {
+            let (q_seq_len, k_seq_len) = (q_shape[rank - 2], k_shape[rank - 2]);
+            let mut ones =
+                unsafe { Tensor::uninitialized_dt(DatumType::F32, &[q_seq_len, k_seq_len])? };
+            ones.fill_t(1.0_f32)?;
+
+            // Build lower triangular matrix
+            let k = tensor0(0_i64);
+            let lower_triangle_ones =
+                Trilu { upper: false }.eval(tvec![ones.into(), k.into()])?.remove(0);
+            let cond_mask = lower_triangle_ones.cast_to::<bool>()?.into_owned();
+
+            // Zeros for lower part
+            let zeros = Tensor::zero_dt(self.inner_dt, &[q_seq_len, k_seq_len])?;
+
+            // -inf for higher part
+            let mut neg_infs =
+                unsafe { Tensor::uninitialized_dt(self.inner_dt, &[q_seq_len, k_seq_len])? };
+            neg_infs.fill_t(f32::NEG_INFINITY)?;
+
+            let causal_mask_tensor = Iff
+                .eval(tvec![
+                    cond_mask.clone().into_tvalue(),
+                    zeros.into_tvalue(),
+                    neg_infs.into_tvalue(),
+                ])?
+                .remove(0);
+
+            mask = Some(causal_mask_tensor);
+        }
+
+        // Computing attention matrix
         let axes = match rank {
             3 => "amk,ank->amn".parse().unwrap(),
             4 => "bhmk,bhnk->bhmn".parse().unwrap(),
             _ => unreachable!(),
         };
-
-        let q_dot_kt = EinSum { axes, operating_dt: DatumType::F32, q_params: None }
+        let q_dot_kt = EinSum { axes, operating_dt: self.inner_dt, q_params: None }
             .eval(tvec![q, k])?
             .remove(0);
+        let scaled_input = Mul.eval(q_dot_kt, scale.into_tvalue(), self.inner_dt)?;
 
-        let scaled_input = Mul.eval(q_dot_kt, scale.into_tvalue(), DatumType::F32)?;
-        let scaled_masked_input = Add.eval(scaled_input.into_tvalue(), mask, DatumType::F32)?;
+        // Apply mask (causal or provided by user)
+        let scaled_masked_input = if let Some(m) = mask {
+            Add.eval(scaled_input.into_tvalue(), m, self.inner_dt)?
+        } else {
+            scaled_input
+        };
+
         let attention_weights =
-            Softmax::new(tvec![2], None, SoftmaxKind::Softmax(SoftmaxExp::Libc))
+            Softmax::new(tvec![rank - 1], None, SoftmaxKind::Softmax(SoftmaxExp::Libc))
                 .eval(tvec![scaled_masked_input.into()])?[0]
                 .clone();
 
+        // Final projection using V
         let axes = match rank {
             3 => "amk,akn->amn".parse().unwrap(),
             4 => "bhmn,bhnv->bhmv".parse().unwrap(),
             _ => unreachable!(),
         };
-        let output = EinSum { axes, operating_dt: DatumType::F32, q_params: None }
+        let output = EinSum { axes, operating_dt: self.dt, q_params: None }
             .eval(tvec![attention_weights, v])?;
         Ok(output)
     }
@@ -94,6 +151,9 @@ impl EvalOp for Sdpa {
 
 impl TypedOp for Sdpa {
     fn output_facts(&self, inputs: &[&TypedFact]) -> TractResult<TVec<TypedFact>> {
+        if self.is_causal {
+            ensure!(inputs.len() == 3, "Mask cannot be provided if is_causal=true")
+        };
         let rank = inputs[0].rank();
         ensure!(rank == 3 || rank == 4, "Input tensors must be 3D or 4D");
         ensure!(
