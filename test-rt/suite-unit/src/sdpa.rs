848480156ecc988dd0c11a5ac4bac1cf2a785b65
Emrick Sinitambirivoutin <emrick.sinitambirivoutin@sonos.com>
2025-08-26 12:34:49 +0200

    Add 4D & grouped query attention testing

--- test-rt/suite-unit/src/sdpa.rs
+++ test-rt/suite-unit/src/sdpa.rs
@@ -6,7 +6,7 @@ use proptest::{
 use tract_core::internal::*;
 use tract_core::ndarray::ArrayD;
 use tract_core::num_traits::Float;
-use tract_ndarray::{s, Array2, Array3, ArrayView2};
+use tract_ndarray::{s, Array2, Array3, Array4, ArrayView2, Ix3, Ix4};
 use tract_transformers::ops::sdpa::Sdpa;
 
 use crate::tensor;
@@ -40,35 +40,80 @@ impl Arbitrary for SdpaProblem<f32> {
     type Strategy = BoxedStrategy<SdpaProblem<f32>>;
 
     fn arbitrary_with(args: Self::Parameters) -> Self::Strategy {
-        (1..3usize, 1..16usize)
-            .prop_flat_map(move |(b, seq_len)| {
-                let q = tensor(&[b, seq_len, args.q_emb]);
-                let k = tensor(&[b, seq_len, args.q_emb]);
-                let v = tensor(&[b, seq_len, args.q_emb]);
+        prop_oneof![generate_3d_problem(args.clone()), generate_4d_problem(args.clone()),].boxed()
+    }
+}
+
+fn generate_3d_problem(args: SdpaProblemParams) -> BoxedStrategy<SdpaProblem<f32>> {
+    (1..3usize, 1..16usize)
+        .prop_flat_map(move |(b, seq_len)| {
+            let q = tensor(&[b, seq_len, args.q_emb]);
+            let k = tensor(&[b, seq_len, args.q_emb]);
+            let v = tensor(&[b, seq_len, args.q_emb]);
+
+            let scale_strategy = prop_oneof![Just(None), (0.1f32..2.0).prop_map(Some)];
+            let causal_strategy = any::<bool>();
+            (Just((b, seq_len)), q, k, v, scale_strategy, causal_strategy)
+        })
+        .prop_flat_map(|((b, seq_len), q, k, v, scale, is_causal)| {
+            let mask_strategy = if is_causal {
+                Just(None).boxed()
+            } else {
+                prop_oneof![Just(None), tensor(&[b, seq_len, seq_len]).prop_map(Some)].boxed()
+            };
+
+            (Just(q), Just(k), Just(v), Just(scale), Just(is_causal), mask_strategy)
+        })
+        .prop_map(|(q, k, v, scale, is_causal, mask)| SdpaProblem {
+            q,
+            k,
+            v,
+            mask,
+            scale,
+            is_causal,
+        })
+        .boxed()
+}
+
+fn generate_4d_problem(args: SdpaProblemParams) -> BoxedStrategy<SdpaProblem<f32>> {
+    let head_strategy = (1..=4usize).prop_map(|i| i * 2).prop_flat_map(|q_heads| {
+        let kv_heads_strategy = (1..=q_heads)
+            .prop_filter("q_heads should be a multiple of q_emb", move |kh| q_heads % *kh == 0);
+        (Just(q_heads), kv_heads_strategy)
+    });
+
+    head_strategy
+        .prop_flat_map(move |(q_heads, kv_heads)| {
+            let head_dim = args.q_emb / q_heads;
+            (1..3usize, 1..16usize).prop_flat_map(move |(b, seq_len)| {
+                let q = tensor(&[b, q_heads, seq_len, head_dim]);
+                let k = tensor(&[b, kv_heads, seq_len, head_dim]);
+                let v = tensor(&[b, kv_heads, seq_len, head_dim]);
 
                 let scale_strategy = prop_oneof![Just(None), (0.1f32..2.0).prop_map(Some)];
                 let causal_strategy = any::<bool>();
-                (Just((b, seq_len)), q, k, v, scale_strategy, causal_strategy)
+                (Just((b, q_heads, seq_len)), q, k, v, scale_strategy, causal_strategy)
             })
-            .prop_flat_map(|((b, seq_len), q, k, v, scale, is_causal)| {
-                let mask_strategy = if is_causal {
-                    Just(None).boxed()
-                } else {
-                    prop_oneof![Just(None), tensor(&[b, seq_len, seq_len]).prop_map(Some)].boxed()
-                };
+        })
+        .prop_flat_map(|((b, q_heads, seq_len), q, k, v, scale, is_causal)| {
+            let mask_strategy = if is_causal {
+                Just(None).boxed()
+            } else {
+                prop_oneof![Just(None), tensor(&[b, q_heads, seq_len, seq_len]).prop_map(Some)]
+                    .boxed()
+            };
 
-                (Just(q), Just(k), Just(v), Just(scale), Just(is_causal), mask_strategy)
-            })
-            .prop_map(|(q, k, v, scale, is_causal, mask)| SdpaProblem {
-                q,
-                k,
-                v,
-                mask,
-                scale,
-                is_causal,
-            })
-            .boxed()
-    }
+            (Just(q), Just(k), Just(v), Just(scale), Just(is_causal), mask_strategy)
+        })
+        .prop_map(|(q, k, v, scale, is_causal, mask)| SdpaProblem {
+            q,
+            k,
+            v,
+            mask,
+            scale,
+            is_causal,
+        })
+        .boxed()
 }
 
 impl SdpaProblem<f32> {
@@ -149,28 +194,74 @@ impl SdpaProblem<f32> {
         output
     }
 
-    fn reference(&self) -> Array3<f32> {
-        let [b, seq_len, _] = self.q.shape() else { unreachable!() };
-        let [_, _, v_emb] = self.v.shape() else { unreachable!() };
+    fn reference_3d(&self) -> Array3<f32> {
+        let q = self.q.view().into_dimensionality::<Ix3>().unwrap();
+        let k = self.k.view().into_dimensionality::<Ix3>().unwrap();
+        let v = self.v.view().into_dimensionality::<Ix3>().unwrap();
+        let mask = self.mask.as_ref().map(|m| m.view().into_dimensionality::<Ix3>().unwrap());
+
+        let [b, seq_len, _] = q.shape() else { unreachable!() };
+        let [_, _, v_emb] = v.shape() else { unreachable!() };
         let mut output = Array3::<f32>::zeros((*b, *seq_len, *v_emb));
+
         for i in 0..*b {
-            let q_slice = self.q.slice(s![i, .., ..]);
-            let k_slice = self.k.slice(s![i, .., ..]);
-            let v_slice = self.v.slice(s![i, .., ..]);
-            let mask_slice = self.mask.as_ref().map(|m| m.slice(s![i, .., ..]));
+            let q_slice = &q.slice(s![i, .., ..]);
+            let k_slice = &k.slice(s![i, .., ..]);
+            let v_slice = &v.slice(s![i, .., ..]);
+            let mask_slice = mask.as_ref().map(|m| m.slice(s![i, .., ..]));
             let output_2d = Self::scaled_dot_product_attention_2d(
-                &q_slice,
-                &k_slice,
-                &v_slice,
+                q_slice,
+                k_slice,
+                v_slice,
                 mask_slice,
                 self.scale,
                 self.is_causal,
             );
-
             output.slice_mut(s![i, .., ..]).assign(&output_2d);
         }
-
         output
+    }
+
+    fn reference_4d(&self) -> Array4<f32> {
+        let q = self.q.view().into_dimensionality::<Ix4>().unwrap();
+        let k = self.k.view().into_dimensionality::<Ix4>().unwrap();
+        let v = self.v.view().into_dimensionality::<Ix4>().unwrap();
+        let mask = self.mask.as_ref().map(|m| m.view().into_dimensionality::<Ix4>().unwrap());
+
+        let [b, q_heads, seq_len, _] = q.shape() else { unreachable!() };
+        let [_, kv_heads, _, v_emb] = v.shape() else { unreachable!() };
+        let mut output = Array4::<f32>::zeros((*b, *q_heads, *seq_len, *v_emb));
+        let repeat_factor = q_heads / kv_heads;
+
+        for batch_idx in 0..*b {
+            for q_head_idx in 0..*q_heads {
+                let kv_head_idx = q_head_idx / repeat_factor;
+
+                let q_slice = q.slice(s![batch_idx, q_head_idx, .., ..]);
+                let k_slice = k.slice(s![batch_idx, kv_head_idx, .., ..]);
+                let v_slice = v.slice(s![batch_idx, kv_head_idx, .., ..]);
+                let mask_slice = mask.as_ref().map(|m| m.slice(s![batch_idx, q_head_idx, .., ..]));
+
+                let output_2d = Self::scaled_dot_product_attention_2d(
+                    &q_slice,
+                    &k_slice,
+                    &v_slice,
+                    mask_slice,
+                    self.scale,
+                    self.is_causal,
+                );
+                output.slice_mut(s![batch_idx, q_head_idx, .., ..]).assign(&output_2d);
+            }
+        }
+        output
+    }
+
+    fn reference(&self) -> ArrayD<f32> {
+        match self.q.ndim() {
+            3 => self.reference_3d().into_dyn(),
+            4 => self.reference_4d().into_dyn(),
+            _ => unreachable!(),
+        }
     }
 }
 
