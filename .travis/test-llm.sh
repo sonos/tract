3689582ab8be26ba1d85fd0ff98eeef9c017d6d4
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-09-02 16:11:19 +0200

    back to original rsz ratio in test-llm

--- .travis/test-llm.sh
+++ .travis/test-llm.sh
@@ -66,7 +66,7 @@ echo "  ###########################################"
 echo "      Alloc max to model size ratio: ${ratio}%."
 echo "  ###########################################"
 
-limit=133
+limit=125
 
 if [ $ratio -gt $limit ]
 then
