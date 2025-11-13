3b69e52c5e2b53e6ca3d840bb3d761f42e48a2ae
LouisChourakiSonos <louis.chouraki@sonos.com>
2025-11-13 14:07:48 +0100

    first clean

--- cuda/src/kernels/minimal_flash_attn.rs
+++ cuda/src/kernels/minimal_flash_attn.rs
@@ -1,7 +1,6 @@
 use cudarc::driver::sys::CUfunction_attribute;
 use cudarc::driver::{CudaFunction, LaunchArgs, LaunchConfig, PushKernelArg};
 use num_traits::One;
-//use std::f32::INFINITY;
 use std::fmt;
 use tract_core::internal::*;
 use tract_core::tract_data::itertools::Itertools;
@@ -11,14 +10,8 @@ use crate::context::{TractCudaStream, cuda_context};
 use crate::kernels::launch_args::LaunchArgsExt;
 use crate::kernels::{LibraryName, WARP_SIZE, get_cuda_view, launch_args};
 
-const CUDA_CC_TURING: i32 = 750;
-const CUDA_CC_AMPERE: i32 = 800;
-const FATTN_KQ_STRIDE: usize = 256;
-
 #[derive(Debug, Clone)]
-pub struct MinimalFlashAttn {
-    pub is_causal: bool,
-}
+pub struct MinimalFlashAttn;
 
 impl fmt::Display for MinimalFlashAttn {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -27,10 +20,6 @@ impl fmt::Display for MinimalFlashAttn {
 }
 
 impl MinimalFlashAttn {
-    pub fn is_supported_dts(q_dt: DatumType, k_dt: DatumType, v_dt: DatumType) -> bool {
-        k_dt == DatumType::F16 && (k_dt == v_dt) && (k_dt == q_dt)
-    }
-
     pub fn name(&self) -> Cow<'_, str> {
         format!("{self}").into()
     }
@@ -41,12 +30,9 @@ impl MinimalFlashAttn {
         block_kv: usize,
         d: usize,
         is_causal: bool,
-        use_mask: bool
+        use_mask: bool,
     ) -> TractResult<String> {
-
-        Ok(format!(
-            "attention_v5_{block_q}_{block_kv}_{d}_{is_causal}_{use_mask}"
-        ))
+        Ok(format!("attention_v5_{block_q}_{block_kv}_{d}_{is_causal}_{use_mask}"))
     }
 
     pub fn output_shape<D: DimLike + One>(
@@ -87,6 +73,7 @@ impl MinimalFlashAttn {
         v: &DeviceTensor,
         mask: Option<&DeviceTensor>,
         scale: f32,
+        is_causal: bool,
     ) -> TractResult<DeviceTensor> {
         let output = unsafe {
             DeviceTensor::uninitialized_dt(
@@ -95,7 +82,7 @@ impl MinimalFlashAttn {
             )?
         };
 
-        self.dispatch_eval(stream, q, k, v, mask, scale, &output)?;
+        self.dispatch_eval(stream, q, k, v, mask, scale, &output, is_causal)?;
         stream.synchronize()?;
         Ok(output)
     }
@@ -110,10 +97,13 @@ impl MinimalFlashAttn {
         mask: Option<&DeviceTensor>,
         scale: f32,
         out: &DeviceTensor,
+        is_causal: bool,
     ) -> TractResult<()> {
         ensure!(q.datum_type() == DatumType::F16 && q.datum_type() == out.datum_type());
+        ensure!(k.datum_type() == DatumType::F16 && k.datum_type() == v.datum_type());
+
         ensure!(out.shape() == self.output_shape(q.shape(), k.shape(), v.shape())?.as_slice());
-        ensure!(!(self.is_causal && mask.is_some()));
+        ensure!(!is_causal || mask.is_none());
         ensure!(mask.is_none_or(|m| m.datum_type() == DatumType::F16));
 
         let ctxt = cuda_context();
@@ -128,30 +118,31 @@ impl MinimalFlashAttn {
         ensure!(k.shape()[0] == b);
 
         let head_ratio = n_qh / k.shape()[1];
-        let block_q = 64; // len_q.next_multiple_of(64).min(64);
-        let block_kv= 32; // k.shape()[2].next_multiple_of(16).min(32);
+        let block_q = 64;
+        let block_kv = 32;
 
         let n_warps = 4;
 
         let num_q_blocks = len_q.div_ceil(block_q);
         let tb_size = n_warps * WARP_SIZE;
         let smem_size = block_q.max(block_kv * 3) * d * size_of::<f16>();
-        //dbg!(block_q, block_kv);
-        ensure!(
-            Self::is_supported_dts(q.datum_type(), k.datum_type(), v.datum_type()),
-            "Unsupported dts Q: {:?} K: {:?} V: {:?} for Cuda Flash Attention Op",
-            q.datum_type(), k.datum_type(), v.datum_type()
-        );
-        let func = ctxt.load_pipeline(LibraryName::MinimalFlashAttn, self.kernel_name(block_q, block_kv, d, self.is_causal, mask.is_some())?)?;
-        //dbg!(func.occupancy_available_dynamic_smem_per_block(num_blocks as _, tb_size as _));
-        func.set_attribute(CUfunction_attribute::CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES, smem_size as _)?;
-        
+
+        let func = ctxt.load_pipeline(
+            LibraryName::MinimalFlashAttn,
+            self.kernel_name(block_q, block_kv, d, is_causal, mask.is_some())?,
+        )?;
+
+        func.set_attribute(
+            CUfunction_attribute::CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES,
+            smem_size as _,
+        )?;
+
         let null_ptr = stream.null::<u8>()?;
 
         let q_view = get_cuda_view(q);
         let k_view = get_cuda_view(k);
         let v_view = get_cuda_view(v);
-        let m_view = mask.map(get_cuda_view).unwrap_or_else(|| null_ptr.as_view());;
+        let m_view = mask.map(get_cuda_view).unwrap_or_else(|| null_ptr.as_view());
         let o_view = get_cuda_view(out);
 
         let mut launch_args = stream.launch_builder(&func);
@@ -167,7 +158,11 @@ impl MinimalFlashAttn {
         launch_args.arg(&k.shape()[2]);
         launch_args.arg(&scale);
 
-        let cfg = LaunchConfig { grid_dim: (num_q_blocks as _, n_qh as _, b as _), block_dim: (tb_size as _, 1, 1), shared_mem_bytes: smem_size as _};
+        let cfg = LaunchConfig {
+            grid_dim: (num_q_blocks as _, n_qh as _, b as _),
+            block_dim: (tb_size as _, 1, 1),
+            shared_mem_bytes: smem_size as _,
+        };
         unsafe {
             launch_args.launch(cfg);
         }
@@ -221,28 +216,31 @@ mod tests {
                 &(0..kv_len).map(|f| f16::from_f32(f as f32 / kv_len as f32)).collect::<Vec<_>>(),
             )?;
 
-            let m = if create_mask { 
-                    Tensor::from_shape(
+            let m = if create_mask {
+                Tensor::from_shape(
                     &m_shape,
                     &(0..m_len).map(|f| f16::from_f32(1f32)).collect::<Vec<_>>(),
                 )?
-            } else { 
+            } else {
                 tensor0(0.0f32) // Unused 
             };
-            
+
             let cuda_m = m.clone().into_device()?;
-            let cuda_output = MinimalFlashAttn { is_causal }.eval(
+            let cuda_output = MinimalFlashAttn.eval(
                 stream,
                 &q.clone().into_device()?,
                 &k.clone().into_device()?,
                 &v.clone().into_device()?,
                 if create_mask { Some(&cuda_m) } else { None },
                 scale,
+                is_causal,
             )?;
 
             let mut ref_inputs = tvec!(q.into(), k.into(), v.into());
 
-            if create_mask { ref_inputs.push(m.into()) };
+            if create_mask {
+                ref_inputs.push(m.into())
+            };
             let ref_output = Sdpa {
                 scale: Some(scale.into()),
                 datum_type: DatumType::F16,
@@ -257,7 +255,7 @@ mod tests {
     }
 
     #[test]
-    fn test_fattn_mma_f16() -> TractResult<()> {
+    fn test_fattn() -> TractResult<()> {
         run_test_case(1, 1, 1, 64, 1, 128, 1.0f32, false, false)?;
         run_test_case(1, 2, 1, 64, 1, 128, 1.0f32, false, true)?;
         run_test_case(1, 2, 2, 0, 1, 64, 1.0f32, false, false)?;
