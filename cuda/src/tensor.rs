7ba16fbe75b7427782b7c1eef139c98d222b66eb
Mathieu Poumeyrol <kali@zoy.org>
2025-11-21 13:54:50 +0100

    factorize launch_copy a bit more

--- cuda/src/tensor.rs
+++ cuda/src/tensor.rs
@@ -4,6 +4,7 @@ use cudarc::driver::{CudaSlice, DevicePtr, PushKernelArg};
 use tract_core::internal::tract_smallvec::ToSmallVec;
 use tract_core::internal::*;
 use tract_core::prelude::{DatumType, TVec};
+use tract_core::tract_data::itertools::izip;
 use tract_core::tract_linalg::block_quant::{BlockQuantFact, BlockQuantValue, Q8_1};
 use tract_gpu::device::DeviceBuffer;
 use tract_gpu::tensor::{DeviceTensor, OwnedDeviceTensor};
@@ -232,6 +233,45 @@ pub fn device_tensor_assign_slice(
         tract_itertools::izip!(dst.shape(), src.shape(), 0..).all(|(d, s, a)| a == axis || s == d)
     );
 
+    let mut shape = src.shape().to_vec();
+    shape[axis] = src_range.len();
+
+    let mut dst_origin = tvec!(0usize; shape.len());
+    dst_origin[axis] = dst_range.start;
+    let src_origin = tvec!(0usize; shape.len());
+
+    device_tensor_launch_copy(
+        stream,
+        &shape,
+        dst,
+        &dst_origin,
+        dst.strides(),
+        src,
+        &src_origin,
+        src.strides(),
+    )
+}
+
+pub fn device_tensor_launch_copy(
+    stream: &TractCudaStream,
+    zone_shape: &[usize],
+    dst: &DeviceTensor,
+    dst_origin: &[usize],
+    dst_strides: &[isize],
+    src: &DeviceTensor,
+    src_origin: &[usize],
+    src_strides: &[isize],
+) -> TractResult<()> {
+    ensure!(src.datum_type() == dst.datum_type());
+    ensure!(src.datum_type().is_copy() && src.datum_type().is_number());
+    ensure!(
+        zone_shape.len() == dst.rank()
+            && zone_shape.len() == src.rank()
+            && zone_shape.len() == dst_origin.len()
+            && zone_shape.len() == dst_strides.len()
+            && zone_shape.len() == src_origin.len()
+            && zone_shape.len() == src_strides.len()
+    );
     let broadcast_kind = BroadcastKind::from_rank(dst.rank()).with_context(|| {
         format!(
             "Unsupported broadcast for assign slice: (in: {:?}, out: {:?})",
@@ -245,26 +285,24 @@ pub fn device_tensor_assign_slice(
     let kernel_name = format!("copy_{broadcast_name}_{tname}");
     let func = cuda_context().load_pipeline(LibraryName::Array, kernel_name)?;
 
-    let mut shape = src.shape().to_vec();
-    shape[axis] = src_range.len();
+    let dst_offset = izip!(dst_origin, dst_strides).map(|(a, b)| a * *b as usize).sum::<usize>()
+        * dst.datum_type().size_of();
+    let dst_len = dst.len() * dst.datum_type().size_of();
+    let dst_view = get_sliced_cuda_view(&dst, dst_offset, dst_len - dst_offset)?;
 
-    let src_offset = src.strides()[axis] as usize * src_range.start * src.datum_type().size_of();
-    let src_bytes = src.len() * src.datum_type().size_of();
-    let src_view = get_sliced_cuda_view(src, src_offset, src_bytes - src_offset)?;
-
-    let dst_offset = dst.strides()[axis] as usize * dst_range.start * dst.datum_type().size_of();
-    let dst_bytes = dst.len() * dst.datum_type().size_of();
-    let dst_view = get_sliced_cuda_view(dst, dst_offset, dst_bytes - dst_offset)?;
+    let src_offset = izip!(src_origin, src_strides).map(|(a, b)| a * *b as usize).sum::<usize>()
+        * src.datum_type().size_of();
+    let src_len = src.len() * src.datum_type().size_of();
+    let src_view = get_sliced_cuda_view(&src, src_offset, src_len - src_offset)?;
 
     let mut launch_args = stream.launch_builder(&func);
     launch_args.arg(&src_view);
     launch_args.arg(&dst_view);
     launch_args.set_slice(src.strides());
-    launch_args.set_slice(&shape);
+    launch_args.set_slice(&zone_shape);
     launch_args.set_slice(dst.strides());
 
-    let cfg = cuda_launch_cfg_for_cpy(&shape);
+    let cfg = cuda_launch_cfg_for_cpy(&zone_shape);
     unsafe { launch_args.launch(cfg)? };
-
     Ok(())
 }
