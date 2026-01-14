7f8ae4e702f6feae849b3c1213595c355cb6aa35
Mathieu Poumeyrol <kali@zoy.org>
2026-01-14 12:43:53 +0100

    depreated edge-contextx

--- .travis/cli-tests.sh
+++ .travis/cli-tests.sh
@@ -97,18 +97,6 @@ $TRACT_RUN $MODELS/GRU128KeywordSpotter-v2-10epochs.onnx \
     --allow-random-input \
     --assert-output-fact 1,3,f32
 
-$CACHE_FILE en_libri_real/model.onnx en_libri_real/io.npz
-$TRACT_RUN $MODELS/en_libri_real/model.onnx \
-    --output-node output \
-    --edge-left-context 5 --edge-right-context 15 \
-    --input-facts-from-bundle $MODELS/en_libri_real/io.npz \
-    -O  \
-    run \
-    --input-from-bundle $MODELS/en_libri_real/io.npz \
-    --approx approximate \
-    --allow-random-input \
-    --assert-output-bundle $MODELS/en_libri_real/io.npz
-    
 $CACHE_FILE mdl-en-2019-Q3-librispeech.onnx
 $TRACT_RUN $MODELS/mdl-en-2019-Q3-librispeech.onnx \
     -O -i S,40,f32 --output-node output --pulse 24 \
