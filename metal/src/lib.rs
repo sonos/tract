b99682e23ce743c11d71da054f1316af9499740b
Mathieu Poumeyrol <kali@zoy.org>
2026-01-16 13:39:15 +0100

    fmt

--- metal/src/lib.rs
+++ metal/src/lib.rs
@@ -14,10 +14,10 @@ use tract_core::internal::*;
 use tract_core::transform::ModelTransform;
 
 use crate::func_constants::{ConstantValues, Value};
-pub use crate::kernels::matmul::MetalGemmImplKind;
 use crate::kernels::LibraryName;
+pub use crate::kernels::matmul::MetalGemmImplKind;
 
-pub use crate::context::{MetalContext, MetalStream, METAL_STREAM};
+pub use crate::context::{METAL_STREAM, MetalContext, MetalStream};
 pub use crate::transform::MetalTransform;
 
 #[derive(Debug)]
