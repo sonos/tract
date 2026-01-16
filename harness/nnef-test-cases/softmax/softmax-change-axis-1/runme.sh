e0923a1affe3263fdb1d221dc15fda34513ac35a
Mathieu Poumeyrol <kali@zoy.org>
2026-01-16 11:36:52 +0100

    ack cli changes

--- harness/nnef-test-cases/softmax/softmax-change-axis-1/runme.sh
+++ harness/nnef-test-cases/softmax/softmax-change-axis-1/runme.sh
@@ -5,7 +5,7 @@ set -ex
 
 : ${TRACT_RUN:=cargo run -p tract $CARGO_OPTS --}
 
-$TRACT_RUN . dump -q --nnef-graph found
+$TRACT_RUN --no-nnef-tract-core . dump -q --nnef-graph found
 
 version=`cargo metadata --format-version 1 | jq -r '.packages | map(select( (.name) == "tract-core") | .version) | .[] '`
 perl -pi -e "s/$version/0.16.10-pre/" found
