e1c4ce360139f9872bacd2ce8aaf552b1227c0cf
Mathieu Poumeyrol <kali@zoy.org>
2026-01-23 16:47:39 +0100

    maintain internal api example

---
+++ examples/nnef-mobilenet-v2/ci.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -ex
+
+wget -q https://sfo2.digitaloceanspaces.com/nnef-public/mobilenet_v2_1.0.onnx.nnef.tgz -O mobilenet_v2_1.0.onnx.nnef.tgz
+
+cargo run
+rm mobilenet_v2_1.0.onnx.nnef.tgz
