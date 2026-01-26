bef5bd41fe7af17b402c7e9510b76b258a9f91e3
Mathieu Poumeyrol <kali@zoy.org>
2026-01-26 09:39:47 +0100

    -> 1.89.0

--- .travis/ci-system-setup.sh
+++ .travis/ci-system-setup.sh
@@ -5,7 +5,7 @@ set -e
 
 if [ -z "$RUSTUP_TOOLCHAIN" ]
 then
-    export RUSTUP_TOOLCHAIN=1.86.0
+    export RUSTUP_TOOLCHAIN=1.89.0
 fi
 
 export RUSTUP_TOOLCHAIN
--- .travis/native.sh
+++ .travis/native.sh
@@ -4,7 +4,7 @@ set -ex
 
 if [ -z "$RUSTUP_TOOLCHAIN" ]
 then
-    export RUSTUP_TOOLCHAIN=1.86.0
+    export RUSTUP_TOOLCHAIN=1.89.0
 fi
 
 rustup update
