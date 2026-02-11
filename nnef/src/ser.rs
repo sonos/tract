000d2ee16807cb4e45f205b1a2417ed0e0b8b8e9
Mathieu Poumeyrol <mathieu.poumeyrol@sonos.com>
2026-02-11 14:11:22 +0100

    etern all constant

--- nnef/src/ser.rs
+++ nnef/src/ser.rs
@@ -364,7 +364,11 @@ impl<'a> IntoAst<'a> {
         if tensor.datum_type() == TDim::datum_type() {
             return Ok(Self::dump_rec_tensor(&tensor.to_array_view::<TDim>()?, tdim).into());
         }
-        if !force_variable && tensor.len() <= 8 && tensor.len() > 0 {
+        if !force_variable
+            && !self.framework.extern_all_constants
+            && tensor.len() <= 8
+            && tensor.len() > 0
+        {
             if tensor.datum_type() == String::datum_type() {
                 return Ok(Self::dump_rec_tensor(&tensor.to_array_view::<String>()?, |f| {
                     string(f)
