808df3ad9228d7c96d7426a0082693a1459a8814
Julien Balian <julien.balian@sonos.com>
2026-02-17 14:20:26 +0100

    fix: add ensure to cudarc on cuda minimal version instead of bail in lib missing symbol

--- cuda/src/lib.rs
+++ cuda/src/lib.rs
@@ -12,6 +12,7 @@ use tract_core::transform::ModelTransform;
 pub use transform::CudaTransform;
 
 use crate::utils::are_culibs_present;
+use crate::utils::ensure_cuda_driver_compatible;
 const Q40_ROW_PADDING: usize = 512;
 
 #[derive(Debug)]
@@ -27,6 +28,7 @@ impl Runtime for CudaRuntime {
         mut model: TypedModel,
         options: &RunOptions,
     ) -> TractResult<Box<dyn Runnable>> {
+        ensure_cuda_driver_compatible()?;
         ensure!(are_culibs_present());
         CudaTransform.transform(&mut model)?;
         model.optimize()?;
