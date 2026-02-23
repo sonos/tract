79665f63474cc74dabf9e1146efe5298adce34c2
Mathieu Poumeyrol <kali@zoy.org>
2026-02-23 11:57:49 +0100

    precision check

---
+++ harness/parakeet-tdt-600m-v3/ci.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -ex
+
+ROOT=$(realpath $(dirname $(realpath $0))/../..)
+. $ROOT/.travis/ci-system-setup.sh
+
+for rt in $TRACT_RUNTIMES
+do
+	for m in preprocessor encoder decoder joint
+	do
+		$CACHE_FILE \
+		asr/608/nvidia--parakeet-tdt-0.6b-v3-f32f32/nvidia--parakeet-tdt-0.6b-v3-f32f32.$m.nnef.tgz \
+	 	asr/608/nvidia--parakeet-tdt-0.6b-v3-f32f32/nvidia--parakeet-tdt-0.6b-v3-f32f32.$m.io.npz
+
+	 	model_prefix=$MODELS/asr/608/nvidia--parakeet-tdt-0.6b-v3-f32f32/nvidia--parakeet-tdt-0.6b-v3-f32f32
+
+	 	$TRACT_RUN $model_prefix.$m.nnef.tgz $rt run \
+	 		--input-from-bundle $model_prefix.$m.io.npz --assert-output-bundle $model_prefix.$m.io.npz \
+	 		--approx approximate
+	 done
+done
