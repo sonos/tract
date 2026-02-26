e3f952d9f9f6d2d71d0b85b739a9ac2670346098
Mathieu Poumeyrol <kali@zoy.org>
2026-02-26 10:45:45 +0100

    disable broken wastime test

--- examples/face_similarity_arcface_onnx/ci.sh
+++ examples/face_similarity_arcface_onnx/ci.sh
@@ -9,11 +9,13 @@ wget -Nq "https://tract-ci-builds.s3.amazonaws.com/model/arcfaceresnet100-8.onnx
 # on win/linux 
 cargo run --release -- --face1 grace_hopper.jpeg --face2 grace_hopper2.jpeg 
 
-wasmtime -V || curl https://wasmtime.dev/install.sh -sSf | bash # install wasmtime
-PATH=$PATH:$HOME/.wasmtime/bin
-rustup target install wasm32-wasip1
-cargo build --target wasm32-wasip1 --release
-wasmtime --dir . ../../target/wasm32-wasip1/release/face_similarity_arcface_onnx.wasm --face1 grace_hopper.jpeg --face2 grace_hopper2.jpeg
+# disabled wasm test, it OOM with recent wasm. is this model too big for wasm32 ?
+
+# wasmtime -V || curl https://wasmtime.dev/install.sh -sSf | bash # install wasmtime
+# PATH=$PATH:$HOME/.wasmtime/bin
+# rustup target install wasm32-wasip1
+# cargo build --target wasm32-wasip1 --release
+# wasmtime --dir . ../../target/wasm32-wasip1/release/face_similarity_arcface_onnx.wasm --face1 grace_hopper.jpeg --face2 grace_hopper2.jpeg
 
 rm yolov8n-face.onnx
 rm arcfaceresnet100-8.onnx
