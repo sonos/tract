d7c448b3a60f31aa7e57240ae0beab53028b9d5e
Mathieu Poumeyrol <kali@zoy.org>
2026-03-06 20:53:20 +0100

    bump dinghy

--- .travis/cross.sh
+++ .travis/cross.sh
@@ -17,7 +17,7 @@ then
      else
          NAME=linux
      fi
-     VERSION=0.8.0
+     VERSION=0.8.4
      wget -q https://github.com/snipsco/dinghy/releases/download/$VERSION/cargo-dinghy-$NAME-$VERSION.tgz -O cargo-dinghy.tgz
      tar vzxf cargo-dinghy.tgz --strip-components 1
      mv cargo-dinghy $HOME/.cargo/bin
