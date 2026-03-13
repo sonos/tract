ab436601336aeb72a4f5346f9b86b3156cc9990a
Mathieu Poumeyrol <kali@zoy.org>
2026-03-13 22:59:42 +0100

    fix tract -> tract-cli

--- examples/nnef-dump-mobilenet-v2/ci.sh
+++ examples/nnef-dump-mobilenet-v2/ci.sh
@@ -5,5 +5,5 @@ set -ex
 wget -nc -q https://s3.amazonaws.com/tract-ci-builds/model/mobilenet_v2_1.4_224.tgz
 tar zxf mobilenet_v2_1.4_224.tgz
 
-cargo run -p tract -- mobilenet_v2_1.4_224_frozen.pb -i 1,224,224,3,f32 dump --nnef mobilenet.nnef.tgz
+cargo run -p tract-cli -- mobilenet_v2_1.4_224_frozen.pb -i 1,224,224,3,f32 dump --nnef mobilenet.nnef.tgz
 cargo run
