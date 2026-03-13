21c1b3ec9c551a448023b8e880a66d91cc7c5506
Mathieu Poumeyrol <kali@zoy.org>
2026-03-13 23:05:47 +0100

    minimal ci.sh for causal_llm example

--- examples/causal_llm/ci.sh
+++ examples/causal_llm/ci.sh
@@ -4,25 +4,11 @@ set -e
 set -o pipefail
 set -x
 
-ROOT=$(dirname $(dirname $(dirname $(realpath $0))))
-. $ROOT/.travis/ci-system-setup.sh
-
-generation=541
-model_id=Qwen--Qwen3-1.7B-q40ef16
-nnef=llm/$generation/$model_id/$model_id.nnef.tgz
-
-$CACHE_FILE $nnef
-
-TOKENIZER=$MODELS/llm/Qwen--Qwen3-1.7B-tokenizer.json
-if [ ! -e "$TOKENIZER" ]
-then
-    mkdir -p $(dirname "$TOKENIZER")
-    wget -q https://huggingface.co/Qwen/Qwen3-1.7B/resolve/main/tokenizer.json -O "$TOKENIZER.tmp"
-    mv "$TOKENIZER.tmp" "$TOKENIZER"
-fi
+wget https://s3.amazonaws.com/tract-ci-builds/tests/llm/541/Qwen--Qwen3-1.7B-q40ef16/Qwen--Qwen3-1.7B-q40ef16.nnef.tgz
+wget https://huggingface.co/Qwen/Qwen3-1.7B/resolve/main/tokenizer.json
 
 OUTPUT=$(cargo run -p causal_llm --bin complete --profile opt-no-lto -- \
-    -t "$TOKENIZER" -m "$MODELS/$nnef" -n 20 "The capital of France is")
+    -t "tokenizer.json" -m Qwen--Qwen3-1.7B-q40ef16.nnef.tgz -n 20 "The capital of France is")
 
 echo "Output: $OUTPUT"
 
