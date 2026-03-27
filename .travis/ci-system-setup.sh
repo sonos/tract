34305d985baca654f4ff43d364865a3f6463292a
Mathieu Poumeyrol <kali@zoy.org>
2026-03-27 09:53:47 +0100

    adjust timeout for llm on cpu

--- .travis/ci-system-setup.sh
+++ .travis/ci-system-setup.sh
@@ -69,7 +69,7 @@ fi
 
 if [ -z "$TRACT_RUN" ]
 then
-    TRACT_RUN="cargo run -p tract-cli $CARGO_EXTRA --profile opt-no-lto --no-default-features --features transformers -- --timeout 60"
+    TRACT_RUN="cargo run -p tract-cli $CARGO_EXTRA --profile opt-no-lto --no-default-features --features transformers --"
     export TRACT_RUN
 fi
 
