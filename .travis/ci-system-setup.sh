3b40cc2cb8f9eb936258c9b54a2bee0a7b3ec066
Mathieu Poumeyrol <kali@zoy.org>
2026-04-03 14:47:19 +0000

    Enable pulse feature in CI harness builds

--- .travis/ci-system-setup.sh
+++ .travis/ci-system-setup.sh
@@ -69,7 +69,7 @@ fi
 
 if [ -z "$TRACT_RUN" ]
 then
-    TRACT_RUN="cargo run -p tract-cli $CARGO_EXTRA --profile opt-no-lto --no-default-features --features transformers --"
+    TRACT_RUN="cargo run -p tract-cli $CARGO_EXTRA --profile opt-no-lto --no-default-features --features transformers,pulse --"
     export TRACT_RUN
 fi
 
