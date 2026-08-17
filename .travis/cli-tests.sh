7931f606cd4e943d4402b3b64818ba28d3616d17
Mathieu Poumeyrol <kali@zoy.org>
2026-08-17 08:04:25 +0000

    asan CI: thread $CARGO_EXTRA into bench-suite build
    Bench build lacked --target, so -Zsanitizer=address leaked into host
    proc-macro/build-script crates; zerocopy 0.8.56 fails to compile that way.

--- .travis/cli-tests.sh
+++ .travis/cli-tests.sh
@@ -151,7 +151,7 @@ echo
 echo $WHITE • benches on full models $NC
 echo
 
-TRACT_BENCH=$(cargo build --message-format json -p tract-cli --features bench-suite --profile opt-no-lto | jq -r 'select(.target.name == "tract" and .executable).executable')
+TRACT_BENCH=$(cargo build --message-format json -p tract-cli $CARGO_EXTRA --features bench-suite --profile opt-no-lto | jq -r 'select(.target.name == "tract" and .executable).executable')
 if "$TRACT_BENCH" bench-suite --manifest .travis/benches.toml 2> bench-suite.err
 then
     cat bench-suite.err
